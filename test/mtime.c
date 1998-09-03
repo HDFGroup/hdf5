7081f2d6e67e1ce5789485e30b6b996e8f51db77
Robb Matzke <matzke@llnl.gov>
1998-09-03 10:31:04 -0500

    [svn-r662] Fixed a typo in a string

--- test/mtime.c
+++ test/mtime.c
@@ -143,7 +143,7 @@ main(void)
     if (0==sb1.mtime) {
 	puts("--SKIP--");
 	puts("   The modification time could not be decoded on this OS.");
-	puts("   Modification times will be mantained in the file bug cannot");
+	puts("   Modification times will be mantained in the file but cannot");
 	puts("   be queried on this system.  See H5O_mtime_decode().");
 	return 0;
     } else if (fabs(HDdifftime(now, sb1.mtime))>60.0) {
