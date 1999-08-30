27e31d9c26835e5e0b3e81d89cd7bd5d330b53f6
Patrick Lu <ptlu@hawkwind.ncsa.uiuc.edu>
1999-08-30 11:08:21 -0500

    [svn-r1616] added __DLL__ in front of all the function prototypes so the dll can see it:

--- src/H5FDmpio.h
+++ src/H5FDmpio.h
@@ -36,18 +36,18 @@ typedef struct H5FD_mpio_dxpl_t {
     
 /* Function prototypes */
 #ifdef HAVE_PARALLEL
-hid_t H5FD_mpio_init(void);
-herr_t H5Pset_fapl_mpio(hid_t fapl_id, MPI_Comm comm, MPI_Info info);
-herr_t H5Pget_fapl_mpio(hid_t fapl_id, MPI_Comm *comm/*out*/,
+__DLL__ hid_t H5FD_mpio_init(void);
+__DLL__ herr_t H5Pset_fapl_mpio(hid_t fapl_id, MPI_Comm comm, MPI_Info info);
+__DLL__herr_t H5Pget_fapl_mpio(hid_t fapl_id, MPI_Comm *comm/*out*/,
 			MPI_Info *info/*out*/);
-herr_t H5Pset_dxpl_mpio(hid_t dxpl_id, H5FD_mpio_xfer_t xfer_mode);
-herr_t H5Pget_dxpl_mpio(hid_t dxpl_id, H5FD_mpio_xfer_t *xfer_mode/*out*/);
-htri_t H5FD_mpio_tas_allsame(H5FD_t *_file, hbool_t newval);
-MPI_Comm H5FD_mpio_communicator(H5FD_t *_file);
-herr_t H5FD_mpio_setup(H5FD_t *_file, MPI_Datatype btype, MPI_Datatype ftype,
+__DLL__herr_t H5Pset_dxpl_mpio(hid_t dxpl_id, H5FD_mpio_xfer_t xfer_mode);
+__DLL__herr_t H5Pget_dxpl_mpio(hid_t dxpl_id, H5FD_mpio_xfer_t *xfer_mode/*out*/);
+__DLL__htri_t H5FD_mpio_tas_allsame(H5FD_t *_file, hbool_t newval);
+__DLL__MPI_Comm H5FD_mpio_communicator(H5FD_t *_file);
+__DLL__herr_t H5FD_mpio_setup(H5FD_t *_file, MPI_Datatype btype, MPI_Datatype ftype,
 		       haddr_t disp, hbool_t use_types);
-herr_t H5FD_mpio_wait_for_left_neighbor(H5FD_t *file);
-herr_t H5FD_mpio_signal_right_neighbor(H5FD_t *file);
+__DLL__herr_t H5FD_mpio_wait_for_left_neighbor(H5FD_t *file);
+__DLL__herr_t H5FD_mpio_signal_right_neighbor(H5FD_t *file);
 
 #endif /*HAVE_PARALLEL*/
 
--- src/H5FDprivate.h
+++ src/H5FDprivate.h
@@ -12,31 +12,31 @@
 
 #define H5FD_has_cmp(id) (NULL!=H5FD_get_class(id)->cmp)
 
-intn H5FD_term_interface(void);
-H5FD_class_t *H5FD_get_class(hid_t id);
-hsize_t H5FD_sb_size(H5FD_t *file);
-herr_t H5FD_sb_encode(H5FD_t *file, char *name/*out*/, uint8_t *buf);
-herr_t H5FD_sb_decode(H5FD_t *file, const char *name, const uint8_t *buf);
-void *H5FD_fapl_get(H5FD_t *file);
-void *H5FD_fapl_copy(hid_t driver_id, const void *fapl);
-herr_t H5FD_fapl_free(hid_t driver_id, void *fapl);
-void *H5FD_dxpl_copy(hid_t driver_id, const void *dxpl);
-herr_t H5FD_dxpl_free(hid_t driver_id, void *dxpl);
-H5FD_t *H5FD_open(const char *name, unsigned flags, hid_t fapl_id,
+__DLL__ intn H5FD_term_interface(void);
+__DLL__ H5FD_class_t *H5FD_get_class(hid_t id);
+__DLL__ hsize_t H5FD_sb_size(H5FD_t *file);
+__DLL__ herr_t H5FD_sb_encode(H5FD_t *file, char *name/*out*/, uint8_t *buf);
+__DLL__ herr_t H5FD_sb_decode(H5FD_t *file, const char *name, const uint8_t *buf);
+__DLL__ void *H5FD_fapl_get(H5FD_t *file);
+__DLL__ void *H5FD_fapl_copy(hid_t driver_id, const void *fapl);
+__DLL__ herr_t H5FD_fapl_free(hid_t driver_id, void *fapl);
+__DLL__ void *H5FD_dxpl_copy(hid_t driver_id, const void *dxpl);
+__DLL__ herr_t H5FD_dxpl_free(hid_t driver_id, void *dxpl);
+__DLL__ H5FD_t *H5FD_open(const char *name, unsigned flags, hid_t fapl_id,
 		  haddr_t maxaddr);
-herr_t H5FD_close(H5FD_t *file);
-int H5FD_cmp(const H5FD_t *f1, const H5FD_t *f2);
-haddr_t H5FD_alloc(H5FD_t *file, H5FD_mem_t type, hsize_t size);
-herr_t H5FD_free(H5FD_t *file, H5FD_mem_t type, haddr_t addr, hsize_t size);
-haddr_t H5FD_realloc(H5FD_t *file, H5FD_mem_t type, haddr_t old_addr,
+__DLL__ herr_t H5FD_close(H5FD_t *file);
+__DLL__ int H5FD_cmp(const H5FD_t *f1, const H5FD_t *f2);
+__DLL__ haddr_t H5FD_alloc(H5FD_t *file, H5FD_mem_t type, hsize_t size);
+__DLL__ herr_t H5FD_free(H5FD_t *file, H5FD_mem_t type, haddr_t addr, hsize_t size);
+__DLL__ haddr_t H5FD_realloc(H5FD_t *file, H5FD_mem_t type, haddr_t old_addr,
 		     hsize_t old_size, hsize_t new_size);
-haddr_t H5FD_get_eoa(H5FD_t *file);
-herr_t H5FD_set_eoa(H5FD_t *file, haddr_t addr);
-haddr_t H5FD_get_eof(H5FD_t *file);
-herr_t H5FD_read(H5FD_t *file, hid_t dxpl_id, haddr_t addr, hsize_t size,
+__DLL__ haddr_t H5FD_get_eoa(H5FD_t *file);
+__DLL__ herr_t H5FD_set_eoa(H5FD_t *file, haddr_t addr);
+__DLL__ haddr_t H5FD_get_eof(H5FD_t *file);
+__DLL__ herr_t H5FD_read(H5FD_t *file, hid_t dxpl_id, haddr_t addr, hsize_t size,
 		 void *buf/*out*/);
-herr_t H5FD_write(H5FD_t *file, hid_t dxpl_id, haddr_t addr, hsize_t size,
+__DLL__ herr_t H5FD_write(H5FD_t *file, hid_t dxpl_id, haddr_t addr, hsize_t size,
 		  const void *buf);
-herr_t H5FD_flush(H5FD_t *file);
+__DLL__ herr_t H5FD_flush(H5FD_t *file);
 
 #endif /* !_H5FDprivate_H */
