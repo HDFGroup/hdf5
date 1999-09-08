d6d756ef965762cb109e8d004dde3768f5bc567b
Patrick Lu <ptlu@hawkwind.ncsa.uiuc.edu>
1999-09-08 15:07:40 -0500

    [svn-r1634] fixed the compilation error where the H5P_DATASET_CREATE and H5P_DATA_XFER were using the old names.

--- test/overhead.c
+++ test/overhead.c
@@ -197,9 +197,9 @@ test(fill_t fill_style, const double splits[],
     if ((file=H5Fcreate(FILE_NAME_1, H5F_ACC_TRUNC, H5P_DEFAULT, fapl))<0) {
 	goto error;
     }
-    if ((dcpl=H5Pcreate(H5P_DATA_CREATE))<0) goto error;
+    if ((dcpl=H5Pcreate(H5P_DATASET_CREATE))<0) goto error;
     if (H5Pset_chunk(dcpl, 1, ch_size)<0) goto error;
-    if ((xfer=H5Pcreate(H5P_DATASET_XFER))<0) goto error;
+    if ((xfer=H5Pcreate(H5P_DATA_XFER))<0) goto error;
     if (H5Pset_btree_ratios(xfer, splits[0], splits[1], splits[2])<0) {
 	goto error;
     }
