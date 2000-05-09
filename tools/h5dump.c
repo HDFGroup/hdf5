f474a093652aad41482db7ed2ddb346aaad0372d
Patrick Lu <ptlu@hawkwind.ncsa.uiuc.edu>
2000-05-09 12:00:38 -0500

    [svn-r2228] added a semicolon after the opaque info is printed

--- tools/h5dump.c
+++ tools/h5dump.c
@@ -433,9 +433,9 @@ H5G_stat_t statbuf;
     case H5T_OPAQUE: 
         printf( "\n");
         indentation (indent+COL);	
-		printf("H5T_OPAQUE\n");
+		printf("H5T_OPAQUE;\n");
         indentation (indent+COL);	
-		printf("OPAQUE_TAG \"%s\"\n", H5Tget_tag(type));
+		printf("OPAQUE_TAG \"%s\";\n", H5Tget_tag(type));
         indentation (indent);
         break;
 
