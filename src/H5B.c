c4ba19fb759750bb8adeb211370d0d5122945f1a
Bill Wendling <wendling@ncsa.uiuc.edu>
2000-05-19 16:34:23 -0500

    [svn-r2284] Duplicate initialization of variable removed.

--- src/H5B.c
+++ src/H5B.c
@@ -219,7 +219,6 @@ H5B_create(H5F_t *f, const H5B_class_t *type, void *udata,
     bt->sizeof_rkey = sizeof_rkey;
     bt->dirty = TRUE;
     bt->ndirty = 0;
-    bt->type = type;
     bt->level = 0;
     bt->left = HADDR_UNDEF;
     bt->right = HADDR_UNDEF;
