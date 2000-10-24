7b7a34b33d7788a4b44e9574dbcef6d9eee8ef72
Albert Cheng <acheng@hdfgroup.org>
2000-10-24 13:28:54 -0500

    [svn-r2723] Purpose:     tidy it up Description:     Change the variable names so that it looks more generic.  Plan to     make it a generic script test template. Platforms tested:     IRIX64-64, linux, solaris 2.7

--- tools/testh5dump.sh
+++ tools/testh5dump.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+# Tests for the h5dump tool
 
-h5dump=h5dump		# a relative name
-cmp='cmp -s'
-diff='diff -c'
+h5tool=h5dump			# The tool name
+h5tool_bin=`pwd`/$h5tool	# The path of the tool binary
+CMP='cmp -s'
+DIFF='diff -c'
 
 nerrors=0
 verbose=yes
@@ -28,30 +30,30 @@ TESTING()
 # the actual output file is calculated by replacing the `.ddl' with
 # `.out'.  The actual output is not removed if $HDF5_NOCLEANUP has a
 # non-zero value.
-DUMP()
+TOOLTEST()
 {
     expect=$srcdir/testfiles/$1
     actual="testfiles/`basename $1 .ddl`.out"
     shift
-    full=`pwd`/$h5dump
+    full=`pwd`/$h5tool
 
     # Run test.
-    TESTING $h5dump $@
+    TESTING $h5tool $@
     (
 	echo "#############################"
-	echo "Expected output for '$h5dump $@'" 
+	echo "Expected output for '$h5tool $@'" 
 	echo "#############################"
 	cd $srcdir/testfiles
-        $RUNSERIAL $full "$@" 2>/dev/null
+        $RUNSERIAL $h5tool_bin "$@" 2>/dev/null
     ) >$actual
     
-    if $cmp $expect $actual; then
+    if $CMP $expect $actual; then
 	echo " PASSED"
     else
 	echo "*FAILED*"
 	echo "    Expected result (*.ddl) differs from actual result (*.out)"
 	nerrors="`expr $nerrors + 1`"
-	test yes = "$verbose" && $diff $expect $actual |sed 's/^/    /'
+	test yes = "$verbose" && $DIFF $expect $actual |sed 's/^/    /'
     fi
 
     # Clean up output file
@@ -69,57 +71,57 @@ DUMP()
 ##############################################################################
 
 # test for displaying groups
-DUMP tgroup-1.ddl tgroup.h5
+TOOLTEST tgroup-1.ddl tgroup.h5
 # test for displaying the selected groups
-DUMP tgroup-2.ddl -g /g2 / /y tgroup.h5
+TOOLTEST tgroup-2.ddl -g /g2 / /y tgroup.h5
 
 # test for displaying simple space datasets
-DUMP tdset-1.ddl tdset.h5
+TOOLTEST tdset-1.ddl tdset.h5
 # test for displaying selected datasets
-DUMP tdset-2.ddl -header -d dset1 /dset2 dset3 tdset.h5
+TOOLTEST tdset-2.ddl -header -d dset1 /dset2 dset3 tdset.h5
 
 # test for displaying attributes
-DUMP tattr-1.ddl tattr.h5
+TOOLTEST tattr-1.ddl tattr.h5
 # test for displaying the selected attributes of string type and scalar space
-DUMP tattr-2.ddl -a attr1 attr4 attr5 tattr.h5
+TOOLTEST tattr-2.ddl -a attr1 attr4 attr5 tattr.h5
 # test for header and error messages
-DUMP tattr-3.ddl -header -a attr2 attr tattr.h5
+TOOLTEST tattr-3.ddl -header -a attr2 attr tattr.h5
 
 # test for displaying soft links
-DUMP tslink-1.ddl tslink.h5
+TOOLTEST tslink-1.ddl tslink.h5
 # test for displaying the selected link
-DUMP tslink-2.ddl -l slink2 tslink.h5
+TOOLTEST tslink-2.ddl -l slink2 tslink.h5
 
 # tests for hard links
-DUMP thlink-1.ddl thlink.h5
-DUMP thlink-2.ddl -d /g1/dset2 /dset1 /g1/g1.1/dset3 thlink.h5
-DUMP thlink-3.ddl -d /g1/g1.1/dset3 /g1/dset2 /dset1 thlink.h5
-DUMP thlink-4.ddl -g /g1 thlink.h5
-DUMP thlink-5.ddl -d /dset1 -g /g2 -d /g1/dset2 thlink.h5
+TOOLTEST thlink-1.ddl thlink.h5
+TOOLTEST thlink-2.ddl -d /g1/dset2 /dset1 /g1/g1.1/dset3 thlink.h5
+TOOLTEST thlink-3.ddl -d /g1/g1.1/dset3 /g1/dset2 /dset1 thlink.h5
+TOOLTEST thlink-4.ddl -g /g1 thlink.h5
+TOOLTEST thlink-5.ddl -d /dset1 -g /g2 -d /g1/dset2 thlink.h5
 
 # tests for compound data types
-DUMP tcomp-1.ddl tcompound.h5
+TOOLTEST tcomp-1.ddl tcompound.h5
 # test for named data types
-DUMP tcomp-2.ddl -t /type1 /type2 /group1/type3 tcompound.h5
+TOOLTEST tcomp-2.ddl -t /type1 /type2 /group1/type3 tcompound.h5
 # test for unamed type 
-DUMP tcomp-3.ddl -t /#5992:0 -g /group2 tcompound.h5
+TOOLTEST tcomp-3.ddl -t /#5992:0 -g /group2 tcompound.h5
 
 #test for the nested compound type
-DUMP tnestcomp-1.ddl tnestedcomp.h5
+TOOLTEST tnestcomp-1.ddl tnestedcomp.h5
 
 # test for options
-DUMP tall-1.ddl tall.h5
-DUMP tall-2.ddl -header -g /g1/g1.1 -a attr2 tall.h5
-DUMP tall-3.ddl -d /g2/dset2.1 -l /g1/g1.2/g1.2.1/slink tall.h5
+TOOLTEST tall-1.ddl tall.h5
+TOOLTEST tall-2.ddl -header -g /g1/g1.1 -a attr2 tall.h5
+TOOLTEST tall-3.ddl -d /g2/dset2.1 -l /g1/g1.2/g1.2.1/slink tall.h5
 
 # test for loop detection
-DUMP tloop-1.ddl tloop.h5
+TOOLTEST tloop-1.ddl tloop.h5
 
 # test for string 
-DUMP tstr-1.ddl tstr.h5
+TOOLTEST tstr-1.ddl tstr.h5
 
 if test $nerrors -eq 0 ; then
-	echo "All h5dump tests passed."
+	echo "All $h5tool tests passed."
 fi
 
 exit $nerrors
