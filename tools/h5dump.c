d83c9494ad727fe9d516c1ae426178a88afc3e3b
Bill Wendling <wendling@ncsa.uiuc.edu>
2000-11-17 12:57:01 -0500

    [svn-r2965] Purpose: 	Usage Statement Reworking Description: 	Changed usage statement to correspond to the Tools.html page.

--- tools/h5dump.c
+++ tools/h5dump.c
@@ -226,9 +226,9 @@ h5dump [-h] [-bb] [-header] [-v] [-V] [-a <names>] [-d <names>] [-g <names>]\n\
   -d <path>     Display the specified dataset(s).\n\
   -g <path>     Display the specified group(s) and all the members.\n\
   -l <path>     Display the value(s) of the specified soft link(s).\n\
-  -o <fname>    Display the raw data of datasets to a separate Output file <fname>.\n\
   -t <names>    Display the specified named data type(s).\n\
   -w <number>   Display the information with the specified maximum number of columns.\n\
+  -o <fname>    Output the raw data of datasets to a separate file <fname>.\n\
 \n\
   <number> is an integer greater than 1.\n\
   <path> is the full path from the root group to the object.\n\
