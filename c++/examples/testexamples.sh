4aab9c01dfb21f8aa6ee926ac7f13b9f2a5f7ff6
Bill Wendling <wendling@ncsa.uiuc.edu>
2000-12-08 11:15:00 -0500

    [svn-r3097] Purpose: 	Add Description: 	Adding a small amount of testing to the C++ library until "real" 	testing can be created. (We use the examples). Platforms tested: 	Linux

---
+++ c++/examples/testexamples.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+CMP='cmp -s'
+DIFF='diff -c'
+
+nerrors=0
+verbose=yes
+
+actual=sample.out
+expect=expected.out
+
+# The build (current) directory might be different than the source directory.
+if test -z "$srcdir"; then
+   srcdir=.
+fi
+
+# Print a line-line message left justified in a field of 70 characters
+# beginning with the word "Testing".
+TESTING()
+{
+    SPACES="                                                               "
+    echo "Testing $* $SPACES" |cut -c1-70 |tr -d '\012'
+}
+
+TESTING C++ Examples
+
+(
+   ./create
+   ./readdata
+   ./writedata
+   ./compound
+   ./extend_ds
+   ./chunks
+   ./h5group
+) > $actual
+
+if $CMP $expect $actual; then
+   echo " PASSED"
+else
+   echo "*FAILED*"
+   echo "    Expected result differs from actual result"
+   nerrors="`expr $nerrors + 1`"
+   test yes = "$verbose" && $DIFF $expect $actual | sed 's/^/    /'
+fi
+
+# Clean up output file
+if test -z "$HDF5_NOCLEANUP"; then
+   rm -f $actual
+fi
+
+if test $nerrors -eq 0 ; then
+   echo "All tests passed."
+fi
+
+exit $nerrors
