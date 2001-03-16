1d771c6949222616e9f32d99daaf4fb793bec7ba
Elena Pourmal <epourmal@hdfgroup.org>
2001-03-16 10:58:05 -0500

    [svn-r3651] Purpose:     Maintenance Description:     [describe the bug, or describe the new feature, etc] Solution:     Added comment block to each subroutine. Platforms tested:     NONE, do it later.

--- fortran/src/H5FDmpioff.f90
+++ fortran/src/H5FDmpioff.f90
@@ -2,9 +2,33 @@
 ! This file contains Fortran90 interfaces for H5P functions needed by || MPI programs.
 !
      MODULE H5FDMPIO
-         USE H5FORTRAN_TYPES
-         USE H5FORTRAN_FLAGS
+         USE H5GLOBAL
          CONTAINS
+
+!----------------------------------------------------------------------
+! Name:		h5pset_fapl_mpio_f 
+!
+! Purpose: 	Stores MPI IO communicator information to the file 
+!		access property list. 
+!
+! Inputs:  	
+!		prp_id		- file access property list identifier
+!		comm		- MPI-2 communicator
+!		info		- MPI-2 info object
+! Outputs:  
+!		hdferr:		- error code		
+!				 	Success:  0
+!				 	Failure: -1   
+! Optional parameters:
+!				NONE
+!
+! Programmer:	Elena Pourmal
+!		November, 2000
+!
+! Modifications:
+!
+! Comment:		
+!----------------------------------------------------------------------
          SUBROUTINE h5pset_fapl_mpio_f(prp_id, comm, info, hdferr) 
             IMPLICIT NONE
             INTEGER(HID_T), INTENT(IN) :: prp_id ! Property list identifier
@@ -18,11 +42,35 @@
             hdferr = h5pset_fapl_mpio_c(prp_id, comm, info)
           END SUBROUTINE h5pset_fapl_mpio_f
 
+!----------------------------------------------------------------------
+! Name:		h5pget_fapl_mpio_f 
+!
+! Purpose: 	Returns MPI communicator information. 	
+!
+! Inputs:  
+!		prp_id		- file access property list identifier
+! Outputs:  
+!		comm		- MPI-2 communicator
+!		info		- MPI-2 info object
+!		hdferr:		- error code		
+!				 	Success:  0
+!				 	Failure: -1   
+! Optional parameters:
+!				NONE
+!
+! Programmer:	Elena Pourmal
+!		November, 2000
+!
+! Modifications: 
+!
+! Comment:		
+!----------------------------------------------------------------------
+
           SUBROUTINE h5pget_fapl_mpio_f(prp_id, comm, info, hdferr) 
             IMPLICIT NONE
             INTEGER(HID_T), INTENT(IN) :: prp_id ! Property list identifier
             INTEGER, INTENT(OUT) :: comm ! buffer to return communicator 
-            INTEGER, INTENT(IN) :: info ! buffer to return info object 
+            INTEGER, INTENT(OUT) :: info ! buffer to return info object 
                                         ! as defined in MPI_FILE_OPEN of MPI-2
             INTEGER, INTENT(OUT) :: hdferr  ! Error code
 
@@ -30,24 +78,76 @@
             hdferr = h5pget_fapl_mpio_c(prp_id, comm, info)
           END SUBROUTINE h5pget_fapl_mpio_f
 
+!----------------------------------------------------------------------
+! Name:		h5pset_dxpl_mpio_f 
+!
+! Purpose: 	Sets data transfer mode. 
+!
+! Inputs:  
+!		prp_id		- data transfer property list identifier
+!		data_xfer_mode	- transfer mode
+!				  Possible values are:
+!				  H5FD_MPIO_INDEPENDENT_F
+!				  H5FD_MPIO_COLLECTIVE_F
+! Outputs:  
+!		hdferr:		- error code		
+!				 	Success:  0
+!				 	Failure: -1   
+! Optional parameters:
+!				NONE
+!
+! Programmer:	Elena Pourmal
+!		November, 2000
+!
+! Modifications: 	
+!
+! Comment:		
+!----------------------------------------------------------------------
+
          SUBROUTINE h5pset_dxpl_mpio_f(prp_id, data_xfer_mode, hdferr) 
             IMPLICIT NONE
             INTEGER(HID_T), INTENT(IN) :: prp_id ! Property list identifier
             INTEGER, INTENT(IN) :: data_xfer_mode ! Data transfer mode. Possible values are:
-                                                  ! H5FD_MPIO_INDEPENDENT_F (0)
-                                                  ! H5FD_MPIO_COLLECTIVE_F  (1)
+                                                  ! H5FD_MPIO_INDEPENDENT_F
+                                                  ! H5FD_MPIO_COLLECTIVE_F
             INTEGER, INTENT(OUT) :: hdferr  ! Error code
 
             INTEGER, EXTERNAL :: h5pset_dxpl_mpio_c 
             hdferr = h5pset_dxpl_mpio_c(prp_id, data_xfer_mode)
           END SUBROUTINE h5pset_dxpl_mpio_f
 
+!----------------------------------------------------------------------
+! Name:		h5pget_dxpl_mpio_f 
+!
+! Purpose: 	Returns the data transfer mode. 
+!
+! Inputs:  
+!		prp_id		- data transfer property list identifier
+! Outputs:  
+!		data_xfer_mode	- transfer mode
+!				  Possible values are:
+!				  H5FD_MPIO_INDEPENDENT_F
+!				  H5FD_MPIO_COLLECTIVE_F
+!		hdferr:		- error code		
+!				 	Success:  0
+!				 	Failure: -1   
+! Optional parameters:
+!				NONE
+!
+! Programmer:	Elena Pourmal
+!		November, 2000
+!
+! Modifications: 
+!
+! Comment:		
+!----------------------------------------------------------------------
+
          SUBROUTINE h5pget_dxpl_mpio_f(prp_id, data_xfer_mode, hdferr) 
             IMPLICIT NONE
             INTEGER(HID_T), INTENT(IN) :: prp_id ! Property list identifier
             INTEGER, INTENT(OUT) :: data_xfer_mode ! Data transfer mode. Possible values are:
-                                                  ! H5FD_MPIO_INDEPENDENT_F (0)
-                                                  ! H5FD_MPIO_COLLECTIVE_F  (1)
+                                                  ! H5FD_MPIO_INDEPENDENT_F
+                                                  ! H5FD_MPIO_COLLECTIVE_F
             INTEGER, INTENT(OUT) :: hdferr  ! Error code
 
             INTEGER, EXTERNAL :: h5pget_dxpl_mpio_c
