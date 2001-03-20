6da03040a2ee268502ef49d340a86ebd8281d344
Albert Cheng <acheng@hdfgroup.org>
2001-03-20 13:29:57 -0500

    [svn-r3674] Peurpose:     Changed the version information to 1.4.0-pre0, preparing for     1.4.1 release. Platforms tested:     Modi4 (IRIX64) serial and parallel modes.

--- src/H5public.h
+++ src/H5public.h
@@ -56,8 +56,8 @@ static char             RcsId[] = "@(#)$Revision$";
 /* Version numbers */
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	4	/* For minor interface/format changes  	     */
-#define H5_VERS_RELEASE	0	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "post0"	/* For pre-releases like snap0       */
+#define H5_VERS_RELEASE	1	/* For tweaks, bug-fixes, or development     */
+#define H5_VERS_SUBRELEASE "pre0"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
