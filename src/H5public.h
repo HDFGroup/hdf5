87456c1137f27203be42691b839ee1c0d72e7d93
Albert Cheng <acheng@hdfgroup.org>
2001-05-11 09:16:53 -0500

    [svn-r3912] Purpose:     Cosmetic adjustment Description:     Changed Version information from just "HDF5 version ..."     to "HDF5 library version" since this is really about a     library implementation. Platforms tested:     eirene (used "h5vers -s ..." to see the effect.)

--- src/H5public.h
+++ src/H5public.h
@@ -63,7 +63,7 @@
 #define H5_VERS_RELEASE	1	/* For tweaks, bug-fixes, or development     */
 #define H5_VERS_SUBRELEASE "post2"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "hdf5 version: 1.4.1-post2"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.4.1-post2"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
