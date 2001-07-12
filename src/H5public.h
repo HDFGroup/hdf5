8a5a5f410e96e35d1466a05026e71e8d7c667aa3
Albert Cheng <acheng@hdfgroup.org>
2001-07-12 13:01:54 -0500

    [svn-r4200] purpose:     Updated for 1.4.2-pre2 testing. Platforms tested:     Eirene

--- src/H5public.h
+++ src/H5public.h
@@ -61,9 +61,9 @@
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	4	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	2	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "pre1"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE "pre2"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.4.2-pre0"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.4.2-pre2"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
