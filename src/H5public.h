c4e98f4c9b47a5bde67a7e05b800b91f64118bf8
Elena Pourmal <epourmal@hdfgroup.org>
2001-07-11 23:18:24 -0500

    [svn-r4195] Purpose:     Maintenance Description:     Changed subrelease version to pre1 for the second round of testing

--- src/H5public.h
+++ src/H5public.h
@@ -61,7 +61,7 @@
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	4	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	2	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "pre0"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE "pre1"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
 #define H5_VERS_INFO    "HDF5 library version: 1.4.2-pre0"      /* Full version string */
 
