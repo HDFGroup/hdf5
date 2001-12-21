302e978dd9efb8ddc33543fcc3673e01c8785d6a
Bill Wendling <wendling@ncsa.uiuc.edu>
2001-12-21 16:11:32 -0500

    [svn-r4751] Purpose: 	Bug Fix Description: 	Added support for HP-UX 11.00 in parallel. We needed to set the 	"MPI_Status" variable to 0 before using it. Solution: 	Set the MPI_Status variable to 0. Platforms tested: 	Linux (pp) and HP-UX (pp)

--- src/H5FDmpio.c
+++ src/H5FDmpio.c
@@ -540,7 +540,7 @@ H5FD_mpio_wait_for_left_neighbor(H5FD_t *_file)
     MPI_Comm comm;
     char msgbuf[1];
     int myid;
-    MPI_Status rcvstat;
+    MPI_Status rcvstat = {0};
 
     FUNC_ENTER(H5FD_mpio_wait_for_left_neighbor, FAIL);
     assert(file);
@@ -1069,7 +1069,7 @@ H5FD_mpio_read(H5FD_t *_file, H5FD_mem_t UNUSED type, hid_t dxpl_id, haddr_t add
     const H5FD_mpio_dxpl_t	*dx=NULL;
     H5FD_mpio_dxpl_t		_dx;
     MPI_Offset			mpi_off, mpi_disp;
-    MPI_Status  		mpi_stat;
+    MPI_Status  		mpi_stat = {0};
     MPI_Datatype		buf_type, file_type;
     int         		size_i, bytes_read, n;
     int				use_types_this_time, used_types_last_time;
@@ -1314,7 +1314,7 @@ H5FD_mpio_write(H5FD_t *_file, H5FD_mem_t UNUSED type, hid_t dxpl_id/*unused*/, 
     const H5FD_mpio_dxpl_t	*dx=NULL;
     H5FD_mpio_dxpl_t		_dx;
     MPI_Offset 		 	mpi_off, mpi_disp;
-    MPI_Status			mpi_stat;
+    MPI_Status			mpi_stat = {0};
     MPI_Datatype		buf_type, file_type;
     int         		size_i, bytes_written;
     int				mpi_rank=-1;
@@ -1508,7 +1508,7 @@ H5FD_mpio_flush(H5FD_t *_file)
     H5FD_mpio_t		*file = (H5FD_mpio_t*)_file;
     int                 mpi_rank=-1;
     uint8_t             byte=0;
-    MPI_Status          mpi_stat;
+    MPI_Status          mpi_stat = {0};
     MPI_Offset          mpi_off;
 
     FUNC_ENTER(H5FD_mpio_flush, FAIL);
