f33cb1352ce4e3329fce60551d6602616ed3ab98
HDF Admin <hdfadmin@ncsa.uiuc.edu>
2002-02-09 04:53:25 -0500

    [svn-r4930] Snapshot version 1.5 release 20

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-HDF5 version 1.5.20 currently under development
+HDF5 version 1.5.21 currently under development
 Please refer to the INSTALL file for installation instructions.
 ------------------------------------------------------------------------------
 
--- src/H5public.h
+++ src/H5public.h
@@ -66,10 +66,10 @@
 /* Version numbers */
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	5	/* For minor interface/format changes  	     */
-#define H5_VERS_RELEASE	20	/* For tweaks, bug-fixes, or development     */
+#define H5_VERS_RELEASE	21	/* For tweaks, bug-fixes, or development     */
 #define H5_VERS_SUBRELEASE ""	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.5.20"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.5.21"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
