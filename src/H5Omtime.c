a48c0628505d605ad09d658b0e6a1346885f22f1
MuQun Yang <ymuqun@hdfgroup.org>
2002-04-30 13:23:21 -0500

    [svn-r5294] Purpose:    turn on windows daylight feature Description:     Currently we have to hard-code daylight feature on windows to make mtime pass. Solution: Platforms tested: linux 2.2.18

--- src/H5Omtime.c
+++ src/H5Omtime.c
@@ -154,10 +154,10 @@ H5O_mtime_decode(H5F_t UNUSED *f, const uint8_t *p,
 
    tz = timebuffer.timezone;
   
-   /*daylight is not handled properly. Currently we just hard-code                        the problem. */ 
-   the_time -=tz*60;
+   /*daylight is not handled properly. Currently we just hard-code the problem. */ 
+/*   the_time -=tz*60;*/
 
-   /*the_time -=tz*60-3600*_daylight;*/
+   the_time -=tz*60-3600*_daylight;
 
    
 }
