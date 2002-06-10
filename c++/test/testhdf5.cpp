cf17bf48dfe2f91043d3c477af7923b269296849
Binh-Minh Ribler <bmribler@hdfgroup.org>
2002-06-10 07:13:28 -0500

    [svn-r5557] Purpose: Copyright
    Description:
        Added copyright notice to C++ API files, including *.h, *.cpp, and
        Makefile.in.
    Platforms:
        Linux 6.2 (eirene)

--- c++/test/testhdf5.cpp
+++ c++/test/testhdf5.cpp
@@ -1,10 +1,18 @@
-/*
- * Copyright (C) 2001 National Center for Supercomputing Applications
- *                    All rights reserved.
- *
- */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  * Copyright by the Board of Trustees of the University of Illinois.         *
+  * All rights reserved.                                                      *
+  *                                                                           *
+  * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+  * terms governing use, modification, and redistribution, is contained in    *
+  * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+  * of the source code distribution tree; Copyright.html can be found at the  *
+  * root level of an installed copy of the electronic HDF5 document set and   *
+  * is linked from the top-level documents page.  It can also be found at     *
+  * http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have     *
+  * access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/*
+/*****************************************************************************
    FILE
    testhdf5.cpp - HDF5 testing framework main file.
 
@@ -25,7 +33,7 @@
    EXPORTED ROUTINES/VARIABLES:
    Two variables are exported: num_errs, and Verbosity.
 
- */
+ ***************************************************************************/
 
 #ifdef __MWERKS__
 #include <console.h>
