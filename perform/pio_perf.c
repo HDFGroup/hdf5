4e184b81a232187ae6f21daf783c35f0bc6dacc1
Albert Cheng <acheng@hdfgroup.org>
2002-06-19 22:44:20 -0500

    [svn-r5686] Purpose:     sync with v1.4 of usage messages. Platforms tested:     eirene pp.

--- perform/pio_perf.c
+++ perform/pio_perf.c
@@ -1,9 +1,16 @@
-/*
- * Copyright (C) 2001, 2002
- *     National Center for Supercomputing Applications
- *     All rights reserved.
- *
- */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have     *
+ * access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /*
  * Parallel HDF5 Performance Testing Code
@@ -969,10 +976,9 @@ print_io_api(long io_types)
 static void
 report_parameters(struct options *opts)
 {
-    int rank;
+    int rank = comm_world_rank_g;
 
-    MPI_Comm_rank(pio_comm_g, &rank);
-
+    print_version("HDF5 Library");	/* print library version */
     HDfprintf(output, "rank %d: ==== Parameters ====\n", rank);
 
     HDfprintf(output, "rank %d: IO API=", rank);
@@ -1288,6 +1294,7 @@ usage(const char *prog)
         printf("     -b, --binary                The elusive binary option\n");
 #endif  /* 0 */
         printf("     -B S, --block-size=S        Interleaved block size\n");
+        printf("                                 [not implemented yet]\n");
         printf("                                 [default: 0 no interleaved IO]\n");
         printf("     -c, --chunk                 Create HDF5 datasets chunked [default: off]\n");
         printf("     -d N, --num-dsets=N         Number of datasets per file [default:1]\n");
