5f486e6e4648a4aad2cbf97a632f14a5254b8c47
HDF Admin <hdfadmin@ncsa.uiuc.edu>
2002-06-20 10:45:12 -0500

    [svn-r5688] Description:     Make a tarball of 1.4.4-pre4 that contains CVS stuff for easier     commit later.  Bump up version information by bin/h5vers -i. Platforms tested:     No test since this is the same process the release script will do.

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-HDF5 version 1.4.4-pre4 currently under development
+HDF5 version 1.4.4-pre5 currently under development
 Please refer to the release_docs/INSTALL file for installation instructions.
 ------------------------------------------------------------------------------
 
--- src/H5public.h
+++ src/H5public.h
@@ -65,9 +65,9 @@
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	4	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	4	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "pre4"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE "pre5"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.4.4-pre4"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.4.4-pre5"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
