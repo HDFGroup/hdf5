f666340233a3695d4a4cca683d4597e901c007cf
MuQun Yang <ymuqun@hdfgroup.org>
2003-05-13 09:26:44 -0500

    [svn-r6851] Purpose: To add macro HDF5TESTDLL for windows libtest library.
    Description:
    Many HDF5 tests will use h5test.c.
    So we create a libtest library to
    1) make tests more organized and
    2) make tests more consistent with other platforms
    3) reduce compiling times
    4) reduce dumplicated warnings possibily generated by h5test.c
    However, we didn't create the corresponding libtest DLL correctly,
    now, some warnings are generated because of this; so for this release,
    we follow exactly DLL-DLL rules, to create an internal libtest DLL for
    our test suites.
    The application who used HDF5DLL will not have extra burdens to add
    HDF5TESTDLL and this will reflect in the release.txt
    
    
    Solution:
    USE DLL import and export library by defining macro HDF5TESTDLL and HDF5TESTUSEDLL
    
    Platforms tested:
    Windows 2000(the macro will not affect on non-WIN32 platforms).
    
    Misc. update:

--- src/H5api_adpt.h
+++ src/H5api_adpt.h
@@ -34,6 +34,18 @@
 #define H5_DLLVAR extern
 #endif /* _HDF5DLL_ */
 
+#if defined(_HDF5TESTDLL_)
+#pragma warning(disable: 4273)	/* Disable the dll linkage warnings */
+#define H5TEST_DLL __declspec(dllexport)
+#define H5TEST_DLLVAR __declspec(dllexport)
+#elif defined(_HDF5TESTUSEDLL_)
+#define H5TEST_DLL __declspec(dllimport)
+#define H5TEST_DLLVAR __declspec(dllimport)
+#else
+#define H5TEST_DLL
+#define H5TEST_DLLVAR extern
+#endif /* _HDF5TESTDLL_ */
+
 // Added to export or to import C++ APIs - BMR (02-15-2002)
 #if defined(HDF5_CPPDLL_EXPORTS) // this name is generated at creation
 #define H5_DLLCPP __declspec(dllexport)
