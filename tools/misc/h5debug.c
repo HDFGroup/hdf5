221c1ef220b49d46fa05ff267f11445f6d1a8b44
Quincey Koziol <koziol@hdfgroup.org>
2003-07-09 13:14:29 -0500

    [svn-r7191] Purpose:     Code cleanup
    Description:
        Track new "debug" modules for library.
    
    Platforms tested:
        h5committested

--- tools/misc/h5debug.c
+++ tools/misc/h5debug.c
@@ -28,14 +28,14 @@
 #define H5O_PACKAGE		/*suppress error about including H5Opkg	  */
 
 #include "H5private.h"
-#include "H5Iprivate.h"
 #include "H5Bprivate.h"
-#include "H5Pprivate.h"
 #include "H5Fpkg.h"
 #include "H5Gprivate.h"
 #include "H5HGprivate.h"
 #include "H5HLprivate.h"
+#include "H5Iprivate.h"
 #include "H5Opkg.h"
+#include "H5Pprivate.h"
 
 /* File drivers */
 #include "H5FDfamily.h"
@@ -128,7 +128,7 @@ main(int argc, char *argv[])
         /*
          * Debug the boot block.
          */
-        status = H5F_debug(f, H5P_DATASET_XFER_DEFAULT, addr, stdout, 0, VCOL);
+        status = H5F_debug(f, H5P_DATASET_XFER_DEFAULT, stdout, 0, VCOL);
 
     } else if (!HDmemcmp(sig, H5HL_MAGIC, H5HL_SIZEOF_MAGIC)) {
         /*
