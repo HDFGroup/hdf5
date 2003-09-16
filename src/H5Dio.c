bf1c2f0e8bf9788c2e47a1b1ac963cc321afab0e
Quincey Koziol <koziol@hdfgroup.org>
2003-09-16 12:25:49 -0500

    [svn-r7478] Purpose:     Bug fix
    Description:
        H5Dwrite was only checking the "top level" of the datatype to stop parallel
    I/O on variable-length datatypes.
    
    Solution:
        Make checks "deeper".
    
    Platforms tested:
        FreeBSD 4.9 (sleipnir)
        h5committest

--- src/H5Dio.c
+++ src/H5Dio.c
@@ -741,11 +741,14 @@ H5D_write(H5D_t *dataset, const H5T_t *mem_type, const H5S_t *mem_space,
     /* If MPI based VFD is used, no VL datatype support yet. */
     /* This is because they use the global heap in the file and we don't */
     /* support parallel access of that yet */
-    if (IS_H5FD_MPI(dataset->ent.file) && H5T_get_class(mem_type)==H5T_VLEN)
+    if (IS_H5FD_MPI(dataset->ent.file) && H5T_detect_class(mem_type, H5T_VLEN)>0)
         HGOTO_ERROR (H5E_DATASET, H5E_UNSUPPORTED, FAIL, "Parallel IO does not support writing VL datatypes yet")
     /* If MPI based VFD is used, no VL datatype support yet. */
     /* This is because they use the global heap in the file and we don't */
     /* support parallel access of that yet */
+    /* We should really use H5T_detect_class() here, but it will be difficult
+     * to detect the type of the reference if it is nested... -QAK
+     */
     if (IS_H5FD_MPI(dataset->ent.file) &&
             H5T_get_class(mem_type)==H5T_REFERENCE &&
             H5T_get_ref_type(mem_type)==H5R_DATASET_REGION)
