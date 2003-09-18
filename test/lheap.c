1440f2c2db7cce110052d5363796b634b676090f
Bill Wendling <wendling@ncsa.uiuc.edu>
2003-09-18 14:27:45 -0500

    [svn-r7490] Purpose:     Update
    Description:
        Removed the call to H5HL_peek
    
    Platforms tested:
        Modi4 (paralle, Fortran)
        Sol (Fortran)
        Linux (C++, Fortran)
        Copper (Parallel, Fortran)
    
    Misc. update:

--- test/lheap.c
+++ test/lheap.c
@@ -110,19 +110,35 @@ main(void)
 	goto error;
     }
     for (i=0; i<NOBJS; i++) {
+        H5HL_t *heap = NULL;
+
 	sprintf(buf, "%03d-", i);
         for (j=4; j<i; j++) buf[j] = '0' + j%10;
         if (j>4) buf[j] = '\0';
-        if (NULL==(s=H5HL_peek(f, H5P_DATASET_XFER_DEFAULT, heap_addr, obj[i]))) {
+
+        if (NULL == (heap = H5HL_protect(f, H5P_DATASET_XFER_DEFAULT, heap_addr))) {
 	    H5_FAILED();
 	    H5Eprint(H5E_DEFAULT, stdout);
 	    goto error;
 	}
+        
+        if (NULL == (s = H5HL_offset_into(f, heap, obj[i]))) {
+	    H5_FAILED();
+	    H5Eprint(H5E_DEFAULT, stdout);
+	    goto error;
+	}
+
 	if (strcmp(s, buf)) {
 	    H5_FAILED();
 	    printf("    i=%d, heap offset=%lu\n", i, (unsigned long)(obj[i]));
 	    printf("    got: \"%s\"\n", s);
 	    printf("    ans: \"%s\"\n", buf);
+	    goto error;
+	}
+
+        if (H5HL_unprotect(f, H5P_DATASET_XFER_DEFAULT, heap, heap_addr) < 0) {
+	    H5_FAILED();
+	    H5Eprint(H5E_DEFAULT, stdout);
 	    goto error;
 	}
     }
