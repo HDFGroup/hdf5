c6c904de02078441796e521c464443a016c0d42b
Elena Pourmal <epourmal@hdfgroup.org>
2003-10-06 16:23:05 -0500

    [svn-r7556] Purpose: Enhancement
    Description: On 64-bit Solaris and on Crays h5_chunk_read example
                 displayed garbage instead of 0. One may mistakenly conclude
                 that example is broken.
                 h5_chunk_read reads data written by h5_extend_write.
                 h5_extend_write extends dataset and does only partial writes. Therefore
                 some data is never written to the file and since fill value
                 is not set up, garbage is read back by h5_chunk_read
    
    Solution: Set up fill value to initialize data.
    
    Platforms tested: arabica 64-bit and Cray T3E.
    
    Misc. update:

--- examples/h5_extend_write.c
+++ examples/h5_extend_write.c
@@ -43,6 +43,7 @@ main (void)
 
     int         data3[2][2] = { {3, 3},
 				{3, 3} };
+    int         fill_val = 0;
 
     /*
      * Create the data space with unlimited dimensions. 
@@ -59,6 +60,7 @@ main (void)
      */
     cparms = H5Pcreate(H5P_DATASET_CREATE);
     status = H5Pset_chunk( cparms, RANK, chunk_dims);
+             H5Pset_fill_value(cparms, H5T_NATIVE_INT, &fill_val);
 
     /*
      * Create a new dataset within the file using cparms
