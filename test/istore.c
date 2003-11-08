dd969f1eadfd2cd500f3f44415b85cfea7216794
Quincey Koziol <koziol@hdfgroup.org>
2003-11-08 09:21:49 -0500

    [svn-r7827] Purpose:     Bug fix
    Description:
        Make --disable-hsizet work properly again by fixing a couple of tests and
    correcting the definition of SSIZET_MAX.
    
    Platforms tested:
        FreeBSD 4.9 (sleipnir)
        h5committest not used, as it doesn't test this configuration.

--- test/istore.c
+++ test/istore.c
@@ -47,7 +47,11 @@ const char *FILENAME[] = {
 #define TEST_DATATYPE   H5T_NATIVE_UCHAR
 
 #define TEST_CHUNK_SIZE 50
+#ifdef H5_HAVE_LARGE_HSIZET
 #define TEST_SPARSE_SIZE 1000000
+#else /* H5_HAVE_LARGE_HSIZET */
+#define TEST_SPARSE_SIZE 1200
+#endif /* H5_HAVE_LARGE_HSIZET */
 
 hsize_t chunk_dims[H5O_LAYOUT_NDIMS];
 hssize_t zero[H5O_LAYOUT_NDIMS];
