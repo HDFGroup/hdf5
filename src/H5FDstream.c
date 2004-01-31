c3b2c4132760e245a5d7af6bacb20265c4df0c84
Quincey Koziol <koziol@hdfgroup.org>
2004-01-31 09:15:24 -0500

    [svn-r8129] Purpose:     Bug fix
    Description:
        Include the private H5FD header instead of the public one, to address a
    failure in building the library with C++ compiler.
    
    Platforms tested:
        FreeBSD 4.9 (sleipnit) w/CC=g++
        not appropriate for h5committest

--- src/H5FDstream.c
+++ src/H5FDstream.c
@@ -27,7 +27,7 @@
 
 #include "H5private.h"		/* Generic Functions			*/
 #include "H5Eprivate.h"		/* Error handling		  	*/
-#include "H5FDpublic.h"		/* Public file drivers				*/
+#include "H5FDprivate.h"	/* File drivers				*/
 #include "H5FDstream.h"		/* Stream file driver			*/
 #include "H5Iprivate.h"		/* IDs			  		*/
 #include "H5MMprivate.h"	/* Memory management			*/
