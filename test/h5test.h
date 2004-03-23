0642ae58be8a66a882e46c381753facea4568337
Albert Cheng <acheng@hdfgroup.org>
2004-03-22 23:43:10 -0500

    [svn-r8273] Purpose: Code cleanup.
    Description:
    The routines residing in testframe.c was defined in testhdf5.h due to
    historical reason.  It really belongs to h5test.h because those routines
    reside in libh55test.a.
    
    Solution:
    Moved them to the right place.  Also removed the duplicated occurance
    of testframe.c in the TEST_SRC.
    
    Platforms tested:
    Tested in o2 (SGI) parallel.

--- test/h5test.h
+++ test/h5test.h
@@ -101,6 +101,7 @@ extern MPI_Info h5_io_info_g;         /* MPI INFO object for IO */
 extern "C" {
 #endif
 
+/* Generally useful testing routines */
 H5TEST_DLL int h5_cleanup(const char *base_name[], hid_t fapl);
 H5TEST_DLL herr_t h5_errors(void *client_data);
 H5TEST_DLL char *h5_fixname(const char *base_name, hid_t fapl, char *fullname,
@@ -111,6 +112,22 @@ H5TEST_DLL void h5_reset(void);
 H5TEST_DLL void h5_show_hostname(void);
 H5TEST_DLL off_t h5_get_file_size(const char *filename);
 H5TEST_DLL int print_func(const char *format, ...);
+
+/* Routines for operating on the list of tests (for the "all in one" tests) */
+H5TEST_DLL void TestUsage(void);
+H5TEST_DLL void AddTest(const char *TheName, void (*TheCall) (void),
+		     void (*Cleanup) (void), const char *TheDescr);
+H5TEST_DLL void TestInfo(const char *ProgName);
+H5TEST_DLL void TestParseCmdLine(int argc, char *argv[], int *Summary, int *CleanUp);
+H5TEST_DLL void PerformTests(void);
+H5TEST_DLL void TestSummary(void);
+H5TEST_DLL void TestCleanup(void);
+H5TEST_DLL void TestInit(void);
+H5TEST_DLL int  GetTestVerbosity(void);
+H5TEST_DLL int  SetTestVerbosity(int newval);
+H5TEST_DLL void ParseTestVerbosity(char *argv);
+H5TEST_DLL int  GetTestNumErrs(void);
+H5TEST_DLL int  TestErrPrintf(const char *format, ...);
 
 #ifdef H5_HAVE_PARALLEL
 H5TEST_DLL int h5_set_info_object(void);
--- test/testhdf5.h
+++ test/testhdf5.h
@@ -107,22 +107,6 @@
 extern "C" {
 #endif
 
-/* Routines for operating on the list of tests (for the "all in one" tests) */
-H5TEST_DLL void TestUsage(void);
-H5TEST_DLL void AddTest(const char *TheName, void (*TheCall) (void),
-		     void (*Cleanup) (void), const char *TheDescr);
-H5TEST_DLL void TestInfo(const char *ProgName);
-H5TEST_DLL void TestParseCmdLine(int argc, char *argv[], int *Summary, int *CleanUp);
-H5TEST_DLL void PerformTests(void);
-H5TEST_DLL void TestSummary(void);
-H5TEST_DLL void TestCleanup(void);
-H5TEST_DLL void TestInit(void);
-H5TEST_DLL int  GetTestVerbosity(void);
-H5TEST_DLL int  SetTestVerbosity(int newval);
-H5TEST_DLL void ParseTestVerbosity(char *argv);
-H5TEST_DLL int  GetTestNumErrs(void);
-H5TEST_DLL int  TestErrPrintf(const char *format, ...);
-
 /* Prototypes for the test routines */
 void                    test_metadata(void);
 void                    test_tbbt(void);
