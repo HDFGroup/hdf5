6ce8bab631f1de7ff4fed4feaebbd204de13b7c7
MuQun Yang <ymuqun@hdfgroup.org>
2004-08-13 07:53:13 -0500

    [svn-r9075] Purpose: Parallel HDF5 Collective chunk IO testing message added
    Description:
    collective chunk IO tests have been verified with the number of process greater than 24 and
    the test is very slow with big number of process.
    
    That may cause confusions to users who run collective chunk IO tests.
    
    
    Solution:
    To avoid possible confusions, A if-block will be used to check whether the number of process
    is greater than 24. If yes, the collective chunk tests will be skipped and a message will be printed out.
    
    Platforms tested:
    To trivial for h5committest,testing platform: eirene.
    
    Misc. update:

--- testpar/testphdf5.c
+++ testpar/testphdf5.c
@@ -465,6 +465,13 @@ int main(int argc, char **argv)
     AddTest("fillvalue", dataset_fillvalue, NULL, 
 	    "dataset fill value", filenames[8]);
 
+    
+    if(mpi_size > 24) {
+      printf("The collective chunk IO test hasn't been tested for the number of process greater than 24\n");
+      printf("Please try with the number of process no greater than 24\n");
+      printf("All collective chunk tests will be skipped \n");
+      goto finish;
+    }
     AddTest("coll_chunked1", coll_chunk1,NULL,
 	    "simple collective chunk io",filenames[9]);
     AddTest("coll_chunked2", coll_chunk2,NULL,
