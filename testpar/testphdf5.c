c7364155af94c6b07f18147075046879d69a271f
Albert Cheng <acheng@hdfgroup.org>
2004-09-09 09:21:01 -0500

    [svn-r9235] Purpose: Shrink HDF5 parallel test (folded what Kent has done in v1.7 to v1.6.)
    Description:
    Previously the collective chunk IO tests created and opened four HDF5 files;
    it turned out that all collective chunk IO tests can use one file for testing.
    
    Solution:
    To use one file for all collective chunk IO tests with trunc option to be set in
    H5Fcreate.
    
    Platforms tested:
    eirene (no need to use h5committest)

--- testpar/testphdf5.c
+++ testpar/testphdf5.c
@@ -39,7 +39,7 @@ void *old_client_data;			/* previous error handler arg.*/
 /* other option flags */
 
 /* FILENAME and filenames must have the same number of names */
-const char *FILENAME[14]={
+const char *FILENAME[11]={
 	    "ParaEg1",
 	    "ParaEg2",
 	    "ParaEg3",
@@ -49,12 +49,9 @@ const char *FILENAME[14]={
             "ParaIndividual",
             "ParaBig",
             "ParaFill",
-	    "ParaCC1",
-	    "ParaCC2",
-	    "ParaCC3",
-            "ParaCC4",
+	    "ParaCC",
 	    NULL};
-char	filenames[14][PATH_MAX];
+char	filenames[11][PATH_MAX];
 hid_t	fapl;				/* file access property list */
 
 #ifdef USE_PAUSE
@@ -430,11 +427,11 @@ int main(int argc, char **argv)
       AddTest("cchunk1", coll_chunk1,NULL,
 	      "simple collective chunk io",filenames[9]);
       AddTest("cchunk2", coll_chunk2,NULL,
-	      "noncontiguous collective chunk io",filenames[10]);
+	      "noncontiguous collective chunk io",filenames[9]);
       AddTest("cchunk3", coll_chunk3,NULL,
-	      "multi-chunk collective chunk io",filenames[11]);
+	      "multi-chunk collective chunk io",filenames[9]);
       AddTest("cchunk4", coll_chunk4,NULL,
-	      "collective to independent chunk io",filenames[12]);
+	      "collective to independent chunk io",filenames[9]);
     }
 
     /* Display testing information */
