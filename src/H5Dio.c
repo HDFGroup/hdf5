29a56c1d2e88c0bd43c87d574f74188a83c5cddb
Quincey Koziol <koziol@hdfgroup.org>
2005-01-06 16:22:22 -0500

    [svn-r9763] Purpose:     Code cleanup
    Description:
        Change pablo mask to conform to the style used by the rest of the library
    
    Platforms tested:
        None needed - very, very minor

--- src/H5Dio.c
+++ src/H5Dio.c
@@ -16,7 +16,7 @@
 
 /* Pablo information */
 /* (Put before include files to avoid problems with inline functions) */
-#define PABLO_MASK	H5Dio_mask
+#define PABLO_MASK	H5D_io_mask
 
 #include "H5private.h"		/* Generic Functions			*/
 #include "H5Dpkg.h"		/* Dataset functions			*/
