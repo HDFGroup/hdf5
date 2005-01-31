7b6f1701bca63dd08c7d86c11a3abf6ece4ea11f
James Laird <jlaird@hdfgroup.org>
2005-01-31 15:51:22 -0500

    [svn-r9896] Purpose: Update
    Description:
    Updated version of autoconf from 2.53 to 2.59
    
    Solution:
    Simply ran autoconf 2.59 to generate a new configure from configure.in.
    Also modified configure.in to remove superfluous characters (can write
    `variable' instead of \`variable').
    
    Added bin/reconfigure.sh script.
    TO RECONFIGURE WITH THE CORRECT VERSION OF AUTOCONF:
    rsh to heping
    /bin/sh bin/reconfigure.sh
    
    Platforms tested:
    Many.

---
+++ reconfigure.sh
@@ -0,0 +1,29 @@
+#! bin/sh
+# Reconfigure for HDF5 1.6
+# A simple script to reconfigure autotools for HDF5.
+# The autotools include a reconfigure script, but this script
+# has the paths of autoconf and automake hardcoded to work on HDF
+# machines.  Thus is enforces that HDF5 always uses the
+# same versions of autotools.
+# Uses autoconf version 2.59
+# Does not use automake
+
+# Discover which machine this script is being run on.
+# Right now, heping is the only machine with the correct
+# versions of autoconf and automake installed.
+
+HOSTNAME=$(hostname)
+
+if [ "$HOSTNAME" != "heping" ]; then
+  echo "HDF5 can only be reconfigured on heping, sorry."
+else
+
+  # If this is heping, run commands in order
+  echo /usr/local/autoconf-2.59/bin/autoheader
+  /usr/local/autoconf-2.59/bin/autoheader
+  echo /usr/local/autoconf-2.59/bin/autoconf
+  /usr/local/autoconf-2.59/bin/autoconf
+
+fi
+
+exit 0
