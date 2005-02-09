e1ef99ed1dbbaa57428cc8cbc8513e6adf6ae5c6
Albert Cheng <acheng@hdfgroup.org>
2005-02-09 06:36:17 -0500

    [svn-r9964] Purpose: bug fix
    Description:
    g++ was not happy with missing protocols of external functions.
    Added print_manager_output(void) and phdiff_dismiss_workers(void)
    to the public declarations.
    
    Platforms tested:
    Tested in sleipnir (g++) and heping (serial, pp)
    
    Misc. update:

--- tools/lib/h5diff.h
+++ tools/lib/h5diff.h
@@ -81,6 +81,10 @@ hsize_t  h5diff(const char *fname1,
                 const char *objname1, 
                 const char *objname2, 
                 diff_opt_t *options);
+void print_manager_output(void);
+#ifdef H5_HAVE_PARALLEL
+void phdiff_dismiss_workers(void);
+#endif
 
 #ifdef __cplusplus
 }
