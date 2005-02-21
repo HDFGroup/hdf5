5098e7a0773ac44e4baaeb7638d1ce93d8c48822
James Laird <jlaird@hdfgroup.org>
2005-02-21 10:53:19 -0500

    [svn-r10056] Purpose: Update
    Description:
    Matthew reinstalled autotools into AFS with architecture directories.
    
    Solution:
    Updated bin/reconfigure.sh script to point to new locations of autotools.
    reconfigure.sh should run on "any" Linux box connected to AFS.
    
    Platforms tested:
    reconfigure.sh tested on heping and eirene.  No other files modified.

--- bin/reconfigure.sh
+++ bin/reconfigure.sh
@@ -14,17 +14,17 @@
 
 
   # Run commands in order
-  echo /afs/ncsa/projects/hdf/packages/automake_1.6.3/bin/aclocal -I /afs/ncsa/projects/hdf/packages/libtool-1.4.2/Linux_2.4/share/aclocal
-  /afs/ncsa/projects/hdf/packages/automake_1.6.3/bin/aclocal -I /afs/ncsa/projects/hdf/packages/libtool-1.4.2/Linux_2.4/share/aclocal
+  echo /afs/ncsa/projects/hdf/packages/automake_1.6.3/Linux_2.4/bin/aclocal -I /afs/ncsa/projects/hdf/packages/libtool-1.4.2/Linux_2.4/share/aclocal
+  /afs/ncsa/projects/hdf/packages/automake_1.6.3/Linux_2.4/bin/aclocal -I /afs/ncsa/projects/hdf/packages/libtool-1.4.2/Linux_2.4/share/aclocal
 
-  echo /afs/ncsa/projects/hdf/packages/autoconf_2.59/bin/autoheader
-  /afs/ncsa/projects/hdf/packages/autoconf_2.59/bin/autoheader
+  echo /afs/ncsa/projects/hdf/packages/autoconf_2.59/Linux_2.4/bin/autoheader
+  /afs/ncsa/projects/hdf/packages/autoconf_2.59/Linux_2.4/bin/autoheader
 
-  echo /afs/ncsa/projects/hdf/packages/automake_1.6.3/bin/automake --foreign
-  /afs/ncsa/projects/hdf/packages/automake_1.6.3/bin/automake --foreign
+  echo /afs/ncsa/projects/hdf/packages/automake_1.6.3/Linux_2.4/bin/automake --foreign
+  /afs/ncsa/projects/hdf/packages/automake_1.6.3/Linux_2.4/bin/automake --foreign
 
-  echo /afs/ncsa/projects/hdf/packages/autoconf_2.59/bin/autoconf
-  /afs/ncsa/projects/hdf/packages/autoconf_2.59/bin/autoconf
+  echo /afs/ncsa/projects/hdf/packages/autoconf_2.59/Linux_2.4/bin/autoconf
+  /afs/ncsa/projects/hdf/packages/autoconf_2.59/Linux_2.4/bin/autoconf
 
 
 exit 0
