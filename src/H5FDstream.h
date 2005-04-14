6797c041f1b4ba59e5f3b8102220148ed94b1141
Elena Pourmal <epourmal@hdfgroup.org>
2005-04-13 19:44:29 -0500

    [svn-r10602] Purpose: Small bug fix
    Description: When stream driver was not enabled, H5FD_STREAM variable                              was not define. Fortran compilation failed since H5FD_STREAM                          was needed to set up fortran global variables.
    
    Solution: Followed MPIO driver model and defined H5FD_STREAM to be -1                           when driver is not enabled.
    
    Platforms tested: heping with --enable(disable)-stream-vfd
    
    Misc. update:

--- src/H5FDstream.h
+++ src/H5FDstream.h
@@ -17,6 +17,12 @@
 #define H5FDstream_H
 
 #ifdef H5_HAVE_STREAM
+#   define H5FD_STREAM  (H5FD_stream_init())
+#else
+#   define H5FD_STREAM (-1)
+#endif /*H5_HAVE_STREAM */ 
+
+#ifdef H5_HAVE_STREAM
 
 /* check what sockets type we have (Unix or Windows sockets)
    Note that only MS compilers require to use Windows sockets
@@ -39,8 +45,6 @@
 #define H5FD_STREAM_INVALID_SOCKET         INVALID_SOCKET
 
 #endif
-
-#define H5FD_STREAM	(H5FD_stream_init())
 
 #ifdef __cplusplus
 extern "C" {
