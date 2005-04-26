8ab8a398171e4296489340601b14c4a4b66c53a4
MuQun Yang <ymuqun@hdfgroup.org>
2005-04-26 10:51:28 -0500

    [svn-r10664] Purpose: H5f90i_gen.h has to be copied to fortran/src for windows. Add this line in the batch file.
    Description:
    
    Solution:
    
    Platforms tested:
    MSVS 6.0 on windows XP
    
    
    Misc. update:

--- windows/copy_hdf.bat
+++ windows/copy_hdf.bat
@@ -1,5 +1,6 @@
 copy src\H5Tinit.c ..\src
 copy src\H5pubconf.h ..\src
+copy fortran\src\H5f90i_gen.h ..\fortran\src
 copy fortran\src\H5fortran_types.f90 ..\fortran\src
 xcopy /e/i/Y *.bat ..\
 copy examples\testExamples_exp_output.txt ..\examples
