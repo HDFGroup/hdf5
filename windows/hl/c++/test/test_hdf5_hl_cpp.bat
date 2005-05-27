9ff8c8c654fd4c22daafc4b28d98b5338acf10a5
Fang Guo <fangguo@ncsa.uiuc.edu>
2005-05-27 14:52:11 -0500

    [svn-r10809] Purpose: Add batch file to test high level c++ table test. Description: Batch file to test high level c++ table test. Solution:
    Platforms tested:
    WIndow XP
    Misc. update:

---
+++ windows/hl/c++/test/test_hdf5_hl_cpp.bat
@@ -0,0 +1,11 @@
+ECHO OFF
+
+REM This batch file will be used to test HDF5 High Level C++ Library.
+REM By Fang GUO
+REM Created on: 05/27/2005
+
+echo.
+echo ===============================================
+echo Testing hl_test_table_cpp -- %1
+echo ===============================================
+hl_test_table_cpp\%1\hl_test_table_cpp
