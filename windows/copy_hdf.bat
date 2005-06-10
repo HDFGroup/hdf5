46323232f3cc160cee2ff29ec8e3fbdc8d37c758
Fang Guo <fangguo@ncsa.uiuc.edu>
2005-06-10 14:15:01 -0500

    [svn-r10895] Purpose: Update batch file. Description: Add batch file for intel and visual c++ .Net Solution:
    Platforms tested:
    
    Misc. update:

--- windows/copy_hdf.bat
+++ windows/copy_hdf.bat
@@ -1,7 +1,27 @@
+@REM File Name   : copy_hdf.bat
+@REM Purpose     : Copy all Files in the following formats from Windows to 
+                   approapriate directory: .bat .c .f90 .h .txt .js 
+@REM             : 
+@REM Written By  : Muqun Yang
+@REM Last Update : June 7, 2005 by Fang GUO
+
 copy src\H5Tinit.c ..\src
 copy src\H5pubconf.h ..\src
 copy fortran\src\H5f90i_gen.h ..\fortran\src
 copy fortran\src\H5fortran_types.f90 ..\fortran\src
 xcopy /e/i/Y *.bat ..\
+copy batch_net\convert.js ..\batch_net\
 copy examples\testExamples_exp_output.txt ..\examples
+
+cd ../batch_net
+copy *.* ..\
+
+cd ../batch_intelc
+copy *.* ..\
+@echo off
+cd ..
+rmdir /s/q batch_net
+rmdir /s/q batch_intelc
+
+cd windows
 
