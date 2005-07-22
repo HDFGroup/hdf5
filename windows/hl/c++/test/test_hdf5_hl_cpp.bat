d32b0fa1de4e24b2b7718b5939a0f30a4566a68c
Fang Guo <fangguo@ncsa.uiuc.edu>
2005-07-22 17:07:05 -0500

    [svn-r11128] Purpose: Improvement Description: high leve c++ dll tests added Solution:
    Platforms tested:
    
    Misc. update:

--- windows/hl/c++/test/test_hdf5_hl_cpp.bat
+++ windows/hl/c++/test/test_hdf5_hl_cpp.bat
@@ -1,11 +1,11 @@
-ECHO OFF
+@ECHO OFF
 
-REM This batch file will be used to test HDF5 High Level C++ Library.
-REM By Fang GUO
-REM Created on: 05/27/2005
+@REM This batch file will be used to test HDF5 High Level C++ Library.
+@REM By Fang GUO
+@REM Created on: 05/27/2005
 
 echo.
 echo ===============================================
-echo Testing hl_test_table_cpp -- %1
+echo Testing hl_test_table_cpp%2 -- %1
 echo ===============================================
-hl_test_table_cpp\%1\hl_test_table_cpp
+hl_test_table_cpp%2\%1\hl_test_table_cpp%2
