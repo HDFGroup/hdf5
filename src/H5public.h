3ea9a3727aa67b4ce4ba6b940ee3aa0f19f372d5
Elena Pourmal <epourmal@hdfgroup.org>
2005-09-06 08:20:55 -0500

    [svn-r11351] Purpose: Maintenance
    Description: Changed version number to 1.6.5-snap0 in anticipation
                 of the new 1.6.5 release
    
    Solution: Ran bin/h5vers script
    
    Platforms tested: N/A
    
    Misc. update:

--- src/H5public.h
+++ src/H5public.h
@@ -74,10 +74,10 @@ extern "C" {
 /* Version numbers */
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	6	/* For minor interface/format changes  	     */
-#define H5_VERS_RELEASE	4	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "post10"	/* For pre-releases like snap0       */
+#define H5_VERS_RELEASE	5	/* For tweaks, bug-fixes, or development     */
+#define H5_VERS_SUBRELEASE "snap0"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.6.4-post10"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.6.5-snap0"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
