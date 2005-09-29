3d21af7eb88d188e9a271d408a8d1e0395472e4e
Fang Guo <fangguo@ncsa.uiuc.edu>
2005-09-29 12:33:09 -0500

    [svn-r11479] Purpose: Maintenance on Windows Description: No Alarm function on Windows Solution:
    Platforms tested:
    MSVS 6.0 on Windows XP
    Misc. update:

--- test/h5test.h
+++ test/h5test.h
@@ -111,13 +111,8 @@ extern MPI_Info h5_io_info_g;         /* MPI INFO object for IO */
  * Alarm definitions to wait up (terminate) a test that runs too long.
  */
 #define alarm_seconds	1200	/* default is 20 minutes */
-#ifndef WIN32
 #define ALARM_ON	HDalarm(alarm_seconds)
 #define ALARM_OFF	HDalarm(0)
-#else
-#define ALARM_ON	
-#define ALARM_OFF	
-#endif
 /* set alarms to N seconds if N > 0, else use default alarm_seconds. */
 #define ALARM_SET(N)	HDalarm((N)>0 ? N : alarm_seconds)
 
