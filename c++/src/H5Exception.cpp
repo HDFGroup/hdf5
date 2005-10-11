84a4d2888695034063261af2f5ac291c4c8a6638
Albert Cheng <acheng@hdfgroup.org>
2005-10-11 01:09:50 -0500

    [svn-r11530] Purpose: a slight typo in copyright notice.  fixed it.
    Platforms tested:
    No test since it was a trivial change in a comment block.

--- c++/src/H5Exception.cpp
+++ c++/src/H5Exception.cpp
@@ -1,7 +1,7 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Copyright by the Board of Trustees of the University of Illinois.         *
  * All rights reserved.                                                      *
- *		                                                              *
+ *                                                                           *
  * This file is part of HDF5.  The full HDF5 copyright notice, including     *
  * terms governing use, modification, and redistribution, is contained in    *
  * the files COPYING and Copyright.html.  COPYING can be found at the root   *
