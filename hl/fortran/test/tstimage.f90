5b4b3d574f5ab89dc7a6159dfcb10ce14c7b1935
Albert Cheng <acheng@hdfgroup.org>
2005-10-11 11:55:29 -0500

    [svn-r11537] Purpose: Fixed the copyright notice.
    Tested in heping.

--- hl/fortran/test/tstimage.f90
+++ hl/fortran/test/tstimage.f90
@@ -5,7 +5,7 @@
 !   This file is part of HDF5.  The full HDF5 copyright notice, including     *
 !   terms governing use, modification, and redistribution, is contained in    *
 !   the files COPYING and Copyright.html.  COPYING can be found at the root   *
-!   of the source errcode distribution tree; Copyright.html can be found at the  *
+!   of the source code distribution tree; Copyright.html can be found at the  *
 !   root level of an installed copy of the electronic HDF5 document set and   *
 !   is linked from the top-level documents page.  It can also be found at     *
 !   http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have     *
