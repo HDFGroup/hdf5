19f59e99983bcf8761e92c8f408f6fdadd45d7e9
Quincey Koziol <koziol@hdfgroup.org>
2005-10-22 10:35:58 -0500

    [svn-r11597] Purpose:     Update copyrights
    Description:
        Updated copyright information in hdf5/bin and top-level and added some
    more files to be skipped to the chkcopyright config file.
    
    Platforms tested:
        None - just eyeballed

--- hl/src/H5TA.h
+++ hl/src/H5TA.h
@@ -1,12 +1,3 @@
-
-/****************************************************************************
- * NCSA HDF                                                                 *
- * Scientific Data Technologies                                             *
- * National Center for Supercomputing Applications                          *
- * University of Illinois at Urbana-Champaign                               *
- * 605 E. Springfield, Champaign IL 61820                                   *
- *                                                                          *
- * For conditions of distribution and use, see the accompanying             *
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Copyright by the Board of Trustees of the University of Illinois.         *
  * All rights reserved.                                                      *
