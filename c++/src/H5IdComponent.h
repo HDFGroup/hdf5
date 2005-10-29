8679f8d3ef051f6c4fc9a2f8d5d0273f333c9442
Quincey Koziol <koziol@hdfgroup.org>
2005-10-29 11:11:34 -0500

    [svn-r11620] Purpose:     Update copyrights.
    Description:
    
    Solution:
        This should address almost all of the "simple" cases in the repository.
    There's still work to do, but it's going to require actually thinking about
    the files in question instead of just copying & pasting.

--- c++/src/H5IdComponent.h
+++ c++/src/H5IdComponent.h
@@ -1,8 +1,8 @@
 // C++ informative line for the emacs editor: -*- C++ -*-
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright by the Board of Trustees of the University of Illinois.	 *
- * All rights reserved.	                                              *
- *	                                                                   *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
  * This file is part of HDF5.  The full HDF5 copyright notice, including     *
  * terms governing use, modification, and redistribution, is contained in    *
  * the files COPYING and Copyright.html.  COPYING can be found at the root   *
