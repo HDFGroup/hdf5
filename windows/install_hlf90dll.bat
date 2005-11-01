f2685bb8b48a3654df01d279a7cec32b4ecf94c9
Fang Guo <fangguo@ncsa.uiuc.edu>
2005-11-01 13:46:25 -0500

    [svn-r11656] Purpose: Add in copyright information Description:
    Solution:
    
    Platforms tested:
    Windows XP
    Misc. update:

--- windows/install_hlf90dll.bat
+++ windows/install_hlf90dll.bat
@@ -1,3 +1,16 @@
+@REM Copyright by the Board of Trustees of the University of Illinois.
+@REM All rights reserved.
+@REM
+@REM This file is part of HDF5.  The full HDF5 copyright notice, including
+@REM terms governing use, modification, and redistribution, is contained in
+@REM the files COPYING and Copyright.html.  COPYING can be found at the root
+@REM of the source code distribution tree; Copyright.html can be found at the
+@REM root level of an installed copy of the electronic HDF5 document set and
+@REM is linked from the top-level documents page.  It can also be found at
+@REM http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have
+@REM access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu.
+
+
 @REM File name: install_hlf90dll.bat
 @REM This batch file is used to copy HDF5 fortran library high level DLLs into system folder.
 @REM By Fang GUO
