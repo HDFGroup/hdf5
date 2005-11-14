bea1e576c5ef5500678f7ce913d835341b625e8f
Raymond Lu <songyulu@hdfgroup.org>
2005-11-14 17:05:22 -0500

    [svn-r11711] Purpose: Bug fix for Windows
    Description: Windows doesn't have unistd.h file.
    
    Solution: Put #ifdef H5_HAVE_UNISTD_H for #include<unistd.h>.
    
    Platforms tested: fuss - simple change.

--- hl/src/lex.yy.c
+++ hl/src/lex.yy.c
@@ -23,8 +23,9 @@
 #define YY_FLEX_MINOR_VERSION 5
 
 #include <stdio.h>
+#ifdef H5_HAVE_UNISTD_H
 #include <unistd.h>
-
+#endif
 
 /* cfront 1.2 defines "c_plusplus" instead of "__cplusplus" */
 #ifdef c_plusplus
