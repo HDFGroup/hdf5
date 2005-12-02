0d008c805c62df3ef4aef960e00c2e2e606da7bf
Fang Guo <fangguo@ncsa.uiuc.edu>
2005-12-02 10:50:05 -0500

    [svn-r11752] Purpose: Maintenance on Windows Files updates due to the difference of copyright information Description:
    Solution:
    
    Platforms tested:
    
    Misc. update:

--- windows/src/H5Tinit.c
+++ windows/src/H5Tinit.c
@@ -1,18 +1,3 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright by the Board of Trustees of the University of Illinois.         *
- * All rights reserved.                                                      *
- *                                                                           *
- * This file is part of HDF5.  The full HDF5 copyright notice, including     *
- * terms governing use, modification, and redistribution, is contained in    *
- * the files COPYING and Copyright.html.  COPYING can be found at the root   *
- * of the source code distribution tree; Copyright.html can be found at the  *
- * root level of an installed copy of the electronic HDF5 document set and   *
- * is linked from the top-level documents page.  It can also be found at     *
- * http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have     *
- * access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-
 /* Generated automatically by H5detect -- do not edit */
 
 
@@ -31,7 +16,7 @@
  * access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
- * Created:		Sep 13, 2005
+ * Created:		Nov 29, 2005
  *			
  *
  * Purpose:		This machine-generated source code contains
