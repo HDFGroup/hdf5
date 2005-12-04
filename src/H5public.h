ac1fcb6d71765cef194b7204dc69c912aca37755
HDF Admin <hdfadmin@ncsa.uiuc.edu>
2005-12-04 18:28:22 -0500

    [svn-r11760] Snapshot version 1.6 release 5 (post1)

--- src/H5public.h
+++ src/H5public.h
@@ -75,9 +75,9 @@ extern "C" {
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	6	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	5	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "post1"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE "post2"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.6.5-post1"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.6.5-post2"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
