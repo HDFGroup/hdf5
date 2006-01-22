b09695738a95d8f1fb823894d1880f28dc16669c
HDF Admin <hdfadmin@ncsa.uiuc.edu>
2006-01-22 04:54:38 -0500

    [svn-r11884] Snapshot version 1.7 release 57

--- src/H5public.h
+++ src/H5public.h
@@ -66,10 +66,10 @@ extern "C" {
 /* Version numbers */
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	7	/* For minor interface/format changes  	     */
-#define H5_VERS_RELEASE	57	/* For tweaks, bug-fixes, or development     */
+#define H5_VERS_RELEASE	58	/* For tweaks, bug-fixes, or development     */
 #define H5_VERS_SUBRELEASE ""	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.7.57"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.7.58"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
