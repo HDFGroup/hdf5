2d175aeb4fdd9135eeee9191c7467c94c2a90e05
Pedro Vicente Nunes <pvn@hdfgroup.org>
2006-03-01 11:26:28 -0500

    [svn-r11995] Purpose: code improvement
    Description:
    add explaining  comments to the functions
    
    Solution:
    
    Platforms tested:
    none
    
    Misc. update:

--- hl/examples/ex_lite1.c
+++ hl/examples/ex_lite1.c
@@ -23,10 +23,13 @@ int main( void )
  int         data[6]={1,2,3,4,5,6};
  herr_t      status;
   
+ /* create a HDF5 file */
  file_id = H5Fcreate ("ex_lite1.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT); 
  
+ /* create and write an integer type dataset named "dset" */
  status = H5LTmake_dataset(file_id,"/dset",RANK,dims,H5T_NATIVE_INT,data);
-       
+ 
+ /* close file */
  status = H5Fclose (file_id);
 
  return 0;
