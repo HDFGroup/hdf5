5d36966aecd60dfdd79fd6b960aaf30bba9ec163
Fang Guo <fangguo@ncsa.uiuc.edu>
2006-03-09 15:31:04 -0500

    [svn-r12049] Purpose: Enable HL DLL on Windows Description: Put a macro before function in order to enable hl dll on windows Solution:
    Platforms tested:
    Windows XP, heping
    Misc. update:

--- hl/src/H5IM.h
+++ hl/src/H5IM.h
@@ -23,20 +23,20 @@ extern "C" {
 
 
 
-herr_t H5IMmake_image_8bit( hid_t loc_id,
+H5_HLDLL herr_t H5IMmake_image_8bit( hid_t loc_id,
                             const char *dset_name,
                             hsize_t width,
                             hsize_t height,
                             const unsigned char *buffer );
 
-herr_t H5IMmake_image_24bit( hid_t loc_id,
+H5_HLDLL herr_t H5IMmake_image_24bit( hid_t loc_id,
                              const char *dset_name,
                              hsize_t width,
                              hsize_t height,
                              const char *interlace,
                              const unsigned char *buffer );
 
-herr_t H5IMget_image_info( hid_t loc_id,
+H5_HLDLL herr_t H5IMget_image_info( hid_t loc_id,
                      const char *dset_name,
                      hsize_t *width,
                      hsize_t *height,
@@ -44,44 +44,44 @@ herr_t H5IMget_image_info( hid_t loc_id,
                      char    *interlace,
                      hssize_t *npals );
 
-herr_t H5IMread_image( hid_t loc_id,
+H5_HLDLL herr_t H5IMread_image( hid_t loc_id,
                        const char *dset_name,
                        unsigned char *buffer );
 
 
 
-herr_t H5IMmake_palette( hid_t loc_id,
+H5_HLDLL herr_t H5IMmake_palette( hid_t loc_id,
                          const char *pal_name,
                          const hsize_t *pal_dims,
                          const unsigned char *pal_data );
 
-herr_t H5IMlink_palette( hid_t loc_id,
+H5_HLDLL herr_t H5IMlink_palette( hid_t loc_id,
                         const char *image_name,
                         const char *pal_name );
 
-herr_t H5IMunlink_palette( hid_t loc_id,
+H5_HLDLL herr_t H5IMunlink_palette( hid_t loc_id,
                            const char *image_name,
                            const char *pal_name );
 
-herr_t H5IMget_npalettes( hid_t loc_id,
+H5_HLDLL herr_t H5IMget_npalettes( hid_t loc_id,
                           const char *image_name,
                           hssize_t *npals );
 
-herr_t H5IMget_palette_info( hid_t loc_id,
+H5_HLDLL herr_t H5IMget_palette_info( hid_t loc_id,
                         const char *image_name,
                         int pal_number,
                         hsize_t *pal_dims );
 
 
-herr_t H5IMget_palette( hid_t loc_id,
+H5_HLDLL herr_t H5IMget_palette( hid_t loc_id,
                         const char *image_name,
                         int pal_number,
                         unsigned char *pal_data );
 
-herr_t H5IMis_image( hid_t loc_id,
+H5_HLDLL herr_t H5IMis_image( hid_t loc_id,
                      const char *dset_name );
 
-herr_t H5IMis_palette( hid_t loc_id,
+H5_HLDLL herr_t H5IMis_palette( hid_t loc_id,
                      const char *dset_name );
 
 #ifdef __cplusplus
