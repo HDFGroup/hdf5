5135181d31d78595e074d4774141c40442ff8508
Quincey Koziol <koziol@hdfgroup.org>
2006-03-22 13:50:19 -0500

    [svn-r12132] Purpose:     Code cleanup
    Description:
        Add H5E header to this header, to make certain that the errors in the
    "inline" routines are handled correctly.
    
    Platforms tested:
        FreeBSD 4.11 (sleipnir)
        Linux 2.4 (chicago)
        Mac OSX (amazon)

--- src/H5Vprivate.h
+++ src/H5Vprivate.h
@@ -19,7 +19,9 @@
 #ifndef H5Vprivate_H
 #define H5Vprivate_H
 
-#include "H5private.h"
+/* Private headers needed by this file */
+#include "H5private.h"		/* Generic Functions			*/
+#include "H5Eprivate.h"		/* Error handling		  	*/
 
 /* Vector comparison functions like Fortran66 comparison operators */
 #define H5V_vector_eq_s(N,V1,V2) (H5V_vector_cmp_s (N, V1, V2)==0)
