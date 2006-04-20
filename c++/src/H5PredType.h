22cfdb228029b6a2e99751aa32066b773c14e3c9
Binh-Minh Ribler <bmribler@hdfgroup.org>
2006-04-20 11:57:29 -0500

    [svn-r12291] Purpose: Maintenance
    Description:
        Changed syntax of #if statements for LEAST to be the same as in
        the development branch for easy maintenance.
    
    Platforms tested:
        Linux 2.4 (heping)
        AIX 5.1 (copper)
        SunOS 5.8 64-bit (sol)

--- c++/src/H5PredType.h
+++ c++/src/H5PredType.h
@@ -152,64 +152,68 @@ class H5_DLLCPP PredType : public AtomType {
 
 	static const PredType NATIVE_INT8;
 	static const PredType NATIVE_UINT8;
-
-#ifdef SIZEOF_INT_LEAST8_T
-	static const PredType NATIVE_INT_LEAST8;
-#endif
-#ifdef SIZEOF_UINT_LEAST8_T
-	static const PredType NATIVE_UINT_LEAST8;
-#endif
-#ifdef SIZEOF_INT_FAST64_T
-	static const PredType NATIVE_INT_FAST8;
-#endif
-#ifdef SIZEOF_UINT_FAST64_T
-	static const PredType NATIVE_UINT_FAST8;
-#endif
-
 	static const PredType NATIVE_INT16;
 	static const PredType NATIVE_UINT16;
-#ifdef SIZEOF_INT_LEAST16_T
-	static const PredType NATIVE_INT_LEAST16;
-#endif
-#ifdef SIZEOF_UINT_LEAST16_T
-	static const PredType NATIVE_UINT_LEAST16;
-#endif
-#ifdef SIZEOF_INT_FAST16_T
-	static const PredType NATIVE_INT_FAST16;
-#endif
-#ifdef SIZEOF_UINT_FAST16_T
-	static const PredType NATIVE_UINT_FAST16;
-#endif
-
 	static const PredType NATIVE_INT32;
 	static const PredType NATIVE_UINT32;
-#ifdef SIZEOF_INT_LEAST32_T
-	static const PredType NATIVE_INT_LEAST32;
-#endif
-#ifdef SIZEOF_UINT_LEAST32_T
-	static const PredType NATIVE_UINT_LEAST32;
-#endif
-#ifdef SIZEOF_INT_FAST32_T
-	static const PredType NATIVE_INT_FAST32;
-#endif
-#ifdef SIZEOF_UINT_FAST32_T
-	static const PredType NATIVE_UINT_FAST32;
-#endif
-
 	static const PredType NATIVE_INT64;
 	static const PredType NATIVE_UINT64;
-#ifdef SIZEOF_INT_LEAST64_T
+
+#if H5_SIZEOF_INT_LEAST8_T != 0
+	static const PredType NATIVE_INT_LEAST8;
+#endif /* H5_SIZEOF_INT_LEAST8_T */
+#if H5_SIZEOF_UINT_LEAST8_T != 0
+	static const PredType NATIVE_UINT_LEAST8;
+#endif /* H5_SIZEOF_UINT_LEAST8_T */
+
+#if H5_SIZEOF_INT_LEAST16_T != 0
+	static const PredType NATIVE_INT_LEAST16;
+#endif /* H5_SIZEOF_INT_LEAST16_T */
+#if H5_SIZEOF_UINT_LEAST16_T != 0
+	static const PredType NATIVE_UINT_LEAST16;
+#endif /* H5_SIZEOF_UINT_LEAST16_T */
+
+#if H5_SIZEOF_INT_LEAST32_T != 0
+	static const PredType NATIVE_INT_LEAST32;
+#endif /* H5_SIZEOF_INT_LEAST32_T */
+#if H5_SIZEOF_UINT_LEAST32_T != 0
+	static const PredType NATIVE_UINT_LEAST32;
+#endif /* H5_SIZEOF_UINT_LEAST32_T */
+
+#if H5_SIZEOF_INT_LEAST64_T != 0
 	static const PredType NATIVE_INT_LEAST64;
-#endif
-#ifdef SIZEOF_UINT_LEAST64_T
+#endif /* H5_SIZEOF_INT_LEAST64_T */
+#if H5_SIZEOF_UINT_LEAST64_T != 0
 	static const PredType NATIVE_UINT_LEAST64;
-#endif
-#ifdef SIZEOF_INT_FAST64_T
+#endif /* H5_SIZEOF_UINT_LEAST64_T */
+
+#if H5_SIZEOF_INT_FAST8_T != 0
+	static const PredType NATIVE_INT_FAST8;
+#endif /* H5_SIZEOF_INT_FAST8_T */
+#if H5_SIZEOF_UINT_FAST8_T != 0
+	static const PredType NATIVE_UINT_FAST8;
+#endif /* H5_SIZEOF_UINT_FAST8_T */
+
+#if H5_SIZEOF_INT_FAST16_T != 0
+	static const PredType NATIVE_INT_FAST16;
+#endif /* H5_SIZEOF_INT_FAST16_T */
+#if H5_SIZEOF_UINT_FAST16_T != 0
+	static const PredType NATIVE_UINT_FAST16;
+#endif /* H5_SIZEOF_UINT_FAST16_T */
+
+#if H5_SIZEOF_INT_FAST32_T != 0
+	static const PredType NATIVE_INT_FAST32;
+#endif /* H5_SIZEOF_INT_FAST32_T */
+#if H5_SIZEOF_UINT_FAST32_T != 0
+	static const PredType NATIVE_UINT_FAST32;
+#endif /* H5_SIZEOF_UINT_FAST32_T */
+
+#if H5_SIZEOF_INT_FAST64_T != 0
 	static const PredType NATIVE_INT_FAST64;
-#endif
-#ifdef SIZEOF_UINT_FAST64_T
+#endif /* H5_SIZEOF_INT_FAST64_T */
+#if H5_SIZEOF_UINT_FAST64_T != 0
 	static const PredType NATIVE_UINT_FAST64;
-#endif
+#endif /* H5_SIZEOF_UINT_FAST64_T */
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 	// These dummy functions do not inherit from DataType - they'll
