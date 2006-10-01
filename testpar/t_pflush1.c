c7a381b324da6cbbc62a841f1fecd320920532e2
Leon Arber <larber@ncsa.uiuc.edu>
2006-10-01 16:27:47 -0500

    [svn-r12697] Purpose:  New Feature
    Description:
        Preliminary test of H5Fflush to verify that it still works when using MPIO collective mode.
    
    Platforms:
        Linux (heping)

---
+++ testpar/t_pflush1.c
@@ -0,0 +1,174 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have     *
+ * access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/*
+ * Programmer:  Leon Arber  <larber@uiuc.edu>
+ *              Sept. 28, 2006.
+ *
+ * Purpose:	This is the first half of a two-part test that makes sure
+ *		that a file can be read after a parallel application crashes as long
+ *		as the file was flushed first.  We simulate a crash by
+ *		calling _exit(0) since this doesn't flush HDF5 caches but
+ *		still exits with success.
+ */
+#include <mpi.h>
+#include "h5test.h"
+
+const char *FILENAME[] = {
+    "flush",
+    "noflush",
+    NULL
+};
+
+static double	the_data[100][100];
+
+/*-------------------------------------------------------------------------
+ * Function:	create_file
+ *
+ * Purpose:	Creates file used in part 1 of the test
+ *
+ * Return:	Success:	0
+ *
+ *		Failure:	1
+ *
+ * Programmer:	Leon Arber
+ *              Sept. 26, 2006
+ *
+ * Modifications:
+ *
+ *-------------------------------------------------------------------------
+ */
+hid_t create_file(char* name, hid_t fapl)
+{ 
+    hid_t	file, dcpl, space, dset, groups, grp, plist;
+    hsize_t	ds_size[2] = {100, 100};
+    hsize_t	ch_size[2] = {5, 5};
+    hsize_t	i, j;
+ 
+
+
+    if ((file=H5Fcreate(name, H5F_ACC_TRUNC, H5P_DEFAULT, fapl))<0) goto error;
+
+    /* Create a chunked dataset */
+    if ((dcpl=H5Pcreate(H5P_DATASET_CREATE))<0) goto error;
+    if (H5Pset_chunk(dcpl, 2, ch_size)<0) goto error;
+    if ((space=H5Screate_simple(2, ds_size, NULL))<0) goto error;
+    if ((dset=H5Dcreate(file, "dset", H5T_NATIVE_FLOAT, space, H5P_DEFAULT))<0)
+	goto error;
+
+    plist = H5Pcreate(H5P_DATASET_XFER);
+    H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
+    
+    
+    /* Write some data */
+    for (i=0; i<ds_size[0]; i++) {
+	/*
+	 * The extra cast in the following statement is a bug workaround
+	 * for the Win32 version 5.0 compiler.
+	 * 1998-11-06 ptl
+	 */
+	for (j=0; j<ds_size[1]; j++) {
+	    the_data[i][j] = (double)(hssize_t)i/(hssize_t)(j+1);
+	}
+    }
+    if (H5Dwrite(dset, H5T_NATIVE_DOUBLE, space, space, plist,
+		the_data)<0) goto error;
+
+    /* Create some groups */
+    if ((groups=H5Gcreate(file, "some_groups", 0))<0) goto error;
+    for (i=0; i<100; i++) {
+	sprintf(name, "grp%02u", (unsigned)i);
+	if ((grp=H5Gcreate(groups, name, 0))<0) goto error;
+	if (H5Gclose(grp)<0) goto error;
+    }
+
+
+    return file;
+
+error:
+        HD_exit(1);
+
+}
+
+/*-------------------------------------------------------------------------
+ * Function:	main
+ *
+ * Purpose:	Part 1 of a two-part H5Fflush() test.
+ *
+ * Return:	Success:	0
+ *
+ *		Failure:	1
+ *
+ * Programmer:	Robb Matzke
+ *              Friday, October 23, 1998
+ *
+ * Modifications:
+ * 		Leon Arber
+ * 		Sept. 26, 2006, expand test to check for failure if H5Fflush is not called.
+ * 		
+ *
+ *-------------------------------------------------------------------------
+ */
+int
+main(int argc, char* argv[])
+{
+    hid_t file, fapl;
+    char	name[1024];
+    const char  *envval = NULL;
+    int mpi_size, mpi_rank;
+    MPI_Comm comm  = MPI_COMM_WORLD;
+    MPI_Info info  = MPI_INFO_NULL;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(comm, &mpi_size);
+    MPI_Comm_rank(comm, &mpi_rank);  
+
+    fapl = H5Pcreate(H5P_FILE_ACCESS);
+    H5Pset_fapl_mpio(fapl, comm, info);
+
+    if(mpi_rank == 0) 
+	TESTING("H5Fflush (part1)");
+    envval = HDgetenv("HDF5_DRIVER");
+    if (envval == NULL) 
+        envval = "nomatch";
+    if (HDstrcmp(envval, "split")) {
+	/* Create the file */
+	h5_fixname(FILENAME[0], fapl, name, sizeof name);
+	file = create_file(name, fapl);
+	/* Flush and exit without closing the library */
+	if (H5Fflush(file, H5F_SCOPE_GLOBAL)<0) goto error;
+
+	/* Create the other file which will not be flushed */
+	h5_fixname(FILENAME[1], fapl, name, sizeof name);
+	file = create_file(name, fapl);
+
+
+	if(mpi_rank == 0) 
+	    PASSED();
+	fflush(stdout);
+	fflush(stderr);
+    }
+    else
+    {
+        SKIPPED();
+        puts("    Test not compatible with current Virtual File Driver");
+    }
+
+    HD_exit(0);
+
+    error:
+        HD_exit(1);
+        return 1;
+
+}
