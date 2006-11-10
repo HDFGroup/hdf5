911e79b273bddac85c698b77718516b8a8ee9ab2
James Laird <jlaird@hdfgroup.org>
2006-11-10 14:56:04 -0500

    [svn-r12887] A fix for linking issues.
    Should disable linking against shared libraries in Fortran for compilers that
    don't support shared libraries.
    
    Should also fix problem when the wrong Fortran file extension was specified.
    
    If these changes don't solve the Daily Test issues, I'll look at backing out
    the autotool version change until I have time to fix them.
    
    Tested on heping, kagiso, juniper.

--- src/H5Edefin.h
+++ src/H5Edefin.h
@@ -181,13 +181,6 @@ hid_t H5E_CANTNEXT_g       = FAIL;      /* Can't move to next iterator location 
 hid_t H5E_BADSELECT_g      = FAIL;      /* Invalid selection */
 hid_t H5E_CANTCOMPARE_g    = FAIL;      /* Can't compare objects */
 
-/* Argument errors */
-hid_t H5E_UNINITIALIZED_g  = FAIL;      /* Information is uinitialized */
-hid_t H5E_UNSUPPORTED_g    = FAIL;      /* Feature is unsupported */
-hid_t H5E_BADTYPE_g        = FAIL;      /* Inappropriate type */
-hid_t H5E_BADRANGE_g       = FAIL;      /* Out of range */
-hid_t H5E_BADVALUE_g       = FAIL;      /* Bad value */
-
 /* B-tree related errors */
 hid_t H5E_NOTFOUND_g       = FAIL;      /* Object not found */
 hid_t H5E_EXISTS_g         = FAIL;      /* Object already exists */
@@ -200,6 +193,13 @@ hid_t H5E_CANTINSERT_g     = FAIL;      /* Unable to insert object */
 hid_t H5E_CANTLIST_g       = FAIL;      /* Unable to list node */
 hid_t H5E_CANTMODIFY_g     = FAIL;      /* Unable to modify record */
 hid_t H5E_CANTREMOVE_g     = FAIL;      /* Unable to remove object */
+
+/* Argument errors */
+hid_t H5E_UNINITIALIZED_g  = FAIL;      /* Information is uinitialized */
+hid_t H5E_UNSUPPORTED_g    = FAIL;      /* Feature is unsupported */
+hid_t H5E_BADTYPE_g        = FAIL;      /* Inappropriate type */
+hid_t H5E_BADRANGE_g       = FAIL;      /* Out of range */
+hid_t H5E_BADVALUE_g       = FAIL;      /* Bad value */
 
 /* Datatype conversion errors */
 hid_t H5E_CANTCONVERT_g    = FAIL;      /* Can't convert datatypes */
