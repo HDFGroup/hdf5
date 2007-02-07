42f1acfcd733c1a9b152aad71cdf30e5e481f345
Albert Cheng <acheng@hdfgroup.org>
2007-02-07 12:55:01 -0500

    [svn-r13256] Updated fortran source code with new THG copyright.
    Tested: kagiso.

--- fortran/test/tH5A.f90
+++ fortran/test/tH5A.f90
@@ -1,4 +1,5 @@
 ! * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+!   Copyright by The HDF Group.                                               *
 !   Copyright by the Board of Trustees of the University of Illinois.         *
 !   All rights reserved.                                                      *
 !                                                                             *
@@ -8,8 +9,8 @@
 !   of the source code distribution tree; Copyright.html can be found at the  *
 !   root level of an installed copy of the electronic HDF5 document set and   *
 !   is linked from the top-level documents page.  It can also be found at     *
-!   http://hdf.ncsa.uiuc.edu/HDF5/doc/Copyright.html.  If you do not have     *
-!   access to either file, you may request a copy from hdfhelp@ncsa.uiuc.edu. *
+!   http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+!   access to either file, you may request a copy from help@hdfgroup.org.     *
 ! * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 !
     SUBROUTINE attribute_test(cleanup, total_error)
