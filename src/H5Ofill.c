8e7d4af1e18511547f223c0991d6eeb6f5eb93e6
Raymond Lu <songyulu@hdfgroup.org>
2007-03-05 16:51:08 -0500

    [svn-r13461] Fix for Bug #763 - failure to create dataset of variable-length data with fill value.  In function H5O_fill_convert, the background buffer was allocated without clearing.  Later in H5T_vlen_disk_write, library thought there's data in this background buffer.  Changed the allocating function to CALLOC.

--- src/H5Ofill.c
+++ src/H5Ofill.c
@@ -852,7 +852,10 @@ H5O_fill_convert(void *_fill, H5T_t *dset_type, hid_t dxpl_id)
                 HGOTO_ERROR(H5E_RESOURCE, H5E_NOSPACE, FAIL, "memory allocation failed for type conversion");
             HDmemcpy(buf, fill->buf, H5T_get_size(fill->type));
         }
-        if (H5T_path_bkg(tpath) && NULL==(bkg=H5MM_malloc(H5T_get_size(dset_type))))
+
+        /* Use CALLOC here to clear the buffer in case later the library thinks there's
+         * data in the background. */
+        if (H5T_path_bkg(tpath) && NULL==(bkg=H5MM_calloc(H5T_get_size(dset_type))))
             HGOTO_ERROR(H5E_RESOURCE, H5E_NOSPACE, FAIL, "memory allocation failed for type conversion");
 
         /* Do the conversion */
