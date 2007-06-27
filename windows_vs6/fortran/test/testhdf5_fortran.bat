6f175a8ff2545f8b6ebc7050e57e141355c6a5ea
Scott Wegner <swegner@hdfgroup.org>
2007-06-27 14:13:45 -0500

    [svn-r13922] Purpose: First step in VS6 Depreciation
    Desciption:
    This is the first major check-in in the process of depreciating Visual Studio 6.0.  We create a new directory, windows_vs6, which now holds all of our previous VS6 project files.  We expect to maintain this directory for only a short time (perhaps one release), so users have time to make the change.
    
    The ./windows directory now contains project files for Visual Studio .Net, as well as existing source and test scripts.  This will allow us to greatly simplify the build process for the majority of our users.
    
    We have created a new solution (.Net equivolent of workspace) that houses the Fortran project files.  This allowed us to remove these files from the main solution, as well as streamline the conversion in the Fortran solution.  It is also much easier now to convert to Visual Studio 2005 files.
    
    Notes:
    I didn't update the MANIFEST with this check-in, I will make those changes shortly.  
    Also, I haven't yet updated the documentation to reflect this check-in.  That is going to require more work.
    We will be giving a seminar in the coming weeks about .Net and how to migrate over.
    
    Tested:
    VS6/VNET/VS2005 on WinXP 32/64-bit

---
+++ windows_vs6/fortran/test/testhdf5_fortran.bat
@@ -0,0 +1,17 @@
+@REM Copyright by The HDF Group.
+@REM Copyright by the Board of Trustees of the University of Illinois.
+@REM All rights reserved.
+@REM
+@REM This file is part of HDF5.  The full HDF5 copyright notice, including
+@REM terms governing use, modification, and redistribution, is contained in
+@REM the files COPYING and Copyright.html.  COPYING can be found at the root
+@REM of the source code distribution tree; Copyright.html can be found at the
+@REM root level of an installed copy of the electronic HDF5 document set and
+@REM is linked from the top-level documents page.  It can also be found at
+@REM http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have
+@REM access to either file, you may request a copy from help@hdfgroup.org.
+
+testhdf5_fortran%2\%1\testhdf5_fortran%2
+flush1_fortran%2\%1\flush1_fortran%2
+copy fflush1.h5 flush2_fortran\%1\fflush1.h5
+flush2_fortran%2\%1\flush2_fortran%2
