fe6c92ebd3e677f3ccd94d487bf5aeadc0c0c3fa
Scott Wegner <swegner@hdfgroup.org>
2007-07-10 13:32:20 -0500

    [svn-r13960] Purpose: Remove obsolete Windows files
    Description:
    Previously, there was a very intricate process for converting VS6 project files to VS.NET format for testing on the command-line.  Now that we maintain VS.NET project files, this system is no longer neccessary.  The same builds are now carried out in hdf5build.BAT, so the previous files are obsolete.
    
    Removing them, and updating the MANIFEST
    
    Tested:
    WinXP

--- windows/copy_hdf.bat
+++ windows/copy_hdf.bat
@@ -23,18 +23,4 @@ copy src\H5pubconf.h ..\src
 copy fortran\src\H5f90i_gen.h ..\fortran\src
 copy fortran\src\H5fortran_types.f90 ..\fortran\src
 xcopy /e/i/Y *.bat ..\
-copy batch_net\convert.js ..\batch_net\
 copy examples\testExamples_exp_output.txt ..\examples
-
-cd ../batch_net
-copy *.* ..\
-
-cd ../batch_intelc
-copy *.* ..\
-@echo off
-cd ..
-rmdir /s/q batch_net
-rmdir /s/q batch_intelc
-
-cd windows
-
