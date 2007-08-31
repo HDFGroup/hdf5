e4f358746faa2b18f47668781567e5ea55da034b
Quincey Koziol <koziol@hdfgroup.org>
2007-08-31 14:53:03 -0500

    [svn-r14133] Description: 	Fix naming change for FORTRAN symbol: H5G_LINK_ERROR -> H5L_TYPE_ERROR
    Tested on:
    	Solaris/32 5.10 (linew)

--- fortran/src/H5_f.c
+++ fortran/src/H5_f.c
@@ -303,7 +303,7 @@ nh5init_flags_c( int_f *h5d_flags, int_f *h5f_flags,
       h5g_flags[2] = H5G_DATASET;
       h5g_flags[3] = H5G_TYPE;
       h5g_flags[4] = H5G_LINK;
-      h5g_flags[5] = H5G_LINK_ERROR;
+      h5g_flags[5] = H5L_TYPE_ERROR;
       h5g_flags[6] = H5L_TYPE_HARD;
       h5g_flags[7] = H5L_TYPE_SOFT;
 
