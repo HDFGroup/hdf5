349702f97d852c5bbde3f3cc5b9c3bdc2d5b15da
Elena Pourmal <epourmal@hdfgroup.org>
2008-02-11 14:09:52 -0500

    [svn-r14547] Maintenance: Changed version number to 1.9.0 as we agreed on the HDF5 developers meeting              on February 8, 2008; modified docs files to point to the documentation snapshots.

--- src/H5public.h
+++ src/H5public.h
@@ -72,9 +72,9 @@ extern "C" {
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	9	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	0	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "snap0"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE ""	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.9.0-snap0"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.9.0"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
