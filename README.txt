b6017c5cd19314da0ee711bcd68848adb542f344
HDF Tester <hdftest@hdfgroup.org>
2008-04-20 19:00:23 -0500

    [svn-r14851] Snapshot version 1.8 release 0 (snap3)

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-HDF5 version 1.8.0-snap3 currently under development
+HDF5 version 1.8.0-snap4 currently under development
 Please refer to the release_docs/INSTALL file for installation instructions.
 ------------------------------------------------------------------------------
 
--- src/H5public.h
+++ src/H5public.h
@@ -72,9 +72,9 @@ extern "C" {
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	8	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	0	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "snap3"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE "snap4"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.8.0-snap3"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.8.0-snap4"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
