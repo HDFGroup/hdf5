1ab76307800c1a1399e2ddccea2b8f698a0543ec
Scot Breitenfeld <brtnfld@hdfgroup.org>
2008-08-26 15:30:38 -0500

    [svn-r15541] Description:
    Put the  DOUBLE PRECISION interfaces in a separate module. The interfaces are included/excluded depending on
    the state of FORTRAN_DEFAULT_REALisDBLE_F which detects if the default REAL is DOUBLE PRECISION. This allows the library to be compiled with -r8 Fortran flag without the user needing to change the source code.

--- fortran/src/H5Pff.f90
+++ fortran/src/H5Pff.f90
@@ -23,54 +23,36 @@
        INTERFACE h5pset_fill_value_f
          MODULE PROCEDURE h5pset_fill_value_integer
          MODULE PROCEDURE h5pset_fill_value_real
-! Comment if on Crays
-         MODULE PROCEDURE h5pset_fill_value_double
-! End comment if on Crays
          MODULE PROCEDURE h5pset_fill_value_char
        END INTERFACE
      
        INTERFACE h5pget_fill_value_f
          MODULE PROCEDURE h5pget_fill_value_integer
          MODULE PROCEDURE h5pget_fill_value_real
-! Comment if on Crays
-         MODULE PROCEDURE h5pget_fill_value_double
-! End comment if on Crays
          MODULE PROCEDURE h5pget_fill_value_char
        END INTERFACE
 
        INTERFACE h5pset_f
          MODULE PROCEDURE h5pset_integer
          MODULE PROCEDURE h5pset_real
-! Comment if on Crays
-         MODULE PROCEDURE h5pset_double
-! End comment if on Crays
          MODULE PROCEDURE h5pset_char
        END INTERFACE
      
        INTERFACE h5pget_f
          MODULE PROCEDURE h5pget_integer
          MODULE PROCEDURE h5pget_real
-! Comment if on Crays
-         MODULE PROCEDURE h5pget_double
-! End comment if on Crays
          MODULE PROCEDURE h5pget_char
        END INTERFACE
 
        INTERFACE h5pregister_f
          MODULE PROCEDURE h5pregister_integer
          MODULE PROCEDURE h5pregister_real
-! Comment if on Crays
-         MODULE PROCEDURE h5pregister_double
-! End comment if on Crays
          MODULE PROCEDURE h5pregister_char
        END INTERFACE
      
        INTERFACE h5pinsert_f
          MODULE PROCEDURE h5pinsert_integer
          MODULE PROCEDURE h5pinsert_real
-! Comment if on Crays
-         MODULE PROCEDURE h5pinsert_double
-! End comment if on Crays
          MODULE PROCEDURE h5pinsert_char
        END INTERFACE
        
@@ -805,74 +787,6 @@
           END SUBROUTINE h5pget_fill_value_real
 
 
-          SUBROUTINE h5pset_fill_value_double(prp_id, type_id, fillvalue, &
-                                               hdferr) 
-!
-!This definition is needed for Windows DLLs
-!DEC$if defined(BUILD_HDF5_DLL)
-!DEC$attributes dllexport :: h5pset_fill_value_double
-!DEC$endif
-!
-            IMPLICIT NONE
-            INTEGER(HID_T), INTENT(IN) :: prp_id ! Property list identifier 
-            INTEGER(HID_T), INTENT(IN) :: type_id ! Datatype identifier of 
-                                                  ! of fillvalue datatype 
-                                                  ! (in memory)
-            DOUBLE PRECISION, INTENT(IN) :: fillvalue   ! Fillvalue
-            INTEGER, INTENT(OUT) :: hdferr  ! Error code
-
-!            INTEGER, EXTERNAL :: h5pset_fill_value_double_c
-!  MS FORTRAN needs explicit interface for C functions called here.
-!
-            INTERFACE
-              INTEGER FUNCTION h5pset_fill_value_double_c(prp_id, type_id, fillvalue)
-              USE H5GLOBAL
-              !DEC$ IF DEFINED(HDF5F90_WINDOWS)
-              !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5PSET_FILL_VALUE_DOUBLE_C'::h5pset_fill_value_double_c
-              !DEC$ ENDIF
-              INTEGER(HID_T), INTENT(IN) :: prp_id
-              INTEGER(HID_T), INTENT(IN) :: type_id
-              DOUBLE PRECISION, INTENT(IN) :: fillvalue
-              END FUNCTION h5pset_fill_value_double_c
-            END INTERFACE
-
-            hdferr = h5pset_fill_value_double_c(prp_id, type_id, fillvalue)
-          END SUBROUTINE h5pset_fill_value_double
-
-
-          SUBROUTINE h5pget_fill_value_double(prp_id, type_id, fillvalue, &
-                                               hdferr) 
-!
-!This definition is needed for Windows DLLs
-!DEC$if defined(BUILD_HDF5_DLL)
-!DEC$attributes dllexport :: h5pget_fill_value_double
-!DEC$endif
-!
-            IMPLICIT NONE
-            INTEGER(HID_T), INTENT(IN) :: prp_id ! Property list identifier 
-            INTEGER(HID_T), INTENT(IN) :: type_id ! Datatype identifier of 
-                                                  ! of fillvalue datatype
-                                                  ! (in memory) 
-            DOUBLE PRECISION, INTENT(IN) :: fillvalue   ! Fillvalue
-            INTEGER, INTENT(OUT) :: hdferr  ! Error code
-
-!            INTEGER, EXTERNAL :: h5pget_fill_value_double_c
-!  MS FORTRAN needs explicit interface for C functions called here.
-!
-            INTERFACE
-              INTEGER FUNCTION h5pget_fill_value_double_c(prp_id, type_id, fillvalue)
-              USE H5GLOBAL
-              !DEC$ IF DEFINED(HDF5F90_WINDOWS)
-              !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5PGET_FILL_VALUE_DOUBLE_C'::h5pget_fill_value_double_c
-              !DEC$ ENDIF
-              INTEGER(HID_T), INTENT(IN) :: prp_id
-              INTEGER(HID_T), INTENT(IN) :: type_id
-              DOUBLE PRECISION :: fillvalue
-              END FUNCTION h5pget_fill_value_double_c
-            END INTERFACE
-
-            hdferr = h5pget_fill_value_double_c(prp_id, type_id, fillvalue)
-          END SUBROUTINE h5pget_fill_value_double
 
           SUBROUTINE h5pset_fill_value_char(prp_id, type_id, fillvalue, &
                                                hdferr) 
@@ -4155,61 +4069,6 @@
         hdferr = h5pset_real_c(prp_id, name , name_len, value)
           END SUBROUTINE h5pset_real
 
-!----------------------------------------------------------------------
-! Name:		h5pset_double
-!
-! Purpose: 	Sets a property list value
-!
-! Inputs:  
-!		prp_id		- iproperty list identifier to modify
-!		name 		- name of property to modify
-!		value		- value to set property to
-! Outputs:  
-!		hdferr:		- error code		
-!				 	Success:  0
-!				 	Failure: -1   
-! Optional parameters:
-!				NONE
-!
-! Programmer:	Elena Pourmal
-!	        October 9, 2002	
-!
-! Modifications: 	
-!
-! Comment:		
-!----------------------------------------------------------------------
-
-          SUBROUTINE h5pset_double(prp_id, name, value, hdferr) 
-!
-!This definition is needed for Windows DLLs
-!DEC$if defined(BUILD_HDF5_DLL)
-!DEC$attributes dllexport :: h5pset_double
-!DEC$endif
-!
-            IMPLICIT NONE
-            INTEGER(HID_T), INTENT(IN) :: prp_id  ! Property list identifier 
-            CHARACTER(LEN=*), INTENT(IN) :: name  ! Name of property to modify
-            DOUBLE PRECISION,   INTENT(IN) :: value ! Property value
-            INTEGER, INTENT(OUT) :: hdferr  ! Error code
-            INTEGER :: name_len
-
-            INTERFACE
-              INTEGER FUNCTION h5pset_double_c(prp_id, name, name_len, value)
-              USE H5GLOBAL
-              !DEC$ IF DEFINED(HDF5F90_WINDOWS)
-              !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5PSET_DOUBLE_C'::h5pset_double_c
-              !DEC$ ENDIF
-              !DEC$ATTRIBUTES reference :: name
-              INTEGER(HID_T), INTENT(IN) :: prp_id
-              CHARACTER(LEN=*), INTENT(IN) :: name
-              INTEGER, INTENT(IN)         :: name_len
-              DOUBLE PRECISION, INTENT(IN) :: value
-              END FUNCTION h5pset_double_c
-            END INTERFACE
-
-            name_len = LEN(name)
-        hdferr = h5pset_double_c(prp_id, name , name_len, value)
-          END SUBROUTINE h5pset_double
 
 !----------------------------------------------------------------------
 ! Name:		h5pset_char
@@ -4383,61 +4242,6 @@
             hdferr = h5pget_real_c(prp_id, name , name_len, value)
           END SUBROUTINE h5pget_real
 
-!----------------------------------------------------------------------
-! Name:		h5pget_double
-!
-! Purpose: 	Gets a property list value
-!
-! Inputs:  
-!		prp_id		- iproperty list identifier to modify
-!		name 		- name of property to modify
-! Outputs:  
-!		value		- value of property
-!		hdferr:		- error code		
-!				 	Success:  0
-!				 	Failure: -1   
-! Optional parameters:
-!				NONE
-!
-! Programmer:	Elena Pourmal
-!	        October 9, 2002	
-!
-! Modifications: 	
-!
-! Comment:		
-!----------------------------------------------------------------------
-
-          SUBROUTINE h5pget_double(prp_id, name, value, hdferr) 
-!
-!This definition is needed for Windows DLLs
-!DEC$if defined(BUILD_HDF5_DLL)
-!DEC$attributes dllexport :: h5pget_double
-!DEC$endif
-!
-            IMPLICIT NONE
-            INTEGER(HID_T), INTENT(IN) :: prp_id  ! Property list identifier 
-            CHARACTER(LEN=*), INTENT(IN) :: name  ! Name of property to modify
-            DOUBLE PRECISION,   INTENT(OUT) :: value ! Property value
-            INTEGER, INTENT(OUT) :: hdferr  ! Error code
-            INTEGER :: name_len
-
-            INTERFACE
-              INTEGER FUNCTION h5pget_double_c(prp_id, name, name_len, value)
-              USE H5GLOBAL
-              !DEC$ IF DEFINED(HDF5F90_WINDOWS)
-              !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5PGET_DOUBLE_C'::h5pget_double_c
-              !DEC$ ENDIF
-              !DEC$ATTRIBUTES reference :: name
-              INTEGER(HID_T), INTENT(IN) :: prp_id
-              CHARACTER(LEN=*), INTENT(IN) :: name
-              INTEGER, INTENT(IN)         :: name_len
-              DOUBLE PRECISION, INTENT(OUT) :: value
-              END FUNCTION h5pget_double_c
-            END INTERFACE
-
-            name_len = LEN(name)
-            hdferr = h5pget_double_c(prp_id, name , name_len, value)
-          END SUBROUTINE h5pget_double
 
 !----------------------------------------------------------------------
 ! Name:		h5pget_char
@@ -5233,67 +5037,6 @@
           END SUBROUTINE h5pregister_real
 
 !----------------------------------------------------------------------
-! Name:		h5pregister_double
-!
-! Purpose: 	Registers a permanent property with a property list class.
-!
-! Inputs:  
-!		class		- property list class to register 
-!                                 permanent property within
-!		name 		- name of property to register
-!               size            - size of property in bytes
-!		value		- default value for property in newly 
-!                                 created property lists
-! Outputs:  
-!		hdferr:		- error code		
-!				 	Success:  0
-!				 	Failure: -1   
-! Optional parameters:
-!				NONE
-!
-! Programmer:	Elena Pourmal
-!	        October 10, 2002	
-!
-! Modifications: 	
-!
-! Comment:		
-!----------------------------------------------------------------------
-
-          SUBROUTINE h5pregister_double(class, name, size, value, hdferr) 
-!
-!This definition is needed for Windows DLLs
-!DEC$if defined(BUILD_HDF5_DLL)
-!DEC$attributes dllexport :: h5pregister_double
-!DEC$endif
-!
-            IMPLICIT NONE
-            INTEGER(HID_T), INTENT(IN) :: class   ! Property list class identifier 
-            CHARACTER(LEN=*), INTENT(IN) :: name  ! Name of property to register
-            INTEGER(SIZE_T), INTENT(IN) :: size  ! size of the property value	
-            DOUBLE PRECISION,   INTENT(IN) :: value        ! Property value
-            INTEGER, INTENT(OUT) :: hdferr  ! Error code
-            INTEGER :: name_len
-
-            INTERFACE
-              INTEGER FUNCTION h5pregister_double_c(class, name, name_len, size, value)
-              USE H5GLOBAL
-              !DEC$ IF DEFINED(HDF5F90_WINDOWS)
-              !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5PREGISTER_DOUBLE_C'::h5pregister_double_c
-              !DEC$ ENDIF
-              !DEC$ATTRIBUTES reference :: name
-              INTEGER(HID_T), INTENT(IN) :: class
-              CHARACTER(LEN=*), INTENT(IN) :: name
-              INTEGER, INTENT(IN)         :: name_len
-              INTEGER(SIZE_T), INTENT(IN) :: size 
-              DOUBLE PRECISION, INTENT(IN) :: value
-              END FUNCTION h5pregister_double_c
-            END INTERFACE
-
-            name_len = LEN(name)
-            hdferr = h5pregister_double_c(class, name , name_len, size, value)
-          END SUBROUTINE h5pregister_double
-
-!----------------------------------------------------------------------
 ! Name:		h5pregister_char
 !
 ! Purpose: 	Registers a permanent property with a property list class.
@@ -5478,65 +5221,6 @@
             hdferr = h5pinsert_real_c(plist, name , name_len, size, value)
           END SUBROUTINE h5pinsert_real
 
-!----------------------------------------------------------------------
-! Name:		h5pinsert_double
-!
-! Purpose: 	Registers a temporary property with a property list class.
-!
-! Inputs:  
-!		plist		- property list identifier
-!                                 permanent property within
-!		name 		- name of property to insert
-!               size            - size of property in bytes
-!		value		- initial value for the property 
-! Outputs:  
-!		hdferr:		- error code		
-!				 	Success:  0
-!				 	Failure: -1   
-! Optional parameters:
-!				NONE
-!
-! Programmer:	Elena Pourmal
-!	        October 10, 2002	
-!
-! Modifications: 	
-!
-! Comment:		
-!----------------------------------------------------------------------
-
-          SUBROUTINE h5pinsert_double(plist, name, size, value, hdferr) 
-!
-!This definition is needed for Windows DLLs
-!DEC$if defined(BUILD_HDF5_DLL)
-!DEC$attributes dllexport :: h5pinsert_double
-!DEC$endif
-!
-            IMPLICIT NONE
-            INTEGER(HID_T), INTENT(IN) :: plist   ! Property list identifier 
-            CHARACTER(LEN=*), INTENT(IN) :: name  ! Name of property to insert 
-            INTEGER(SIZE_T), INTENT(IN) :: size   ! Size of the property value	
-            DOUBLE PRECISION, INTENT(IN) :: value ! Property value
-            INTEGER, INTENT(OUT) :: hdferr        ! Error code
-            INTEGER :: name_len
-
-            INTERFACE
-              INTEGER FUNCTION h5pinsert_double_c(plist, name, name_len, size, value)
-              USE H5GLOBAL
-              !DEC$ IF DEFINED(HDF5F90_WINDOWS)
-              !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5PINSERT_DOUBLE_C'::h5pinsert_double_c
-              !DEC$ ENDIF
-              !DEC$ATTRIBUTES reference :: name
-              INTEGER(HID_T), INTENT(IN) :: plist
-              CHARACTER(LEN=*), INTENT(IN) :: name
-              INTEGER, INTENT(IN)         :: name_len
-              INTEGER(SIZE_T), INTENT(IN) :: size 
-              DOUBLE PRECISION, INTENT(IN) :: value
-              END FUNCTION h5pinsert_double_c
-            END INTERFACE
-
-            name_len = LEN(name)
-            hdferr = h5pinsert_double_c(plist, name , name_len, size, value)
-          END SUBROUTINE h5pinsert_double
 
 !----------------------------------------------------------------------
 ! Name:		h5pinsert_char
