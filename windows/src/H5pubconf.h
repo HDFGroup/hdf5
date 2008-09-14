260228e645ed33f0e4ee0e3c363eaa68372b3425
HDF Tester <hdftest@hdfgroup.org>
2008-09-14 17:50:44 -0500

    [svn-r15621] Snapshot version 1.8 release 1 (snap4)

--- windows/src/H5pubconf.h
+++ windows/src/H5pubconf.h
@@ -469,13 +469,13 @@
 #define H5_PACKAGE_NAME "HDF5"
 
 /* Define to the full name and version of this package. */
-#define H5_PACKAGE_STRING "HDF5 1.8.1-snap4"
+#define H5_PACKAGE_STRING "HDF5 1.8.1-snap5"
 
 /* Define to the one symbol short name of this package. */
 #define H5_PACKAGE_TARNAME "hdf5"
 
 /* Define to the version of this package. */
-#define H5_PACKAGE_VERSION "1.8.1-snap4"
+#define H5_PACKAGE_VERSION "1.8.1-snap5"
 
 /* Width for printf() for type `long long' or `__int64', use `ll' */
 #define H5_PRINTF_LL_WIDTH "I64"
@@ -632,7 +632,7 @@
 /* #undef H5_USING_MEMCHECKER */
 
 /* Version number of package */
-#define H5_VERSION "1.8.1-snap4"
+#define H5_VERSION "1.8.1-snap5"
 
 /* Define if vsnprintf() returns the correct value for formatted strings that
    don't fit into size allowed */
