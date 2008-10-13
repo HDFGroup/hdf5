166931c024aa8fa895f6f6cfdf2e1fd49e3d7d63
Scott Wegner <swegner@hdfgroup.org>
2008-10-13 11:51:31 -0500

    [svn-r15845] Purpose: Add Windows-specific version of HDftruncate
    Description:
    Windows doesn't include a version of the system call ftruncate.  There is a similar call, _chsize_s, which performs very similarly.  Thus, we map HDftruncate to _chsize_s in our Windows header file, H5win32defs.h.
    
    Tested:
    VS2005 on WinXP

--- src/H5win32defs.h
+++ src/H5win32defs.h
@@ -33,6 +33,7 @@ typedef __int64             h5_stat_size_t;
 #define HDdup(F)            _dup(F)
 #define HDfdopen(N,S)       _fdopen(N,S)
 #define HDfileno(F)         _fileno(F)
+#define HDftruncate(F,L)    _chsize_s(F,L)
 #define HDfstat(F,B)        _fstati64(F,B)
 #define HDisatty(F)         _isatty(F)
 #define HDstat(S,B)         _stati64(S,B)
