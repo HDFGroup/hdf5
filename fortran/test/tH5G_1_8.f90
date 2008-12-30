0e779a215c14343022fe9e180fe1802eb6d96cec
Scot Breitenfeld <brtnfld@hdfgroup.org>
2008-12-30 11:27:29 -0500

    [svn-r16231] Description:
    Added chunk property to the data set because changes in the cC function H5Dset_extent now require/check that the data set is chunked.

--- fortran/test/tH5G_1_8.f90
+++ fortran/test/tH5G_1_8.f90
@@ -1485,6 +1485,7 @@ SUBROUTINE link_info_by_idx_check(group_id, linkname, n, &
 
   INTEGER :: i
   INTEGER :: tmp1, tmp2
+  INTEGER(HID_T) :: crp_list
 
 !  WRITE(*,*) "link creation property lists (w/new group format)"
 
@@ -1541,9 +1542,11 @@ SUBROUTINE link_info_by_idx_check(group_id, linkname, n, &
   !/* Create a dataspace */
   CALL h5screate_simple_f(2, dims, space_id, error)
   CALL check("test_lcpl.h5screate_simple_f",error,total_error)
+  CALL h5pcreate_f(H5P_DATASET_CREATE_F, crp_list, error)
+  CALL h5pset_chunk_f(crp_list, 2, dims, error)
 
   ! /* Create a dataset using the default LCPL */
-  CALL h5dcreate_f(file_id, "/dataset", H5T_NATIVE_INTEGER, space_id, dset_id, error)
+  CALL h5dcreate_f(file_id, "/dataset", H5T_NATIVE_INTEGER, space_id, dset_id, error, crp_list)
   CALL check("test_lcpl.h5dcreate_f", error, total_error)
   CALL h5dclose_f(dset_id, error)
   CALL check("test_lcpl.h5dclose_f", error, total_error)
