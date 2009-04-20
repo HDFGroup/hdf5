cd48d93298414e48d8aa5dc3dbd2c2701b152e2c
Pedro Vicente Nunes <pvn@hdfgroup.org>
2009-04-20 16:06:54 -0500

    [svn-r16811] merge 16810 added the words "Not comparable" in the not comparable messages tested: linux

--- tools/lib/h5diff_dset.c
+++ tools/lib/h5diff_dset.c
@@ -267,7 +267,7 @@ hsize_t diff_datasetid( hid_t did1,
     if (storage_size1==0 || storage_size2==0)
     {
         if ( (options->m_verbose||options->m_list_not_cmp) && obj1_name && obj2_name)
-            parallel_print("<%s> or <%s> are empty datasets\n", obj1_name, obj2_name);
+            parallel_print("Not comparable: <%s> or <%s> are empty datasets\n", obj1_name, obj2_name);
         can_compare=0;
         options->not_cmp=1;
     }
@@ -317,7 +317,7 @@ hsize_t diff_datasetid( hid_t did1,
     {
         if ((options->m_verbose||options->m_list_not_cmp) && obj1_name && obj2_name) 
         {
-            parallel_print("<%s> has sign %s ", obj1_name, get_sign(sign1));
+            parallel_print("Not comparable: <%s> has sign %s ", obj1_name, get_sign(sign1));
             parallel_print("and <%s> has sign %s\n", obj2_name, get_sign(sign2));
         }
         
@@ -698,7 +698,7 @@ int diff_can_type( hid_t       f_tid1, /* file data type */
             if ( is_compound )
             {
                 
-                parallel_print("<%s> has a class %s and <%s> has a class %s\n",
+                parallel_print("Not comparable: <%s> has a class %s and <%s> has a class %s\n",
                     obj1_name, get_class(tclass1),
                     obj2_name, get_class(tclass2) );
                 
@@ -748,7 +748,7 @@ int diff_can_type( hid_t       f_tid1, /* file data type */
 
         if ( (options->m_verbose||options->m_list_not_cmp) && obj1_name && obj2_name)
         {
-            parallel_print("<%s> and <%s> are of class %s\n",
+            parallel_print("Not comparable: <%s> and <%s> are of class %s\n",
                 obj1_name,obj2_name,get_class(tclass2) );
         }
         can_compare = 0;
@@ -793,7 +793,7 @@ int diff_can_type( hid_t       f_tid1, /* file data type */
 
         if ( (options->m_verbose||options->m_list_not_cmp) && obj1_name && obj2_name)
         {
-            parallel_print("<%s> has rank %d, dimensions ", obj1_name, rank1);
+            parallel_print("Not comparable: <%s> has rank %d, dimensions ", obj1_name, rank1);
             print_dimensions(rank1,dims1);
             parallel_print(", max dimensions ");
             print_dimensions(rank1,maxdim1);
@@ -836,7 +836,7 @@ int diff_can_type( hid_t       f_tid1, /* file data type */
     {
         if ( (options->m_verbose||options->m_list_not_cmp) && obj1_name && obj2_name)
         {
-            parallel_print("<%s> has rank %d, dimensions ", obj1_name, rank1);
+            parallel_print("Not comparable: <%s> has rank %d, dimensions ", obj1_name, rank1);
             print_dimensions(rank1,dims1);
             if (maxdim1 && maxdim2) 
             {
@@ -895,7 +895,7 @@ int diff_can_type( hid_t       f_tid1, /* file data type */
             
             if ( (options->m_verbose||options->m_list_not_cmp) && obj1_name && obj2_name)
             {
-                parallel_print("<%s> has %d members ", obj1_name, nmembs1);
+                parallel_print("Not comparable: <%s> has %d members ", obj1_name, nmembs1);
                 parallel_print("<%s> has %d members ", obj2_name, nmembs2);
                 parallel_print("\n");
             }
