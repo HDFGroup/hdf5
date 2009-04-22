be8cb6411aef28ebdf3c7f757a881ea984f4411d
Raymond Lu <songyulu@hdfgroup.org>
2009-04-22 13:39:10 -0500

    [svn-r16835] Allow an error class to use other class's major or minor errors.  Added a test for it, too.
    Tested on jam - simple change.

--- src/H5Eint.c
+++ src/H5Eint.c
@@ -362,8 +362,9 @@ H5E_walk2_cb(unsigned n, const H5E_error2_t *err_desc, void *client_data)
     if(min_ptr->msg)
         min_str = min_ptr->msg;
 
-    /* Get error class info */
-    cls_ptr = maj_ptr->cls;
+    /* Get error class info.  Don't use the class of the major or minor error because
+     * they might be different. */
+    cls_ptr = (H5E_cls_t *)H5I_object_verify(err_desc->cls_id, H5I_ERROR_CLASS);
 
     /* Print error class header if new class */
     if(eprint->cls.lib_name == NULL || HDstrcmp(cls_ptr->lib_name, eprint->cls.lib_name)) {
