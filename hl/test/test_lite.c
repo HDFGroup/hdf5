ffed17cb9cd8460dc08affa87cabc34effd12746
Scot Breitenfeld <brtnfld@hdfgroup.org>
2009-05-11 12:19:01 -0500

    [svn-r16935] Description:
    Added test for H5LTget_dataset_ndims
    
    Tested: smirom

--- hl/test/test_lite.c
+++ hl/test/test_lite.c
@@ -99,6 +99,9 @@ static int test_dsets( void )
     if ( H5Dread ( dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data_int_out ) < 0 )
         goto out;
 
+    if ( H5LTget_dataset_ndims(file_id, DSET0_NAME, &rank ))
+      goto out;
+
     if ( H5Dclose( dataset_id ) < 0 )
         goto out;
 
