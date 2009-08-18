347c41995bfbf01e52fc4636297138b2a1ab3f45
Quincey Koziol <koziol@hdfgroup.org>
2009-08-18 15:22:14 -0500

    [svn-r17376] Description: 	Make similar change to windows VFD as sec2 VFD, when converting from a family file to a single file.
    	Tweak file sizes expected for parallel tests.
    
    Tested on:
    	tg-login3, w/parallel
    	Windows (post facto)

--- src/H5F.c
+++ src/H5F.c
@@ -2837,7 +2837,7 @@ H5Fget_info(hid_t obj_id, H5F_info_t *finfo)
 
     /* Check for superblock extension info */
     if(H5F_super_size(f, H5AC_ind_dxpl_id, NULL, &finfo->super_ext_size) < 0)
-        HGOTO_ERROR(H5E_FILE, H5E_CANTGET, FAIL, "Unable to retrieve superblock sizes")
+        HGOTO_ERROR(H5E_FILE, H5E_CANTGET, FAIL, "Unable to retrieve superblock extension size")
 
     /* Check for SOHM info */
     if(H5F_addr_defined(f->shared->sohm_addr))
