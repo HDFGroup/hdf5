8d6a34dfbc93073926c108fbbe5422d6095c6570
Allen Byrne <byrn@hdfgroup.org>
2010-10-22 09:07:30 -0500

    [svn-r19662] Correct Project name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,12 +4,12 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "HDF5.1.8")
+set(CTEST_PROJECT_NAME "HDF5.1.8.6")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "nei.hdfgroup.uiuc.edu")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=HDF5.1.8")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=HDF5.1.8.6")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(UPDATE_TYPE svn)
