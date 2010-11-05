3841249157c5c825a5b8455328b0f694740d73e1
Allen Byrne <byrn@hdfgroup.org>
2010-11-05 16:28:29 -0500

    [svn-r19737] Added another version of H5detect warning to ignore list Added H5convert warning to ignore list
    Added flush1 to list of memcheck ignore - design precludes valgrind correctness

--- config/cmake/CTestCustom.cmake
+++ config/cmake/CTestCustom.cmake
@@ -3,6 +3,8 @@ SET (CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1500)
 SET (CTEST_CUSTOM_WARNING_EXCEPTION
     ${CTEST_CUSTOM_WARNING_EXCEPTION}
     "H5detect.c.[0-9]+.[ \t]*:[ \t]*warning C4090"
+    "H5detect.c.[0-9]+.[ \t]*:[ \t]*warning:[ \t]*passing argument"
+    "H5Tconv.c[0-9]+.[ \t]*:[ \t]*warning:[ \t]*comparison is always false due to limited range of data type"
     "testhdf5.h.[0-9]+.[ \t]*:[ \t]*warning C4005"
     "H5Ztrans.c.[0-9]+.[ \t]*:[ \t]*warning C4244"
     "SZIP.src.[0-9a-zA-Z]*warning"
@@ -12,6 +14,7 @@ SET (CTEST_CUSTOM_WARNING_EXCEPTION
  
 SET (CTEST_CUSTOM_MEMCHECK_IGNORE
     ${CTEST_CUSTOM_MEMCHECK_IGNORE}
+    flush1
     h5test-clear-objects
     h5perform-clear-objects
     hl_test-clear-objects
