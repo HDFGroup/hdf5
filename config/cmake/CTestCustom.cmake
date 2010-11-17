e0225c649678cd867c8a545ab8e3fe541fc5ccb7
Allen Byrne <byrn@hdfgroup.org>
2010-11-17 12:00:17 -0500

    [svn-r19807] Corrected REGEX of warning exception

--- config/cmake/CTestCustom.cmake
+++ config/cmake/CTestCustom.cmake
@@ -5,10 +5,10 @@ SET (CTEST_CUSTOM_WARNING_EXCEPTION
     "H5detect.c.[0-9]+.[ \t]*:[ \t]*warning C4090"
     "H5detect.c.[0-9]+.[ \t]*:[ \t]*warning:[ \t]*passing argument"
     "H5detect.c[0-9 \t:]*warning:[ \t]*passing argument"
-    "H5Tconv.c[0-9]+.[ \t]*:[ \t]*warning:[ \t]*comparison is always false due to limited range of data type"
+    "H5Tconv.c[0-9 \t:]*warning:[ \t]*comparison is always false due to limited range of data type"
     "testhdf5.h.[0-9]+.[ \t]*:[ \t]*warning C4005"
     "H5Ztrans.c.[0-9]+.[ \t]*:[ \t]*warning C4244"
-    "SZIP.src.[0-9a-zA-Z]*warning"
+    "SZIP.src.[0-9a-zA-Z]*[ \t]*:[ \t]*warning"
     "POSIX name for this item is deprecated"
     "disabling jobserver mode"
 )
