c7782121eedc332660a85ce1a4fa97ed3289cf49
Raymond Lu <songyulu@hdfgroup.org>
2011-01-24 15:39:17 -0500

    [svn-r19983] I added a test case for dataset with scale-offset filter into cross_read.c and updated the data files from BE, LE, and VMS.  This is the test for bug 2131 that dataset of FLOAT with scale-offset filter can't be read across BE and LE.
    Tested on jam and linew.

--- test/cross_read.c
+++ test/cross_read.c
@@ -17,8 +17,8 @@
  * Programmer:  Raymond Lu <slu@ncsa.uiuc.edu>
  *              Thursday, March 23, 2006
  *
- * Purpose:     Check if floating-point data created on OpenVMS (VAX type), Solaris,
- *              and Linux machines can be read on the machine running this test.
+ * Purpose:     Check if floating-point data created on OpenVMS, big-endian, and
+ *              little-endian machines can be read on the machine running this test.
  */
 
 #include "h5test.h"
@@ -31,11 +31,28 @@ const char *FILENAME[] = {
     NULL
 };
 
-#define DATASETNAME "Array"
-#define NX 5           /* output buffer dimensions */
-#define NY 6
-#define RANK         2
+#define DATASETNAME        "Array"
+#define DATASETNAME2       "Scale_offset_float_data"
+#define DATASETNAME3       "Scale_offset_int_data"
+#define NX 		6         
+#define NY 		6
 
+
+/*-------------------------------------------------------------------------
+ * Function:    read_data
+ *
+ * Purpose:     Read data from a data file.
+ *
+ * Return:      Success:        0
+ *              Failure:        -1
+ *
+ * Programmer:  Raymond Lu
+ *              21 January 2011
+ *
+ * Modifications:
+ *
+ *-------------------------------------------------------------------------
+ */
 static int read_data(char *fname)
 {
     char        pathname[1024];
@@ -43,10 +60,14 @@ static int read_data(char *fname)
     hid_t       file, dataset;         /* handles */
     hid_t       datatype;
     hid_t	dt;
-    double      data_in[NX][NY]; /* input buffer */
-    double      data_out[NX][NY]; /* output buffer */
+    float       data_in[NX][NY]; /* input buffer */
+    float       data_out[NX][NY]; /* output buffer */
+    int         int_data_in[NX][NY]; /* input buffer */
+    int         int_data_out[NX][NY]; /* output buffer */
     int         i, j;
     unsigned 	nerrors = 0;
+    const char  *not_supported= "    Scaleoffset filter is not enabled.";
+    const char  *not_fixed= "    Scaleoffset filter bug (2131) is not fixed yet.";
 
     pathname[0] = '\0';
     /* Generate correct name for test file by prepending the source path */
@@ -55,6 +76,20 @@ static int read_data(char *fname)
         strcat(pathname, "/");
     }
     strcat(pathname, fname);
+
+    /*
+     * Open the file.
+     */
+    if((file = H5Fopen(pathname, H5F_ACC_RDONLY, H5P_DEFAULT)) < 0)
+        TEST_ERROR;
+
+    TESTING("	regular dataset");
+    
+    /* 
+     * Open the regular dataset.
+     */
+    if((dataset = H5Dopen2(file, DATASETNAME, H5P_DEFAULT)) < 0)
+        TEST_ERROR;
 
     /*
      * Data and output buffer initialization.
@@ -71,15 +106,8 @@ static int read_data(char *fname)
      * 2 3 4 5 6 7
      * 3 4 5 6 7 8
      * 4 5 6 7 8 9
+     * 5 6 7 8 9 10
      */
-
-    /*
-     * Open the file and the dataset.
-     */
-    if((file = H5Fopen(pathname, H5F_ACC_RDONLY, H5P_DEFAULT)) < 0)
-        TEST_ERROR;
-    if((dataset = H5Dopen2(file, DATASETNAME, H5P_DEFAULT)) < 0)
-        TEST_ERROR;
 
     /*
      * Get datatype and dataspace handles and then query
@@ -100,7 +128,8 @@ static int read_data(char *fname)
     /* Check results */
     for (j=0; j<NX; j++) {
         for (i=0; i<NY; i++) {
-            if (data_out[j][i] != data_in[j][i]) {
+            /* if (data_out[j][i] != data_in[j][i]) { */
+            if (!DBL_ABS_EQUAL(data_out[j][i], data_in[j][i])) {
                 if (!nerrors++) {
                     H5_FAILED();
                     printf("element [%d][%d] is %g but should have been %g\n",
@@ -116,7 +145,6 @@ static int read_data(char *fname)
     H5Tclose(dt);
     H5Tclose(datatype);
     H5Dclose(dataset);
-    H5Fclose(file);
 
     /* Failure */
     if (nerrors) {
@@ -125,6 +153,147 @@ static int read_data(char *fname)
     }
 
     PASSED();
+
+    TESTING("	dataset of FLOAT with scale-offset filter");
+#ifdef TMP
+#ifdef H5_HAVE_FILTER_SCALEOFFSET
+    /* 
+     * Open the dataset with scale-offset filter.
+     */
+    if((dataset = H5Dopen2(file, DATASETNAME2, H5P_DEFAULT)) < 0)
+        TEST_ERROR;
+
+    /*
+     * Data and output buffer initialization.
+     */
+    for (j = 0; j < NX; j++) {
+	for (i = 0; i < NY; i++) {
+	    data_in[j][i] = ((float)(i + j + 1))/3;
+	    data_out[j][i] = 0;
+        }
+    }
+
+    /*
+     * Get datatype and dataspace handles and then query
+     * dataset class, order, size, rank and dimensions.
+     */
+    if((dt = H5Dget_type(dataset)) < 0)     /* datatype handle */
+        TEST_ERROR;
+    if((datatype = H5Tget_native_type(dt, H5T_DIR_DEFAULT)) < 0)
+        TEST_ERROR;
+
+    /*
+     * Read data from hyperslab in the file into the hyperslab in
+     * memory and display.
+     */
+    if(H5Dread(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, data_out) < 0)
+        TEST_ERROR;
+
+    /* Check results */
+    for (j=0; j<NX; j++) {
+        for (i=0; i<NY; i++) {
+            if (!DBL_REL_EQUAL(data_out[j][i], data_in[j][i], 0.001)) {
+                if (!nerrors++) {
+                    H5_FAILED();
+                    printf("element [%d][%d] is %g but should have been %g\n",
+                           j, i, data_out[j][i], data_in[j][i]);
+                }
+            }
+        }
+    }
+
+    /*
+     * Close/release resources.
+     */
+    H5Tclose(dt);
+    H5Tclose(datatype);
+    H5Dclose(dataset);
+
+    /* Failure */
+    if (nerrors) {
+        printf("total of %d errors out of %d elements\n", nerrors, NX*NY);
+        return 1;
+    }
+
+    PASSED();
+#else /*H5_HAVE_FILTER_SCALEOFFSET*/
+    SKIPPED();
+    puts(not_supported);
+#endif /*H5_HAVE_FILTER_SCALEOFFSET*/
+#else /*TMP*/
+    SKIPPED();
+    puts(not_fixed);
+#endif /*TMP*/
+
+    TESTING("	dataset of INT with scale-offset filter");
+
+#ifdef H5_HAVE_FILTER_SCALEOFFSET
+    /* 
+     * Open the dataset with scale-offset filter.
+     */
+    if((dataset = H5Dopen2(file, DATASETNAME3, H5P_DEFAULT)) < 0)
+        TEST_ERROR;
+
+    /*
+     * Data and output buffer initialization.
+     */
+    for (j = 0; j < NX; j++) {
+	for (i = 0; i < NY; i++) {
+	    int_data_in[j][i] = i + j;
+	    int_data_out[j][i] = 0;
+        }
+    }
+
+    /*
+     * Get datatype and dataspace handles and then query
+     * dataset class, order, size, rank and dimensions.
+     */
+    if((dt = H5Dget_type(dataset)) < 0)     /* datatype handle */
+        TEST_ERROR;
+    if((datatype = H5Tget_native_type(dt, H5T_DIR_DEFAULT)) < 0)
+        TEST_ERROR;
+
+    /*
+     * Read data from hyperslab in the file into the hyperslab in
+     * memory and display.
+     */
+    if(H5Dread(dataset, datatype, H5S_ALL, H5S_ALL, H5P_DEFAULT, int_data_out) < 0)
+        TEST_ERROR;
+
+    /* Check results */
+    for (j=0; j<NX; j++) {
+        for (i=0; i<NY; i++) {
+            if (int_data_out[j][i] != int_data_in[j][i]) {
+                if (!nerrors++) {
+                    H5_FAILED();
+                    printf("element [%d][%d] is %d but should have been %d\n",
+                           j, i, int_data_out[j][i], int_data_in[j][i]);
+                }
+            }
+        }
+    }
+
+    /*
+     * Close/release resources.
+     */
+    H5Tclose(dt);
+    H5Tclose(datatype);
+    H5Dclose(dataset);
+
+    /* Failure */
+    if (nerrors) {
+        printf("total of %d errors out of %d elements\n", nerrors, NX*NY);
+        return 1;
+    }
+
+    PASSED();
+
+#else /*H5_HAVE_FILTER_SCALEOFFSET*/
+    SKIPPED();
+    puts(not_supported);
+#endif /*H5_HAVE_FILTER_SCALEOFFSET*/
+
+    H5Fclose(file);
     return 0;
 
 error:
@@ -134,6 +303,20 @@ error:
     return 1;
 }
 
+
+/*-------------------------------------------------------------------------
+ * Function:    main
+ *
+ * Purpose:     Tests the basic features of Virtual File Drivers
+ *
+ * Return:      Success:        exit(0)
+ *              Failure:        exit(1)
+ *
+ * Programmer:  Raymond Lu
+ *              Tuesday, Sept 24, 2002
+ *
+ *-------------------------------------------------------------------------
+ */
 int main(void)
 {
     char        filename[1024];
@@ -141,15 +324,15 @@ int main(void)
 
     h5_reset();
 
-    TESTING("reading data created on OpenVMS");
+    puts("Testing reading data created on OpenVMS");
     h5_fixname(FILENAME[0], H5P_DEFAULT, filename, sizeof filename);
     nerrors += read_data(filename);
 
-    TESTING("reading data created on Linux");
+    puts("Testing reading data created on Linux");
     h5_fixname(FILENAME[1], H5P_DEFAULT, filename, sizeof filename);
     nerrors += read_data(filename);
 
-    TESTING("reading data created on Solaris");
+    puts("Testing reading data created on Solaris");
     h5_fixname(FILENAME[2], H5P_DEFAULT, filename, sizeof filename);
     nerrors += read_data(filename);
 
--- test/gen_cross.c
+++ test/gen_cross.c
@@ -27,17 +27,39 @@
 #include "h5test.h"
 
 #define H5FILE_NAME        "data.h5"
-#define DATASETNAME "Array"
-#define NX     5                      /* dataset dimensions */
-#define NY     6
-#define RANK   2
+#define DATASETNAME        "Array"
+#define DATASETNAME2       "Scale_offset_float_data"
+#define DATASETNAME3       "Scale_offset_int_data"
+#define NX                 6
+#define NY                 6
+#define RANK               2
+#define CHUNK0             3
+#define CHUNK1             3
 
+int create_normal_dset(hid_t fid, hid_t sid);
+int create_scale_offset_dset_float(hid_t fid, hid_t sid);
+int create_scale_offset_dset_int(hid_t fid, hid_t sid);
+
+
+/*-------------------------------------------------------------------------
+ * Function:    create_normal_dset
+ *
+ * Purpose:     Create a regular dataset of FLOAT datatype.
+ *
+ * Return:      Success:        0
+ *              Failure:        -1
+ *
+ * Programmer:  Raymond Lu
+ *              Some time ago
+ *
+ * Modifications:
+ *
+ *-------------------------------------------------------------------------
+ */
 int
-main (void)
+create_normal_dset(hid_t fid, hid_t sid)
 {
-    hid_t       file, dataset;         /* file and dataset handles */
-    hid_t       datatype, dataspace;   /* handles */
-    hsize_t     dimsf[2];              /* dataset dimensions */
+    hid_t       dataset;         /* file and dataset handles */
     herr_t      status;
     float       data[NX][NY];          /* data to write */
     int         i, j;
@@ -55,7 +77,200 @@ main (void)
      * 2 3 4 5 6 7
      * 3 4 5 6 7 8
      * 4 5 6 7 8 9
+     * 5 6 7 8 9 10
      */
+
+    /*
+     * Create a new dataset within the file using defined dataspace and
+     * datatype and default dataset creation properties.
+     */
+    dataset = H5Dcreate2(fid, DATASETNAME, H5T_NATIVE_FLOAT, sid,
+			H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+    /*
+     * Write the data to the dataset using default transfer properties.
+     */
+    status = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL,
+		      H5P_DEFAULT, data);
+
+    /*
+     * Close/release resources.
+     */
+    H5Dclose(dataset);
+
+    return 0;
+}
+
+
+/*-------------------------------------------------------------------------
+ * Function:    create_scale_offset_dset_float
+ *
+ * Purpose:     Create a dataset of FLOAT datatype with scale-offset filter
+ *
+ * Return:      Success:        0
+ *              Failure:        -1
+ *
+ * Programmer:  Raymond Lu
+ *              21 January 2011
+ *
+ * Modifications:
+ *
+ *-------------------------------------------------------------------------
+ */
+int
+create_scale_offset_dset_float(hid_t fid, hid_t sid)
+{
+#ifdef H5_HAVE_FILTER_SCALEOFFSET
+    hid_t       dataset;         /* dataset handles */
+    hid_t       dcpl;
+    herr_t      status;
+    float       data[NX][NY];          /* data to write */
+    hsize_t     chunk[RANK] = {CHUNK0, CHUNK1};
+    int         i, j;
+
+    /*
+     * Data and output buffer initialization.
+     */
+    for (j = 0; j < NX; j++) {
+	for (i = 0; i < NY; i++)
+	    data[j][i] = ((float)(i + j + 1))/3;
+    }
+
+    /*
+     * Create the dataset creation property list, add the Scale-Offset
+     * filter and set the chunk size.
+     */
+    dcpl = H5Pcreate (H5P_DATASET_CREATE);
+    status = H5Pset_scaleoffset (dcpl, H5Z_SO_FLOAT_DSCALE, 3);
+    status = H5Pset_chunk (dcpl, RANK, chunk);
+
+    /*
+     * Create a new dataset within the file using defined dataspace and
+     * datatype and default dataset creation properties.
+     */
+    dataset = H5Dcreate2(fid, DATASETNAME2, H5T_NATIVE_FLOAT, sid,
+			H5P_DEFAULT, dcpl, H5P_DEFAULT);
+
+    /*
+     * Write the data to the dataset using default transfer properties.
+     */
+    status = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL,
+		      H5P_DEFAULT, data);
+
+    /*
+     * Close/release resources.
+     */
+    H5Pclose(dcpl);
+    H5Dclose(dataset);
+
+#else /* H5_HAVE_FILTER_SCALEOFFSET */
+    const char          *not_supported= "Scaleoffset filter is not enabled. Can't create the dataset.";
+
+    puts(not_supported);
+#endif /* H5_HAVE_FILTER_SCALEOFFSET */
+
+    return 0;
+}
+
+
+/*-------------------------------------------------------------------------
+ * Function:    create_scale_offset_dset_int
+ *
+ * Purpose:     Create a dataset of INT datatype with scale-offset filter
+ *
+ * Return:      Success:        0
+ *              Failure:        -1
+ *
+ * Programmer:  Raymond Lu
+ *              21 January 2011
+ *
+ * Modifications:
+ *
+ *-------------------------------------------------------------------------
+ */
+int
+create_scale_offset_dset_int(hid_t fid, hid_t sid)
+{
+#ifdef H5_HAVE_FILTER_SCALEOFFSET
+    hid_t       dataset;         /* dataset handles */
+    hid_t       dcpl;
+    herr_t      status;
+    int         data[NX][NY];          /* data to write */
+    hsize_t     chunk[RANK] = {CHUNK0, CHUNK1};
+    int         i, j;
+
+    /*
+     * Data and output buffer initialization.
+     */
+    for (j = 0; j < NX; j++) {
+	for (i = 0; i < NY; i++)
+	    data[j][i] = i + j;
+    }
+    /*
+     * 0 1 2 3 4 5
+     * 1 2 3 4 5 6
+     * 2 3 4 5 6 7
+     * 3 4 5 6 7 8
+     * 4 5 6 7 8 9
+     * 5 6 7 8 9 10
+     */
+
+    /*
+     * Create the dataset creation property list, add the Scale-Offset
+     * filter and set the chunk size.
+     */
+    dcpl = H5Pcreate (H5P_DATASET_CREATE);
+    status = H5Pset_scaleoffset (dcpl, H5Z_SO_INT, H5Z_SO_INT_MINBITS_DEFAULT);
+    status = H5Pset_chunk (dcpl, RANK, chunk);
+
+    /*
+     * Create a new dataset within the file using defined dataspace and
+     * datatype and default dataset creation properties.
+     */
+    dataset = H5Dcreate2(fid, DATASETNAME3, H5T_NATIVE_INT, sid,
+			H5P_DEFAULT, dcpl, H5P_DEFAULT);
+
+    /*
+     * Write the data to the dataset using default transfer properties.
+     */
+    status = H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL,
+		      H5P_DEFAULT, data);
+
+    /*
+     * Close/release resources.
+     */
+    H5Pclose(dcpl);
+    H5Dclose(dataset);
+
+#else /* H5_HAVE_FILTER_SCALEOFFSET */
+    const char          *not_supported= "Scaleoffset filter is not enabled. Can't create the dataset.";
+
+    puts(not_supported);
+#endif /* H5_HAVE_FILTER_SCALEOFFSET */
+
+    return 0;
+}
+
+
+/*-------------------------------------------------------------------------
+ * Function:    main
+ *
+ * Purpose:     Create a file for cross_read.c test.
+ *
+ * Return:      Success:        exit(0)
+ *              Failure:        exit(1)
+ *
+ * Programmer:  Raymond Lu
+ *              Some time ago
+ *
+ *-------------------------------------------------------------------------
+ */
+int
+main (void)
+{
+    hid_t       file;         /* file and dataset handles */
+    hid_t       dataspace;
+    hsize_t     dimsf[RANK];
 
     /*
      * Create a new file using H5F_ACC_TRUNC access,
@@ -72,31 +287,19 @@ main (void)
     dimsf[1] = NY;
     dataspace = H5Screate_simple(RANK, dimsf, NULL);
 
-    /*
-     * Define datatype for the data in the file.
-     * We will store little endian INT numbers.
-     */
-    datatype = H5Tcopy(H5T_NATIVE_DOUBLE);
+    /* Create a regular dataset */
+    create_normal_dset(file, dataspace);
 
-    /*
-     * Create a new dataset within the file using defined dataspace and
-     * datatype and default dataset creation properties.
-     */
-    dataset = H5Dcreate2(file, DATASETNAME, datatype, dataspace,
-			H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    /* Create a dataset of FLOAT with scale-offset filter */
+    create_scale_offset_dset_float(file, dataspace);
 
-    /*
-     * Write the data to the dataset using default transfer properties.
-     */
-    status = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL,
-		      H5P_DEFAULT, data);
+    /* Create a dataset of INT with scale-offset filter */
+    create_scale_offset_dset_int(file, dataspace);
 
     /*
      * Close/release resources.
      */
     H5Sclose(dataspace);
-    H5Tclose(datatype);
-    H5Dclose(dataset);
     H5Fclose(file);
 
     return 0;
