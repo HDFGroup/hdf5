68688ba87e41e8b6d48118385483ac08580bf422
Raymond Lu <songyulu@hdfgroup.org>
2011-06-07 14:22:03 -0500

    [svn-r20947] Issue 4278 - When reading data fails, the error message should say which filter isn't registered.  This is the follow-up commit.  In my first checkin, I took out the line "H5Z_SZIP->encoder_present = SZ_encoder_enabled()" by mistake.  It caused SZIP test to fail.  I'm puting it back now.
    Tested on jam - simple change.

--- src/H5Z.c
+++ src/H5Z.c
@@ -96,6 +96,7 @@ H5Z_init_interface (void)
 	HGOTO_ERROR (H5E_PLINE, H5E_CANTINIT, FAIL, "unable to register fletcher32 filter")
 #endif /* H5_HAVE_FILTER_FLETCHER32 */
 #ifdef H5_HAVE_FILTER_SZIP
+    H5Z_SZIP->encoder_present = SZ_encoder_enabled();
     if (H5Z_register (H5Z_SZIP)<0)
 	HGOTO_ERROR (H5E_PLINE, H5E_CANTINIT, FAIL, "unable to register szip filter")
 #endif /* H5_HAVE_FILTER_SZIP */
