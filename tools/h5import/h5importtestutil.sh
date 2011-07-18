6c0f470d9dc4cc1d5e75687fef7245aa2d2fd0c6
Allen Byrne <byrn@hdfgroup.org>
2011-07-18 10:37:26 -0500

    [svn-r21111] Renamed test files to more descriptive name
    Tested: local linux

--- tools/h5import/h5importtestutil.sh
+++ tools/h5import/h5importtestutil.sh
@@ -57,47 +57,48 @@ test -d $TESTDIR || mkdir $TESTDIR
 #       Comment '#' without space can be used.
 # --------------------------------------------------------------------
 LIST_HDF5_TEST_FILES="
-$SRC_H5IMPORT_TESTFILES/test1.h5
-$SRC_H5IMPORT_TESTFILES/test2.h5
-$SRC_H5IMPORT_TESTFILES/test3.h5
-$SRC_H5IMPORT_TESTFILES/test4.h5
-$SRC_H5IMPORT_TESTFILES/test5.h5
-$SRC_H5IMPORT_TESTFILES/test6.h5
-$SRC_H5IMPORT_TESTFILES/test7.h5
-$SRC_H5IMPORT_TESTFILES/test8.h5
-$SRC_H5IMPORT_TESTFILES/test9.h5
-$SRC_H5IMPORT_TESTFILES/test10.h5
-$SRC_H5IMPORT_TESTFILES/test11.h5
-$SRC_H5IMPORT_TESTFILES/test12.h5
-$SRC_H5IMPORT_TESTFILES/test13.h5
-$SRC_H5IMPORT_TESTFILES/test14.h5
-$SRC_H5IMPORT_TESTFILES/test15.h5
+$SRC_H5IMPORT_TESTFILES/binfp64.h5
+$SRC_H5IMPORT_TESTFILES/binin8.h5
+$SRC_H5IMPORT_TESTFILES/binin8w.h5
+$SRC_H5IMPORT_TESTFILES/binin16.h5
+$SRC_H5IMPORT_TESTFILES/binin32.h5
+$SRC_H5IMPORT_TESTFILES/binuin16.h5
+$SRC_H5IMPORT_TESTFILES/binuin32.h5
+$SRC_H5IMPORT_TESTFILES/txtfp32.h5
+$SRC_H5IMPORT_TESTFILES/txtfp64.h5
+$SRC_H5IMPORT_TESTFILES/txtin8.h5
+$SRC_H5IMPORT_TESTFILES/txtin16.h5
+$SRC_H5IMPORT_TESTFILES/txtin32.h5
+$SRC_H5IMPORT_TESTFILES/txtuin16.h5
+$SRC_H5IMPORT_TESTFILES/txtuin32.h5
+$SRC_H5IMPORT_TESTFILES/txtstr.h5
+$SRC_H5IMPORT_TESTFILES/textpfe.h5
 "
 
 LIST_OTHER_TEST_FILES="
-$SRC_H5IMPORT_TESTFILES/binfp32.conf
 $SRC_H5IMPORT_TESTFILES/binfp64.conf
 $SRC_H5IMPORT_TESTFILES/binin8.conf
+$SRC_H5IMPORT_TESTFILES/binin8w.conf
 $SRC_H5IMPORT_TESTFILES/binin16.conf
 $SRC_H5IMPORT_TESTFILES/binin32.conf
 $SRC_H5IMPORT_TESTFILES/binuin16.conf
 $SRC_H5IMPORT_TESTFILES/binuin32.conf
-$SRC_H5IMPORT_TESTFILES/textfp32.conf
-$SRC_H5IMPORT_TESTFILES/textfp64.conf
-$SRC_H5IMPORT_TESTFILES/textin8.conf
-$SRC_H5IMPORT_TESTFILES/textin16.conf
-$SRC_H5IMPORT_TESTFILES/textin32.conf
-$SRC_H5IMPORT_TESTFILES/textuin16.conf
-$SRC_H5IMPORT_TESTFILES/textuin32.conf
+$SRC_H5IMPORT_TESTFILES/txtfp32.conf
+$SRC_H5IMPORT_TESTFILES/txtfp64.conf
+$SRC_H5IMPORT_TESTFILES/txtin8.conf
+$SRC_H5IMPORT_TESTFILES/txtin16.conf
+$SRC_H5IMPORT_TESTFILES/txtin32.conf
+$SRC_H5IMPORT_TESTFILES/txtuin16.conf
+$SRC_H5IMPORT_TESTFILES/txtuin32.conf
 $SRC_H5IMPORT_TESTFILES/textpfe.conf
-$SRC_H5IMPORT_TESTFILES/textstr.conf
-$SRC_H5IMPORT_TESTFILES/fp1.txt
-$SRC_H5IMPORT_TESTFILES/fp2.txt
-$SRC_H5IMPORT_TESTFILES/in1.txt
-$SRC_H5IMPORT_TESTFILES/in16.txt
-$SRC_H5IMPORT_TESTFILES/in32.txt
-$SRC_H5IMPORT_TESTFILES/in64.txt
-$SRC_H5IMPORT_TESTFILES/str.txt
+$SRC_H5IMPORT_TESTFILES/txtstr.conf
+$SRC_H5IMPORT_TESTFILES/txtfp32.txt
+$SRC_H5IMPORT_TESTFILES/txtfp64.txt
+$SRC_H5IMPORT_TESTFILES/txtuin32.txt
+$SRC_H5IMPORT_TESTFILES/txtin16.txt
+$SRC_H5IMPORT_TESTFILES/txtin32.txt
+$SRC_H5IMPORT_TESTFILES/textpfe64.txt
+$SRC_H5IMPORT_TESTFILES/txtstr.txt
 "
 
 #
@@ -164,64 +165,76 @@ rm -f  output.h5 log1 tx* b* *.dat
 COPY_TESTFILES_TO_TESTDIR
 
 mkdir tmp_testfiles
-cp $TESTDIR/*.h5 tmp_testfiles/
+$CP $TESTDIR/*.h5 ./tmp_testfiles/
 
 $RUNSERIAL ./h5importtest
 
 ################################################
-###			  T H E   T E S T S
+###        T H E   T E S T S
 ################################################
 
 TESTING "ASCII I32 rank 3 - Output BE " ;
-TOOLTEST $TESTDIR/in32.txt -c $TESTDIR/textin32.conf -o test1.h5
+TOOLTEST $TESTDIR/txtin16.txt -c $TESTDIR/txtin32.conf -o txtin32.h5
 
 TESTING "ASCII I16 rank 3 - Output LE - CHUNKED - extended" 
-TOOLTEST $TESTDIR/in16.txt -c $TESTDIR/textin16.conf -o test2.h5
+TOOLTEST $TESTDIR/txtin16.txt -c $TESTDIR/txtin16.conf -o txtin16.h5
+
 
 TESTING "ASCII I8 - rank 3 - Output I8 LE-Chunked+Extended+Compressed " 
-TOOLTEST $TESTDIR/in16.txt -c $TESTDIR/textin8.conf  -o test3.h5
+TOOLTEST $TESTDIR/txtin16.txt -c $TESTDIR/txtin8.conf  -o txtin8.h5
 
 TESTING "ASCII UI32 - rank 3 - Output BE" 
-TOOLTEST $TESTDIR/in1.txt -c $TESTDIR/textuin32.conf -o test4.h5
+TOOLTEST $TESTDIR/txtuin32.txt -c $TESTDIR/txtuin32.conf -o txtuin32.h5
 
 TESTING "ASCII UI16 - rank 2 - Output LE+Chunked+Compressed " 
-TOOLTEST $TESTDIR/in1.txt -c $TESTDIR/textuin16.conf -o test5.h5
+TOOLTEST $TESTDIR/txtuin32.txt -c $TESTDIR/txtuin16.conf -o txtuin16.h5
 
 TESTING "ASCII F32 - rank 3 - Output LE " 
-TOOLTEST $TESTDIR/fp1.txt -c $TESTDIR/textfp32.conf -o test6.h5
+TOOLTEST $TESTDIR/txtfp32.txt -c $TESTDIR/txtfp32.conf -o txtfp32.h5
+
 
 TESTING "ASCII F64 - rank 3 - Output BE + CHUNKED+Extended+Compressed " 
-TOOLTEST $TESTDIR/fp2.txt -c $TESTDIR/textfp64.conf -o test7.h5
+TOOLTEST $TESTDIR/txtfp64.txt -c $TESTDIR/txtfp64.conf -o txtfp64.h5
 
 TESTING "BINARY F64 - rank 3 - Output LE+CHUNKED+Extended+Compressed " 
-TOOLTEST binfp64.bin -c $TESTDIR/binfp64.conf -o test8.h5
+TOOLTEST binfp64.bin -c $TESTDIR/binfp64.conf -o binfp64.h5
+
 
 TESTING "BINARY I16 - rank 3 - Output order LE + CHUNKED + extended " 
-TOOLTEST binin16.bin -c $TESTDIR/binin16.conf -o test9.h5
+TOOLTEST binin16.bin -c $TESTDIR/binin16.conf -o binin16.h5
+
 
 TESTING "BINARY I8 - rank 3 - Output I16LE + Chunked+Extended+Compressed " 
-TOOLTEST binin8.bin -c $TESTDIR/binin8.conf  -o test10.h5
+TOOLTEST binin8.bin -c $TESTDIR/binin8.conf  -o binin8.h5
+
 
 TESTING "BINARY I32 - rank 3 - Output BE + CHUNKED " 
-TOOLTEST binin32.bin -c $TESTDIR/binin32.conf -o test11.h5
+TOOLTEST binin32.bin -c $TESTDIR/binin32.conf -o binin32.h5
+
 
 TESTING "BINARY UI16 - rank 3 - Output byte BE + CHUNKED " 
-TOOLTEST binuin16.bin -c $TESTDIR/binuin16.conf -o test12.h5
+TOOLTEST binuin16.bin -c $TESTDIR/binuin16.conf -o binuin16.h5
 
 TESTING "BINARY UI32 - rank 3 - Output LE + CHUNKED " 
-TOOLTEST binuin32.bin -c $TESTDIR/binuin32.conf -o test13.h5
+TOOLTEST binuin32.bin -c $TESTDIR/binuin32.conf -o binuin32.h5
+
 
 TESTING "STR" 
-TOOLTEST $TESTDIR/str.txt -c $TESTDIR/textstr.conf -o test14.h5
+TOOLTEST $TESTDIR/txtstr.txt -c $TESTDIR/txtstr.conf -o txtstr.h5
+
+TESTING "BINARY I8 CR LF EOF" 
+TOOLTEST binin8w.bin -c $TESTDIR/binin8w.conf -o binin8w.h5
 
 TESTING "ASCII F64 - rank 1 - INPUT-CLASS TEXTFPE " 
-TOOLTEST $TESTDIR/in64.txt -c $TESTDIR/textpfe.conf -o test15.h5
+TOOLTEST $TESTDIR/textpfe64.txt -c $TESTDIR/textpfe.conf -o textpfe.h5
+
+
 
 rm -f  txtin32.txt txtin16.txt *.bin *.h5
 rm -rf tmp_testfiles
 else
-	echo "** h5import or h5importtest not available ***"
-	nerrors="` expr $nerrors + 1 `";
+  echo "** h5import or h5importtest not available ***"
+  nerrors="` expr $nerrors + 1 `";
 fi
 
 #
