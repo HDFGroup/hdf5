be78781ac664bde9097380f9e400403f00412c0b
HDF Tester <hdftest@hdfgroup.org>
2011-07-31 09:01:13 -0500

    [svn-r21158] Snapshot version 1.9 release 86

--- windows/src/H5pubconf.h
+++ windows/src/H5pubconf.h
@@ -473,13 +473,13 @@
 #define H5_PACKAGE_NAME "HDF5"
 
 /* Define to the full name and version of this package. */
-#define H5_PACKAGE_STRING "HDF5 1.9.86"
+#define H5_PACKAGE_STRING "HDF5 1.9.87"
 
 /* Define to the one symbol short name of this package. */
 #define H5_PACKAGE_TARNAME "hdf5"
 
 /* Define to the version of this package. */
-#define H5_PACKAGE_VERSION "1.9.86"
+#define H5_PACKAGE_VERSION "1.9.87"
 
 /* Width for printf() for type `long long' or `__int64', use `ll' */
 #define H5_PRINTF_LL_WIDTH "I64"
@@ -636,7 +636,7 @@
 /* #undef H5_USING_MEMCHECKER */
 
 /* Version number of package */
-#define H5_VERSION "1.9.86"
+#define H5_VERSION "1.9.87"
 
 /* Define if vsnprintf() returns the correct value for formatted strings that
    don't fit into size allowed */
