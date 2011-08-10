ec76ca98c1a11b4943d39ff392d214c61eebc7cb
Allen Byrne <byrn@hdfgroup.org>
2011-08-10 16:14:21 -0500

    [svn-r21217] Change CTEST_NAME to HDF5 - the project name Implement InitOnceExecuteOnce - HDFFV-5929 Change command to generate output source files
    Tested: local linux, windows

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,15 +4,14 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
+SET (CTEST_PROJECT_NAME "HDF5")
 
 SET (CTEST_DROP_METHOD "http")
 IF (CDASH_LOCAL)
-  SET (CTEST_PROJECT_NAME "HDF5.1.8")
   SET (CTEST_NIGHTLY_START_TIME "20:00:00 CST")
   SET (CTEST_DROP_SITE "nei.hdfgroup.uiuc.edu")
   SET (CTEST_DROP_LOCATION "/cdash/submit.php?project=HDF5.1.8")
 ELSE (CDASH_LOCAL)
-  SET (CTEST_PROJECT_NAME "HDF518")
   SET (CTEST_NIGHTLY_START_TIME "00:00:00 EST")
   SET (CTEST_DROP_SITE "cdash.hdfgroup.uiuc.edu")
   SET (CTEST_DROP_LOCATION "/submit.php?project=HDF518")
