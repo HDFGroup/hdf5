38aa51077a46d9ce9fce2aa342f25b176e57078c
John Mainzer <mainzer@hdfgroup.org>
2011-09-14 17:43:06 -0500

    [svn-r21394] Updated tuning of fraction of tests skipped in t_shapesame.c as a function of express test level to reflect results of testing with Ray.
    Tested parallel on jam -- no further testing as the change was very minor.

--- testpar/t_shapesame.c
+++ testpar/t_shapesame.c
@@ -2251,7 +2251,7 @@ contig_hs_dr_pio_test(ShapeSameTestMethods sstest_type)
      * test level.  Note that any value in excess of 4880 will cause all
      * sub tests to be skipped.
      */
-    int         max_skips_tbl[4] = {0, 4, 32, 256};
+    int         max_skips_tbl[4] = {0, 4, 64, 1024};
     hid_t	dset_type = H5T_NATIVE_UINT;
     int64_t	total_tests = 0;
     int64_t	tests_run = 0;
@@ -4534,7 +4534,7 @@ ckrbrd_hs_dr_pio_test(ShapeSameTestMethods sstest_type)
      * test level.  Note that any value in excess of 4880 will cause all
      * sub tests to be skipped.
      */
-    int         max_skips_tbl[4] = {0, 4, 32, 256};
+    int         max_skips_tbl[4] = {0, 4, 64, 1024};
     int64_t     total_tests = 0;
     int64_t     tests_run = 0;
     int64_t     tests_skipped = 0;
