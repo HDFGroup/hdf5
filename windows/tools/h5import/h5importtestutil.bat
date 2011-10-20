fd619754ac6c01025528e878497aae032b9a5b78
Allen Byrne <byrn@hdfgroup.org>
2011-10-20 15:17:06 -0500

    [svn-r21627] update legacy test script with new names of files

--- windows/tools/h5import/h5importtestutil.bat
+++ windows/tools/h5import/h5importtestutil.bat
@@ -104,49 +104,52 @@ goto main
     rem On Windows, echo gives a carriage return, so we store the TESTING params
     rem and call TESTING from TOOLTEST.  --SJW 8/27/07
     set testing=ASCII I32 rank 3 - Output BE 
-    call :tooltest %srcdir%\testfiles\in32.txt -c %srcdir%\testfiles\textin32.conf -o test1.h5
+    call :tooltest %srcdir%\testfiles\in32.txt -c %srcdir%\testfiles\txtin32.conf -o txtin32.h5
 
     set testing=ASCII I16 rank 3 - Output LE - CHUNKED - extended
-    call :tooltest %srcdir%\testfiles\in16.txt -c %srcdir%\testfiles\textin16.conf -o test2.h5
+    call :tooltest %srcdir%\testfiles\in16.txt -c %srcdir%\testfiles\txtin16.conf -o txtin16.h5
 
     set testing=ASCII I8 - rank 3 - Output I16 LE-Chunked+Extended+Compressed
-    call :tooltest %srcdir%\testfiles\in16.txt -c %srcdir%\testfiles\textin8.conf  -o test3.h5
+    call :tooltest %srcdir%\testfiles\in16.txt -c %srcdir%\testfiles\txtin8.conf  -o txtin8.h5
 
     set testing=ASCII UI32 - rank 3 - Output BE
-    call :tooltest %srcdir%\testfiles\in1.txt -c %srcdir%\testfiles\textuin32.conf -o test4.h5
+    call :tooltest %srcdir%\testfiles\in1.txt -c %srcdir%\testfiles\txtuin32.conf -o txtuin32.h5
 
     set testing=ASCII UI16 - rank 2 - Output LE+Chunked+Compressed
-    call :tooltest %srcdir%\testfiles\in1.txt -c %srcdir%\testfiles\textuin16.conf -o test5.h5
+    call :tooltest %srcdir%\testfiles\in1.txt -c %srcdir%\testfiles\txtuin16.conf -o txtuin16.h5
 
     set testing=ASCII F32 - rank 3 - Output LE
-    call :tooltest %srcdir%\testfiles\fp1.txt -c %srcdir%\testfiles\textfp32.conf -o test6.h5
+    call :tooltest %srcdir%\testfiles\fp1.txt -c %srcdir%\testfiles\txtfp32.conf -o txtfp32.h5
 
     set testing=ASCII F64 - rank 3 - Output BE + CHUNKED+Extended+Compressed
-    call :tooltest %srcdir%\testfiles\fp2.txt -c %srcdir%\testfiles\textfp64.conf -o test7.h5
+    call :tooltest %srcdir%\testfiles\fp2.txt -c %srcdir%\testfiles\txtfp64.conf -o txtfp64.h5
 
     set testing=BINARY F64 - rank 3 - Output LE+CHUNKED+Extended+Compressed
-    call :tooltest binfp64.bin -c %srcdir%\testfiles\binfp64.conf -o test8.h5
+    call :tooltest binfp64.bin -c %srcdir%\testfiles\binfp64.conf -o binfp64.h5
 
     set testing=BINARY I16 - rank 3 - Output order LE + CHUNKED + extended
-    call :tooltest binin16.bin -c %srcdir%\testfiles\binin16.conf -o test9.h5
+    call :tooltest binin16.bin -c %srcdir%\testfiles\binin16.conf -o binin16.h5
 
     set testing=BINARY I8 - rank 3 - Output I16LE + Chunked+Extended+Compressed
-    call :tooltest binin8.bin -c %srcdir%\testfiles\binin8.conf  -o test10.h5
+    call :tooltest binin8.bin -c %srcdir%\testfiles\binin8.conf  -o binin8.h5
 
     set testing=BINARY I32 - rank 3 - Output BE + CHUNKED
-    call :tooltest binin32.bin -c %srcdir%\testfiles\binin32.conf -o test11.h5
+    call :tooltest binin32.bin -c %srcdir%\testfiles\binin32.conf -o binin32.h5
 
     set testing=BINARY UI16 - rank 3 - Output byte BE + CHUNKED
-    call :tooltest binuin16.bin -c %srcdir%\testfiles\binuin16.conf -o test12.h5
+    call :tooltest binuin16.bin -c %srcdir%\testfiles\binuin16.conf -o binuin16.h5
 
     set testing=BINARY UI32 - rank 3 - Output LE + CHUNKED
-    call :tooltest binuin32.bin -c %srcdir%\testfiles\binuin32.conf -o test13.h5
+    call :tooltest binuin32.bin -c %srcdir%\testfiles\binuin32.conf -o binuin32.h5
 
     set testing=STR 
-    call :tooltest %srcdir%\testfiles\str.txt -c %srcdir%\testfiles\textstr.conf -o test14.h5
+    call :tooltest %srcdir%\testfiles\str.txt -c %srcdir%\testfiles\txtstr.conf -o txtstr.h5
+
+    set testing=BINARY I8 CR LF EOF"
+    call :tooltest binin8w.bin -c %srcdir%\testfiles\binin8w.conf -o binin8w.h5
     
     set testing=ASCII F64 - rank 1 - INPUT-CLASS TEXTFPE 
-    call :tooltest %srcdir%\testfiles\in64.txt -c %srcdir%\testfiles\textpfe.conf -o test15.h5
+    call :tooltest %srcdir%\testfiles\in64.txt -c %srcdir%\testfiles\textpfe.conf -o textpfe.h5
 
 
     del /f *.txt *.bin *.h5
