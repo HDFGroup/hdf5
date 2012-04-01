982f1ff8ff1687cc62e310c991c0d56929b271fd
Quincey Koziol <koziol@hdfgroup.org>
2012-04-01 17:32:04 -0500

    [svn-r22229] Description: 	Add 'H5O_mcdt_search_cb_t' to bin/trace script and re-run the bin/reconfigure script.
    Tested on:
            FreeBSD/32 8.2 (loyalty) w/gcc4.6, w/C++ & FORTRAN, in debug mode
            FreeBSD/64 8.2 (freedom) w/gcc4.6, w/C++ & FORTRAN, in debug mode
            Linux/32 2.6 (jam) w/PGI compilers, w/default API=1.8.x,
                    w/C++ & FORTRAN, w/threadsafe, in debug mode
            Linux/64-amd64 2.6 (koala) w/Intel compilers, w/default API=1.6.x,
                    w/C++ & FORTRAN, in production mode

--- src/H5Pocpypl.c
+++ src/H5Pocpypl.c
@@ -469,6 +469,7 @@ H5Padd_merge_committed_dtype_path(hid_t plist_id, const char *path)
     herr_t ret_value = SUCCEED; /* Return value */
 
     FUNC_ENTER_API(FAIL)
+    H5TRACE2("e", "i*s", plist_id, path);
 
     /* Check parameters */
     if(!path)
@@ -528,6 +529,7 @@ H5Pfree_merge_committed_dtype_paths(hid_t plist_id)
     herr_t ret_value = SUCCEED; /* Return value */
 
     FUNC_ENTER_API(FAIL)
+    H5TRACE1("e", "i", plist_id);
 
     /* Get the plist structure */
     if(NULL == (plist = H5P_object_verify(plist_id, H5P_OBJECT_COPY)))
@@ -575,6 +577,7 @@ H5Pset_mcdt_search_cb(hid_t plist_id, H5O_mcdt_search_cb_t func, void *op_data)
     herr_t ret_value = SUCCEED; 	/* Return value */
 
     FUNC_ENTER_API(FAIL)
+    H5TRACE3("e", "ix*x", plist_id, func, op_data);
 
     /* Check if the callback function is NULL and the user data is non-NULL.
      * This is almost certainly an error as the user data will not be used. */
@@ -623,6 +626,7 @@ H5Pget_mcdt_search_cb(hid_t plist_id, H5O_mcdt_search_cb_t *func, void **op_data
     herr_t ret_value = SUCCEED; /* Return value */
 
     FUNC_ENTER_API(FAIL)
+    H5TRACE3("e", "i*x**x", plist_id, func, op_data);
 
     /* Get the plist structure */
     if(NULL == (plist = H5P_object_verify(plist_id, H5P_OBJECT_COPY)))
