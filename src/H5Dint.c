dee21e9e82cecc5a15d62e3fec02f3c46eb8231b
Vailin Choi <vchoi@hdfgroup.org>
2012-04-05 14:14:38 -0500

    [svn-r22252] Fixed a bug in H5Ocopy(): When copying an opened object, call the object's flush class action to ensure that cached data is flushed so that H5Ocopy will get the correct data.  (HDFFV-7853) this line, and those below, will be ignored--
    _M   .
    M    test/objcopy.c
    M    src/H5Ocopy.c
    M    src/H5Goh.c
    M    src/H5Dint.c
    M    src/H5Opkg.h
    M    src/H5Doh.c
    M    src/H5Toh.c
    M    src/H5O.c
    M    src/H5Dpkg.h
    M    release_docs/RELEASE.txt
    _M   fortran

--- src/H5Dint.c
+++ src/H5Dint.c
@@ -68,7 +68,6 @@ static herr_t H5D_init_space(H5F_t *file, const H5D_t *dset, const H5S_t *space)
 static herr_t H5D_update_oh_info(H5F_t *file, hid_t dxpl_id, H5D_t *dset,
     hid_t dapl_id);
 static herr_t H5D_open_oid(H5D_t *dataset, hid_t dapl_id, hid_t dxpl_id);
-static herr_t H5D_flush_real(H5D_t *dataset, hid_t dxpl_id);
 
 
 /*********************/
