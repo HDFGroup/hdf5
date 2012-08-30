60daa9be2b660c4583f581c85ea2177b060c4fcb
Raymond Lu <songyulu@hdfgroup.org>
2012-08-30 11:00:31 -0500

    [svn-r22728] I forgot to include H5MMprivate.h header file for using H5MM_free function.
    Tested on koala.

--- src/H5Dio.c
+++ src/H5Dio.c
@@ -28,6 +28,7 @@
 #include "H5Eprivate.h"		/* Error handling		  	*/
 #include "H5FLprivate.h"	/* Free Lists                           */
 #include "H5Iprivate.h"		/* IDs			  		*/
+#include "H5MMprivate.h"        /* Memory management                    */
 #include "H5Sprivate.h"		/* Dataspace			  	*/
 
 #ifdef H5_HAVE_PARALLEL
