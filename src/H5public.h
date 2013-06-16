bdbd06d85d6f1ea3b088ac3f180c23e1389aefaa
HDF Tester <hdftest@hdfgroup.org>
2013-06-16 03:34:51 -0500

    [svn-r23778] Snapshot version 1.9 release 154

--- src/H5public.h
+++ src/H5public.h
@@ -75,10 +75,10 @@ extern "C" {
 /* Version numbers */
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	9	/* For minor interface/format changes  	     */
-#define H5_VERS_RELEASE	154	/* For tweaks, bug-fixes, or development     */
+#define H5_VERS_RELEASE	155	/* For tweaks, bug-fixes, or development     */
 #define H5_VERS_SUBRELEASE ""	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.9.154"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.9.155"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
