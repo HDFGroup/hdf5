b3b29e4485dbdf4eef82f71da8f5b6a5e2509b8e
Allen Byrne <byrn@hdfgroup.org>
2013-09-27 09:01:48 -0500

    [svn-r24206] Fix warning message from missing if in configure

--- config/cmake/ConfigureChecks.cmake
+++ config/cmake/ConfigureChecks.cmake
@@ -565,7 +565,9 @@ IF (NOT APPLE)
   IF (NOT H5_SIZEOF_SSIZE_T)
     SET (H5_SIZEOF_SSIZE_T 0)
   ENDIF (NOT H5_SIZEOF_SSIZE_T)
-  H5_CHECK_TYPE_SIZE (ptrdiff_t    H5_SIZEOF_PTRDIFF_T)
+  IF (NOT WINDOWS)
+    H5_CHECK_TYPE_SIZE (ptrdiff_t    H5_SIZEOF_PTRDIFF_T)
+  ENDIF (NOT WINDOWS)
 ENDIF (NOT APPLE)
 
 H5_CHECK_TYPE_SIZE (off_t          H5_SIZEOF_OFF_T)
@@ -1069,7 +1071,11 @@ H5ConversionTests (H5_ULONG_TO_FLOAT_ACCURATE "Checking IF accurately converting
 # 64-bit machines, where the short program below tests if round-up is
 # correctly handled.
 #
-H5ConversionTests (H5_ULONG_TO_FP_BOTTOM_BIT_ACCURATE "Checking IF accurately converting unsigned long long to floating-point values")
+IF (CMAKE_SYSTEM MATCHES "solaris2.*")
+  H5ConversionTests (H5_ULONG_TO_FP_BOTTOM_BIT_ACCURATE "Checking IF accurately converting unsigned long long to floating-point values")
+ELSE (CMAKE_SYSTEM MATCHES "solaris2.*")
+  SET(H5_ULONG_TO_FP_BOTTOM_BIT_ACCURATE 1)
+ENDIF (CMAKE_SYSTEM MATCHES "solaris2.*")
 # ----------------------------------------------------------------------
 # Set the flag to indicate that the machine can accurately convert
 # 'float' or 'double' to 'unsigned long long' values.
