b22ff7fa4a176d458c6d297f9d9dc43605d22313
Elena Pourmal <epourmal@hdfgroup.org>
2014-04-14 07:24:55 -0500

    [svn-r25038] Maintenance: Changed version in 1_8_13 to 1.8.13-pre1

--- src/H5public.h
+++ src/H5public.h
@@ -94,10 +94,10 @@ extern "C" {
 /* Version numbers */
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	8	/* For minor interface/format changes  	     */
-#define H5_VERS_RELEASE	14	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "snap0"	/* For pre-releases like snap0       */
+#define H5_VERS_RELEASE	13	/* For tweaks, bug-fixes, or development     */
+#define H5_VERS_SUBRELEASE "pre1"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.8.14-snap0"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.8.13-pre1"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
