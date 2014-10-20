f45faa4612c12e2db390550040b0d435bd6af350
Albert Cheng <acheng@hdfgroup.org>
2014-10-19 21:42:36 -0500

    [svn-r25722] The return type of H5A_create was changed from hid_t to H5A_t * but the test was not changed (still doing H5A_create(...) < 0). Fixed the error by changing to compare against NULL.
    Tested: ADA AIX machine where the old code was flagged as an error by the AIX compiler.

--- src/H5Adeprec.c
+++ src/H5Adeprec.c
@@ -183,10 +183,10 @@ H5Acreate1(hid_t loc_id, const char *name, hid_t type_id, hid_t space_id,
     if(NULL == (type = (H5T_t *)H5I_object_verify(type_id, H5I_DATATYPE)))
 	HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a type")
     if(NULL == (space = (H5S_t *)H5I_object_verify(space_id, H5I_DATASPACE)))
-	HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a data space")
+	HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a dataspace")
 
     /* Go do the real work for attaching the attribute to the dataset */
-    if((attr = H5A_create(&loc, name, type, space, plist_id, H5AC_dxpl_id)) < 0)
+    if(NULL==(attr = H5A_create(&loc, name, type, space, plist_id, H5AC_dxpl_id)))
 	HGOTO_ERROR(H5E_ATTR, H5E_CANTINIT, FAIL, "unable to create attribute")
 
     /* Register the new attribute and get an ID for it */
