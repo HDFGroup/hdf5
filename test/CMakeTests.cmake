fc93c6fe70ea7db9bb6518729b05eabf7d85868a
Allen Byrne <byrn@hdfgroup.org>
2015-02-02 12:01:15 -0500

    [svn-r26106] Disable flush1/flush2 test under debug VFD on Windows.

--- test/CMakeTests.cmake
+++ test/CMakeTests.cmake
@@ -605,23 +605,88 @@ if (HDF5_TEST_VFD)
     set (H5_VFD_TESTS ${H5_VFD_TESTS} big)
   endif (NOT CYGWIN)
 
-  MACRO (ADD_VFD_TEST vfdname resultcode)
-    foreach (test ${H5_VFD_TESTS})
+  MACRO (CHECK_VFD_TEST vfdtest vfdname resultcode)
+    if (${vfdtest} STREQUAL "flush1" OR ${vfdtest} STREQUAL "flush2")
+      if (${vfdname} STREQUAL "multi" OR ${vfdname} STREQUAL "split")
+        if (NOT BUILD_SHARED_LIBS AND NOT CMAKE_BUILD_TYPE MATCHES Debug)
+          add_test (
+              NAME VFD-${vfdname}-${vfdtest} 
+              COMMAND "${CMAKE_COMMAND}"
+                  -D "TEST_PROGRAM=$<TARGET_FILE:${vfdtest}>"
+                  -D "TEST_ARGS:STRING="
+                  -D "TEST_VFD:STRING=${vfdname}"
+                  -D "TEST_EXPECT=${resultcode}"
+                  -D "TEST_OUTPUT=${vfdtest}"
+                  -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
+                  -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
+          )
+          set_tests_properties (VFD-${vfdname}-${vfdtest} PROPERTIES
+              ENVIRONMENT "srcdir=${HDF5_TEST_BINARY_DIR}/${vfdname}"
+              WORKING_DIRECTORY ${HDF5_TEST_BINARY_DIR}/${vfdname}
+          )
+        else (NOT BUILD_SHARED_LIBS AND NOT CMAKE_BUILD_TYPE MATCHES Debug)
+          add_test (
+              NAME VFD-${vfdname}-${vfdtest}
+              COMMAND ${CMAKE_COMMAND} -E echo "SKIP VFD-${vfdname}-${vfdtest}"
+          )
+        endif(NOT BUILD_SHARED_LIBS AND NOT CMAKE_BUILD_TYPE MATCHES Debug)
+      else (${vfdname} STREQUAL "multi" OR ${vfdname} STREQUAL "split")
+        add_test (
+            NAME VFD-${vfdname}-${vfdtest} 
+            COMMAND "${CMAKE_COMMAND}"
+                -D "TEST_PROGRAM=$<TARGET_FILE:${vfdtest}>"
+                -D "TEST_ARGS:STRING="
+                -D "TEST_VFD:STRING=${vfdname}"
+                -D "TEST_EXPECT=${resultcode}"
+                -D "TEST_OUTPUT=${vfdtest}"
+                -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
+                -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
+        )
+        set_tests_properties (VFD-${vfdname}-${vfdtest} PROPERTIES
+            ENVIRONMENT "srcdir=${HDF5_TEST_BINARY_DIR}/${vfdname}"
+            WORKING_DIRECTORY ${HDF5_TEST_BINARY_DIR}/${vfdname}
+        )
+      endif (${vfdname} STREQUAL "multi" OR ${vfdname} STREQUAL "split")
+    else (${vfdtest} STREQUAL "flush1" OR ${vfdtest} STREQUAL "flush2")
       add_test (
-        NAME VFD-${vfdname}-${test} 
-        COMMAND "${CMAKE_COMMAND}"
-            -D "TEST_PROGRAM=$<TARGET_FILE:${test}>"
-            -D "TEST_ARGS:STRING="
-            -D "TEST_VFD:STRING=${vfdname}"
-            -D "TEST_EXPECT=${resultcode}"
-            -D "TEST_OUTPUT=${vfdname}-${test}"
-            -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
-            -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
+          NAME VFD-${vfdname}-${vfdtest} 
+          COMMAND "${CMAKE_COMMAND}"
+              -D "TEST_PROGRAM=$<TARGET_FILE:${vfdtest}>"
+              -D "TEST_ARGS:STRING="
+              -D "TEST_VFD:STRING=${vfdname}"
+              -D "TEST_EXPECT=${resultcode}"
+              -D "TEST_OUTPUT=${vfdtest}"
+              -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
+              -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
       )
-      set_tests_properties (VFD-${vfdname}-${test} PROPERTIES
+      set_tests_properties (VFD-${vfdname}-${vfdtest} PROPERTIES
           ENVIRONMENT "srcdir=${HDF5_TEST_BINARY_DIR}/${vfdname}"
           WORKING_DIRECTORY ${HDF5_TEST_BINARY_DIR}/${vfdname}
       )
+    endif (${vfdtest} STREQUAL "flush1" OR ${vfdtest} STREQUAL "flush2")
+  ENDMACRO (CHECK_VFD_TEST vfdtest vfdname resultcode)
+
+  MACRO (ADD_VFD_TEST vfdname resultcode)
+    foreach (test ${H5_VFD_TESTS})
+      if (WIN32)
+        CHECK_VFD_TEST (${test} ${vfdname} ${resultcode})
+      else (WIN32)
+        add_test (
+          NAME VFD-${vfdname}-${test} 
+          COMMAND "${CMAKE_COMMAND}"
+              -D "TEST_PROGRAM=$<TARGET_FILE:${test}>"
+              -D "TEST_ARGS:STRING="
+              -D "TEST_VFD:STRING=${vfdname}"
+              -D "TEST_EXPECT=${resultcode}"
+              -D "TEST_OUTPUT=${vfdname}-${test}"
+              -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
+              -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
+        )
+        set_tests_properties (VFD-${vfdname}-${test} PROPERTIES
+            ENVIRONMENT "srcdir=${HDF5_TEST_BINARY_DIR}/${vfdname}"
+            WORKING_DIRECTORY ${HDF5_TEST_BINARY_DIR}/${vfdname}
+        )
+      endif (WIN32)
     endforeach (test ${H5_VFD_TESTS})
     set_tests_properties (VFD-${vfdname}-flush2 PROPERTIES DEPENDS VFD-${vfdname}-flush1)
     set_tests_properties (VFD-${vfdname}-flush1 PROPERTIES TIMEOUT 10)
