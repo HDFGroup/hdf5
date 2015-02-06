eea4f451722a3801cc62d0cfa972b2b564b234ca
Dana Robinson <derobins@hdfgroup.org>
2015-02-05 20:02:30 -0500

    [svn-r26135] Updated autogen.sh to always run API-oriented scripts. Removed the -s option from autogen.sh. Removed several generated H5E files from version control as well as H5version.h since these will always be regenerated.
    Part of: HDFFV-9120
    
    Tested on: Local Ubuntu VM

--- autogen.sh
+++ autogen.sh
@@ -30,21 +30,17 @@
 # Note that aclocal will attempt to include libtool's share/aclocal
 # directory.
 #
-# This script takes two options:
+# This script takes one potential option:
 #
 # -p, --production
 #
 # When this is selected, the autotools versions are set to the paths
 # and versions used by The HDF Group to produce the released versions
-# of the library. # -s/--process_source
+# of the library.
 #
-#
-# -s, --process_source
-#
-# When this is selected, several scripts are run that process the HDF5
-# source code to add things like trace and error macros. It is not
-# necessary to run these if you have just updated configure.ac or
-# Makefile.am files.
+# NOTE: This is probably temporary. Once we update our dev machines
+# to have recent versions of the autotools this option will probably
+# be removed.
 
 echo
 echo "**************************"
@@ -52,11 +48,10 @@ echo "* HDF5 autogen.sh script *"
 echo "**************************"
 echo
 
-# Defaults are not production, don't run source processing.
+# Default is not production
 production=false
-process_source=false
 
-optspec=":hps-"
+optspec=":hp-"
 while getopts "$optspec" optchar; do
     case "${optchar}" in
     -)
@@ -66,9 +61,6 @@ while getopts "$optspec" optchar; do
                 echo
                 production=true
                 ;;
-            process_source)
-                process_source=true
-                ;;
             *)
                 if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
                     echo "Unknown option --${OPTARG}" >&2
@@ -76,14 +68,12 @@ while getopts "$optspec" optchar; do
                 ;;
         esac;;
     h)
-        echo "usage: $0 [-s|--process_source] [-p|--production]"
+        echo "usage: $0 [-p|--production]"
         echo
         echo "      -p      Used by THG to ensure that particular versions"
         echo "              of the autotools are used and hard-codes"
         echo "              autotools paths to THG machines. Not for"
         echo "              non-HDF-Group users!"
-        echo
-        echo "      -s      Run source/API processsing scripts."
         echo
         echo "  NOTE: Each autotool can be set via an environment variable."
         echo "        These are documented inside this autogen.sh script."
@@ -93,9 +83,6 @@ while getopts "$optspec" optchar; do
     p)
         echo "Setting production mode..."
         production=true
-        ;;
-    s)
-        process_source=true
         ;;
     *)
         if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
@@ -229,6 +216,7 @@ fi
 # in Makefile.am. You can ignore this suggestion.
 
 echo ${HDF5_LIBTOOLIZE}
+${HDF5_LIBTOOLIZE} --version 
 ${HDF5_LIBTOOLIZE} || exit 1
 echo
 echo "NOTE: You can ignore the warning about adding -I m4."
@@ -239,54 +227,54 @@ if test -e "${LIBTOOL_DIR}/../share/aclocal" ; then
     aclocal_include="-I ${LIBTOOL_DIR}/../share/aclocal"
 fi
 echo ${HDF5_ACLOCAL} ${aclocal_include}
+${HDF5_ACLOCAL} --version
 ${HDF5_ACLOCAL} ${aclocal_include} || exit 1
 echo
 
 echo ${HDF5_AUTOHEADER}
+${HDF5_AUTOHEADER} --version
 ${HDF5_AUTOHEADER} || exit 1
 echo
 
 echo ${HDF5_AUTOMAKE} --add-missing
+${HDF5_AUTOMAKE} --version
 ${HDF5_AUTOMAKE} --add-missing || exit 1
 echo
 
 echo ${HDF5_AUTOCONF}
+${HDF5_AUTOCONF} --version
 ${HDF5_AUTOCONF} || exit 1
 echo
 
-# If source processing was eanbled using -s/--process_source, run the
-# source processing scripts.
-if [ "$process_source" = true ] ; then
+# Run scripts that process source.
 
-    # Run trace script
-    # The trace script adds H5TRACE macros to library source files.  It should
-    # have no effect on files that don't have HDF5 API macros in them.
-    echo
-    echo "Running trace script:"
-    echo "NOTE: NO TRACE warnings in H5E code are normal and expected."
-    bin/trace src/H5*.c || exit 1
+# Run trace script
+# The trace script adds H5TRACE macros to library source files.  It should
+# have no effect on files that don't have HDF5 API macros in them.
+echo
+echo "Running trace script:"
+echo "NOTE: NO TRACE warnings in H5E code are normal and expected."
+bin/trace src/H5*.c || exit 1
 
-    # Run make_err
-    # make_err automatically generates the H5E headers that create error message
-    # types for HDF5.
-    echo
-    echo "Running error generation script:"
-    bin/make_err src/H5err.txt || exit 1
+# Run make_err
+# make_err automatically generates the H5E headers that create error message
+# types for HDF5.
+echo
+echo "Running error generation script:"
+bin/make_err src/H5err.txt || exit 1
 
-    # Run make_vers
-    # make_vers automatically generates the public headers that define the API version
-    # macros for HDF5.
-    echo
-    echo "Running API version generation script:"
-    bin/make_vers src/H5vers.txt || exit 1
+# Run make_vers
+# make_vers automatically generates the public headers that define the API version
+# macros for HDF5.
+echo
+echo "Running API version generation script:"
+bin/make_vers src/H5vers.txt || exit 1
 
-    # Run flex
-    # automatically generates the lexical file for hl/src/H5LTanalyze.c
-    echo
-    echo "Running flex generation script:"
-    bin/genltanalyze || exit 1
-
-fi # process_source
+# Run flex
+# automatically generates the lexical file for hl/src/H5LTanalyze.c
+echo
+echo "Running flex generation script:"
+bin/genltanalyze || exit 1
 
 exit 0
 
