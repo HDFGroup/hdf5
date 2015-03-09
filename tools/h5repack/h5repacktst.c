6b1176f34e780cd597f32991ae3952ea371c66fe
Dana Robinson <derobins@hdfgroup.org>
2015-03-09 14:53:48 -0500

    [svn-r26407] Missed an H5_HAVE_FILTER_SHUFFLE line.
    Part of: HDFFV-9086
    
    Tested on: jam (minor change)

--- tools/h5repack/h5repacktst.c
+++ tools/h5repack/h5repacktst.c
@@ -1354,7 +1354,7 @@ int main (void)
 
     TESTING("    several global filters");
 
-#if defined (H5_HAVE_FILTER_DEFLATE) && defined (H5_HAVE_FILTER_SHUFFLE)
+#if defined (H5_HAVE_FILTER_DEFLATE)
 
     if (h5repack_init (&pack_options, 0) < 0)
         GOERROR;
