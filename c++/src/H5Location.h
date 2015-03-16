96c424d8bc5f47f12826a86668702b857b70fb3c
Binh-Minh Ribler <bmribler@hdfgroup.org>
2015-03-16 18:23:16 -0500

    [svn-r26462] Description:     Fixed typo that caused failure on Windows. Platforms tested:     Linux/32 2.6 (jam)

--- c++/src/H5Location.h
+++ c++/src/H5Location.h
@@ -151,7 +151,7 @@ class H5_DLLCPP H5Location : public IdComponent {
 	H5Location(const hid_t loc_id);
 
 	// Copy constructor.
-	H5Location(const H5Location& original);
+	// H5Location(const H5Location& original);
 
 	// Creates a reference to an HDF5 object or a dataset region.
 	void p_reference(void* ref, const char* name, hid_t space_id, H5R_type_t ref_type) const;
