c923afdf50173d8fd80d252375eb01f3eb8fdf8e
Allen Byrne <byrn@hdfgroup.org>
2015-03-30 12:32:55 -0500

    [svn-r26654] Bring trunk CMake changes to branch.
    Tested: local linux

--- test/flush2.c
+++ test/flush2.c
@@ -187,7 +187,7 @@ main(void)
         PASSED()
     else
     {
-#if defined H5_HAVE_WIN32_API && defined _HDF5USEDLL_
+#if defined H5_HAVE_WIN32_API && !defined (hdf5_EXPORTS)
     SKIPPED();
     puts("   DLL will flush the file even when calling _exit, skip this test temporarily");
 #elif defined H5_VMS
@@ -210,7 +210,7 @@ main(void)
         PASSED()
     else
     {
-#if defined H5_HAVE_WIN32_API && defined _HDF5USEDLL_
+#if defined H5_HAVE_WIN32_API && !defined (hdf5_EXPORTS)
     SKIPPED();
     puts("   DLL will flush the file even when calling _exit, skip this test temporarily");
 #elif defined H5_VMS
