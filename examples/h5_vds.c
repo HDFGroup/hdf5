fc0fba145e327423103039283db67fdeb6395727
Elena Pourmal <epourmal@hdfgroup.org>
2015-06-16 16:42:48 -0500

    [svn-r27220] Maintenance: Added copyrigth; removed read buffer initialization since fill value feature works now.
    Tested on jam.

--- examples/h5_vds.c
+++ examples/h5_vds.c
@@ -1,3 +1,18 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the files COPYING and Copyright.html.  COPYING can be found at the root   *
+ * of the source code distribution tree; Copyright.html can be found at the  *
+ * root level of an installed copy of the electronic HDF5 document set and   *
+ * is linked from the top-level documents page.  It can also be found at     *
+ * http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+ * access to either file, you may request a copy from help@hdfgroup.org.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 /************************************************************
 
   This example illustrates the concept of virtual dataset.
@@ -222,11 +237,6 @@ main (void)
         free(filename);
         free(dsetname);
         free(buf);
-    }
-    /* EIP Initialize read buffer with -2 since fille values for VDS are not implemented yet */
-    for (i=0; i<VDSDIM0; i++) {
-        for (j=0; j<VDSDIM1; j++)
-            rdata[i][j] = -2;
     }
 
     /*
