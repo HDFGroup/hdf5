e040db8331952bc417b1aba78f1e6809028532f4
Allen Byrne <byrn@hdfgroup.org>
2015-09-09 14:36:47 -0500

    [svn-r27725] Add initial commands to properly build the generated files. Added HDF5_GENERATE_HEADERS ON to cacheinit.cmake file.
    Tested: local linux

--- config/cmake/cacheinit.cmake
+++ config/cmake/cacheinit.cmake
@@ -8,6 +8,8 @@ set (CMAKE_INSTALL_FRAMEWORK_PREFIX "Library/Frameworks" CACHE STRING "Framework
 
 set (HDF_PACKAGE_EXT "" CACHE STRING "Name of HDF package extension" FORCE)
 
+set (HDF5_GENERATE_HEADERS ON CACHE BOOL "Rebuild Generated Files" FORCE)
+
 set (HDF5_BUILD_FORTRAN ON CACHE BOOL "Build FORTRAN support" FORCE)
 
 set (HDF5_BUILD_GENERATORS OFF CACHE BOOL "Build Test Generators" FORCE)
