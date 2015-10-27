84b1e9e137fc5fd395556bcaaa50eef09bc8723c
Dana Robinson <derobins@hdfgroup.org>
2015-10-27 10:24:56 -0500

    [svn-r28237] Merge of r28228-9 from trunk.
    accum test still has issues
    
    Tested on: Ubuntu 15.04 (Linux 3.19 x86_64) gcc 4.9.2
               serial autotools and CMake (3.3.2)

--- src/H5Omtime.c
+++ src/H5Omtime.c
@@ -186,12 +186,6 @@ H5O_mtime_decode(H5F_t H5_ATTR_UNUSED *f, hid_t H5_ATTR_UNUSED dxpl_id, H5O_t H5
     int	i;
     struct tm	tm;
     void        *ret_value = NULL;      /* Return value */
-#if _MSC_VER >= 1900  // VS 2015
-// In gcc and in Visual Studio prior to VS 2015 'timezone' is a global
-// variable declared in time.h. That variable was deprecated and in VS 2015
-// is removed, with _get_timezone replacing it.
-    long timezone = 0;
-#endif
 
     FUNC_ENTER_NOAPI_NOINIT
 
@@ -232,11 +226,7 @@ H5O_mtime_decode(H5F_t H5_ATTR_UNUSED *f, hid_t H5_ATTR_UNUSED dxpl_id, H5O_t H5
     /* BSD-like systems */
     the_time += tm.tm_gmtoff;
 #elif defined(H5_HAVE_TIMEZONE)
-    #if _MSC_VER >= 1900  // VS 2015
-        HDget_timezone(&timezone);
-    #endif
-    /* GNU/Linux systems */
-    the_time -= timezone - (tm.tm_isdst ? 3600 : 0);
+    the_time -= HDget_timezone() - (tm.tm_isdst ? 3600 : 0);
 #else
     /*
      * The catch-all.  If we can't convert a character string universal
