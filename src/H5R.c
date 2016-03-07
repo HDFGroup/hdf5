b4aa4a88b1c2c4e4af54668e77847ec1648ac13e
Jerome Soumagne <jsoumagne@hdfgroup.org>
2016-03-07 16:35:43 -0600

    Some more fixes to H5R to separate int/ext refs

--- src/H5R.c
+++ src/H5R.c
@@ -1823,7 +1823,6 @@ done:
 ssize_t
 H5R__get_attr_name(H5F_t *f, href_t _ref, char *name, size_t size)
 {
-    H5O_loc_t oloc;             /* Object location describing object for reference */
     ssize_t ret_value = -1;     /* Return value */
     const uint8_t *p = NULL;    /* Pointer to reference to decode */
     size_t attr_name_len;       /* Length of the attribute name */
@@ -1833,13 +1832,8 @@ H5R__get_attr_name(H5F_t *f, href_t _ref, char *name, size_t size)
     FUNC_ENTER_NOAPI_NOINIT_NOERR
 
     /* Check args */
-    HDassert(f);
     HDassert(ref);
     HDassert((ref->ref_type == H5R_ATTR) || (ref->ref_type == H5R_EXT_ATTR));
-
-    /* Initialize the object location */
-    H5O_loc_reset(&oloc);
-    oloc.file = f;
 
     /* Point to reference buffer now */
     p = (const uint8_t *)ref->ref.serial.buf;
@@ -1861,6 +1855,14 @@ H5R__get_attr_name(H5F_t *f, href_t _ref, char *name, size_t size)
         /* Skip the path name */
         p += pathname_len;
     } else {
+        H5O_loc_t oloc; /* Object location describing object for reference */
+
+        HDassert(f);
+
+        /* Initialize the object location */
+        H5O_loc_reset(&oloc);
+        oloc.file = f;
+
         /* Get the object oid for the dataset */
         H5F_addr_decode(oloc.file, &p, &(oloc.addr));
     }
