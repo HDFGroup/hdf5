89fbe00dec8187305b518d91c3ddb7d910665f79
Larry Knox <lrknox@hdfgroup.org>
2017-04-17 14:32:16 -0500

    Merge pull request #426 in HDFFV/hdf5 from ~LRKNOX/hdf5_lrk:hdf5_1_10 to hdf5_1_10
    * commit '54957d37f5aa73912763dbb6e308555e863c43f4':
      Commit copyright header change for src/H5PLpkg.c which was added after running script to make changes.
      Add new files in release_docs to MANIFEST. Cimmit changes to Makefile.in(s) and H5PL.c that resulted from running autogen.sh.
      Merge pull request #407 in HDFFV/hdf5 from ~LRKNOX/hdf5_lrk:hdf5_1_10_1 to hdf5_1_10_1
      Change copyright headers to replace url referring to file to be removed and replace it with new url for COPYING file.

--- config/cmake/userblockTest.cmake
+++ config/cmake/userblockTest.cmake
@@ -1,3 +1,14 @@
+#
+# Copyright by The HDF Group.
+# All rights reserved.
+#
+# This file is part of HDF5.  The full HDF5 copyright notice, including
+# terms governing use, modification, and redistribution, is contained in
+# the COPYING file, which can be found at the root of the source code
+# distribution tree, or in https://support.hdfgroup.org/ftp/HDF5/releases.
+# If you do not have access to either file, you may request a copy from
+# help@hdfgroup.org.
+#
 # userblockTest.cmake executes a command and captures the output in a file. File is then compared
 # against a reference file. Exit status of command can also be compared.
 
