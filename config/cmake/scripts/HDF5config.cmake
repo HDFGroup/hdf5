20d67d61716d9003d0518c9e8f448cd960b3bc9a
HDF Tester <hdftest@jam.ad.hdfgroup.org>
2017-08-08 16:17:20 -0500

    Snapshot version 1.8 release 20 (snap1)

--- config/cmake/scripts/HDF5config.cmake
+++ config/cmake/scripts/HDF5config.cmake
@@ -38,7 +38,7 @@ cmake_minimum_required (VERSION 3.2.2 FATAL_ERROR)
 ##############################################################################
 
 set (CTEST_SOURCE_VERSION "1.8.20")
-set (CTEST_SOURCE_VERSEXT "-snap1")
+set (CTEST_SOURCE_VERSEXT "-snap2")
 
 ##############################################################################
 # handle input parameters to script.
