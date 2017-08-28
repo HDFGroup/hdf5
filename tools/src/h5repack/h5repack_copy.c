8e71bbec754ef11728dab6108b570ffbc7228407
Allen Byrne <byrn@hdfgroup.org>
2017-08-28 10:27:21 -0500

    HDFFV-10282 merge from develop

--- tools/src/h5repack/h5repack_copy.c
+++ tools/src/h5repack/h5repack_copy.c
@@ -782,7 +782,6 @@ int do_copy_objects(hid_t fidin, hid_t fidout, trav_table_t *travt,
 
         switch (travt->objs[i].type) {
             case H5TRAV_TYPE_UNKNOWN:
-                HDassert(0);
                 break;
 
                 /*-------------------------------------------------------------------------
@@ -1486,12 +1485,11 @@ static int copy_user_block(const char *infile, const char *outfile, hsize_t size
     int infid = -1, outfid = -1; /* File descriptors */
 
     /* User block must be any power of 2 equal to 512 or greater (512, 1024, 2048, etc.) */
-    HDassert(size > 0);
 
     /* Open files */
-    if ((infid = HDopen(infile, O_RDONLY, 0)) < 0)
+    if ((infid = HDopen(infile, O_RDONLY)) < 0)
         HGOTO_ERROR(FAIL, H5E_tools_min_id_g, "HDopen failed");
-    if ((outfid = HDopen(outfile, O_WRONLY, 0644)) < 0)
+    if ((outfid = HDopen(outfile, O_WRONLY)) < 0)
         HGOTO_ERROR(FAIL, H5E_tools_min_id_g, "HDopen failed");
 
     /* Copy the userblock from the input file to the output file */
@@ -1583,7 +1581,8 @@ void print_user_block(const char *filename, hid_t fid)
     }
 
     /* open file */
-    if((fh = HDopen(filename, O_RDONLY, 0)) < 0) {
+    if((fh = HDopen(filename, O_RDONLY)) < 0) {
+        error_msg("failed to open file\n");
         HGOTO_ERROR(H5E_tools_g, H5E_tools_min_id_g, "HDopen failed");
     }
 
