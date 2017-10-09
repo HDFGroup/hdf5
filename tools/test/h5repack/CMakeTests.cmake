bd6de98d175b8fee9e7ba6a647a380a7b4295e1b
Allen Byrne <byrn@hdfgroup.org>
2017-10-09 13:21:32 -0500

    Undo commit

--- tools/test/h5repack/CMakeTests.cmake
+++ tools/test/h5repack/CMakeTests.cmake
@@ -1377,7 +1377,11 @@
   ADD_H5_UD_TEST (plugin_test 0 h5repack_layout.h5 -v -f UD=257,0,1,9)
   ADD_H5_UD_TEST (plugin_none 0 h5repack_layout.UD.h5 -v -f NONE)
   # check for no parameters
-  ADD_H5_UD_TEST (plugin_zero 0 h5repack_layout.h5 -v -f UD=250,0,0)
+  set (TESTRETVAL 255)
+  if (WIN32)
+    set (TESTRETVAL -1)
+  endif ()
+  ADD_H5_UD_TEST (plugin_zero ${TESTRETVAL} h5repack_layout.h5 -v -f UD=250,0,0)
 
   if (HDF5_TEST_VFD)
     # Run test with different Virtual File Driver
