34d56c3cfe9b0319f3a048a51205988a60b10430
Dana Robinson <derobins@hdfgroup.org>
2017-11-16 13:09:01 -0800

    Fixed a missing H5I_VOL case that raised a warning.

--- src/H5Oint.c
+++ src/H5Oint.c
@@ -1617,6 +1617,7 @@ H5O_get_loc(hid_t object_id)
         case H5I_ATTR:
         case H5I_REFERENCE:
         case H5I_VFL:
+        case H5I_VOL:
         case H5I_GENPROP_CLS:
         case H5I_GENPROP_LST:
         case H5I_ERROR_CLASS:
