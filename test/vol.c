643dd58e474892cee0807c0b586081bcc08c744f
Dana Robinson <derobins@hdfgroup.org>
2017-12-14 01:06:52 -0800

    Wired up H5Lmove().

--- test/vol.c
+++ test/vol.c
@@ -29,6 +29,7 @@
 #define NATIVE_VOL_TEST_ATTRIBUTE_NAME  "test_dataset"
 #define NATIVE_VOL_TEST_HARD_LINK_NAME  "test_hard_link"
 #define NATIVE_VOL_TEST_SOFT_LINK_NAME  "test_soft_link"
+#define NATIVE_VOL_TEST_MOVE_LINK_NAME  "test_move_link"
 
 #define N_ELEMENTS  10
 
@@ -694,6 +695,10 @@ test_basic_link_operation(void)
     if (H5Lexists(fid, NATIVE_VOL_TEST_SOFT_LINK_NAME, H5P_DEFAULT) < 0)
         TEST_ERROR;
     if (H5Lexists(fid, NATIVE_VOL_TEST_HARD_LINK_NAME, H5P_DEFAULT) < 0)
+        TEST_ERROR;
+
+    /* H5Lmove */
+    if (H5Lmove(fid, NATIVE_VOL_TEST_HARD_LINK_NAME, fid, NATIVE_VOL_TEST_MOVE_LINK_NAME, H5P_DEFAULT, H5P_DEFAULT) < 0)
         TEST_ERROR;
 
     if (H5Fclose(fid) < 0)
