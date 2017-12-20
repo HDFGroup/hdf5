c925dd41d59830a7f792bb8d0b26546b95cdf6d0
Dana Robinson <derobins@hdfgroup.org>
2017-12-19 21:04:20 -0800

    Wired up H5Fmount/unmount.

--- src/H5Fmount.c
+++ src/H5Fmount.c
@@ -430,44 +430,55 @@ H5F_is_mount(const H5F_t *file)
 
 
 /*-------------------------------------------------------------------------
- * Function:	H5Fmount
+ * Function:    H5Fmount
  *
- * Purpose:	Mount file CHILD_ID onto the group specified by LOC_ID and
- *		NAME using mount properties PLIST_ID.
+ * Purpose:     Mount file CHILD_ID onto the group specified by LOC_ID and
+ *              NAME using mount properties PLIST_ID.
  *
- * Return:	Non-negative on success/Negative on failure
- *
- * Programmer:	Robb Matzke
- *              Tuesday, October  6, 1998
+ * Return:      SUCCEED/FAIL
  *
  *-------------------------------------------------------------------------
  */
 herr_t
 H5Fmount(hid_t loc_id, const char *name, hid_t child_id, hid_t plist_id)
 {
-    H5G_loc_t	loc;
-    H5F_t	*child = NULL;
-    herr_t      ret_value = SUCCEED;       /* Return value */
+    H5VL_object_t *obj;
+    H5VL_object_t *file;
+    H5I_type_t  type;
+    herr_t      ret_value = SUCCEED; /* Return value */
 
     FUNC_ENTER_API(FAIL)
     H5TRACE4("e", "i*sii", loc_id, name, child_id, plist_id);
 
     /* Check arguments */
-    if(H5G_loc(loc_id, &loc) < 0)
-	HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a location")
-    if(!name || !*name)
-	HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "no name")
-    if(NULL == (child = (H5F_t *)H5I_object_verify(child_id, H5I_FILE)))
-	HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a file")
-    if(H5P_DEFAULT == plist_id)
+    if (!name || !*name)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "no name")
+    if (H5P_DEFAULT == plist_id)
         plist_id = H5P_FILE_MOUNT_DEFAULT;
     else
-        if(TRUE != H5P_isa_class(plist_id, H5P_FILE_MOUNT))
+        if (TRUE != H5P_isa_class(plist_id, H5P_FILE_MOUNT))
             HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not property list")
 
-    /* Do the mount */
-    if(H5F_mount(&loc, name, child, plist_id, H5AC_ind_read_dxpl_id) < 0)
-	HGOTO_ERROR(H5E_FILE, H5E_MOUNT, FAIL, "unable to mount file")
+    type = H5I_get_type(loc_id);
+    if (H5I_FILE != type && H5I_GROUP != type)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a file or group object")
+
+    /* Get the group/file object */
+    if (NULL == (obj = (H5VL_object_t *)H5I_object(loc_id)))
+        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "invalid file identifier")
+
+    /* Get the file object */
+    if (NULL == (file = (H5VL_object_t *)H5I_object(child_id)))
+        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "invalid file identifier")
+
+    /* Check if both objects are associated with the same VOL plugin */
+    if (obj->vol_info->vol_cls->value != file->vol_info->vol_cls->value)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "Can't mount file onto object from different VOL plugin")
+
+    /* Perform the mount operation */
+    if (H5VL_file_specific(obj->vol_obj, obj->vol_info->vol_cls, H5VL_FILE_MOUNT, H5AC_ind_read_dxpl_id, 
+                          H5_REQUEST_NULL, type, name, file->vol_obj, plist_id) < 0)
+        HGOTO_ERROR(H5E_FILE, H5E_MOUNT, FAIL, "unable to mount file")
 
 done:
     FUNC_LEAVE_API(ret_value)
@@ -475,42 +486,46 @@ done:
 
 
 /*-------------------------------------------------------------------------
- * Function:	H5Funmount
+ * Function:    H5Funmount
  *
- * Purpose:	Given a mount point, dissassociate the mount point's file
- *		from the file mounted there.  Do not close either file.
+ * Purpose:     Given a mount point, dissassociate the mount point's file
+ *              from the file mounted there. Do not close either file.
  *
- *		The mount point can either be the group in the parent or the
- *		root group of the mounted file (both groups have the same
- *		name).  If the mount point was opened before the mount then
- *		it's the group in the parent, but if it was opened after the
- *		mount then it's the root group of the child.
+ *              The mount point can either be the group in the parent or the
+ *              root group of the mounted file (both groups have the same
+ *              name). If the mount point was opened before the mount then
+ *              it's the group in the parent, but if it was opened after the
+ *              mount then it's the root group of the child.
  *
- * Return:	Non-negative on success/Negative on failure
- *
- * Programmer:	Robb Matzke
- *              Tuesday, October  6, 1998
+ * Return:      SUCCEED/FAIL
  *
  *-------------------------------------------------------------------------
  */
 herr_t
 H5Funmount(hid_t loc_id, const char *name)
 {
-    H5G_loc_t	loc;
+    H5VL_object_t *obj;
+    H5I_type_t  type;
     herr_t      ret_value=SUCCEED;       /* Return value */
 
     FUNC_ENTER_API(FAIL)
     H5TRACE2("e", "i*s", loc_id, name);
 
     /* Check args */
-    if(H5G_loc(loc_id, &loc) < 0)
-	HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a location")
-    if(!name || !*name)
-	HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "no name")
+    if (!name || !*name)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "no name")
 
-    /* Unmount */
-    if (H5F_unmount(&loc, name, H5AC_ind_read_dxpl_id) < 0)
-	HGOTO_ERROR(H5E_FILE, H5E_MOUNT, FAIL, "unable to unmount file")
+    type = H5I_get_type(loc_id);
+    if (H5I_FILE != type && H5I_GROUP != type)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not a file or group object")
+
+    /* Get the group/file object */
+    if (NULL == (obj = (H5VL_object_t *)H5I_object(loc_id)))
+        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "invalid file identifier")
+
+    if (H5VL_file_specific(obj->vol_obj, obj->vol_info->vol_cls, H5VL_FILE_UNMOUNT, 
+                          H5AC_ind_read_dxpl_id, H5_REQUEST_NULL, type, name) < 0)
+        HGOTO_ERROR(H5E_FILE, H5E_MOUNT, FAIL, "unable to unmount file")
 
 done:
     FUNC_LEAVE_API(ret_value)
