f22d8726707e26b97df38962b404754119e8abd7
Allen Byrne <byrn@hdfgroup.org>
2018-01-22 11:09:58 -0600

    HDFFV-10385 some platforms need this var

--- config/cmake_ext_mod/HDFMacros.cmake
+++ config/cmake_ext_mod/HDFMacros.cmake
@@ -125,7 +125,7 @@ macro (HDF_SET_LIB_OPTIONS libtarget libname libtype)
   set_target_properties (${libtarget}
       PROPERTIES
          OUTPUT_NAME
-               ${LIB_RELEASE_NAME}$<$<CONFIG:Debug>:${LIB_DEBUG_SUFFIX}>
+               ${LIB_RELEASE_NAME}
          OUTPUT_NAME_DEBUG
                ${LIB_DEBUG_NAME}
          OUTPUT_NAME_RELEASE
