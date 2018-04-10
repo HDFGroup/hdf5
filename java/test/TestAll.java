77243b8b540fb7000d706b4c67fff4fb25a7e3a2
Allen Byrne <byrn@hdfgroup.org>
2018-04-10 10:27:03 -0500

    Reorder of tests for debug APIs option

--- java/test/TestAll.java
+++ java/test/TestAll.java
@@ -18,9 +18,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { TestH5.class,
-        TestH5Eregister.class,
-        TestH5Edefault.class,
-        TestH5E.class,
+        TestH5Eparams.class, TestH5Eregister.class, TestH5Edefault.class, TestH5E.class,
         TestH5Fparams.class, TestH5Fbasic.class, TestH5F.class, TestH5Fswmr.class,
         TestH5Gbasic.class, TestH5G.class, TestH5Giterate.class,
         TestH5Sbasic.class, TestH5S.class,
