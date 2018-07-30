929675899fcfb9c2cdd6f8f4644a0524fa1579bb
Binh-Minh Ribler <bmribler@hdfgroup.org>
2018-07-30 11:04:16 -0500

    Merge pull request #1158 in HDFFV/hdf5 from ~BMRIBLER/hdf5_bmr_cpp4:develop to develop
    Typos in comments
    
    * commit '7d9f5ed49a560fe8801b77cce8ffb60a2ec5e846':
      Fixed document format Platforms tested:     Linux/64 (jelly) (only in comment sections)
      Fixed missing backslash

--- c++/src/H5VarLenType.cpp
+++ c++/src/H5VarLenType.cpp
@@ -133,6 +133,7 @@ VarLenType::VarLenType(const H5Location& loc, const H5std_string& dtype_name) : 
 // Function:    VarLenType::decode
 ///\brief       Returns an VarLenType object via DataType* by decoding the
 ///             binary object description of this type.
+///
 ///\exception   H5::DataTypeIException
 // Programmer   Binh-Minh Ribler - Aug 2017
 //--------------------------------------------------------------------------
