2a95ab5acbe032f58891b653f56b9db8a4938d95
Allen Byrne <byrn@hdfgroup.org>
2018-10-04 11:40:33 -0500

    TRILABS-21 add toolchain related info and files

--- config/intel.cmake
+++ config/intel.cmake
@@ -1,8 +1,10 @@
+# Uncomment the following to use cross-compiling
 #set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_COMPILER_VENDOR "intel")
 
 set(CMAKE_C_COMPILER icc)
 set(CMAKE_CXX_COMPILER icpc)
 set(CMAKE_Fortran_COMPILER ifort)
- 
-set(PRESET_PRINTF_LL "ll")
+
+# the following is used if cross-compiling
+set(PRESET_PRINTF_LL "l")
