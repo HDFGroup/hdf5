7e00924d1b9d235b988e63556e3f2c2644bae543
Allen Byrne <byrn@hdfgroup.org>
2018-10-18 14:44:27 -0500

    EED-258 Add modules uses toolchain files

---
+++ config/GCC.cmake
@@ -0,0 +1,10 @@
+# Uncomment the following to use cross-compiling
+#set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_COMPILER_VENDOR "GCC")
+
+set(CMAKE_C_COMPILER cc)
+set(CMAKE_CXX_COMPILER c++)
+set(CMAKE_Fortran_COMPILER gfortran)
+
+# the following is used if cross-compiling
+set(CMAKE_CROSSCOMPILING_EMULATOR "")
