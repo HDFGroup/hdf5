16cbd591cdac327da2d1576ab8751c14dbb40760
Jordan Henderson <jhenderson@hdfgroup.org>
2018-12-17 12:01:20 -0600

    align H5Arename behavior with H5Arename_by_name

--- src/H5A.c
+++ src/H5A.c
@@ -1201,10 +1201,12 @@ H5Arename(hid_t loc_id, const char *old_name, const char *new_name)
     H5TRACE3("e", "i*s*s", loc_id, old_name, new_name);
 
     /* check arguments */
-    if(!old_name || !new_name)
-        HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "name is nil")
     if(H5I_ATTR == H5I_get_type(loc_id))
         HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "location is not valid for an attribute")
+    if(!old_name || !*old_name)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "no old attribute name")
+    if(!new_name || !*new_name)
+        HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "no new attribute name")
 
     /* Avoid thrashing things if the names are the same */
     if(HDstrcmp(old_name, new_name)) {
