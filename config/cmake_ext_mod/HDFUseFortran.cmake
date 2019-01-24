1e411d15c6ca9310d75d98c984163b9f435d50e4
Allen Byrne <byrn@hdfgroup.org>
2019-01-24 15:57:31 -0600

    CMake updates

--- config/cmake_ext_mod/HDFUseFortran.cmake
+++ config/cmake_ext_mod/HDFUseFortran.cmake
@@ -48,9 +48,9 @@ set (H5_FC_FUNC_ "H5_FC_FUNC_(name,NAME) ${CMAKE_MATCH_1}")
 #-----------------------------------------------------------------------------
 macro (CHECK_FORTRAN_FEATURE FUNCTION CODE VARIABLE)
     message (STATUS "Testing Fortran ${FUNCTION}")
-    if (CMAKE_REQUIRED_LIBRARIES)
+    if (HDF5_REQUIRED_LIBRARIES)
       set (CHECK_FUNCTION_EXISTS_ADD_LIBRARIES
-          "-DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}")
+          "-DLINK_LIBRARIES:STRING=${HDF5_REQUIRED_LIBRARIES}")
     else ()
       set (CHECK_FUNCTION_EXISTS_ADD_LIBRARIES)
     endif ()
