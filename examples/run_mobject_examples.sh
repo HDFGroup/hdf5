d03d27cf0a47ae3944ddb209d8cca993b5b642ea
Dana Robinson <derobins@hdfgroup.org>
2019-02-27 06:07:26 -0800

    Added -g to RADOS examples compile flags.

--- examples/run_mobject_examples.sh
+++ examples/run_mobject_examples.sh
@@ -3,17 +3,16 @@
 # Build mobject examples.
 # Assumes mobject stuff can be found
 echo "Building mobject examples"
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_file_create h5rados_file_create.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_file_open h5rados_file_open.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_dset_create h5rados_dset_create.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_dset_open h5rados_dset_open.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_group_create h5rados_group_create.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_group_open h5rados_group_open.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_dset_write h5rados_dset_write.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_dset_read h5rados_dset_read.c
-
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_dset_rpartial h5rados_dset_rpartial.c
-../../../bin/h5pcc -DHDF5_USE_MOBJECT -o h5rados_dset_wpartial h5rados_dset_wpartial.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_file_create h5rados_file_create.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_file_open h5rados_file_open.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_dset_create h5rados_dset_create.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_dset_open h5rados_dset_open.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_group_create h5rados_group_create.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_group_open h5rados_group_open.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_dset_write h5rados_dset_write.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_dset_read h5rados_dset_read.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_dset_rpartial h5rados_dset_rpartial.c
+../../../bin/h5pcc -DHDF5_USE_MOBJECT -g -o h5rados_dset_wpartial h5rados_dset_wpartial.c
 echo "DONE"
 echo
 
