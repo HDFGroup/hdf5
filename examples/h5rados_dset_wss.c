ae78522092017f58d87de41f21f71a943f42d78e
Dana Robinson <derobins@hdfgroup.org>
2019-04-02 15:50:20 -0700

    * Updated copyright information for RADOS/mobject-store code. * Removed an EFF macro * Test scripts use /bin/sh instead of bin/bash

--- examples/h5rados_dset_wss.c
+++ examples/h5rados_dset_wss.c
@@ -1,3 +1,21 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the root of the source code       *
+ * distribution tree, or in https://support.hdfgroup.org/ftp/HDF5/releases.  *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* This source code was developed under the Mochi project
+ * (https://www.mcs.anl.gov/research/projects/mochi), supported by the U.S.
+ * Department of Energy, Office of Science, under contract DE-AC02-06CH11357.
+ */
+
 #include "h5rados_example.h"
 #include <time.h>
 
