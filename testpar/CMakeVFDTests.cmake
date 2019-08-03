0147a6c713c784f5df9189fc7334c65f54958434
Allen Byrne <byrn@hdfgroup.org>
2019-08-03 17:28:50 -0500

    Fix VFD tests and repack

--- testpar/CMakeVFDTests.cmake
+++ testpar/CMakeVFDTests.cmake
@@ -15,32 +15,33 @@
 ###           T E S T I N G                                                ###
 ##############################################################################
 ##############################################################################
-  set (VFD_LIST
-      sec2
-      stdio
-      core
-      split
-      multi
-      family
-  )
+set (VFD_LIST
+    sec2
+    stdio
+    core
+    core_paged
+    split
+    multi
+    family
+)
 
-  set (H5P_VFD_TESTS
-      t_pflush1
-      t_pflush2
-  )
+set (H5P_VFD_TESTS
+    t_pflush1
+    t_pflush2
+)
 
-  if (DIRECT_VFD)
-    set (VFD_LIST ${VFD_LIST} direct)
-  endif ()
+if (DIRECT_VFD)
+  set (VFD_LIST ${VFD_LIST} direct)
+endif ()
 
 foreach (vfdtest ${VFD_LIST})
   file (MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/${vfdtest}")
 endforeach ()
 
-  macro (ADD_VFD_TEST vfdname resultcode)
-    if (NOT HDF5_ENABLE_USING_MEMCHECKER)
-      foreach (h5_test ${H5P_VFD_TESTS})
-        add_test (
+macro (ADD_VFD_TEST vfdname resultcode)
+  if (NOT HDF5_ENABLE_USING_MEMCHECKER)
+    foreach (h5_test ${H5P_VFD_TESTS})
+      add_test (
           NAME MPI_TEST_VFD-${vfdname}-${h5_test}
           COMMAND "${CMAKE_COMMAND}"
               -D "TEST_EMULATOR=${CMAKE_CROSSCOMPILING_EMULATOR}"
@@ -51,16 +52,22 @@ endforeach ()
               -D "TEST_OUTPUT=${vfdname}-${h5_test}.out"
               -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
               -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
-        )
-        set_tests_properties (MPI_TEST_VFD-${vfdname}-${h5_test} PROPERTIES
-            ENVIRONMENT "srcdir=${HDF5_TEST_PAR_BINARY_DIR}/${vfdname}"
-            WORKING_DIRECTORY ${HDF5_TEST_PAR_BINARY_DIR}/${vfdname}
-        )
-      endforeach ()
-    endif ()
-  endmacro ()
+      )
+      set_tests_properties (MPI_TEST_VFD-${vfdname}-${h5_test} PROPERTIES
+          ENVIRONMENT "srcdir=${HDF5_TEST_PAR_BINARY_DIR}/${vfdname}"
+          WORKING_DIRECTORY ${HDF5_TEST_PAR_BINARY_DIR}/${vfdname}
+      )
+    endforeach ()
+  endif ()
+endmacro ()
 
-  # Run test with different Virtual File Driver
-  foreach (h5_vfd ${VFD_LIST})
-    ADD_VFD_TEST (${h5_vfd} 0)
-  endforeach ()
+##############################################################################
+##############################################################################
+###           T H E   T E S T S                                            ###
+##############################################################################
+##############################################################################
+
+# Run test with different Virtual File Driver
+foreach (h5_vfd ${VFD_LIST})
+  ADD_VFD_TEST (${h5_vfd} 0)
+endforeach ()
