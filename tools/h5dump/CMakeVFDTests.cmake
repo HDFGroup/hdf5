7a7f89d5250f8a5bca572fdd21ae01b44db1f8d6
Allen Byrne <byrn@hdfgroup.org>
2019-08-07 09:58:27 -0500

    Correct merge issues with VFD tests

--- tools/h5dump/CMakeVFDTests.cmake
+++ tools/h5dump/CMakeVFDTests.cmake
@@ -16,18 +16,36 @@
 ##############################################################################
 ##############################################################################
 
-set (VFD_LIST
+set (VFD_H5DUMP_LIST
     sec2
     stdio
     core
+    core_paged
     split
     multi
     family
 )
 
 if (DIRECT_VFD)
-  set (VFD_LIST ${VFD_LIST} direct)
+  set (VFD_H5DUMP_LIST ${VFD_H5DUMP_LIST} direct)
 endif ()
+
+# --------------------------------------------------------------------
+# Copy all the HDF5 files from the source directory into the test directory
+# --------------------------------------------------------------------
+set (HDF5_VFD_H5DUMP_FILES
+  packedbits
+)
+
+foreach (vfdtest ${VFD_H5DUMP_LIST})
+  file (MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/${vfdname}")
+  foreach (h5_tfile ${HDF5_VFD_H5DUMP_FILES})
+    HDFTEST_COPY_FILE("${HDF5_TOOLS_DIR}/testfiles/${h5_tfile}.h5" "${PROJECT_BINARY_DIR}/${vfdtest}/${h5_tfile}.h5" "HDF5_VFD_H5DUMP_files")
+    HDFTEST_COPY_FILE("${HDF5_TOOLS_DIR}/testfiles/${h5_tfile}.ddl" "${PROJECT_BINARY_DIR}/${vfdtest}/${h5_tfile}.ddl" "HDF5_VFD_H5DUMP_files")
+  endforeach ()
+endforeach ()
+
+add_custom_target(HDF5_VFD_H5DUMP_files ALL COMMENT "Copying files needed by HDF5_VFD_H5DUMP tests" DEPENDS ${HDF5_VFD_H5DUMP_files_list})
 
 ##############################################################################
 ##############################################################################
@@ -35,59 +53,28 @@ endif ()
 ##############################################################################
 ##############################################################################
 
-macro (ADD_VFD_TEST vfdname resultcode)
+if (NOT BUILD_SHARED_LIBS)
+  set (tgt_ext "")
+else ()
+  set (tgt_ext "-shared")
+endif ()
+
+macro (ADD_VFD_H5DUMP_TEST vfdname resultfile resultcode)
   if (NOT HDF5_ENABLE_USING_MEMCHECKER)
-    file (MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/${vfdname}")
     add_test (
-        NAME H5REPACK-${vfdname}-h5repacktest-clear-objects
-        COMMAND ${CMAKE_COMMAND} -E remove
-              bounds_latest_latest.h5
-              h5repack_attr.h5
-              h5repack_attr_refs.h5
-              h5repack_deflate.h5
-              h5repack_early.h5
-              h5repack_ext.h5
-              h5repack_fill.h5
-              h5repack_filters.h5
-              h5repack_fletcher.h5
-              h5repack_hlink.h5
-              h5repack_layout.h5
-              h5repack_layouto.h5
-              h5repack_layout2.h5
-              h5repack_layout3.h5
-              h5repack_layout.UD.h5
-              h5repack_named_dtypes.h5
-              h5repack_nested_8bit_enum.h5
-              h5repack_nested_8bit_enum_deflated.h5
-              h5repack_nbit.h5
-              h5repack_objs.h5
-              h5repack_refs.h5
-              h5repack_shuffle.h5
-              h5repack_soffset.h5
-              h5repack_szip.h5
-              # fsm
-              h5repack_aggr.h5
-              h5repack_fsm_aggr_nopersist.h5
-              h5repack_fsm_aggr_persist.h5
-              h5repack_none.h5
-              h5repack_paged_nopersist.h5
-              h5repack_paged_persist.h5
-        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/${vfdname}
-    )
-    add_test (
-        NAME H5REPACK_VFD-${vfdname}-h5repacktest
+        NAME H5DUMP_VFD-${vfdname}-${resultfile}-h5dump
         COMMAND "${CMAKE_COMMAND}"
             -D "TEST_EMULATOR=${CMAKE_CROSSCOMPILING_EMULATOR}"
-            -D "TEST_PROGRAM=$<TARGET_FILE:h5repacktest>"
-            -D "TEST_ARGS:STRING="
+            -D "TEST_PROGRAM=$<TARGET_FILE:h5dump${tgt_ext}>"
+            -D "TEST_ARGS:STRING=${ARGN}"
             -D "TEST_VFD:STRING=${vfdname}"
             -D "TEST_EXPECT=${resultcode}"
-            -D "TEST_OUTPUT=${vfdname}-h5repacktest.out"
+            -D "TEST_OUTPUT=${resultfile}.out"
+            -D "TEST_REFERENCE=${resultfile}.ddl"
             -D "TEST_FOLDER=${PROJECT_BINARY_DIR}/${vfdname}"
             -P "${HDF_RESOURCES_DIR}/vfdTest.cmake"
     )
-    set_tests_properties (H5REPACK_VFD-${vfdname}-h5repacktest PROPERTIES DEPENDS H5REPACK_VFD-${vfdname}-h5repacktest-clear-objects)
-    set_tests_properties (H5REPACK_VFD-${vfdname}-h5repacktest PROPERTIES TIMEOUT ${CTEST_SHORT_TIMEOUT})
+    set_tests_properties (H5DUMP_VFD-${vfdname}-${resultfile}-h5dump PROPERTIES TIMEOUT ${CTEST_SHORT_TIMEOUT})
   endif ()
 endmacro ()
 
@@ -98,6 +85,7 @@ endmacro ()
 ##############################################################################
 
 # Run test with different Virtual File Driver
-foreach (vfd ${VFD_LIST})
-  ADD_VFD_TEST (${vfd} 0)
+foreach (vfd ${VFD_H5DUMP_LIST})
+  # test for signed/unsigned datasets
+  ADD_VFD_H5DUMP_TEST (${vfd} packedbits 0 --enable-error-stack packedbits.h5)
 endforeach ()
