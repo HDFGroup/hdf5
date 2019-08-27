d42de626b604b75963fd593a145fd9c9b9e20602
Dana Robinson <derobins@hdfgroup.org>
2019-08-27 07:03:51 -0700

    Fixed a bug where we incorrectly pass a lock struct to fcntl for file locking instead of a pointer.

--- src/H5system.c
+++ src/H5system.c
@@ -631,7 +631,7 @@ Pflock(int fd, int operation) {
     flk.l_pid = 0;              /* not used with set */
 
     /* Lock or unlock */
-    if(HDfcntl(fd, F_SETLK, flk) < 0)
+    if(HDfcntl(fd, F_SETLK, &flk) < 0)
         return -1;
 
     return 0;
