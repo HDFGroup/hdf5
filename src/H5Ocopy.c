f8ab081b1f1abcbf76c151d7960ee97a556b4be9
Chris Hogan <chogan@hdfgroup.org>
2019-10-28 16:24:54 -0500

    Use API context to store/retrieve LCPL when creating intermediate groups

--- src/H5Ocopy.c
+++ src/H5Ocopy.c
@@ -227,6 +227,9 @@ H5Ocopy(hid_t src_loc_id, const char *src_name, hid_t dst_loc_id,
         if(TRUE != H5P_isa_class(ocpypl_id, H5P_OBJECT_COPY))
             HGOTO_ERROR(H5E_ARGS, H5E_BADTYPE, FAIL, "not object copy property list")
 
+    /* Set the LCPL for the API context */
+    H5CX_set_lcpl(lcpl_id);
+
     /* Set up collective metadata if appropriate */
     if(H5CX_set_loc(src_loc_id) < 0)
         HGOTO_ERROR(H5E_OHDR, H5E_CANTSET, FAIL, "can't set collective metadata read info")
