fb4212aafbb8c75dbabf4c9c0be5f890953827ed
Larry Knox <lrknox@hdfgroup.org>
2019-11-13 13:10:00 -0600

    Reduce output to log with -V instead of -VV in ctest command. Try to run parallel tests with 6 processes.

--- .gitlab/nnsa.sh
+++ .gitlab/nnsa.sh
@@ -171,8 +171,10 @@ elif [[ $UNAME == p90* ]]; then
 #    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DMPIEXEC_EXECUTABLE:STRING=mpirun")' >> hdf5-$HDF5_VER/config/cmake/scripts/HPC/sbatch-HDF5options.cmake
 #    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DMPIEXEC_PREFLAGS:STRING=--mca;io;ompio")' >> hdf5-$HDF5_VER/config/cmake/scripts/HPC/sbatch-HDF5options.cmake
 
-    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DHDF5_BUILD_TOOLS:BOOL=OFF")' >> hdf5-$HDF5_VER/config/cmake/scripts/HPC/sbatch-HDF5options.cmake
-    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DHDF5_BUILD_EXAMPLES:BOOL=OFF")' >> hdf5-$HDF5_VER/config/cmake/scripts/HPC/sbatch-HDF5options.cmake
+    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DHDF5_BUILD_TOOLS:BOOL=OFF")' >> hdf5-$HDF5_VER/config/cmake/scripts/HDF5options.cmake
+    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DHDF5_BUILD_EXAMPLES:BOOL=OFF")' >> hdf5-$HDF5_VER/config/cmake/scripts/HDF5options.cmake
+    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DMPIEXEC_NUMPROC_FLAG:STRING=-n")' >> hdf5-$HDF5_VER/config/cmake/scripts/HDF5options.cmake
+    echo 'set (ADD_BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DMPIEXEC_MAX_NUMPROCS:STRING=6")' >> hdf5-$HDF5_VER/config/cmake/scripts/HDF5options.cmake
 
 #    perl -i -pe "s/^CMD=\"ctest.*/CMD=\"ctest . -R MPI_TEST_ -E MPI_TEST_testphdf5|MPI_TEST_t_filters_parallel|MPI_TEST_t_shapesame -C Release -T test\"/" hdf5-$HDF5_VER/bin/batch/ctestP.sl.in.cmake
 
@@ -260,19 +262,19 @@ elif [[ $UNAME == lassen* ]]; then
 fi
 module list
 
-icnt=-1
+icnt=0
 for master_mod in $MASTER_MOD; do
 
-  icnt=$(($icnt+1))
+  #icnt=$(($icnt+1))
   num_CC=${CC_VER[$icnt]} # number of compilers for the  master_mod
 
-  icnt=$(($icnt+1))
   module load $master_mod # Load the master module
 
-  module unload ${CC_VER[$icnt]} # unload the general compiler type
+  icnt=$(($icnt+1))
+  COMPILER_TYPE=`echo ${CC_VER[icnt]} | sed 's/\/.*//'`
+  module unload ${COMPILER_TYPE} # unload the general compiler type
 
   for i in `seq 1 $num_CC`; do # loop over compiler versions
-    icnt=$(($icnt+1))
     cc_ver=${CC_VER[$icnt]} # compiler version
     rm -fr build
 
@@ -285,10 +287,12 @@ for master_mod in $MASTER_MOD; do
 
     module list
 
-    echo "timeout 3h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX=\"$HDF5_VER-$master_mod-$cc_ver\",${CTEST_OPTS}MPI=true,BUILD_GENERATOR=Unix,LOCAL_SUBMIT=true,MODEL=HPC -C Release -VV -O hdf5.log"
-    timeout 3h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX="$HDF5_VER-$master_mod--$cc_ver",${CTEST_OPTS}MPI=true,BUILD_GENERATOR=Unix,LOCAL_SUBMIT=true,MODEL=HPC -C Release -VV -O hdf5.log
+    echo "timeout 3h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX=\"$HDF5_VER-$master_mod-$cc_ver\",${CTEST_OPTS}MPI=true,BUILD_GENERATOR=Unix,LOCAL_SUBMIT=true,MODEL=HPC -C Release -V -O hdf5.log"
+    timeout 3h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX="$HDF5_VER-$master_mod--$cc_ver",${CTEST_OPTS}MPI=true,BUILD_GENERATOR=Unix,LOCAL_SUBMIT=true,MODEL=HPC -C Release -V -O hdf5.log
 
     module unload $cc_ver  # unload the compiler with version
+
+    icnt=$(($icnt+1))
 
     #rm -fr build
   done
