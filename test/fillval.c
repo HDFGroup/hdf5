10911a11767aee10c5bc1def143dad1a5f5576cc
David Young <dyoung@hdfgroup.org>
2019-11-25 17:30:08 -0600

    Oops, remove more C99 designated initializers for VS 2010 compatibility.

--- test/fillval.c
+++ test/fillval.c
@@ -758,10 +758,14 @@ test_rdwr_cases(hid_t file, hid_t dcpl, const char *dname, void *_fillval,
     int        fillval=(-1), val_rd, should_be;
     int        i, j, *buf=NULL, odd;
     unsigned    u;
-    comp_datatype       rd_c, fill_c = {.a = 0, .x = 0, .y = 0, .z = 0},
-                        should_be_c;
+    comp_datatype       rd_c, fill_c, should_be_c;
     comp_datatype    *buf_c=NULL;
     H5D_space_status_t  allocation;
+
+    fill_c.a = 0;
+    fill_c.x = 0;
+    fill_c.y = 0;
+    fill_c.z = 0;
 
     if(datatype == H5T_INTEGER) {
         fillval = *(int*)_fillval;
