1814f0363f114f74c6b60849df81710bda34394f
Allen Byrne <byrn@hdfgroup.org>
2019-12-13 11:52:38 -0600

    Fix duplicate and varname

--- tools/lib/h5tools_dump.c
+++ tools/lib/h5tools_dump.c
@@ -4030,7 +4030,7 @@ h5tools_dump_data(FILE *stream, const h5tool_format_t *info, h5tools_context_t *
     h5tools_render_element(stream, &outputformat, ctx, &buffer, &curr_pos, (size_t)ncols, (hsize_t)0, (hsize_t)0);
 
     if (H5Tget_class(f_type) == H5T_REFERENCE) {
-        ctx->indent_level++;
+        datactx->indent_level++;
         H5TOOLS_DEBUG(H5E_tools_min_dbg_id_g, "reference class type");
         if (!H5Tequal(f_type, H5T_STD_REF) && !H5Tequal(f_type, H5T_STD_REF_DSETREG) && !H5Tequal(f_type, H5T_STD_REF_OBJ)) {
             HGOTO_DONE(SUCCEED);
@@ -4260,13 +4260,12 @@ h5tools_dump_data(FILE *stream, const h5tool_format_t *info, h5tools_context_t *
                 } /* end switch */
 
                 H5TOOLS_DEBUG(H5E_tools_min_dbg_id_g, "finished reference loop:%d",i);
-            } /* end for(i = 0; i < ndims; i++, ctx->cur_elmt++, elmt_counter++) */
+            } /* end for(i = 0; i < ndims; i++, datactx->cur_elmt++, elmt_counter++) */
             HDfree(ref_buf);
         }
-        ctx->indent_level--;
+        datactx->indent_level--;
     }
     else {
-        h5tools_context_t datactx = *ctx;            /* print context  */
         H5TOOLS_DEBUG(H5E_tools_min_dbg_id_g, "Print all the values");
         string_dataformat = *info;
         if((datactx.display_char && H5Tget_size(f_type) == 1) && (H5Tget_class(f_type) == H5T_INTEGER)) {
