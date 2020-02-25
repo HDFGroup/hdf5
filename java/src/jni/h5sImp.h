6b1e2d835fbbdbea242443837b3694f48e89591a
Allen Byrne <byrn@hdfgroup.org>
2020-02-25 09:21:14 -0600

    Fix Fortran macro use and jni comment

--- java/src/jni/h5sImp.h
+++ java/src/jni/h5sImp.h
@@ -51,7 +51,8 @@ Java_hdf_hdf5lib_H5__1H5Scopy
 #ifdef notdef
 /* 10/28/99 -- added code to copy the array -- this is not used,
  * but serves as a reminder in case we try to implement this in
- */ the future....
+ * the future....
+ */ 
 /*
  *  Note:  the argument coord is actually long coord[][], which has been
  *         flattened by the caller.
