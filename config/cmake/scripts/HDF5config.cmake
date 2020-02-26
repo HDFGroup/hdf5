13f5b3aee20d1d65f56dc08f088e0f218da5cf37
Allen Byrne <byrn@hdfgroup.org>
2020-02-26 13:54:34 -0600

    Update examples and fix old version references.

--- config/cmake/scripts/HDF5config.cmake
+++ config/cmake/scripts/HDF5config.cmake
@@ -42,9 +42,9 @@ set (CTEST_SOURCE_VERSEXT "")
 ##############################################################################
 # handle input parameters to script.
 #BUILD_GENERATOR - which CMake generator to use, required
-#INSTALLDIR - HDF5-1.13.0 root folder
+#INSTALLDIR - HDF5-1.13.x root folder
 #CTEST_CONFIGURATION_TYPE - Release, Debug, RelWithDebInfo
-#CTEST_SOURCE_NAME - name of source folder; HDF5-1.13.0
+#CTEST_SOURCE_NAME - name of source folder; HDF5-1.13.x
 #MODEL - CDash group name
 #HPC - run alternate configurations for HPC machines; sbatch, bsub, raybsub, qsub
 #MPI - enable MPI
@@ -186,8 +186,6 @@ else ()
   set (CTEST_SITE "${SITE_OS_NAME}")
   set (CTEST_CMAKE_GENERATOR "Unix Makefiles")
 endif ()
-###################################################################
-
 ###################################################################
 
 ###################################################################
