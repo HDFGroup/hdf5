665b71f94ec9ca81360c96867a1df5882b2ab19c
vchoi <vchoi@jelly.ad.hdfgroup.org>
2020-04-07 19:22:22 -0500

    A fix in the cleaning up code for datatype when datatype initialization via H5D__init_type() fails. The code to fix the problem is the same as what is done in H5D__open_oid().

--- src/H5Dint.c
+++ src/H5Dint.c
@@ -1327,7 +1327,7 @@ H5D__create(H5F_t *file, hid_t type_id, const H5S_t *space, hid_t dcpl_id,
     ret_value = new_dset;
 
 done:
-    if(!ret_value && new_dset && new_dset->shared) {
+    if(!ret_value && new_dset) {
         if(new_dset->shared) {
             if(layout_init)
                 if(new_dset->shared->layout.ops->dest && (new_dset->shared->layout.ops->dest)(new_dset) < 0)
@@ -1346,8 +1346,16 @@ done:
                     HDONE_ERROR(H5E_DATASET, H5E_CANTRESET, NULL, "unable to reset external file list info")
             if(new_dset->shared->space && H5S_close(new_dset->shared->space) < 0)
                 HDONE_ERROR(H5E_DATASET, H5E_CLOSEERROR, NULL, "unable to release dataspace")
-            if(new_dset->shared->type && H5I_dec_ref(new_dset->shared->type_id) < 0)
-                HDONE_ERROR(H5E_DATASET, H5E_CLOSEERROR, NULL, "unable to release datatype")
+            if(new_dset->shared->type) {
+                if(new_dset->shared->type_id > 0) {
+                    if(H5I_dec_ref(new_dset->shared->type_id) < 0)
+                        HDONE_ERROR(H5E_DATASET, H5E_CLOSEERROR, NULL, "unable to release datatype")
+                } /* end if */
+                else {
+                    if(H5T_close_real(new_dset->shared->type) < 0)
+                        HDONE_ERROR(H5E_DATASET, H5E_CLOSEERROR, NULL, "unable to release datatype")
+                } /* end else */
+            } /* end if */
             if(H5F_addr_defined(new_dset->oloc.addr)) {
                 if(H5O_dec_rc_by_loc(&(new_dset->oloc)) < 0)
                     HDONE_ERROR(H5E_DATASET, H5E_CANTDEC, NULL, "unable to decrement refcount on newly created object")
