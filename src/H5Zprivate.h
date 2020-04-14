3663f6c2aa4933143b6de17e91bd13c9a64ad5bd
Binh-Minh Ribler <bmribler@hdfgroup.org>
2020-04-14 14:50:56 -0500

    Fixing HDFFV-10933 Description:     Updated the original fix by KY in commit             200a77d8c3e51663c375aafffff607ae9b438f4e     - use internal functions instead of public API     - add another condition in a for loop, but need to confirm     - move some code into the subroutine for a cleaner look.
    Platforms tested:
            Linux/64 (jelly)

--- src/H5Zprivate.h
+++ src/H5Zprivate.h
@@ -26,6 +26,7 @@ typedef struct H5Z_filter_info_t H5Z_filter_info_t;
 
 /* Private headers needed by this file */
 #include "H5Tprivate.h"		/* Datatypes				*/
+#include "H5Sprivate.h"		/* Dataspace				*/
 
 /**************************/
 /* Library Private Macros */
@@ -85,8 +86,7 @@ H5_DLL herr_t H5Z_pipeline(const struct H5O_pline_t *pline,
 			    size_t *nbytes/*in,out*/, size_t *buf_size/*in,out*/,
                             void **buf/*in,out*/);
 H5_DLL H5Z_class2_t *H5Z_find(H5Z_filter_t id);
-//H5_DLL htri_t H5Z_can_ignore(hid_t dcpl_id);
-H5_DLL htri_t H5Z_has_optional_filter(hid_t dcpl_id);
+H5_DLL htri_t H5Z_has_optional_filter(hid_t dcpl_id, const H5T_t *type, const H5S_t *space);
 H5_DLL herr_t H5Z_can_apply(hid_t dcpl_id, hid_t type_id);
 H5_DLL herr_t H5Z_set_local(hid_t dcpl_id, hid_t type_id);
 H5_DLL herr_t H5Z_can_apply_direct(const struct H5O_pline_t *pline);
