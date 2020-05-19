b4492dba5070d73bac83ec27c644e4e8d4e3871f
David Young <dyoung@hdfgroup.org>
2020-05-19 14:56:15 -0500

    Reduce diffs with `develop`.

--- test/tselect.c
+++ test/tselect.c
@@ -1612,7 +1612,7 @@ test_select_hyper_contig3(hid_t dset_type, hid_t xfer_plist)
 ****************************************************************/
 static void
 verify_select_hyper_contig_dr__run_test(const uint16_t *cube_buf,
-    size_t H5_ATTR_SANITY_CHECK cube_size, unsigned edge_size, unsigned cube_rank)
+    size_t H5_ATTR_NDEBUG_UNUSED cube_size, unsigned edge_size, unsigned cube_rank)
 {
     const uint16_t     *cube_ptr;           /* Pointer into the cube buffer */
     uint16_t        expected_value;     /* Expected value in dataset */
