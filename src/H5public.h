e5b104245022e59669c283e46470b5571db7bdad
hdftest <hdftest@hdfgroup.org>
2020-07-18 23:19:33 -0500

    Snapshot version 1.12.1-1 Update version to 1.12.1-2

--- src/H5public.h
+++ src/H5public.h
@@ -97,9 +97,9 @@ extern "C" {
 #define H5_VERS_MAJOR	1	/* For major interface/format changes  	     */
 #define H5_VERS_MINOR	12	/* For minor interface/format changes  	     */
 #define H5_VERS_RELEASE	1	/* For tweaks, bug-fixes, or development     */
-#define H5_VERS_SUBRELEASE "1"	/* For pre-releases like snap0       */
+#define H5_VERS_SUBRELEASE "2"	/* For pre-releases like snap0       */
 				/* Empty string for real releases.           */
-#define H5_VERS_INFO    "HDF5 library version: 1.12.1-1"      /* Full version string */
+#define H5_VERS_INFO    "HDF5 library version: 1.12.1-2"      /* Full version string */
 
 #define H5check()	H5check_version(H5_VERS_MAJOR,H5_VERS_MINOR,	      \
 				        H5_VERS_RELEASE)
