04e775b127987821d8e1c3993221598f348e7226
David Young <dyoung@hdfgroup.org>
2020-07-21 11:15:44 -0500

    Straggler from last: update declaration of vfd_swmr_create_fapl().

--- test/vfd_swmr_common.h
+++ test/vfd_swmr_common.h
@@ -95,7 +95,8 @@ H5TEST_DLL int print_metadata_retries_info(hid_t fid);
 H5TEST_DLL void block_signals(sigset_t *);
 H5TEST_DLL void restore_signals(sigset_t *);
 H5TEST_DLL void await_signal(hid_t);
-H5TEST_DLL hid_t vfd_swmr_create_fapl(bool, bool, bool);
+H5TEST_DLL hid_t vfd_swmr_create_fapl(bool, bool, bool, const char *, ...)
+    H5_ATTR_FORMAT(printf, 4, 5);
 
 H5TEST_DLL void dbgf(int, const char *, ...) H5_ATTR_FORMAT(printf, 2, 3);
 H5TEST_DLL void evsnprintf(char *, size_t, const char *, va_list);
