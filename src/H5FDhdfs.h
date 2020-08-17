350c9e9b05b86b7b6660c1b596b7e1ddb5fe0854
Dana Robinson <derobins@hdfgroup.org>
2020-08-17 12:35:40 -0700

    Brings over S3 and HDFS VFD changes from develop

--- src/H5FDhdfs.h
+++ src/H5FDhdfs.h
@@ -22,8 +22,13 @@
 #ifndef H5FDhdfs_H
 #define H5FDhdfs_H
 
-#define H5FD_HDFS (H5FD_hdfs_init())
+#ifdef H5_HAVE_LIBHDFS
+#define H5FD_HDFS       (H5FD_hdfs_init())
+#else /* H5_HAVE_LIBHDFS */
+#define H5FD_HDFS       (-1)
+#endif /* H5_HAVE_LIBHDFS */
 
+#ifdef H5_HAVE_LIBHDFS
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -114,6 +119,7 @@ H5_DLL herr_t H5Pset_fapl_hdfs(hid_t fapl_id, H5FD_hdfs_fapl_t *fa);
 #ifdef __cplusplus
 }
 #endif
+#endif /* H5_HAVE_LIBHDFS */
 
 #endif /* ifndef H5FDhdfs_H */
 
