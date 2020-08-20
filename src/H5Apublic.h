93861b25384016682b783f0758d5d8c698fab6bf
bljones <bljones@hdfgroup.org>
2020-08-20 16:22:14 -0500

    Removed trailing spaces

--- src/H5Apublic.h
+++ src/H5Apublic.h
@@ -112,30 +112,30 @@ H5_DLL hid_t   H5Acreate2(hid_t loc_id, const char *attr_name, hid_t type_id,
  *
  * \return \hid_tv{attribute}
  *
- * \details H5Acreate_by_name() creates an attribute, \p attr_name, which is 
+ * \details H5Acreate_by_name() creates an attribute, \p attr_name, which is
  *          attached to the object specified by \p loc_id and \p obj_name.
  *
- *          \p loc_id is a location identifier; \p obj_name is the object 
- *          name relative to \p loc_id. If \p loc_id fully specifies the 
- *          object to which the attribute is to be attached, \p obj_name 
+ *          \p loc_id is a location identifier; \p obj_name is the object
+ *          name relative to \p loc_id. If \p loc_id fully specifies the
+ *          object to which the attribute is to be attached, \p obj_name
  *          should be '.' (a dot).
  *
  *          The attribute name, \p attr_name, must be unique for the object.
  *
- *          The attribute is created with the specified datatype and 
- *          dataspace, \p type_id and \p space_id, which are created with 
+ *          The attribute is created with the specified datatype and
+ *          dataspace, \p type_id and \p space_id, which are created with
  *          the H5T and H5S interfaces respectively.
  *
- *          The attribute creation and access property lists are currently 
- *          unused, but will be used in the future for optional attribute 
- *          creation and access properties. These property lists should 
+ *          The attribute creation and access property lists are currently
+ *          unused, but will be used in the future for optional attribute
+ *          creation and access properties. These property lists should
  *          currently be #H5P_DEFAULT.
  *
- *          The link access property list, \p lapl_id, may provide 
- *          information regarding the properties of links required to access 
- *          the object, \p obj_name. 
+ *          The link access property list, \p lapl_id, may provide
+ *          information regarding the properties of links required to access
+ *          the object, \p obj_name.
  *
- *          The attribute identifier returned by this function must be 
+ *          The attribute identifier returned by this function must be
  *          released with H5close() or resource leaks will develop.
  *
  * \since 1.8.0
@@ -189,26 +189,26 @@ H5_DLL hid_t   H5Aopen(hid_t obj_id, const char *attr_name, hid_t aapl_id);
  * \return \hid_tv{attribute}
  *
  * \details H5Aopen_by_name() opens an existing attribute, \p attr_name,
- *          that is attached to an object specified by location and name, 
+ *          that is attached to an object specified by location and name,
  *          \p loc_id and \p obj_name, respectively.
  *
- *          \p loc_id specifies a location from which the target object can 
- *          be located and \p obj_name is an object name relative to 
- *          \p loc_id. If \p loc_id fully specifies the object to which the 
+ *          \p loc_id specifies a location from which the target object can
+ *          be located and \p obj_name is an object name relative to
+ *          \p loc_id. If \p loc_id fully specifies the object to which the
  *          attribute is attached, \p obj_name should be '.' (a dot).
  *
- *          The attribute access property list, \p aapl_id, is currently 
+ *          The attribute access property list, \p aapl_id, is currently
  *          unused and should currently be #H5P_DEFAULT.
  *
- *          The link access property list, \p lapl_id, may provide 
- *          information regarding the properties of links required to access 
- *          the object, \p obj_name. 
+ *          The link access property list, \p lapl_id, may provide
+ *          information regarding the properties of links required to access
+ *          the object, \p obj_name.
  *
- *          This function, H5Aopen(), or H5Aopen_by_idx() must be called 
- *          before an attribute can be accessed for any further purpose, 
+ *          This function, H5Aopen(), or H5Aopen_by_idx() must be called
+ *          before an attribute can be accessed for any further purpose,
  *          including reading, writing, or any modification.
  *
- *          The attribute identifier returned by this function must be 
+ *          The attribute identifier returned by this function must be
  *          released with H5Aclose() or resource leaks will develop.
  *
  * \since 1.8.0
@@ -232,29 +232,29 @@ H5_DLL hid_t   H5Aopen_by_name(hid_t loc_id, const char *obj_name,
  *
  * \return \hid_tv{attribute}
  *
- * \details H5Aopen_by_idx() opens an existing attribute that is attached 
- *          to an object specified by location and name, \p loc_id  and  
- *          \p obj_name, respectively. If \p loc_id fully specifies the 
- *          object to which the attribute is attached, \p obj_name, should 
+ * \details H5Aopen_by_idx() opens an existing attribute that is attached
+ *          to an object specified by location and name, \p loc_id and 
+ *          \p obj_name, respectively. If \p loc_id fully specifies the
+ *          object to which the attribute is attached, \p obj_name, should
  *          be  '.'  (a dot).
  *
- *          The attribute is identified by an index type, an index traversal 
- *          order, and a position in the index, \p idx_type, \p order and 
- *          \p n, respectively. These parameters and their valid values are 
+ *          The attribute is identified by an index type, an index traversal
+ *          order, and a position in the index, \p idx_type, \p order and
+ *          \p n, respectively. These parameters and their valid values are
  *          discussed in the description of H5Aiterate2().
  *
- *          The attribute access property list, \p aapl_id, is currently 
+ *          The attribute access property list, \p aapl_id, is currently
  *          unused and should currently be #H5P_DEFAULT.
  *
- *          The link access property list, \p lapl_id, may provide 
- *          information regarding the properties of links required to access 
- *          the object, \p obj_name. 
+ *          The link access property list, \p lapl_id, may provide
+ *          information regarding the properties of links required to access
+ *          the object, \p obj_name.
  *
- *          This function, H5Aopen(), or H5Aopen_by_name() must be called 
- *          before an attribute can be accessed for any further purpose, 
+ *          This function, H5Aopen(), or H5Aopen_by_name() must be called
+ *          before an attribute can be accessed for any further purpose,
  *          including reading, writing, or any modification.
  *
- *          The attribute identifier returned by this function must be 
+ *          The attribute identifier returned by this function must be
  *          released with H5Aclose() or resource leaks will develop.
  *
  * \since 1.8.0
@@ -355,9 +355,9 @@ H5_DLL herr_t  H5Aclose(hid_t attr_id);
  *
  * \return \hid_tv{attribute dataspace}
  *
- * \details  H5Aget_space() retrieves a copy of the dataspace for an 
- *           attribute. The dataspace identifier returned from this 
- *           function must be released with H5Sclose() or resource leaks 
+ * \details  H5Aget_space() retrieves a copy of the dataspace for an
+ *           attribute. The dataspace identifier returned from this
+ *           function must be released with H5Sclose() or resource leaks
  *           will develop.
  *
  * \since 1.0.0
@@ -374,12 +374,12 @@ H5_DLL hid_t   H5Aget_space(hid_t attr_id);
  * \return \hid_t{datatype}
  *
  * \details H5Aget_type() retrieves a copy of the datatype for an attribute.
- *          The datatype is reopened if it is a named type before returning 
- *          it to the application. The datatypes returned by this function 
- *          are always read-only. If an error occurs when atomizing the 
+ *          The datatype is reopened if it is a named type before returning
+ *          it to the application. The datatypes returned by this function
+ *          are always read-only. If an error occurs when atomizing the
  *          return datatype, then the datatype is closed.
  *
- *          The datatype identifier returned from this function must be 
+ *          The datatype identifier returned from this function must be
  *          released with H5Tclose() or resource leaks will develop.
  *
  * \since 1.0.0
