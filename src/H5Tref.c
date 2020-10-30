16eab69bcd5219bef52cf9de88d8e1fa7557fc48
Dana Robinson <derobins@hdfgroup.org>
2020-10-29 17:17:15 -0700

    Replaces C std library calls with H5MM equivalents
    Mostly H5MM_memcpy in newer VFDs

--- src/H5Tref.c
+++ src/H5Tref.c
@@ -513,7 +513,7 @@ H5T__ref_mem_read(H5VL_object_t H5_ATTR_UNUSED *src_file, const void *src_buf, s
 
     /* Memory-to-memory conversion to support vlen conversion */
     if (NULL == dst_file) {
-        HDmemcpy(dst_buf, src_buf, dst_size);
+        H5MM_memcpy(dst_buf, src_buf, dst_size);
         HGOTO_DONE(ret_value);
     }
 
@@ -602,7 +602,7 @@ H5T__ref_mem_write(H5VL_object_t *src_file, const void *src_buf, size_t src_size
 
     /* Memory-to-memory conversion to support vlen conversion */
     if (NULL == src_file) {
-        HDmemcpy(dst_buf, src_buf, src_size);
+        H5MM_memcpy(dst_buf, src_buf, src_size);
         HGOTO_DONE(ret_value);
     }
 
