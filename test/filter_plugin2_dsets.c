5ec73a5ac3395fc2a2f05c8195e7c91fe0e9d37b
Sean McBride <sean@rogue-research.com>
2021-03-04 16:06:30 -0500

    More warning fixes (#400)
    * Fixed -Wunused-local-typedef warning
    
    * Fixed -Wformat warnings
    
    In one case also removed a `z` character. There was a `z%d` that I think was supposed to be `%zd`
    
    * Fixed -Wshorten-64-to-32 warnings
    
    * Fixed -Wself-assign warnings
    
    * Fixed -Wreserved-id-macro warnings
    
    * Commit format changes from clang-format, clang version 10.0.1.
    
    * Fixed -Wself-assign warnings
    
    * Fixed -Wunused-local-typedef warning
    
    * Fixed -Wformat warnings
    
    In two cases also removed a `z` character. There was a `z%d` that was supposed to be `%zd`.
    
    * Fixed -Wshorten-64-to-32 warnings
    
    * Fixed -Wreserved-id-macro warnings
    
    * Fixed -Wself-assign warnings
    
    * Format source.
    
    * Remove blank lines to pass format check.
    
    Co-authored-by: Larry Knox <lrknox@hdfgroup.org>

--- test/filter_plugin2_dsets.c
+++ test/filter_plugin2_dsets.c
@@ -73,7 +73,7 @@ mult_div_value(unsigned int flags, size_t cd_nelmts, const unsigned int *cd_valu
         return 0;
 
     /* Assignment to eliminate unused parameter warning */
-    cd_values = cd_values;
+    (void)cd_values;
 
     if (flags & H5Z_FLAG_REVERSE) {
         /* READ - Divide the original value by MULTIPLIER */
