b0ce859b2c2f3862df5c00c7c84f8abd777f3a62
vchoi <vchoi@jelly.ad.hdfgroup.org>
2021-03-04 13:36:39 -0600

    Add the two VFD SWMR demo programs to "examples" directory. Provide instructions on compiling and running the demos.

---
+++ examples/nbcompat.c
@@ -0,0 +1,17 @@
+#include "nbcompat.h"
+
+size_t
+strlcpy(char *dst, const char *src, size_t size)
+{
+    char *d;
+    const char *s;
+
+    for (d = dst, s = src; (s - src) < size; d++, s++) {
+        *d = *s;
+        if (*s == '\0')
+            return s - src;
+    }
+
+    dst[size - 1] = '\0';
+    return size;
+}
