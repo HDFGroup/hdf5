00487402c9dd42bab8efbe005fe1624004619c3c
Quincey Koziol <koziol@lbl.gov>
2021-03-25 14:13:59 -0500

    Improve MPI error reporting, handled failed operations in parallel tests more nicely, and clean up MPI_Allreduce for determining whether to break collective I/O

--- testpar/testphdf5.c
+++ testpar/testphdf5.c
@@ -33,9 +33,6 @@ int ngroups   = 512;             /* number of groups to create in root
 int facc_type       = FACC_MPIO; /*Test file access type */
 int dxfer_coll_type = DXFER_COLLECTIVE_IO;
 
-H5E_auto2_t old_func;        /* previous error handler */
-void *      old_client_data; /* previous error handler arg.*/
-
 /* other option flags */
 
 /* FILENAME and filenames must have the same number of names.
