19da35850acdba9540a018e076f68f0fb25005af
Dana Robinson <derobins@hdfgroup.org>
2021-05-24 05:05:38 -0700

    H5L cleanup

--- src/H5Ocopy.c
+++ src/H5Ocopy.c
@@ -262,7 +262,7 @@ H5O_copy(const H5G_loc_t *loc, const char *src_name, H5G_loc_t *dst_loc, const c
     H5G_loc_t  src_loc;             /* Source object group location */
     H5G_name_t src_path;            /* Opened source object hier. path */
     H5O_loc_t  src_oloc;            /* Opened source object object location */
-    htri_t     dst_exists;          /* Does destination name exist already? */
+    hbool_t    dst_exists;          /* Does destination name exist already? */
     hbool_t    loc_found = FALSE;   /* Location at 'name' found */
     hbool_t    obj_open  = FALSE;   /* Entry at 'name' found */
     herr_t     ret_value = SUCCEED; /* Return value */
@@ -276,9 +276,10 @@ H5O_copy(const H5G_loc_t *loc, const char *src_name, H5G_loc_t *dst_loc, const c
     HDassert(dst_name && *dst_name);
 
     /* Check if destination name already exists */
-    if ((dst_exists = H5L_exists_tolerant(dst_loc, dst_name)) < 0)
+    dst_exists = FALSE;
+    if (H5L_exists_tolerant(dst_loc, dst_name, &dst_exists) < 0)
         HGOTO_ERROR(H5E_OHDR, H5E_CANTGET, FAIL, "unable to check if destination name exists")
-    if (TRUE == dst_exists)
+    if (dst_exists)
         HGOTO_ERROR(H5E_OHDR, H5E_EXISTS, FAIL, "destination object already exists")
 
     /* Set up opened group location to fill in */
@@ -1283,10 +1284,10 @@ done:
 } /* end H5O__copy_comm_dt_cmp */
 
 /*-------------------------------------------------------------------------
- * Function:    H5O_copy_search_comm_dt_attr_cb
+ * Function:    H5O__copy_search_comm_dt_attr_cb
  *
  * Purpose:     Callback for H5O_attr_iterate_real from
- *              H5O_copy_search_comm_dt_check.  Checks if the attribute's
+ *              H5O__copy_search_comm_dt_check.  Checks if the attribute's
  *              datatype is committed.  If it is, adds it to the merge
  *              committed dt skiplist present in udata if it does not match
  *              any already present.
@@ -1299,7 +1300,7 @@ done:
  *-------------------------------------------------------------------------
  */
 static herr_t
-H5O_copy_search_comm_dt_attr_cb(const H5A_t *attr, void *_udata)
+H5O__copy_search_comm_dt_attr_cb(const H5A_t *attr, void *_udata)
 {
     H5O_copy_search_comm_dt_ud_t * udata        = (H5O_copy_search_comm_dt_ud_t *)_udata;
     H5T_t *                        dt           = NULL;    /* Datatype */
@@ -1308,7 +1309,7 @@ H5O_copy_search_comm_dt_attr_cb(const H5A_t *attr, void *_udata)
     hbool_t                        obj_inserted = FALSE;   /* Object inserted into skip list */
     herr_t                         ret_value    = SUCCEED; /* Return value */
 
-    FUNC_ENTER_NOAPI_NOINIT
+    FUNC_ENTER_STATIC
 
     /* Sanity checks */
     HDassert(attr);
@@ -1361,10 +1362,10 @@ done:
     }     /* end if */
 
     FUNC_LEAVE_NOAPI(ret_value)
-} /* end H5O_copy_search_comm_dt_attr_cb */
+} /* end H5O__copy_search_comm_dt_attr_cb */
 
 /*-------------------------------------------------------------------------
- * Function:    H5O_copy_search_comm_dt_check
+ * Function:    H5O__copy_search_comm_dt_check
  *
  * Purpose:     Check if the object at obj_oloc is or contains a reference
  *              to a committed datatype.  If it does, adds it to the merge
@@ -1379,7 +1380,7 @@ done:
  *-------------------------------------------------------------------------
  */
 static herr_t
-H5O_copy_search_comm_dt_check(H5O_loc_t *obj_oloc, H5O_copy_search_comm_dt_ud_t *udata)
+H5O__copy_search_comm_dt_check(H5O_loc_t *obj_oloc, H5O_copy_search_comm_dt_ud_t *udata)
 {
     H5O_copy_search_comm_dt_key_t *key          = NULL;  /* Skiplist key */
     haddr_t *                      addr         = NULL;  /* Destination address */
@@ -1388,7 +1389,7 @@ H5O_copy_search_comm_dt_check(H5O_loc_t *obj_oloc, H5O_copy_search_comm_dt_ud_t 
     const H5O_obj_class_t *        obj_class = NULL;     /* Type of object */
     herr_t                         ret_value = SUCCEED;  /* Return value */
 
-    FUNC_ENTER_NOAPI_NOINIT
+    FUNC_ENTER_STATIC
 
     /* Sanity checks */
     HDassert(obj_oloc);
@@ -1457,7 +1458,7 @@ H5O_copy_search_comm_dt_check(H5O_loc_t *obj_oloc, H5O_copy_search_comm_dt_ud_t 
 
     /* Search within attributes */
     attr_op.op_type      = H5A_ATTR_OP_LIB;
-    attr_op.u.lib_op     = H5O_copy_search_comm_dt_attr_cb;
+    attr_op.u.lib_op     = H5O__copy_search_comm_dt_attr_cb;
     udata->obj_oloc.file = obj_oloc->file;
     udata->obj_oloc.addr = obj_oloc->addr;
     if (H5O_attr_iterate_real((hid_t)-1, obj_oloc, H5_INDEX_NAME, H5_ITER_NATIVE, (hsize_t)0, NULL, &attr_op,
@@ -1479,14 +1480,14 @@ done:
     }     /* end if */
 
     FUNC_LEAVE_NOAPI(ret_value)
-} /* end H5O_copy_search_comm_dt_check */
+} /* end H5O__copy_search_comm_dt_check */
 
 /*-------------------------------------------------------------------------
  * Function:    H5O__copy_search_comm_dt_cb
  *
  * Purpose:     H5G_visit callback to add committed datatypes to the merge
  *              committed dt skiplist.  Mostly a wrapper for
- *              H5O_copy_search_comm_dt_check.
+ *              H5O__copy_search_comm_dt_check.
  *
  * Return:      Non-negative on success/Negative on failure
  *
@@ -1529,7 +1530,7 @@ H5O__copy_search_comm_dt_cb(hid_t H5_ATTR_UNUSED group, const char *name, const 
         obj_found = TRUE;
 
         /* Check object and add to skip list if appropriate */
-        if (H5O_copy_search_comm_dt_check(&obj_oloc, udata) < 0)
+        if (H5O__copy_search_comm_dt_check(&obj_oloc, udata) < 0)
             HGOTO_ERROR(H5E_OHDR, H5E_CANTGET, H5_ITER_ERROR, "can't check object")
     } /* end if */
 
@@ -1627,7 +1628,7 @@ H5O__copy_search_comm_dt(H5F_t *file_src, H5O_t *oh_src, H5O_loc_t *oloc_dst /*i
                     H5E_clear_stack(NULL);
                 else
                     /* Check object and add to skip list if appropriate */
-                    if (H5O_copy_search_comm_dt_check(&obj_oloc, &udata) < 0) {
+                    if (H5O__copy_search_comm_dt_check(&obj_oloc, &udata) < 0) {
                     if (H5G_loc_free(&obj_loc) < 0)
                         HERROR(H5E_OHDR, H5E_CANTRELEASE, "can't free location");
                     HGOTO_ERROR(H5E_OHDR, H5E_CANTGET, FAIL, "can't check object")
