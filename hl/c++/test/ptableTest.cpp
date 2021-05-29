1b5c01d19c57590af2083faf0c163205e9926a69
Dana Robinson <derobins@hdfgroup.org>
2021-05-29 15:16:55 -0700

    C++ build and warning updates
    * Fixes all warnings on C++ (with gcc 9.3)
    * Updates CMake and Autotools C++ builds

--- hl/c++/test/ptableTest.cpp
+++ hl/c++/test/ptableTest.cpp
@@ -307,7 +307,7 @@ TestCompress()
         if (HDstrncmp(filter_name, "deflate", 7) != 0)
             H5_FAILED()
     }
-    catch (Exception e) {
+    catch (Exception const &) {
         H5_FAILED();
         return 1;
     }
@@ -605,8 +605,8 @@ const int     STRING_LENGTH = 19; // including terminating NULL
 int
 TestHDFFV_9758()
 {
-    hid_t  strtype;
-    hid_t  compound_type;
+    hid_t  strtype       = H5I_INVALID_HID;
+    hid_t  compound_type = H5I_INVALID_HID;
     herr_t err;
     struct s1_t {
         int    a;
@@ -620,9 +620,9 @@ TestHDFFV_9758()
 
     for (hsize_t i = 0; i < NUM_PACKETS; i++) {
         s1[i].a = static_cast<int>(i);
-        s1[i].b = 1.f * static_cast<float>(i * i);
-        s1[i].c = 1. / (i + 1);
-        HDsprintf(s1[i].d, "string%d", (int)i);
+        s1[i].b = 1.0f * static_cast<float>(i * i);
+        s1[i].c = 1.0 / static_cast<double>(i + 1);
+        HDsprintf(s1[i].d, "string%" PRIuHSIZE "", i);
         s1[i].e = static_cast<int>(100 + i);
     }
 
