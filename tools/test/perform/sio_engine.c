7b4883bb9980ff10f80edb301adda2811ef2eeb5
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2021-06-07 18:08:09 -0500

    Remove private H5_GCC_DIAG_OFF/ON from standalone program (#734)

--- tools/test/perform/sio_engine.c
+++ tools/test/perform/sio_engine.c
@@ -1264,13 +1264,6 @@ done:
  * Programmer:  Albert Cheng 2001/12/12
  * Modifications: Support for file drivers. Christian Chilan, April, 2008
  */
-/* Disable warning for "format not a string literal" here -QAK */
-/*
- *      This pragma only needs to surround the snprintf() calls with
- *      'temp' in the code below, but early (4.4.7, at least) gcc only
- *      allows diagnostic pragmas to be toggled outside of functions.
- */
-H5_GCC_DIAG_OFF("format-nonliteral")
 static void
 do_cleanupfile(iotype iot, char *filename)
 {
@@ -1333,4 +1326,3 @@ do_cleanupfile(iotype iot, char *filename)
         }
     }
 }
-H5_GCC_DIAG_ON("format-nonliteral")
