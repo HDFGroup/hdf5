60fe36e2401895a6b6707099c126956b95bdd321
Larry Knox <lrknox@hdfgroup.org>
2021-07-26 12:51:02 -0500

    Remove line added to generated hl/src/H5LTanalyze.c file. Issue #854 created for creating a fix.

--- hl/src/H5LTanalyze.c
+++ hl/src/H5LTanalyze.c
@@ -1947,7 +1947,7 @@ static int yy_get_next_buffer (void)
 					yyrestart( yyin );
 
 					/*FALLTHROUGH*/
-                                        __attribute__((fallthrough)); 
+                                         
 				case EOB_ACT_END_OF_FILE:
 					{
 					if ( yywrap(  ) )
