44083f0fd6fc8305b6a00233dd2537329651ac6d
Larry Knox <lrknox@hdfgroup.org>
2021-09-09 13:30:12 -0500

    Commit changes to Makefile.ins and 4 src/H5E*.h files from running bin/reconfigure.

--- src/H5Eterm.h
+++ src/H5Eterm.h
@@ -20,40 +20,60 @@
 
 /* Reset major error IDs */
     
-H5E_SYM_g=    
-H5E_FILE_g=    
-H5E_DATATYPE_g=    
-H5E_LINK_g=    
 H5E_DATASET_g=    
-H5E_ATOM_g=    
-H5E_RESOURCE_g=    
-H5E_INTERNAL_g=    
-H5E_CACHE_g=    
-H5E_SOHM_g=    
 H5E_FUNC_g=    
-H5E_RS_g=    
-H5E_PLIST_g=    
-H5E_BTREE_g=    
-H5E_NONE_MAJOR_g=    
-H5E_SLIST_g=    
-H5E_DATASPACE_g=    
-H5E_ARGS_g=    
-H5E_REFERENCE_g=    
-H5E_FSPACE_g=    
-H5E_EFL_g=    
-H5E_PLINE_g=    
-H5E_ERROR_g=    
-H5E_ATTR_g=    
-H5E_HEAP_g=    
-H5E_VFL_g=    
-H5E_OHDR_g=    
-H5E_IO_g=    
-H5E_TST_g=    
 H5E_STORAGE_g=    
-H5E_PLUGIN_g= (-1);
+H5E_FILE_g=    
+H5E_SOHM_g=    
+H5E_SYM_g=    
+H5E_PLUGIN_g=    
+H5E_VFL_g=    
+H5E_INTERNAL_g=    
+H5E_BTREE_g=    
+H5E_REFERENCE_g=    
+H5E_DATASPACE_g=    
+H5E_RESOURCE_g=    
+H5E_PLIST_g=    
+H5E_LINK_g=    
+H5E_DATATYPE_g=    
+H5E_RS_g=    
+H5E_HEAP_g=    
+H5E_OHDR_g=    
+H5E_ATOM_g=    
+H5E_ATTR_g=    
+H5E_NONE_MAJOR_g=    
+H5E_IO_g=    
+H5E_SLIST_g=    
+H5E_EFL_g=    
+H5E_TST_g=    
+H5E_ARGS_g=    
+H5E_ERROR_g=    
+H5E_PLINE_g=    
+H5E_FSPACE_g=    
+H5E_CACHE_g= (-1);
 
 /* Reset minor error IDs */
 
+
+/* Generic low-level file I/O errors */    
+H5E_SEEKERROR_g=    
+H5E_READERROR_g=    
+H5E_WRITEERROR_g=    
+H5E_CLOSEERROR_g=    
+H5E_OVERFLOW_g=    
+H5E_FCNTL_g=
+
+/* Resource errors */    
+H5E_NOSPACE_g=    
+H5E_CANTALLOC_g=    
+H5E_CANTCOPY_g=    
+H5E_CANTFREE_g=    
+H5E_ALREADYEXISTS_g=    
+H5E_CANTLOCK_g=    
+H5E_CANTUNLOCK_g=    
+H5E_CANTGC_g=    
+H5E_CANTGETSIZE_g=    
+H5E_OBJOPEN_g=
 
 /* Heap errors */    
 H5E_CANTRESTORE_g=    
@@ -63,15 +83,35 @@ H5E_CANTATTACH_g=
 H5E_CANTUPDATE_g=    
 H5E_CANTOPERATE_g=
 
-/* No error */    
-H5E_NONE_MINOR_g=
+/* Function entry/exit interface errors */    
+H5E_CANTINIT_g=    
+H5E_ALREADYINIT_g=    
+H5E_CANTRELEASE_g=
 
-/* Link related errors */    
-H5E_TRAVERSE_g=    
-H5E_NLINKS_g=    
-H5E_NOTREGISTERED_g=    
-H5E_CANTMOVE_g=    
-H5E_CANTSORT_g=
+/* Property list errors */    
+H5E_CANTGET_g=    
+H5E_CANTSET_g=    
+H5E_DUPCLASS_g=    
+H5E_SETDISALLOWED_g=
+
+/* Free space errors */    
+H5E_CANTMERGE_g=    
+H5E_CANTREVIVE_g=    
+H5E_CANTSHRINK_g=
+
+/* Object header related errors */    
+H5E_LINKCOUNT_g=    
+H5E_VERSION_g=    
+H5E_ALIGNMENT_g=    
+H5E_BADMESG_g=    
+H5E_CANTDELETE_g=    
+H5E_BADITER_g=    
+H5E_CANTPACK_g=    
+H5E_CANTRESET_g=    
+H5E_CANTRENAME_g=
+
+/* System level errors */    
+H5E_SYSERRSTR_g=
 
 /* I/O pipeline errors */    
 H5E_NOFILTER_g=    
@@ -81,21 +121,14 @@ H5E_SETLOCAL_g=
 H5E_NOENCODER_g=    
 H5E_CANTFILTER_g=
 
-/* System level errors */    
-H5E_SYSERRSTR_g=
-
-/* Argument errors */    
-H5E_UNINITIALIZED_g=    
-H5E_UNSUPPORTED_g=    
-H5E_BADTYPE_g=    
-H5E_BADRANGE_g=    
-H5E_BADVALUE_g=
-
 /* Group related errors */    
 H5E_CANTOPENOBJ_g=    
 H5E_CANTCLOSEOBJ_g=    
 H5E_COMPLEN_g=    
 H5E_PATH_g=
+
+/* No error */    
+H5E_NONE_MINOR_g=
 
 /* Plugin errors */    
 H5E_OPENERROR_g=
@@ -111,13 +144,13 @@ H5E_BADFILE_g=
 H5E_TRUNCATED_g=    
 H5E_MOUNT_g=
 
-/* Dataspace errors */    
-H5E_CANTCLIP_g=    
-H5E_CANTCOUNT_g=    
-H5E_CANTSELECT_g=    
-H5E_CANTNEXT_g=    
-H5E_BADSELECT_g=    
-H5E_CANTCOMPARE_g=
+/* Object atom related errors */    
+H5E_BADATOM_g=    
+H5E_BADGROUP_g=    
+H5E_CANTREGISTER_g=    
+H5E_CANTINC_g=    
+H5E_CANTDEC_g=    
+H5E_NOIDS_g=
 
 /* Cache related errors */    
 H5E_CANTFLUSH_g=    
@@ -136,44 +169,32 @@ H5E_CANTDIRTY_g=
 H5E_CANTEXPUNGE_g=    
 H5E_CANTRESIZE_g=
 
-/* Free space errors */    
-H5E_CANTMERGE_g=    
-H5E_CANTREVIVE_g=    
-H5E_CANTSHRINK_g=
-
-/* Datatype conversion errors */    
-H5E_CANTCONVERT_g=    
-H5E_BADSIZE_g=
+/* Link related errors */    
+H5E_TRAVERSE_g=    
+H5E_NLINKS_g=    
+H5E_NOTREGISTERED_g=    
+H5E_CANTMOVE_g=    
+H5E_CANTSORT_g=
 
 /* Parallel MPI errors */    
 H5E_MPI_g=    
 H5E_MPIERRSTR_g=    
 H5E_CANTRECV_g=
 
-/* Property list errors */    
-H5E_CANTGET_g=    
-H5E_CANTSET_g=    
-H5E_DUPCLASS_g=    
-H5E_SETDISALLOWED_g=
+/* Dataspace errors */    
+H5E_CANTCLIP_g=    
+H5E_CANTCOUNT_g=    
+H5E_CANTSELECT_g=    
+H5E_CANTNEXT_g=    
+H5E_BADSELECT_g=    
+H5E_CANTCOMPARE_g=
 
-/* Generic low-level file I/O errors */    
-H5E_SEEKERROR_g=    
-H5E_READERROR_g=    
-H5E_WRITEERROR_g=    
-H5E_CLOSEERROR_g=    
-H5E_OVERFLOW_g=    
-H5E_FCNTL_g=
-
-/* Object header related errors */    
-H5E_LINKCOUNT_g=    
-H5E_VERSION_g=    
-H5E_ALIGNMENT_g=    
-H5E_BADMESG_g=    
-H5E_CANTDELETE_g=    
-H5E_BADITER_g=    
-H5E_CANTPACK_g=    
-H5E_CANTRESET_g=    
-H5E_CANTRENAME_g=
+/* Argument errors */    
+H5E_UNINITIALIZED_g=    
+H5E_UNSUPPORTED_g=    
+H5E_BADTYPE_g=    
+H5E_BADRANGE_g=    
+H5E_BADVALUE_g=
 
 /* B-tree related errors */    
 H5E_NOTFOUND_g=    
@@ -188,29 +209,8 @@ H5E_CANTLIST_g=
 H5E_CANTMODIFY_g=    
 H5E_CANTREMOVE_g=
 
-/* Resource errors */    
-H5E_NOSPACE_g=    
-H5E_CANTALLOC_g=    
-H5E_CANTCOPY_g=    
-H5E_CANTFREE_g=    
-H5E_ALREADYEXISTS_g=    
-H5E_CANTLOCK_g=    
-H5E_CANTUNLOCK_g=    
-H5E_CANTGC_g=    
-H5E_CANTGETSIZE_g=    
-H5E_OBJOPEN_g=
-
-/* Object atom related errors */    
-H5E_BADATOM_g=    
-H5E_BADGROUP_g=    
-H5E_CANTREGISTER_g=    
-H5E_CANTINC_g=    
-H5E_CANTDEC_g=    
-H5E_NOIDS_g=
-
-/* Function entry/exit interface errors */    
-H5E_CANTINIT_g=    
-H5E_ALREADYINIT_g=    
-H5E_CANTRELEASE_g= (-1);
+/* Datatype conversion errors */    
+H5E_CANTCONVERT_g=    
+H5E_BADSIZE_g= (-1);
 
 #endif /* H5Eterm_H */
