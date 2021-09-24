108c7397372751efe5f953c854673cbcfc7356ca
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2021-09-24 15:02:33 -0500

    1.8 Merge config changes from dev (#1032)
    * Merge config changes from dev
    
    * Revert added git file changes
    
    * Merge MS clang needs EHsc flag #1035

--- config/sanitizer/code-coverage.cmake
+++ config/sanitizer/code-coverage.cmake
@@ -99,8 +99,7 @@ if(CODE_COVERAGE AND NOT CODE_COVERAGE_ADDED)
             ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}
     DEPENDS ccov-clean)
 
-  if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
+  if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
     # Messages
     message(STATUS "Building with llvm Code Coverage Tools")
 
@@ -206,8 +205,7 @@ function(target_code_coverage TARGET_NAME)
   if(CODE_COVERAGE)
 
     # Add code coverage instrumentation to the target's linker command
-    if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang"
-       OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
+    if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
       target_compile_options(${TARGET_NAME} PRIVATE -fprofile-instr-generate
                                                     -fcoverage-mapping)
       set_property(
@@ -229,8 +227,7 @@ function(target_code_coverage TARGET_NAME)
 
     # Add shared library to processing for 'all' targets
     if(target_type STREQUAL "SHARED_LIBRARY" AND target_code_coverage_ALL)
-      if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang"
-         OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
+      if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
         add_custom_target(
           ccov-run-${TARGET_NAME}
           COMMAND echo "-object=$<TARGET_FILE:${TARGET_NAME}>" >>
@@ -250,8 +247,7 @@ function(target_code_coverage TARGET_NAME)
 
     # For executables add targets to run and produce output
     if(target_type STREQUAL "EXECUTABLE")
-      if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang"
-         OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Cc]lang")
+      if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?Cc]lang")
 
         # If there are shared objects to also work with, generate the string to
         # add them here
@@ -409,8 +405,7 @@ endfunction()
 # any subdirectories. To add coverage instrumentation to only specific targets,
 # use `target_code_coverage`.
 function(add_code_coverage)
-  if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang"
-     OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
+  if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
     add_compile_options(-fprofile-instr-generate -fcoverage-mapping)
     add_link_options(-fprofile-instr-generate -fcoverage-mapping)
   elseif(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
@@ -435,8 +430,7 @@ function(add_code_coverage_all_targets)
                         "${multi_value_keywords}" ${ARGN})
 
   if(CODE_COVERAGE)
-    if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang"
-       OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
+    if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
 
       # Merge the profile data for all of the run executables
       add_custom_target(
