383fd055affb3e003828fc42fba726583ebeebca
myang6 <myang6@hdfgroup.org>
2021-10-18 17:17:26 -0500

    Make sure to check the file point.

--- src/H5Fvfd_swmr.c
+++ src/H5Fvfd_swmr.c
@@ -1923,6 +1923,8 @@ H5F_post_vfd_swrm_log_entry(H5F_t *f, int entry_type_code, char *body)
     unsigned int    elap_min, elap_sec, elap_msec;
 
     FUNC_ENTER_NOAPI(FAIL)
+    if(f==NULL) 
+        HGOTO_DONE(TRUE)
     if(f->shared->vfd_swmr_log_on == false)
         HGOTO_DONE(TRUE)
     if(HDclock_gettime(CLOCK_MONOTONIC, &current_time)<0) 
