6d4d0fb13db06895fc6ea5f98ae0b79b44aa98af
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2021-10-20 08:25:06 -0500

    Move test utilities to utils/test folder (#1109)
    * Move test utilities to utils/test folder
    
    * Fix makefile assignment
    
    * Add new dir
    
    * add new folder
    
    * Correct copied makefile
    
    * Fix dir typo
    
    * Add missing include dir
    
    * Remove unnecessary lib link
    
    * Correct dependent dirs
    
    * Fix conditional checks
    
    * Disable test if not built
    
    * fix path to executable
    
    * Use fixture for swmr_vfd check
    
    * Add release note
    
    * Correct shell tests and c++ flag warning
    
    * Update autotools c++ warning
    
    * Fix typo

--- tools/test/h5repack/CMakeVFDTests.cmake
+++ tools/test/h5repack/CMakeVFDTests.cmake
@@ -16,36 +16,7 @@
 ##############################################################################
 ##############################################################################
 
-set (VFD_LIST
-    sec2
-    stdio
-    core
-    core_paged
-    split
-    multi
-    family
-    splitter
-    #log - log VFD currently has file space allocation bugs
-)
-
-if (H5_HAVE_DIRECT)
-  set (VFD_LIST ${VFD_LIST} direct)
-endif ()
-if (H5_HAVE_PARALLEL)
-  set (VFD_LIST ${VFD_LIST} mpio)
-endif ()
-if (H5_HAVE_MIRROR_VFD)
-  set (VFD_LIST ${VFD_LIST} mirror)
-endif ()
-if (H5_HAVE_ROS3_VFD)
-  set (VFD_LIST ${VFD_LIST} ros3)
-endif ()
-if (H5_HAVE_LIBHDFS)
-  set (VFD_LIST ${VFD_LIST} hdfs)
-endif ()
-if (H5_HAVE_WINDOWS)
-  set (VFD_LIST ${VFD_LIST} windows)
-endif ()
+H5_SET_VFD_LIST()
 
 ##############################################################################
 ##############################################################################
@@ -55,7 +26,6 @@ endif ()
 
 macro (ADD_VFD_TEST vfdname resultcode)
   if (NOT HDF5_ENABLE_USING_MEMCHECKER)
-    file (MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/${vfdname}")
     add_test (
         NAME H5REPACK-${vfdname}-h5repacktest-clear-objects
         COMMAND ${CMAKE_COMMAND} -E remove
