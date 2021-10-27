97d0cdc06d5ce18e8e8e27c93f48c59aca22a526
Allen Byrne <byrn@hdfgroup.org>
2021-10-27 13:58:19 -0500

    Adjust help text

--- tools/src/h5repack/h5repack_main.c
+++ tools/src/h5repack/h5repack_main.c
@@ -97,7 +97,7 @@ usage(const char *prog)
     PRINTVALSTREAM(rawoutstream,
                    "   -v N, --verbose=N       Verbose mode, print object information.\n");
     PRINTVALSTREAM(rawoutstream,
-                  "    N - is an integer greater than 1, 2 displays read/write timing\n");
+                  "       N - is an integer greater than 1, 2 displays read/write timing\n");
     PRINTVALSTREAM(rawoutstream, "   -V, --version           Print version number and exit\n");
     PRINTVALSTREAM(rawoutstream, "   -n, --native            Use a native HDF5 type when repacking\n");
     PRINTVALSTREAM(rawoutstream,
