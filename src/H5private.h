121c10d772f3d445615244547883bfba2c26743e
Scot Breitenfeld <brtnfld@hdfgroup.org>
2021-11-05 15:40:18 -0500

    misc

--- src/H5private.h
+++ src/H5private.h
@@ -682,7 +682,7 @@ typedef off_t       h5_stat_size_t;
 #define HDcosh(X) cosh(X)
 #endif
 #ifndef HDcreat
-#define HDcreat(S, M) create(S, M)
+#define HDcreat(S, M) creat(S, M)
 #endif
 #ifndef HDctermid
 #define HDctermid(S) ctermid(S)
