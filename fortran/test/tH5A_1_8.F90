ed1bd26510172362ab823dc865cf8583bfc70c1d
Scot Breitenfeld <brtnfld@hdfgroup.org>
2021-12-06 18:22:37 -0600

    fixed variable in fortran test

--- fortran/test/tH5A_1_8.F90
+++ fortran/test/tH5A_1_8.F90
@@ -777,7 +777,6 @@ SUBROUTINE test_attr_info_by_idx(new_format, fcpl, fapl, total_error)
   INTEGER :: Input1
   INTEGER(HSIZE_T) :: hzero = 0_HSIZE_T
   INTEGER :: minusone = -1
-  INTEGER(HSIZE_T) :: html
 
   data_dims = 0
 
@@ -840,10 +839,6 @@ SUBROUTINE test_attr_info_by_idx(new_format, fcpl, fapl, total_error)
            my_dataset = dset3
         END SELECT
 
-        !  Check for query on non-existent attribute
-
-        n = 0
-
         ! -- CHECK PASSING AN INTEGER CONSTANT IN DIFFERENT FORMS --
 
         ! 1) call by passing an integer with the _hsize_t declaration
@@ -896,8 +891,8 @@ SUBROUTINE test_attr_info_by_idx(new_format, fcpl, fapl, total_error)
            !  Verify information for new attribute
 
 !EP        CALL attr_info_by_idx_check(my_dataset, attrname, INT(j,HSIZE_T), use_index(i), total_error )
-           html = j
-           CALL attr_info_by_idx_check(my_dataset, attrname, html, use_index(i), total_error )
+           n = INT(j, HSIZE_T)
+           CALL attr_info_by_idx_check(my_dataset, attrname, n, use_index(i), total_error )
 
            !CHECK(ret, FAIL, "attr_info_by_idx_check");
         ENDDO
