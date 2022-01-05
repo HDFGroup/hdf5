28477a4f42462191a91b882c5ac902616a56112e
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-01-05 14:15:58 -0600

    OESS-168: Address @lrknox and @gnuoyd reviews.

--- utils/mirror_vfd/mirror_writer.c
+++ utils/mirror_vfd/mirror_writer.c
@@ -868,7 +868,7 @@ receive_communique(struct mirror_session *session, struct sock_comm *comm)
     mirror_log(session->loginfo, V_INFO, "received %zd bytes", read_ret);
     if (HEXDUMP_XMITS) {
         mirror_log(session->loginfo, V_ALL, "```", read_ret);
-        mirror_log_bytes(session->loginfo, V_ALL, (size_t)read_ret, (const unsigned char *)comm->raw);
+        mirror_log_bytes(session->loginfo, V_ALL, read_ret, (const unsigned char *)comm->raw);
         mirror_log(session->loginfo, V_ALL, "```");
     } /* end if hexdump transmissions received */
 
