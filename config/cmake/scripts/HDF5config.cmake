abb0ed979001ba5e1407f3080ce1936797153a53
Larry Knox <lrknox@hdfgroup.org>
2022-02-28 08:47:59 -0600

    Update version to 1.13.2-1 after 1.13.1 release. Add Makefile.in to MANIFEST for addition of utils/tools and h5dwalk.

--- config/cmake/scripts/HDF5config.cmake
+++ config/cmake/scripts/HDF5config.cmake
@@ -37,7 +37,7 @@ cmake_minimum_required (VERSION 3.12)
 #     CTEST_SOURCE_NAME  -  source folder
 ##############################################################################
 
-set (CTEST_SOURCE_VERSION "1.13.1")
+set (CTEST_SOURCE_VERSION "1.13.2")
 set (CTEST_SOURCE_VERSEXT "-1")
 
 ##############################################################################
