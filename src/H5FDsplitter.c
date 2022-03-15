9a16876dc74e536fcdd3c415c2798cc61108e8cf
Dana Robinson <derobins@hdfgroup.org>
2022-03-15 11:47:46 -0700

    Misc fixes highlighted by the checkposix script

--- src/H5FDsplitter.c
+++ src/H5FDsplitter.c
@@ -527,20 +527,20 @@ H5FD__splitter_get_default_wo_path(char *new_path, size_t new_path_len, const ch
         HGOTO_ERROR(H5E_VFL, H5E_CANTSET, FAIL, "filename exceeds max length")
 
     /* Determine if filename contains a ".h5" extension. */
-    if ((file_extension = strstr(base_filename, ".h5"))) {
+    if ((file_extension = HDstrstr(base_filename, ".h5"))) {
         /* Insert the suffix between the filename and ".h5" extension. */
         HDstrcpy(new_path, base_filename);
-        file_extension = strstr(new_path, ".h5");
+        file_extension = HDstrstr(new_path, ".h5");
         HDsprintf(file_extension, "%s%s", suffix, ".h5");
     }
-    else if ((file_extension = strrchr(base_filename, '.'))) {
+    else if ((file_extension = HDstrrchr(base_filename, '.'))) {
         char *new_extension_loc = NULL;
 
         /* If the filename doesn't contain a ".h5" extension, but contains
          * AN extension, just insert the suffix before that extension.
          */
         HDstrcpy(new_path, base_filename);
-        new_extension_loc = strrchr(new_path, '.');
+        new_extension_loc = HDstrrchr(new_path, '.');
         HDsprintf(new_extension_loc, "%s%s", suffix, file_extension);
     }
     else {
