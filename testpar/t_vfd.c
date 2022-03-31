08879aef3c23d22b9fcfa00a6093f8e6644c0535
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2022-03-31 11:42:39 -0500

    fixed spacing

--- testpar/t_vfd.c
+++ testpar/t_vfd.c
@@ -2697,6 +2697,7 @@ vector_write_test_2(int file_name_id, int mpi_rank, int mpi_size, H5FD_mpio_xfer
         else {
 
             H5_FAILED();
+
             if ((disp_failure_mssgs) || (show_progress)) {
                 HDfprintf(stdout, "%s: failure_mssg = \"%s\"\n", fcn_name, failure_mssg);
             }
@@ -4301,6 +4302,7 @@ vector_write_test_7(int file_name_id, int mpi_rank, int mpi_size, H5FD_mpio_xfer
     return (!pass);
 
 } /* vector_write_test_7() */
+
 /*-------------------------------------------------------------------------
  * Function:    main
  *
