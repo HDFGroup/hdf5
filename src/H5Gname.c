5f00066eacdf2b2ddb3cf92635bea99eb1aee85e
jhendersonHDF <jhenderson@hdfgroup.org>
2022-05-03 12:02:01 -0500

    Hdf5 1 12 Miscellaneous warnings fixes (#1718)
    * Fixes const issues in the version 2 B-trees (#1172)
    
    The operations that were changed are fundamentally not const since the
    shadow operation can modify the node structure when SWMR is in use.
    
    * Quiets const warning in H5RS code (#1181)
    
    * Avoid calling H5Ropen_object with a misaligned H5R_ref_t: copy the (#1171)
    
    * Avoid calling H5Ropen_object with a misaligned H5R_ref_t: copy the
    raw H5R_ref_t bytes to a heap buffer that's known to have the right
    alignment.
    
    * Committing clang-format changes
    
    * Use an automatic H5R_ref_t instead of malloc'ing one.  Go ahead and
    initialize the H5R_ref_t to all-0s so that arbitrary stack content
    doesn't foul things up.  Bail out with an error if `size` exceeds
    `sizeof(H5R_ref_t)`.
    
    * Committing clang-format changes
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Miscellaneous warnings fixes
    
    Co-authored-by: Dana Robinson <43805+derobins@users.noreply.github.com>
    Co-authored-by: David Young <dyoung@hdfgroup.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/H5Gname.c
+++ src/H5Gname.c
@@ -307,7 +307,7 @@ H5G__build_fullpath(const char *prefix, const char *name)
     /* Build full path */
     HDstrncpy(full_path, prefix, orig_path_len + 1);
     if (need_sep)
-        HDstrncat(full_path, "/", (size_t)1);
+        HDstrcat(full_path, "/");
     HDstrncat(full_path, name, name_len);
 
     /* Create reference counted string for path */
