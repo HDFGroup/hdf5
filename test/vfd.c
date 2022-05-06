a8d506ffce6edbf4b9b14bc8fa3f5afcd0f96e3d
vchoi <vchoi@jelly.ad.hdfgroup.org>
2022-05-06 16:26:58 -0500

    Fix failing check for Windows: Add the VFD SWMR feature flag also to test_windows() (which is sec2) in test/vfd.c.

--- test/vfd.c
+++ test/vfd.c
@@ -177,6 +177,8 @@ test_sec2(void)
         TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_SUPPORTS_SWMR_IO))
         TEST_ERROR
+    if (!(driver_flags & H5FD_FEAT_SUPPORTS_VFD_SWMR))
+        TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_DEFAULT_VFD_COMPATIBLE))
         TEST_ERROR
     /* Check for extra flags not accounted for above */
@@ -319,6 +321,8 @@ test_core(void)
     if (!(driver_flags & H5FD_FEAT_AGGREGATE_SMALLDATA))
         TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_ALLOW_FILE_IMAGE))
+        TEST_ERROR
+    if (!(driver_flags & H5FD_FEAT_SUPPORTS_VFD_SWMR))
         TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_CAN_USE_FILE_IMAGE_CALLBACKS))
         TEST_ERROR
@@ -2036,6 +2040,8 @@ test_stdio(void)
         TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_DATA_SIEVE))
         TEST_ERROR
+    if (!(driver_flags & H5FD_FEAT_SUPPORTS_VFD_SWMR))
+        TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_AGGREGATE_SMALLDATA))
         TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_DEFAULT_VFD_COMPATIBLE))
@@ -2161,12 +2167,15 @@ test_windows(void)
         TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_SUPPORTS_SWMR_IO))
         TEST_ERROR
+    if (!(driver_flags & H5FD_FEAT_SUPPORTS_VFD_SWMR))
+        TEST_ERROR
     if (!(driver_flags & H5FD_FEAT_DEFAULT_VFD_COMPATIBLE))
         TEST_ERROR
     /* Check for extra flags not accounted for above */
     if (driver_flags != (H5FD_FEAT_AGGREGATE_METADATA | H5FD_FEAT_ACCUMULATE_METADATA | H5FD_FEAT_DATA_SIEVE |
                          H5FD_FEAT_AGGREGATE_SMALLDATA | H5FD_FEAT_POSIX_COMPAT_HANDLE |
-                         H5FD_FEAT_SUPPORTS_SWMR_IO | H5FD_FEAT_DEFAULT_VFD_COMPATIBLE))
+                         H5FD_FEAT_SUPPORTS_SWMR_IO | H5FD_FEAT_SUPPORTS_VFD_SWMR |
+                         H5FD_FEAT_DEFAULT_VFD_COMPATIBLE))
         TEST_ERROR
 
     if ((file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, fapl)) < 0)
