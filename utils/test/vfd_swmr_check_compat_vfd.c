6c35efd341d2ced97833d26c16bd98854c780b5e
Dana Robinson <derobins@hdfgroup.org>
2022-05-11 15:52:38 -0700

    Check for parallel HDF5 before running SWMR acceptance tests

---
+++ utils/test/vfd_swmr_check_compat_vfd.c
@@ -0,0 +1,66 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Board of Trustees of the University of Illinois.         *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the root of the source code       *
+ * distribution tree, or in https://www.hdfgroup.org/licenses.               *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* Purpose:     This is a small program that checks if the HDF5_DRIVER
+ *              environment variable is set to a value that supports SWMR.
+ *              It also checks for parallel HDF5, where page buffering
+ *              is disabled and thus can't be used for VFD SWMR.
+ *
+ *              It is intended for use in shell scripts.
+ */
+
+#include "h5test.h"
+
+/* This file needs to access the file driver testing code */
+#define H5FD_FRIEND /*suppress error about including H5FDpkg            */
+#define H5FD_TESTING
+#include "H5FDpkg.h" /* File drivers                                     */
+
+/*-------------------------------------------------------------------------
+ * Function:    main
+ *
+ * Purpose:     Inspects the HDF5_DRIVER environment variable, which
+ *              determines the VFD that the test harness will use with
+ *              the majority of the tests.
+ *
+ *              Also checks for parallel HDF5, which does not support
+ *              VFD SWMR.
+ *
+ * Return:      VFD supports SWMR:          EXIT_SUCCESS
+ *
+ *              VFD does not support SWMR
+ *              or failure:                 EXIT_FAILURE
+ *
+ *-------------------------------------------------------------------------
+ */
+int
+main(void)
+{
+#ifdef H5_HAVE_PARALLEL
+
+    return EXIT_FAILURE;
+
+#else
+
+    char *driver = NULL;
+
+    driver = HDgetenv(HDF5_DRIVER);
+
+    if (H5FD__supports_swmr_test(driver))
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
+
+#endif
+
+} /* end main() */
