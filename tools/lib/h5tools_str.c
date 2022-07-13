HDFGroup/hdf5

commit b9d1d66ab84b492691d3c73e71b1374a7da19e50

Develop HDFFV-11310 (#1811)
* Rework java vl check in read/write. Handle old refs in h5dump

* Committing clang-format changes

* Java changes allow default VL reads

* Fix JNI utility for old refs

* HDFFV-11310 - implement vlen read/write for atomic types.

* format check fix

* Mostly format fixes

* More format issues

* Two format changes

* Use JNI names for sizeof

* format change

* fix size typo

* Change to older method to initialize list

* remove unused var

* format fix

* switch writeVL functions to use datatype instead of java type

* Add VL option to generic read/write check

* Correct function name typo

* Add JIRA issue

* Correct note to match change in code.

* HDFFV-11318 add VL references as byte arrays

* Add release note and format changes

* Another format update

* Fix unreleased allocations

* Fix format

* format correction

Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

tools/lib/h5tools_str.c
@@ -1202,10 +1202,49 @@ h5tools_str_sprint(h5tools_str_t *str, const h5tool_format_t *info, hid_t contai
                     else if (H5Tequal(type, H5T_STD_REF_DSETREG)) {
                         /* if(nsize == H5R_DSET_REG_REF_BUF_SIZE) */
                         H5TOOLS_DEBUG("H5T_REFERENCE:H5T_STD_REF_DSETREG");
+                        h5tools_str_append(str, H5_TOOLS_DATASET);
+                        h5tools_str_sprint_old_reference(str, container, vp);
                     }
                     else if (H5Tequal(type, H5T_STD_REF_OBJ)) {
                         /* if (nsize == H5R_OBJ_REF_BUF_SIZE) */
+                        /*
+                         * Object references -- show the type and OID of the referenced object.
+                         */
+                        H5O_info1_t oi;
+
                         H5TOOLS_DEBUG("H5T_REFERENCE:H5T_STD_REF_OBJ");
+                        obj = H5Rdereference2(container, H5P_DEFAULT, H5R_OBJECT, vp);
+                        H5Oget_info2(obj, &oi, H5O_INFO_BASIC);
+
+                        /* Print object type and close object */
+                        switch (oi.type) {
+                            case H5O_TYPE_GROUP:
+                                h5tools_str_append(str, H5_TOOLS_GROUP);
+                                break;
+
+                            case H5O_TYPE_DATASET:
+                                h5tools_str_append(str, H5_TOOLS_DATASET);
+                                break;
+
+                            case H5O_TYPE_NAMED_DATATYPE:
+                                h5tools_str_append(str, H5_TOOLS_DATATYPE);
+                                break;
+
+                            case H5O_TYPE_UNKNOWN:
+                            case H5O_TYPE_NTYPES:
+                            default:
+                                h5tools_str_append(str, "%u-", (unsigned)oi.type);
+                                break;
+                        } /* end switch */
+                        H5Oclose(obj);
+
+                        /* Print OID */
+                        if (info->obj_hidefileno)
+                            h5tools_str_append(str, info->obj_format, oi.addr);
+                        else
+                            h5tools_str_append(str, info->obj_format, oi.fileno, oi.addr);
+
+                        h5tools_str_sprint_old_reference(str, container, vp);
                     } /* end else if (H5Tequal(type, H5T_STD_REF_OBJ)) */
                 }
                 break;
@@ -1286,6 +1325,7 @@ h5tools_str_sprint(h5tools_str_t *str, const h5tool_format_t *info, hid_t contai
                 nelmts = ((hvl_t *)((void *)cp_vp))->len;
 
                 for (i = 0; i < nelmts; i++) {
+                    H5TOOLS_DEBUG("H5T_VLEN %d of %ld", i, nelmts);
                     if (i)
                         h5tools_str_append(str, "%s", OPT(info->vlen_sep, "," OPTIONAL_LINE_BREAK));
 
@@ -1346,6 +1386,48 @@ h5tools_str_sprint(h5tools_str_t *str, const h5tool_format_t *info, hid_t contai
 
     H5TOOLS_ENDDEBUG(" with %s", ret_value);
     return ret_value;
+}
+
+/*-------------------------------------------------------------------------
+ * Function:    h5tools_str_sprint_old_reference
+ *
+ * Purpose: Object reference -- show the name of the old referenced object.
+ *
+ * Return:  Nothing
+ *-------------------------------------------------------------------------
+ */
+void
+h5tools_str_sprint_old_reference(h5tools_str_t *str, hid_t container, void *vp)
+{
+    hid_t obj    = H5I_INVALID_HID;
+    hid_t region = H5I_INVALID_HID;
+    char  ref_name[1024];
+
+    H5TOOLS_START_DEBUG(" ");
+
+    h5tools_str_append(str, " \"");
+    obj = H5Rdereference2(container, H5P_DEFAULT, H5R_DATASET_REGION, vp);
+    if (obj >= 0) {
+        region = H5Rget_region(container, H5R_DATASET_REGION, vp);
+        if (region >= 0) {
+            H5Rget_name(obj, H5R_DATASET_REGION, vp, (char *)ref_name, 1024);
+            h5tools_str_append(str, "%s", ref_name);
+
+            H5Sclose(region);
+        } /* end if (region >= 0) */
+        H5Dclose(obj);
+    } /* end if (obj >= 0) */
+    else {
+        obj = H5Rdereference2(container, H5P_DEFAULT, H5R_OBJECT, vp);
+        if (obj >= 0) {
+            H5Rget_name(obj, H5R_OBJECT, vp, (char *)ref_name, 1024);
+            h5tools_str_append(str, "%s", ref_name);
+            H5Dclose(obj);
+        }
+    }
+    h5tools_str_append(str, "\"");
+
+    H5TOOLS_ENDDEBUG(" ");
 }
 
 /*-------------------------------------------------------------------------

config/sanitizer/code-coverage.cmake
@@ -214,7 +214,7 @@ function(target_code_coverage TARGET_NAME)
     # Add code coverage instrumentation to the target's linker command
     if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
       target_compile_options(${TARGET_NAME} PRIVATE -fprofile-instr-generate
-                                                    -fcoverage-mapping)
+                                                    -fcoverage-mapping --coverage)
       set_property(
         TARGET ${TARGET_NAME}
         APPEND_STRING
@@ -225,7 +225,7 @@ function(target_code_coverage TARGET_NAME)
         PROPERTY LINK_FLAGS "-fcoverage-mapping ")
     elseif(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
       target_compile_options(${TARGET_NAME} PRIVATE -fprofile-arcs
-                                                    -ftest-coverage)
+                                                    -ftest-coverage --coverage)
       target_link_libraries(${TARGET_NAME} PRIVATE gcov)
     endif()
 
@@ -413,10 +413,10 @@ endfunction()
 # use `target_code_coverage`.
 function(add_code_coverage)
   if(CMAKE_C_COMPILER_ID MATCHES "[Cc]lang" OR CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
-    add_compile_options(-fprofile-instr-generate -fcoverage-mapping)
-    add_link_options(-fprofile-instr-generate -fcoverage-mapping)
+    add_compile_options(-fprofile-instr-generate -fcoverage-mapping --coverage)
+    add_link_options(-fprofile-instr-generate -fcoverage-mapping --coverage)
   elseif(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-    add_compile_options(-fprofile-arcs -ftest-coverage)
+    add_compile_options(-fprofile-arcs -ftest-coverage --coverage)
     link_libraries(gcov)
   endif()
 endfunction()

java/src/hdf/hdf5lib/H5.java
@@ -1242,6 +1242,10 @@ public class H5 implements java.io.Serializable {
             log.trace("H5Aread_string type");
             status = H5Aread_string(attr_id, mem_type_id, (String[])obj);
         }
+        else if (H5.H5Tget_class(mem_type_id) == HDF5Constants.H5T_VLEN) {
+            log.trace("H5AreadVL type");
+            status = H5AreadVL(attr_id, mem_type_id, (Object[])obj);
+        }
         else {
             // Create a data buffer to hold the data into a Java Array
             HDFArray theArray = new HDFArray(obj);
@@ -1787,6 +1791,10 @@ public class H5 implements java.io.Serializable {
         else if (is1D && (dataClass.getComponentType() == String.class)) {
             log.trace("H5Dwrite_string type");
             status = H5Awrite_string(attr_id, mem_type_id, (String[])obj);
+        }
+        else if (H5.H5Tget_class(mem_type_id) == HDF5Constants.H5T_VLEN) {
+            log.trace("H5AwriteVL type");
+            status = H5AwriteVL(attr_id, mem_type_id, (Object[])obj);
         }
         else {
             HDFArray theArray = new HDFArray(obj);
@@ -2772,6 +2780,11 @@ public class H5 implements java.io.Serializable {
             status = H5Dread_string(dataset_id, mem_type_id, mem_space_id, file_space_id, xfer_plist_id,
                                     (String[])obj);
         }
+        else if (H5.H5Tget_class(mem_type_id) == HDF5Constants.H5T_VLEN) {
+            log.trace("H5DreadVL type");
+            status =
+                H5DreadVL(dataset_id, mem_type_id, mem_space_id, file_space_id, xfer_plist_id, (Object[])obj);
+        }
         else {
             // Create a data buffer to hold the data into a Java Array
             HDFArray theArray = new HDFArray(obj);
@@ -3449,6 +3462,11 @@ public class H5 implements java.io.Serializable {
             log.trace("H5Dwrite_string type");
             status = H5Dwrite_string(dataset_id, mem_type_id, mem_space_id, file_space_id, xfer_plist_id,
                                      (String[])obj);
+        }
+        else if (H5.H5Tget_class(mem_type_id) == HDF5Constants.H5T_VLEN) {
+            log.trace("H5DwriteVL type");
+            status = H5DwriteVL(dataset_id, mem_type_id, mem_space_id, file_space_id, xfer_plist_id,
+                                (Object[])obj);
         }
         else {
             HDFArray theArray = new HDFArray(obj);
@@ -9356,9 +9374,8 @@ public class H5 implements java.io.Serializable {
         byte[] buf        = theArray.emptyBytes();
 
         int status = H5Pget_fill_value(plist_id, type_id, buf);
-        if (status >= 0) {
+        if (status >= 0)
             obj = theArray.arrayify(buf);
-        }
 
         return status;
     }
@@ -11018,8 +11035,6 @@ public class H5 implements java.io.Serializable {
     public synchronized static native void H5Rdestroy(byte[] ref_ptr)
         throws HDF5LibraryException, NullPointerException, IllegalArgumentException;
 
-    // Info //
-
     /**
      * H5Rget_type retrieves the type of a reference.
      *
@@ -11075,8 +11090,6 @@ public class H5 implements java.io.Serializable {
      **/
     public synchronized static native byte[] H5Rcopy(byte[] src_ref_ptr)
         throws HDF5LibraryException, NullPointerException, IllegalArgumentException;
-
-    // Dereference //
 
     /**
      * H5Ropen_object opens that object and returns an identifier.
@@ -11223,8 +11236,6 @@ public class H5 implements java.io.Serializable {
      **/
     public synchronized static native int H5Rget_obj_type3(byte[] ref_ptr, long rapl_id)
         throws HDF5LibraryException, NullPointerException, IllegalArgumentException;
-
-    // Get name //
 
     /**
      * H5Rget_file_name retrieves the file name for the object, region or attribute reference pointed to.

java/src/hdf/hdf5lib/HDF5Constants.java
@@ -16,7 +16,7 @@ package hdf.hdf5lib;
 import hdf.hdf5lib.structs.H5O_token_t;
 
 /**
- * /** This class contains C constants and enumerated types of HDF5 library. The
+ * This class contains C constants and enumerated types of HDF5 library. The
  * values of these constants are obtained from the library by calling J2C(int
  * jconstant), where jconstant is any of the private constants which start their
  * name with "JH5" need to be converted.
@@ -62,31 +62,62 @@ public class HDF5Constants {
     public static final int H5_ITER_NATIVE = H5_ITER_NATIVE();
     /** Common iteration orders, Number of iteration orders */
     public static final int H5_ITER_N = H5_ITER_N();
-    /** */
+    /** The version of the H5AC_cache_config_t in use */
     public static final int H5AC_CURR_CACHE_CONFIG_VERSION = H5AC_CURR_CACHE_CONFIG_VERSION();
-    /** */
+    /** The maximum length of the trace file path */
     public static final int H5AC_MAX_TRACE_FILE_NAME_LEN = H5AC_MAX_TRACE_FILE_NAME_LEN();
-    /** */
+    /**
+     *    When metadata_write_strategy is set to this value, only process
+     *    zero is allowed to write dirty metadata to disk.  All other
+     *    processes must retain dirty metadata until they are informed at
+     *    a sync point that the dirty metadata in question has been written
+     *    to disk.
+     */
     public static final int H5AC_METADATA_WRITE_STRATEGY_PROCESS_ZERO_ONLY =
         H5AC_METADATA_WRITE_STRATEGY_PROCESS_ZERO_ONLY();
-    /** */
+    /**
+     *    In the distributed metadata write strategy, process zero still makes
+     *    the decisions as to what entries should be flushed, but the actual
+     *    flushes are distributed across the processes in the computation to
+     *    the extent possible.
+     */
     public static final int H5AC_METADATA_WRITE_STRATEGY_DISTRIBUTED =
         H5AC_METADATA_WRITE_STRATEGY_DISTRIBUTED();
-    /** */
+    /** Don't attempt to increase the size of the cache automatically */
     public static final int H5C_incr_off = H5C_incr_off();
-    /** */
+    /**
+     *  Attempt to increase the size of the cache
+     *              whenever the average hit rate over the last epoch drops
+     *              below the value supplied in the lower_hr_threshold
+     *              field
+     */
     public static final int H5C_incr_threshold = H5C_incr_threshold();
-    /** */
+    /** Don't perform flash increases in the size of the cache */
     public static final int H5C_flash_incr_off = H5C_flash_incr_off();
-    /** */
+    /** increase the current maximum cache size by x * flash_multiple less any free space in the cache */
     public static final int H5C_flash_incr_add_space = H5C_flash_incr_add_space();
-    /** */
+    /** Don't attempt to decrease the size of the cache automatically */
     public static final int H5C_decr_off = H5C_decr_off();
-    /** */
+    /**
+     *  Attempt to decrease the size of the cache
+     *              whenever the average hit rate over the last epoch rises
+     *              above the value supplied in the upper_hr_threshold
+     *              field
+     */
     public static final int H5C_decr_threshold = H5C_decr_threshold();
-    /** */
+    /**
+     *  At the end of each epoch, search the cache for
+     *              entries that have not been accessed for at least the number
+     *              of epochs specified in the epochs_before_eviction field, and
+     *              evict these entries
+     */
     public static final int H5C_decr_age_out = H5C_decr_age_out();
-    /** */
+    /**
+     *  Same as age_out, but we only
+     *              attempt to reduce the cache size when the hit rate observed
+     *              over the last epoch exceeds the value provided in the
+     *              upper_hr_threshold field
+     */
     public static final int H5C_decr_age_out_with_threshold = H5C_decr_age_out_with_threshold();
     /** */
     public static final int H5D_CHUNK_IDX_BTREE = H5D_CHUNK_IDX_BTREE();
@@ -912,11 +943,11 @@ public class HDF5Constants {
     /** */
     public static final int H5R_MAXTYPE = H5R_MAXTYPE();
     /** */
-    public static final int H5R_REF_BUF_SIZE = H5R_REF_BUF_SIZE();
+    public static final int H5R_DSET_REG_REF_BUF_SIZE = H5R_DSET_REG_REF_BUF_SIZE();
     /** */
     public static final int H5R_OBJ_REF_BUF_SIZE = H5R_OBJ_REF_BUF_SIZE();
     /** */
-    public static final int H5R_DSET_REG_REF_BUF_SIZE = H5R_DSET_REG_REF_BUF_SIZE();
+    public static final int H5R_REF_BUF_SIZE = H5R_REF_BUF_SIZE();
     /** */
     public static final int H5R_OBJECT = H5R_OBJECT();
     /** */
@@ -2395,11 +2426,11 @@ public class HDF5Constants {
 
     private static native final int H5R_MAXTYPE();
 
-    private static native final int H5R_REF_BUF_SIZE();
+    private static native final int H5R_DSET_REG_REF_BUF_SIZE();
 
     private static native final int H5R_OBJ_REF_BUF_SIZE();
 
-    private static native final int H5R_DSET_REG_REF_BUF_SIZE();
+    private static native final int H5R_REF_BUF_SIZE();
 
     private static native final int H5R_OBJECT();
 

java/src/hdf/hdf5lib/HDF5GroupInfo.java
@@ -158,7 +158,7 @@ public class HDF5GroupInfo {
     public long getOID() { return objno[0]; }
 
     /**
-     * /** Converts this object to a String representation.
+     * Converts this object to a String representation.
      *
      * @return a string representation of this object
      */

java/src/jni/h5aImp.h
@@ -22,7 +22,7 @@ extern "C" {
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Acreate
+ * Method:    _H5Acreate
  * Signature: (JLjava/lang/String;JJJ)J
  */
 JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Acreate(JNIEnv *, jclass, jlong, jstring, jlong, jlong,
@@ -30,14 +30,14 @@ JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Acreate(JNIEnv *, jclass, jlong,
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Aopen_name
+ * Method:    _H5Aopen_name
  * Signature: (JLjava/lang/String;)J
  */
 JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Aopen_1name(JNIEnv *, jclass, jlong, jstring);
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Aopen_idx
+ * Method:    _H5Aopen_idx
  * Signature: (JI)J
  */
 JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Aopen_1idx(JNIEnv *, jclass, jlong, jint);
@@ -137,14 +137,14 @@ JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Awrite_1double(JNIEnv *, jclass, jl
 /*
  * Class:     hdf_hdf5lib_H5
  * Method:    H5AreadVL
- * Signature: (JJ[Ljava/lang/String;)I
+ * Signature: (JJ[Ljava/util/ArrayList;)I
  */
 JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5AreadVL(JNIEnv *, jclass, jlong, jlong, jobjectArray);
 
 /*
  * Class:     hdf_hdf5lib_H5
  * Method:    H5AwriteVL
- * Signature: (JJ[Ljava/lang/String;)I
+ * Signature: (JJ[Ljava/util/ArrayList;)I
  */
 JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5AwriteVL(JNIEnv *, jclass, jlong, jlong, jobjectArray);
 
@@ -185,14 +185,14 @@ JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Aread_1reg_1ref(JNIEnv *, jclass, j
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Aget_space
+ * Method:    _H5Aget_space
  * Signature: (J)J
  */
 JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Aget_1space(JNIEnv *, jclass, jlong);
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Aget_type
+ * Method:    _H5Aget_type
  * Signature: (J)J
  */
 JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Aget_1type(JNIEnv *, jclass, jlong);
@@ -220,7 +220,7 @@ JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Adelete(JNIEnv *, jclass, jlong, js
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Aclose
+ * Method:    _H5Aclose
  * Signature: (J)I
  */
 JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5__1H5Aclose(JNIEnv *, jclass, jlong);
@@ -350,7 +350,7 @@ JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Aopen_1by_1name(JNIEnv *, jclass
 
 /*
  * Class:     hdf_hdf5lib_H5
- * Method:    H5Aget_create_plist
+ * Method:    _H5Aget_create_plist
  * Signature: (J)J
  */
 JNIEXPORT jlong JNICALL Java_hdf_hdf5lib_H5__1H5Aget_1create_1plist(JNIEnv *, jclass, jlong);

commit 077e6c30e71540698f5ed31f0b5cec389a443ed9

Fix for a tools compile issue when deprecated symbols are disabled
Introduced in #1811, also uses VOL token instead of addr

tools/lib/h5tools_str.c
@@ -1196,7 +1196,7 @@ h5tools_str_sprint(h5tools_str_t *str, const h5tool_format_t *info, hid_t contai
                             case H5R_MAXTYPE:
                             default:
                                 break;
-                        } /* end switch */
+                        }
                         H5TOOLS_DEBUG("H5T_REFERENCE:H5T_STD_REF end");
                     }
                     else if (H5Tequal(type, H5T_STD_REF_DSETREG)) {
@@ -1210,11 +1210,12 @@ h5tools_str_sprint(h5tools_str_t *str, const h5tool_format_t *info, hid_t contai
                         /*
                          * Object references -- show the type and OID of the referenced object.
                          */
-                        H5O_info1_t oi;
+                        H5O_info2_t oi;
+                        char *      obj_tok_str = NULL;
 
                         H5TOOLS_DEBUG("H5T_REFERENCE:H5T_STD_REF_OBJ");
                         obj = H5Rdereference2(container, H5P_DEFAULT, H5R_OBJECT, vp);
-                        H5Oget_info2(obj, &oi, H5O_INFO_BASIC);
+                        H5Oget_info3(obj, &oi, H5O_INFO_BASIC);
 
                         /* Print object type and close object */
                         switch (oi.type) {
@@ -1235,14 +1236,21 @@ h5tools_str_sprint(h5tools_str_t *str, const h5tool_format_t *info, hid_t contai
                             default:
                                 h5tools_str_append(str, "%u-", (unsigned)oi.type);
                                 break;
-                        } /* end switch */
+                        }
                         H5Oclose(obj);
 
                         /* Print OID */
+                        H5Otoken_to_str(obj, &oi.token, &obj_tok_str);
+
                         if (info->obj_hidefileno)
-                            h5tools_str_append(str, info->obj_format, oi.addr);
+                            h5tools_str_append(str, info->obj_format, obj_tok_str);
                         else
-                            h5tools_str_append(str, info->obj_format, oi.fileno, oi.addr);
+                            h5tools_str_append(str, info->obj_format, oi.fileno, obj_tok_str);
+
+                        if (obj_tok_str) {
+                            H5free_memory(obj_tok_str);
+                            obj_tok_str = NULL;
+                        }
 
                         h5tools_str_sprint_old_reference(str, container, vp);
                     } /* end else if (H5Tequal(type, H5T_STD_REF_OBJ)) */