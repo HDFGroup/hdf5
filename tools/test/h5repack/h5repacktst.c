2d0100401ff0cc2967a8b5bfe8f557995f2166c8
derobins <derobins@hdfgroup.org>
2022-07-18 02:45:17 -0700

    Fixes warnings when the deflate filter is not built

--- tools/test/h5repack/h5repacktst.c
+++ tools/test/h5repack/h5repacktst.c
@@ -2530,8 +2530,12 @@ make_deflate(hid_t loc_id)
     hid_t      sid              = H5I_INVALID_HID; /* dataspace ID */
     hsize_t    dims[RANK]       = {DIM1, DIM2};
     hsize_t    chunk_dims[RANK] = {CDIM1, CDIM2};
+#if defined(H5_HAVE_FILTER_DEFLATE)
     hobj_ref_t bufref[1]; /* reference */
     hsize_t    dims1r[1] = {1};
+#else
+    (void)loc_id;
+#endif
 
     /* Create and fill array */
     struct {
