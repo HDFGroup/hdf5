21aa61e3397802c70ed509e4f147a028814aedcd
Mark Kittisopikul <markkitt@gmail.com>
2022-08-04 22:56:36 -0400

    Revert "Revert H5Dchunk_iter changes in hdf5_1_12_1 (#733)"
    This reverts commit 10abe9a8b4985a18a0c88615686184d233d7b845.

--- src/H5Dpkg.h
+++ src/H5Dpkg.h
@@ -565,6 +565,7 @@ H5_DLL herr_t  H5D__get_chunk_info(const H5D_t *dset, const H5S_t *space, hsize_
                                    unsigned *filter_mask, haddr_t *offset, hsize_t *size);
 H5_DLL herr_t  H5D__get_chunk_info_by_coord(const H5D_t *dset, const hsize_t *coord, unsigned *filter_mask,
                                             haddr_t *addr, hsize_t *size);
+H5_DLL herr_t  H5D__chunk_iter(const H5D_t *dset, H5D_chunk_iter_op_t cb, void *op_data);
 H5_DLL haddr_t H5D__get_offset(const H5D_t *dset);
 H5_DLL herr_t  H5D__vlen_get_buf_size(H5D_t *dset, hid_t type_id, hid_t space_id, hsize_t *size);
 H5_DLL herr_t  H5D__vlen_get_buf_size_gen(H5VL_object_t *vol_obj, hid_t type_id, hid_t space_id,
