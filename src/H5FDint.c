fc7ea57b2aa7ddee52e791cf9d322085a9698caf
Neil Fortner <nfortne2@hdfgroup.org>
2022-09-12 11:48:53 -0500

    Fix memory bug in selection I/O

--- src/H5FDint.c
+++ src/H5FDint.c
@@ -1170,7 +1170,7 @@ H5FD_read_selection(H5FD_t *file, H5FD_mem_t type, uint32_t count, H5S_t **mem_s
 
             if ((file_space_ids[num_spaces] = H5I_register(H5I_DATASPACE, file_spaces[num_spaces], TRUE)) <
                 0) {
-                if (H5I_dec_app_ref(mem_space_ids[num_spaces]) < 0)
+                if (NULL == H5I_remove(mem_space_ids[num_spaces]))
                     HDONE_ERROR(H5E_VFL, H5E_CANTDEC, FAIL, "problem freeing id")
                 HGOTO_ERROR(H5E_VFL, H5E_CANTREGISTER, FAIL, "unable to register dataspace ID")
             }
@@ -1200,11 +1200,13 @@ done:
         }
     }
 
-    /* Cleanup dataspace arrays */
+    /* Cleanup dataspace arrays.  Use H5I_remove() so we only close the IDs and
+     * not the underlying dataspaces, which were not created by this function.
+     */
     for (i = 0; i < num_spaces; i++) {
-        if (H5I_dec_app_ref(mem_space_ids[i]) < 0)
+        if (NULL == H5I_remove(mem_space_ids[i]))
             HDONE_ERROR(H5E_VFL, H5E_CANTDEC, FAIL, "problem freeing id")
-        if (H5I_dec_app_ref(file_space_ids[i]) < 0)
+        if (NULL == H5I_remove(file_space_ids[i]))
             HDONE_ERROR(H5E_VFL, H5E_CANTDEC, FAIL, "problem freeing id")
     }
     if (mem_space_ids != mem_space_ids_local)
@@ -1805,7 +1807,7 @@ H5FD_write_selection(H5FD_t *file, H5FD_mem_t type, uint32_t count, H5S_t **mem_
 
             if ((file_space_ids[num_spaces] = H5I_register(H5I_DATASPACE, file_spaces[num_spaces], TRUE)) <
                 0) {
-                if (H5I_dec_app_ref(mem_space_ids[num_spaces]) < 0)
+                if (NULL == H5I_remove(mem_space_ids[num_spaces]))
                     HDONE_ERROR(H5E_VFL, H5E_CANTDEC, FAIL, "problem freeing id")
                 HGOTO_ERROR(H5E_VFL, H5E_CANTREGISTER, FAIL, "unable to register dataspace ID")
             }
@@ -1835,11 +1837,13 @@ done:
         }
     }
 
-    /* Cleanup dataspace arrays */
+    /* Cleanup dataspace arrays.  Use H5I_remove() so we only close the IDs and
+     * not the underlying dataspaces, which were not created by this function.
+     */
     for (i = 0; i < num_spaces; i++) {
-        if (H5I_dec_app_ref(mem_space_ids[i]) < 0)
+        if (NULL == H5I_remove(mem_space_ids[i]))
             HDONE_ERROR(H5E_VFL, H5E_CANTDEC, FAIL, "problem freeing id")
-        if (H5I_dec_app_ref(file_space_ids[i]) < 0)
+        if (NULL == H5I_remove(file_space_ids[i]))
             HDONE_ERROR(H5E_VFL, H5E_CANTDEC, FAIL, "problem freeing id")
     }
     if (mem_space_ids != mem_space_ids_local)
