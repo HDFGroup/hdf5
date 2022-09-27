f4d386b86d6e5a568aee28054644190a3d67d996
Neil Fortner <nfortne2@hdfgroup.org>
2022-09-27 13:59:25 -0500

    Enable hyperslab combinations in mdset test

--- test/mdset.c
+++ test/mdset.c
@@ -333,8 +333,7 @@ test_mdset(size_t niter, unsigned flags, hid_t fapl_id)
                 sel_type = HDrandom() % 3;
                 if (sel_type == 0) {
                     /* Hyperslab */
-                    size_t nhs =
-                        (size_t)1; //((HDrandom() % MAX_HS) + 1); /* Number of hyperslabs */ /*!FIXME -NAF */
+                    size_t nhs = (size_t)((HDrandom() % MAX_HS) + 1); /* Number of hyperslabs */
                     size_t max_hs_x = (MAX_HS_X <= dset_dims[k][0])
                                           ? MAX_HS_X
                                           : dset_dims[k][0]; /* Determine maximum hyperslab size in X */
