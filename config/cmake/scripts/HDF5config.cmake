c22652b8eceedf51bed7cee2fea1d0d518a8ceb0
Larry Knox <lrknox@hdfgroup.org>
2022-10-26 14:58:22 -0500

    Updates for HDF5 1.13.3 release October 26, 2022     Updated so numbers for release     Set version to 1.13.2     Switch default build mode to production for release in configure.ac,            lines 435 and 442.     Switch AM_MAINTAINER_MODE to disabled with bin/switch_maint_mode     Set HDF5_GENERATE_HEADERS to OFF in src/CMakeLists.txt.     Commit Makefile.ins and other generated files for release            except 2 ...~ files and autom4te.cache.

--- config/cmake/scripts/HDF5config.cmake
+++ config/cmake/scripts/HDF5config.cmake
@@ -38,7 +38,7 @@ cmake_minimum_required (VERSION 3.18)
 ##############################################################################
 
 set (CTEST_SOURCE_VERSION "1.13.3")
-set (CTEST_SOURCE_VERSEXT "-1")
+set (CTEST_SOURCE_VERSEXT "")
 
 ##############################################################################
 # handle input parameters to script.
