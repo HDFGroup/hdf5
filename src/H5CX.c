b159713cde866cea1e3d5d908db15cb415f5ea70
Ray Lu <songyulu@hdfgroup.org>
2022-11-16 21:18:07 -0600

    Minor change: split one condition check into two for clarity.

--- src/H5CX.c
+++ src/H5CX.c
@@ -1630,9 +1630,11 @@ H5CX_get_vol_wrap_ctx(void **vol_wrap_ctx)
     /* No error is expected at this point.  But in case an application calls H5VLwrap_register
      * which doesn't reset the API context and there is no context, returns an relevant error here
      */
-    if (!head || !(*head))
-        HGOTO_ERROR(H5E_CONTEXT, H5E_CANTGET, FAIL,
-                    "the API context isn't available, e.g. when an application calls H5VLwrap_register")
+    if (!head)
+        HGOTO_ERROR(H5E_CONTEXT, H5E_UNINITIALIZED, FAIL, "the API context isn't available")
+
+    if (!(*head))
+        HGOTO_ERROR(H5E_CONTEXT, H5E_CANTGET, FAIL, "unable to get the current API context")
 
     /* Check for value that was set */
     if ((*head)->ctx.vol_wrap_ctx_valid)
