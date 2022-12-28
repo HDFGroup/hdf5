64c88b1c8a1d43bc58915f80d4d2d2587b9d0e8b
Dana Robinson <derobins@hdfgroup.org>
2022-12-28 08:00:45 -0800

    Fixed missing *

--- src/H5VLpublic.h
+++ src/H5VLpublic.h
@@ -404,7 +404,7 @@ H5_DLL herr_t H5VLunregister_connector(hid_t connector_id);
  * \since 1.12.0
  */
 H5_DLL herr_t H5VLquery_optional(hid_t obj_id, H5VL_subclass_t subcls, int opt_type, uint64_t *flags);
-/*
+/**
  * \ingroup H5VL
  * \brief Determines whether an object ID represents a native
  *        VOL connector object.
