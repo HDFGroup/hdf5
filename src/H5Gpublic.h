ee8cb288cc011578e2a28ae53e1701b7a8b99868
Scot Breitenfeld <brtnfld@hdfgroup.org>
2023-02-01 09:33:13 -0600

    removed immaterial \see APIs

--- src/H5Gpublic.h
+++ src/H5Gpublic.h
@@ -119,7 +119,7 @@ extern "C" {
  *
  * \since 1.8.0
  *
- * \see H5Gopen2(), H5Gclose()
+ * \see H5Gopen2()
  *
  */
 H5_DLL hid_t H5Gcreate2(hid_t loc_id, const char *name, hid_t lcpl_id, hid_t gcpl_id, hid_t gapl_id);
@@ -211,7 +211,7 @@ H5_DLL hid_t H5Gcreate_anon(hid_t loc_id, hid_t gcpl_id, hid_t gapl_id);
  *
  * \since 1.8.0
  *
- * \see H5Gcreate2(), H5Gclose()
+ * \see H5Gcreate2()
  *
  */
 H5_DLL hid_t H5Gopen2(hid_t loc_id, const char *name, hid_t gapl_id);
@@ -246,8 +246,6 @@ H5_DLL hid_t  H5Gopen_async(hid_t loc_id, const char *name, hid_t gapl_id, hid_t
  *
  * \since 1.8.0
  *
- * \see H5Gcreate2(), H5Pclose()
- *
  */
 H5_DLL hid_t H5Gget_create_plist(hid_t group_id);
 
@@ -273,8 +271,6 @@ H5_DLL hid_t H5Gget_create_plist(hid_t group_id);
  * \storage_type
  *
  * \since 1.8.0
- *
- * \see H5Gcreate2(), H5Gclose()
  *
  */
 H5_DLL herr_t H5Gget_info(hid_t loc_id, H5G_info_t *ginfo);
@@ -319,8 +315,6 @@ H5_DLL herr_t H5Gget_info_async(hid_t loc_id, H5G_info_t *ginfo /*out*/, hid_t e
  * \storage_type
  *
  * \since 1.8.0
- *
- * \see H5Gcreate2(), H5Gclose()
  *
  */
 H5_DLL herr_t H5Gget_info_by_name(hid_t loc_id, const char *name, H5G_info_t *ginfo, hid_t lapl_id);
@@ -381,8 +375,6 @@ H5_DLL herr_t H5Gget_info_by_name_async(hid_t loc_id, const char *name, H5G_info
  *
  * \since 1.8.0
  *
- * \see H5Gcreate2(), H5Gclose()
- *
  */
 H5_DLL herr_t H5Gget_info_by_idx(hid_t loc_id, const char *group_name, H5_index_t idx_type,
                                  H5_iter_order_t order, hsize_t n, H5G_info_t *ginfo, hid_t lapl_id);
@@ -426,8 +418,6 @@ H5_DLL herr_t H5Gget_info_by_idx_async(hid_t loc_id, const char *group_name, H5_
  *
  * \since 1.8.0
  *
- * \see H5Gcreate2(), H5Gclose()
- *
  */
 H5_DLL herr_t H5Gflush(hid_t group_id);
 
@@ -450,8 +440,6 @@ H5_DLL herr_t H5Gflush(hid_t group_id);
  *          with the same identifier.
  *
  * \since 1.8.0
- *
- * \see H5Gcreate2(), H5Gclose()
  *
  */
 H5_DLL herr_t H5Grefresh(hid_t group_id);
