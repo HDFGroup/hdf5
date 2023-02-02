29c3521960be37499f0c7487d5d63c9e4f5a3cc9
Allen Byrne <byrn@hdfgroup.org>
2023-02-02 10:08:46 -0600

    Update and fix compiler flags

--- config/toolchain/intel.cmake
+++ config/toolchain/intel.cmake
@@ -1,12 +1,12 @@
 # Uncomment the following to use cross-compiling
 #set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_COMPILER_VENDOR "intel")
+set (CMAKE_COMPILER_VENDOR "intel")
 
-if(USE_SANITIZER)
-  set(CMAKE_C_COMPILER icl)
-  set(CMAKE_CXX_COMPILER icl++)
-  set(CMAKE_Fortran_COMPILER ifort)
+if (USE_LLVM)
+  set(CMAKE_C_COMPILER icx)
+  set(CMAKE_CXX_COMPILER icpx)
+  set(CMAKE_Fortran_COMPILER ifx)
   set(INTEL_CLANG ON)
 else ()
   set(CMAKE_C_COMPILER icc)
