2d55270d4931ae878e1e624eb824a68b3082606e
Scot Breitenfeld <brtnfld@hdfgroup.org>
2023-02-22 11:51:51 -0600

    enabled parallel running on h5fuse.sh

---
+++ utils/subfiling_vfd/h5fuse_p_opt.c
@@ -0,0 +1,21 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main() {
+
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    printf("%d %d ",world_rank, world_size);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
