e190055df39e7e88ffdb829958b66b0a6384b675
Dana Robinson <43805+derobins@users.noreply.github.com>
2023-03-18 15:40:41 -0700

    Remove unused H5MP and H5HP packages (#2591)

--- test/testhdf5.h
+++ test/testhdf5.h
@@ -202,7 +202,6 @@ extern "C" {
 /* Prototypes for the test routines */
 void test_metadata(void);
 void test_checksum(void);
-void test_heap(void);
 void test_refstr(void);
 void test_file(void);
 void test_h5o(void);
