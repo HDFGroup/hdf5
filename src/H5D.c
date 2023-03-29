da9310d08fc3ee9dd8e24f900d0947c51f8a2b00
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-03-29 14:15:58 -0400

    Port h5dchunk_iter_hsizet_size #2074 to hdf5_1_12 branch (#2329)

--- src/H5D.c
+++ src/H5D.c
@@ -1137,32 +1137,6 @@ done:
  *              H5D_chunk_iter_op_t cb  IN: User callback function, called for every chunk.
  *              void *op_data           IN/OUT: Optional user data passed on to user callback.
  *
- * Callback information:
- *      H5D_chunk_iter_op_t is defined as:
- *
- *        typedef int (*H5D_chunk_iter_op_t)(
- *            const hsize_t *offset,
- *            uint32_t filter_mask,
- *            haddr_t addr,
- *            uint32_t size,
- *            void *op_data);
- *
- *      H5D_chunk_iter_op_t parameters:
- *          hsize_t *offset;        IN/OUT: Array of starting logical coordinates of chunk.
- *          uint32_t filter_mask;   IN: Filter mask of chunk.
- *          haddr_t addr;           IN: Offset in file of chunk data.
- *          uint32_t nbytes;        IN: Size in number of bytes of chunk data in file.
- *          void *op_data;          IN/OUT: Pointer to any user-defined data
- *                                  associated with the operation.
- *
- *      The return values from an operator are:
- *          Zero (H5_ITER_CONT) causes the iterator to continue, returning zero when all
- *              elements have been processed.
- *          Positive (H5_ITER_STOP) causes the iterator to immediately return that positive
- *              value, indicating short-circuit success.
- *          Negative (H5_ITER_ERROR) causes the iterator to immediately return that value,
- *              indicating failure.
- *
  * Return:      Non-negative on success, negative on failure
  *
  * Programmer:  Gaute Hope
