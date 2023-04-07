bc2517b33d8bd5d23c2352d38b97a23d07c92833
Larry Knox <lrknox@hdfgroup.org>
2023-04-07 07:49:26 -0500

    Update HDF5 version after 1.10.10 release. (#2681)
    * Update HDF5 version after 1.10.10 release.
    Update so numbers to match 1.10.10 release so numbers.
    Update HISTORY-1_10.txt.
    Clean RELEASE.txt entries.
    
    * Missed one version change.

--- config/cmake/scripts/HDF5config.cmake
+++ config/cmake/scripts/HDF5config.cmake
@@ -37,8 +37,8 @@ cmake_minimum_required (VERSION 3.12)
 #     CTEST_SOURCE_NAME  -  source folder
 ##############################################################################
 
-set (CTEST_SOURCE_VERSION "1.10.10")
-set (CTEST_SOURCE_VERSEXT "-2")
+set (CTEST_SOURCE_VERSION "1.10.11")
+set (CTEST_SOURCE_VERSEXT "-1")
 
 ##############################################################################
 # handle input parameters to script.
