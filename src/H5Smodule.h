225c3919b7e0d7f6f83dcb20ee2f6937fce76e0b
Dana Robinson <43805+derobins@users.noreply.github.com>
2023-04-17 19:10:44 -0700

    Removes programmer/date lines from src headers (#2747)

--- src/H5Smodule.h
+++ src/H5Smodule.h
@@ -11,12 +11,9 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /*
- * Programmer:	Quincey Koziol
- *		Saturday, September 12, 2015
- *
- * Purpose:	This file contains declarations which define macros for the
- *		H5S package.  Including this header means that the source file
- *		is part of the H5S package.
+ * Purpose: This file contains declarations which define macros for the
+ *          H5S package.  Including this header means that the source file
+ *          is part of the H5S package.
  */
 #ifndef H5Smodule_H
 #define H5Smodule_H
