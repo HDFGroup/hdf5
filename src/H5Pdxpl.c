0f32c8cce7e8be1b3981d72e84ba7aeb4b773701
vchoi-hdfgroup.org <vchoi@hdfgroup.org>
2023-05-16 12:25:57 -0500

    Changes made based on review comments.

--- src/H5Pdxpl.c
+++ src/H5Pdxpl.c
@@ -478,7 +478,7 @@ H5P__dxfr_reg_prop(H5P_genclass_t *pclass)
         HGOTO_ERROR(H5E_PLIST, H5E_CANTINSERT, FAIL, "can't insert property into class")
 
     /* Register the actual selection I/O mode property */
-    /* (Note: this property should not have an encode/decode callback -QAK) */
+    /* (Note: this property should not have an encode/decode callback) */
     if (H5P__register_real(pclass, H5D_XFER_ACTUAL_SELECTION_IO_MODE_NAME,
                            H5D_XFER_ACTUAL_SELECTION_IO_MODE_SIZE, &H5D_def_actual_selection_io_mode_g, NULL,
                            NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL) < 0)
