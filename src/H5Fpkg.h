81bc34ac4c3f42532edee32095651f6bcd5e55a2
Dana Robinson <43805+derobins@users.noreply.github.com>
2023-05-30 08:33:52 -0700

    Only pass efc pointer to H5F__efc_open (#3026)
    The H5F__efc_open() API call only needs the efc pointer, so there is
    no need to pass the entire file struct in.

--- src/H5Fpkg.h
+++ src/H5Fpkg.h
@@ -452,7 +452,7 @@ H5_DLL herr_t H5F__set_mpi_atomicity(H5F_t *file, hbool_t flag);
 
 /* External file cache routines */
 H5_DLL H5F_efc_t *H5F__efc_create(unsigned max_nfiles);
-H5_DLL H5F_t   *H5F__efc_open(H5F_t *parent, const char *name, unsigned flags, hid_t fcpl_id, hid_t fapl_id);
+H5_DLL H5F_t   *H5F__efc_open(H5F_efc_t *efc, const char *name, unsigned flags, hid_t fcpl_id, hid_t fapl_id);
 H5_DLL unsigned H5F__efc_max_nfiles(H5F_efc_t *efc);
 H5_DLL herr_t   H5F__efc_release(H5F_efc_t *efc);
 H5_DLL herr_t   H5F__efc_destroy(H5F_efc_t *efc);
