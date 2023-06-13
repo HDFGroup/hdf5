716eb0e90f5a3a5ce05aff2ef668157ba41f5a4c
Allen Byrne <byrn@hdfgroup.org>
2023-06-13 10:23:50 -0500

    Correct initialization

--- src/H5trace.c
+++ src/H5trace.c
@@ -3982,7 +3982,7 @@ H5_trace(const double *returning, const char *func, const char *type, ...)
     hssize_t          i;
     FILE             *out                 = H5_debug_g.trace;
     static hbool_t    is_first_invocation = TRUE;
-    H5_timer_t        function_timer      = {0.0, 0.0, 0.0};
+    H5_timer_t        function_timer      = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, FALSE};
     H5_timevals_t     function_times      = {0.0, 0.0, 0.0};
     static H5_timer_t running_timer;
     H5_timevals_t     running_times;
