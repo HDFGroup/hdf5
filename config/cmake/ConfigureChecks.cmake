1abf80de97825f99b1dc079ffecccf08c8c9a1b3
Dana Robinson <derobins@hdfgroup.org>
2023-06-14 23:01:28 -0700

    Update comments to reflect newer systems

--- config/cmake/ConfigureChecks.cmake
+++ config/cmake/ConfigureChecks.cmake
@@ -899,9 +899,9 @@ endmacro ()
 #-----------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
-# Set the flag to indicate that the machine is using a special algorithm to convert
+# Set the flag to indicate that the machine is using a special algorithm toconvert
 # 'long double' to '(unsigned) long' values.  (This flag should only be set for
-# the IBM Power6 Linux.  When the bit sequence of long double is
+# the IBM Power Linux.  When the bit sequence of long double is
 # 0x4351ccf385ebc8a0bfcc2a3c3d855620, the converted value of (unsigned)long
 # is 0x004733ce17af227f, not the same as the library's conversion to 0x004733ce17af2282.
 # The machine's conversion gets the correct value.  We define the macro and disable
@@ -911,7 +911,7 @@ H5ConversionTests (${HDF_PREFIX}_LDOUBLE_TO_LONG_SPECIAL  "Checking IF your syst
 # ----------------------------------------------------------------------
 # Set the flag to indicate that the machine is using a special algorithm
 # to convert some values of '(unsigned) long' to 'long double' values.
-# (This flag should be off for all machines, except for IBM Power6 Linux,
+# (This flag should be off for all machines, except for IBM Power Linux,
 # when the bit sequences are 003fff..., 007fff..., 00ffff..., 01ffff...,
 # ..., 7fffff..., the compiler uses a unknown algorithm.  We define a
 # macro and skip the test for now until we know about the algorithm.
