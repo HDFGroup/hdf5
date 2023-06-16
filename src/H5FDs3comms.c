cef648eb14e9e611c81e710318924c27445878d8
mattjala <124107509+mattjala@users.noreply.github.com>
2023-06-16 11:04:44 -0500

    Track s3 i/o when S3COMMS_DEBUG enabled (#3139)
    * Track s3 i/o when S3COMMS_DEBUG enabled

--- src/H5FDs3comms.c
+++ src/H5FDs3comms.c
@@ -1281,6 +1281,11 @@ H5FD_s3comms_s3r_read(s3r_t *handle, haddr_t offset, size_t len, void *dest)
             HGOTO_ERROR(H5E_ARGS, H5E_BADVALUE, FAIL, "unable to format HTTP Range value");
     }
 
+#if S3COMMS_DEBUG
+    HDfprintf(stderr, "%s: Bytes %" PRIuHADDR " - %" PRIuHADDR ", Request Size: %zu\n", handle->httpverb,
+              offset, offset + len, len);
+#endif
+
     /*******************
      * COMPILE REQUEST *
      *******************/
