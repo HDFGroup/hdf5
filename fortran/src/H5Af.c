d47920976ebf35621f610e5a975ba5c0c2491a6d
Scot Breitenfeld <brtnfld@hdfgroup.org>
2023-07-19 11:52:11 -0500

    removed the use of encoded single apostrophe in fortran source

--- fortran/src/H5Af.c
+++ fortran/src/H5Af.c
@@ -259,7 +259,7 @@ done:
  *                          H5_ITER_NATIVE   - No particular order, whatever is fastest
  *                          H5_ITER_N	     - Number of iteration orders
  *
- *  n  - Attribute’s position in index
+ *  n  - Attribute's position in index
  *  attr_id  - Attribute identifier
  *  size  - Buffer size ! *TEST* check for 0 value *CHECK* should this return the correct value
  *
@@ -331,7 +331,7 @@ done:
  *
  *  corder_valid - Indicates whether the creation order data is valid for this attribute
  *  corder - Is a positive integer containing the creation order of the attribute
- *  cset - Indicates the character set used for the attribute’s name
+ *  cset - Indicates the character set used for the attribute's name
  *  data_size - indicates the size, in the number of characters, of the attribute
  *
  * RETURNS
@@ -388,13 +388,13 @@ done:
  *                          H5_ITER_NATIVE   - No particular order, whatever is fastest
  *                          H5_ITER_N	     - Number of iteration orders
  *
- *  n - Attribute’s position in index
+ *  n - Attribute's position in index
  *  lapl_id - Link access property list
  * OUTPUTS
  *
  *  corder_valid - Indicates whether the creation order data is valid for this attribute
  *  corder - Is a positive integer containing the creation order of the attribute
- *  cset - Indicates the character set used for the attribute’s name
+ *  cset - Indicates the character set used for the attribute's name
  *  data_size - indicates the size, in the number of characters, of the attribute
  *
  * RETURNS
@@ -455,7 +455,7 @@ done:
  *
  *  corder_valid - Indicates whether the creation order data is valid for this attribute
  *  corder - Is a positive integer containing the creation order of the attribute
- *  cset - Indicates the character set used for the attribute’s name
+ *  cset - Indicates the character set used for the attribute's name
  *  data_size - indicates the size, in the number of characters, of the attribute
  *
  * RETURNS
