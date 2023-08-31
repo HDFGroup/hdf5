c86f9af06d37da6e34110f4717a0c4d431bbe34e
Matthew Larson <matthewjlar@gmail.com>
2023-08-31 09:57:50 -0500

    Allow building of VOL from local source

--- CMakeVOL.cmake
+++ CMakeVOL.cmake
@@ -30,20 +30,16 @@ endfunction ()
 option (HDF5_VOL_ALLOW_EXTERNAL "Allow building of external HDF5 VOL connectors with FetchContent" OFF)
 mark_as_advanced (HDF5_VOL_ALLOW_EXTERNAL)
 if (HDF5_VOL_ALLOW_EXTERNAL)
-  if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "NO" OR NOT HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT")
-    message (FATAL_ERROR "HDF5_ALLOW_EXTERNAL_SUPPORT must be set to 'GIT' or 'LOCAL_SOURCE_DIR' to allow building of external HDF5 VOL connectors")
-  endif ()
-
-  if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_SOURCE_DIR" AND NOT HDF5_VOL_PATH_TEMP)
-    message(FATAL_ERROR "HDF5_VOL_PATH_TEMP must have a path to VOL source when HDF5_ALLOW_EXTERNAL_SUPPORT = LOCAL_SOURCE_DIR")
-  endif ()
+  
+  if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "NO" OR (NOT HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT" AND NOT HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_DIR"))
+    message (FATAL_ERROR "HDF5_ALLOW_EXTERNAL_SUPPORT must be set to 'GIT' or 'LOCAL_DIR' to allow building of external HDF5 VOL connectors")
+  endif()
 
   # For compatibility, set some variables that projects would
   # typically look for after calling find_package(HDF5)
   set (HDF5_FOUND 1)
   set (HDF5_LIBRARIES "${HDF5_LIBSH_TARGET};${LINK_LIBS};${LINK_COMP_LIBS};$<$<BOOL:${HDF5_ENABLE_PARALLEL}>:MPI::MPI_C>")
   set (HDF5_INCLUDE_DIRS "${HDF5_SRC_INCLUDE_DIRS};${HDF5_SRC_BINARY_DIR};$<$<BOOL:${HDF5_ENABLE_PARALLEL}>:${MPI_C_INCLUDE_DIRS}>")
-  set (HDF5_DIR "${HDF5_SOURCE_DIR}")
 
   set (HDF5_C_LIBRARIES "${HDF5_LIBRARIES}")
   
@@ -63,19 +59,19 @@ if (HDF5_VOL_ALLOW_EXTERNAL)
     while (vol_idx_len LESS vol_idx_num_digits)
       string (PREPEND vol_idx_fixed "0")
       math (EXPR vol_idx_len "${vol_idx_len}+1")
-    endwhile ()
+    endwhile()
+
+    set (HDF5_VOL_URL${vol_idx_fixed} "" CACHE STRING "Git repository URL of an external HDF5 VOL connector to build")
+    mark_as_advanced (HDF5_VOL_URL${vol_idx_fixed})
+
+    set (HDF5_VOL_PATH${vol_idx_fixed} "" CACHE STRING "Path to the source directory of an external HDF5 VOL connector to build")
+    mark_as_advanced (HDF5_VOL_PATH${vol_idx_fixed})
 
     if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT")
-      # TODO should create cmake/user facing var called "HDF5_VOL_URL" or "HDF5_VOL_DIR" while HDF5_VOL_SOURCE stays an internal cmake var name
-      set (HDF5_VOL_SOURCE "HDF5_VOL_URL${vol_idx_fixed}")
-      set (${HDF5_VOL_SOURCE} "" CACHE STRING "Git repository URL of an external HDF5 VOL connector to build")
-      mark_as_advanced (HDF5_VOL_URL${vol_idx_fixed})
-    elseif (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_SOURCE_DIR")
-      set (HDF5_VOL_SOURCE "HDF5_VOL_DIR${vol_idx_fixed}")
-      set (${HDF5_VOL_SOURCE} "" CACHE STRING "Path to the source directory of an external HDF5 VOL connector to build")
-      mark_as_advanced (HDF5_VOL_PATH${vol_idx_fixed})
+      set (HDF5_VOL_SOURCE "${HDF5_VOL_URL${vol_idx_fixed}}")
+    elseif(HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_DIR")
+      set (HDF5_VOL_SOURCE "${HDF5_VOL_PATH${vol_idx_fixed}}")
     else()
-      set(A "TODO")
     endif()
 
     if (NOT "${HDF5_VOL_SOURCE}" STREQUAL "")
@@ -83,9 +79,10 @@ if (HDF5_VOL_ALLOW_EXTERNAL)
       string (FIND "${HDF5_VOL_SOURCE}" "/" hdf5_vol_name_pos REVERSE)
       if (hdf5_vol_name_pos EQUAL -1)
         if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT")
-          message (SEND_ERROR "Invalid URL '${HDF5_VOL_URL${vol_idx_fixed}}' specified for HDF5_VOL_URL${vol_idx_fixed}")
-        else ()
-          message (SEND_ERROR "Invalid source path '${HDF5_VOL_DIR${vol_idx_fixed}}' specified for HDF5_VOL_DIR${vol_idx_fixed}")
+          message (SEND_ERROR "Invalid URL '${HDF5_VOL_SOURCE}' specified for HDF5_VOL_URL${vol_idx_fixed}")
+        else()
+          message (SEND_ERROR "Invalid source path '${HDF5_VOL_SOURCE}' specified for HDF5_VOL_PATH${vol_idx_fixed}")
+        endif()
       endif ()
 
       math (EXPR hdf5_vol_name_pos "${hdf5_vol_name_pos}+1")
@@ -100,17 +97,11 @@ if (HDF5_VOL_ALLOW_EXTERNAL)
 
       # Set some cache variables that can be set by users when building
       set ("HDF5_VOL_${hdf5_vol_name_upper}_NAME" "" CACHE STRING "Name of VOL connector to set for the HDF5_VOL_CONNECTOR environment variable")
-      if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT")
-        set ("HDF5_VOL_${hdf5_vol_name_upper}_BRANCH" "main" CACHE STRING "Git branch (or tag) to use when building VOL connector '${hdf5_vol_name}'")
-        mark_as_advanced ("HDF5_VOL_${hdf5_vol_name_upper}_BRANCH")
-      else ()
-        set ("HDF5_VOL_${hdf5_vol_name_upper}_SOURCE_DIR" "${HDF5_VOL_SOURCE_DIR}" CACHE STRING "Local source directory to use when building VOL connector '${hdf5_vol_name}'")
-        mark_as_advanced ("HDF5_VOL_${hdf5_vol_name_upper}_SOURCE_DIR")
-      endif()
-
+      set ("HDF5_VOL_${hdf5_vol_name_upper}_BRANCH" "main" CACHE STRING "Git branch (or tag) to use when building VOL connector '${hdf5_vol_name}'")
       option ("HDF5_VOL_${hdf5_vol_name_upper}_TEST_PARALLEL" "Whether to test VOL connector '${hdf5_vol_name}' against the parallel API tests" OFF)
 
       mark_as_advanced ("HDF5_VOL_${hdf5_vol_name_upper}_NAME")
+      mark_as_advanced ("HDF5_VOL_${hdf5_vol_name_upper}_BRANCH")
       mark_as_advanced ("HDF5_VOL_${hdf5_vol_name_upper}_TEST_PARALLEL")
 
       if (HDF5_TEST_API)
@@ -119,25 +110,27 @@ if (HDF5_VOL_ALLOW_EXTERNAL)
         endif ()
       endif ()
 
-      if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT" AND"${HDF5_VOL_${hdf5_vol_name_upper}_BRANCH}" STREQUAL "")
+      if (("${HDF5_ALLOW_EXTERNAL_SUPPORT}" MATCHES "GIT") AND ("${HDF5_VOL_${hdf5_vol_name_upper}_BRANCH}" STREQUAL ""))
         message (SEND_ERROR "HDF5_VOL_${hdf5_vol_name_upper}_BRANCH must be set to a valid git branch name (or git tag) to build VOL connector '${hdf5_vol_name}'")
       endif ()
 
-      if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "SOURCE_DIR" AND ${HDF5_VOL_{hdf5_vol_name_upper}_SOURCE_DIR} STREQUAL "")
-        message (SEND_ERROR "${HDF5_VOL_{hdf5_vol_name_upper}_SOURCE_DIR} must be set to a valid local path to build VOL connector '${hdf5_vol_name}'")
-      endif()
+      if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_DIR")
+        if (NOT (EXISTS ${HDF5_VOL_SOURCE} AND IS_DIRECTORY ${HDF5_VOL_SOURCE}))
+          message (FATAL_ERROR "HDF5_VOL_PATH${vol_idx_fixed} must be an absolute path to a valid directory")
+        else()
+        endif()
+      endif ()
 
       if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT")
         FetchContent_Declare (HDF5_VOL_${hdf5_vol_name_lower}
-        GIT_REPOSITORY "${HDF5_VOL_URL${vol_idx_fixed}}"
+        GIT_REPOSITORY "${HDF5_VOL_SOURCE}"
         GIT_TAG "${HDF5_VOL_${hdf5_vol_name_upper}_BRANCH}"
         )
-      elseif (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "SOURCE_DIR")
+      elseif(HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_DIR")
         FetchContent_Declare (HDF5_VOL_${hdf5_vol_name_lower}
-        SOURCE_DIR "${HDF5_VOL_SOURCE_LOCAL}"
+        SOURCE_DIR "${HDF5_VOL_SOURCE}"
         )
-      else ()
-        set (A "TODO")
+      else()
       endif()
 
       FetchContent_GetProperties(HDF5_VOL_${hdf5_vol_name_lower})
@@ -145,7 +138,12 @@ if (HDF5_VOL_ALLOW_EXTERNAL)
         FetchContent_Populate(HDF5_VOL_${hdf5_vol_name_lower})
 
         if (NOT EXISTS "${hdf5_vol_${hdf5_vol_name_lower}_SOURCE_DIR}/CMakeLists.txt")
-          message (SEND_ERROR "The git repository branch '${HDF5_VOL_${hdf5_vol_name_upper}_BRANCH}' for VOL connector '${hdf5_vol_name}' does not appear to contain a CMakeLists.txt file")
+          if (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "GIT")
+            message (SEND_ERROR "The git repository branch '${HDF5_VOL_${hdf5_vol_name_upper}_BRANCH}' for VOL connector '${hdf5_vol_name}' does not appear to contain a CMakeLists.txt file")
+          elseif (HDF5_ALLOW_EXTERNAL_SUPPORT MATCHES "LOCAL_DIR")
+            message(SEND_ERROR "The local directory '${HDF5_VOL_SOURCE}' for VOL connector '${hdf5_vol_name}' does not appear to contain a CMakeLists.txt file")
+          else()
+          endif()
         endif ()
 
         # If there are any calls to find_package(HDF5) in the connector's
