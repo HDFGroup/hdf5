b4d9fca3e5c508cb6e7ed30a69047c802b32d729
vchoi-hdfgroup.org <vchoi@hdfgroup.org>
2023-09-12 12:55:51 -0500

    Print out data values that fail when comparing data.

--- test/cmpd_dset.c
+++ test/cmpd_dset.c
@@ -158,6 +158,28 @@ compare_stype4_data(void *expect_buf, void *rbuf)
 
             H5_FAILED();
             printf("    i=%d\n", i);
+            printf(
+                "    exp_buf={a=%d, b=%d, c=[%d,%d,%d,%d,%d,%d,%d,%d], d=%d, e=%d, f=%f, g=%f, "
+                "h=[%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f], i=%f, j=%f, k=%f, l=%f, m=%f, n=%f, o=%ld, p=%ld, q=%ld}\n",
+                s1_ptr->a, s1_ptr->b, s1_ptr->c[0], s1_ptr->c[1], s1_ptr->c[2], s1_ptr->c[3], s1_ptr->c[4],
+                s1_ptr->c[5], s1_ptr->c[6], s1_ptr->c[7], s1_ptr->d, s1_ptr->e, (double)s1_ptr->f, (double)s1_ptr->g,
+                (double)s1_ptr->h[0], (double)s1_ptr->h[1], (double)s1_ptr->h[2], (double)s1_ptr->h[3],
+                (double)s1_ptr->h[4], (double)s1_ptr->h[5], (double)s1_ptr->h[6], (double)s1_ptr->h[7],
+                (double)s1_ptr->h[8], (double)s1_ptr->h[9], (double)s1_ptr->h[10], (double)s1_ptr->h[11],
+                (double)s1_ptr->h[12], (double)s1_ptr->h[13], (double)s1_ptr->h[14], (double)s1_ptr->h[15],
+                (double)s1_ptr->i, (double)s1_ptr->j, s1_ptr->k, s1_ptr->l, s1_ptr->m, s1_ptr->n,
+                s1_ptr->o, s1_ptr->p, s1_ptr->q);
+            printf(
+                "    rbuf={a=%d, b=%d, c=[%d,%d,%d,%d,%d,%d,%d,%d], d=%d, e=%d, f=%f, g=%f, "
+                "h=[%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f], i=%f, j=%f, k=%f, l=%f, m=%f, n=%f, o=%ld, p=%ld, q=%ld}\n",
+                s2_ptr->a, s2_ptr->b, s2_ptr->c[0], s2_ptr->c[1], s2_ptr->c[2], s2_ptr->c[3], s2_ptr->c[4],
+                s2_ptr->c[5], s2_ptr->c[6], s2_ptr->c[7], s2_ptr->d, s2_ptr->e, (double)s2_ptr->f, (double)s2_ptr->g,
+                (double)s2_ptr->h[0], (double)s2_ptr->h[1], (double)s2_ptr->h[2], (double)s2_ptr->h[3],
+                (double)s2_ptr->h[4], (double)s2_ptr->h[5], (double)s2_ptr->h[6], (double)s2_ptr->h[7],
+                (double)s2_ptr->h[8], (double)s2_ptr->h[9], (double)s2_ptr->h[10], (double)s2_ptr->h[11],
+                (double)s2_ptr->h[12], (double)s2_ptr->h[13], (double)s2_ptr->h[14], (double)s2_ptr->h[15],
+                (double)s2_ptr->i, (double)s2_ptr->j, s2_ptr->k, s2_ptr->l, s2_ptr->m, s2_ptr->n,
+                s1_ptr->o, s1_ptr->p, s1_ptr->q);
             goto error;
         }
     } /* end for */
@@ -168,6 +190,45 @@ error:
     return FAIL;
 
 } /* compare_stype4_data() */
+
+#ifdef OUT
+static int
+compare_stype4_data(void *expect_buf, void *rbuf)
+{
+    int i;
+
+    for (i = 0; i < (int)(NX * NY); i++) {
+        stype4 *s1_ptr;
+        stype1 *s2_ptr;
+        s1_ptr = ((stype4 *)expect_buf) + i;
+        s2_ptr = ((stype1 *)rbuf) + i;
+
+        if (s1_ptr->a != s2_ptr->a || s1_ptr->b != s2_ptr->b || s1_ptr->c[0] != s2_ptr->c[0] ||
+            s1_ptr->c[2] != s2_ptr->c[2] || s1_ptr->c[4] != s2_ptr->c[4] || s1_ptr->c[6] != s2_ptr->c[6] ||
+            s1_ptr->d != s2_ptr->d || s1_ptr->e != s2_ptr->e || !H5_FLT_ABS_EQUAL(s1_ptr->f, s2_ptr->f) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->g, s2_ptr->g) || !H5_FLT_ABS_EQUAL(s1_ptr->h[1], s2_ptr->h[1]) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->h[3], s2_ptr->h[3]) || !H5_FLT_ABS_EQUAL(s1_ptr->h[5], s2_ptr->h[5]) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->h[7], s2_ptr->h[7]) || !H5_FLT_ABS_EQUAL(s1_ptr->h[9], s2_ptr->h[9]) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->h[11], s2_ptr->h[11]) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->h[13], s2_ptr->h[13]) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->h[15], s2_ptr->h[15]) || !H5_FLT_ABS_EQUAL(s1_ptr->i, s2_ptr->i) ||
+            !H5_FLT_ABS_EQUAL(s1_ptr->j, s2_ptr->j) || !H5_DBL_ABS_EQUAL(s1_ptr->k, s2_ptr->k) ||
+            !H5_DBL_ABS_EQUAL(s1_ptr->l, s2_ptr->l) || !H5_DBL_ABS_EQUAL(s1_ptr->m, s2_ptr->m) ||
+            !H5_DBL_ABS_EQUAL(s1_ptr->n, s2_ptr->n) ) {
+
+            H5_FAILED();
+            printf("    i=%d\n", i);
+            goto error;
+        }
+    } /* end for */
+
+    return SUCCEED;
+
+error:
+    return FAIL;
+
+} /* compare_stype4_data() */
+#endif
 
 /*-------------------------------------------------------------------------
  * Function:    test_select_src_subset
@@ -357,7 +418,7 @@ static unsigned
 test_select_dst_subset(char *fname, hid_t fapl, hid_t in_dxpl, unsigned set_fillvalue, unsigned set_buf)
 {
     hid_t          fid;
-    hid_t          rew_tid, src_tid;
+    hid_t          rew_tid, src_tid, dst_tid;
     hid_t          did;
     hid_t          sid;
     hid_t          dcpl;
@@ -377,6 +438,9 @@ test_select_dst_subset(char *fname, hid_t fapl, hid_t in_dxpl, unsigned set_fill
         goto error;
 
     if ((rew_tid = create_stype4()) < 0)
+        goto error;
+
+    if ((dst_tid = create_stype1()) < 0)
         goto error;
 
     /* Create the data space */
@@ -424,8 +488,15 @@ test_select_dst_subset(char *fname, hid_t fapl, hid_t in_dxpl, unsigned set_fill
     if (H5Dwrite(did, rew_tid, H5S_ALL, H5S_ALL, dxpl, rew_buf) < 0)
         goto error;
 
+    initialize_stype4(rew_buf, (size_t)NX * NY);
+
+    memset(rbuf, 0, sizeof(rbuf));
     if (H5Dread(did, rew_tid, H5S_ALL, H5S_ALL, dxpl, rbuf) < 0)
         goto error;
+#ifdef OUT
+    if (H5Dread(did, dst_tid, H5S_ALL, H5S_ALL, dxpl, rbuf) < 0)
+        goto error;
+#endif
 
     if (compare_stype4_data(save_rew_buf, rbuf) < 0)
         goto error;
@@ -441,17 +512,19 @@ test_select_dst_subset(char *fname, hid_t fapl, hid_t in_dxpl, unsigned set_fill
     if ((did = H5Dcreate2(fid, DSET_NAME[3], src_tid, sid, H5P_DEFAULT, dcpl, H5P_DEFAULT)) < 0)
         goto error;
 
-    /* Restore the copy as the buffer may be clobbered due to H5Pset_modify_write_buf() */
-    memcpy(rew_buf, save_rew_buf, sizeof(stype4) * NX * NY);
-
     initialize_stype4(rew_buf, (size_t)NX * NY);
 
     /* Write data to the dataset with rew_tid */
     if (H5Dwrite(did, rew_tid, H5S_ALL, H5S_ALL, dxpl, rew_buf) < 0)
         goto error;
 
+    memset(rbuf, 0, sizeof(rbuf));
     if (H5Dread(did, rew_tid, H5S_ALL, H5S_ALL, dxpl, rbuf) < 0)
         goto error;
+#ifdef OUT
+    if (H5Dread(did, dst_tid, H5S_ALL, H5S_ALL, dxpl, rbuf) < 0)
+        goto error;
+#endif
 
     if (compare_stype4_data(save_rew_buf, rbuf) < 0)
         goto error;
@@ -778,7 +851,7 @@ test_compounds_selection_io(void)
                         else
                             printf("without H5Pset_buffer:\n");
 
-                        nerrs += test_select_compound(fname, fapl, dxpl, set_fillvalue, set_buf);
+                        nerrs += test_select_compound(fname, fapl, dxpl, set_fillvalue, set_buf); 
                         nerrs += test_select_src_subset(fname, fapl, dxpl, set_fillvalue, set_buf);
                         nerrs += test_select_dst_subset(fname, fapl, dxpl, set_fillvalue, set_buf);
 
