058b76920b1131c118a8e338ebdcf67788a141a0
vchoi-hdfgroup.org <vchoi@hdfgroup.org>
2023-09-18 17:46:49 -0500

    Change the memset call again in H5Dscatgath_read() This change will fail the superset read in the compound test.

--- src/H5Dscatgath.c
+++ src/H5Dscatgath.c
@@ -541,6 +541,10 @@ H5D__scatgath_read(const H5D_io_info_t *io_info, const H5D_dset_io_info_t *dset_
                 (size_t)MIN(dset_info->type_info.request_nelmts, (dset_info->nelmts - smine_start));
         }
 
+        memset(tmp_buf, 0,
+               smine_nelmts *
+                 MAX(dset_info->type_info.src_type_size, dset_info->type_info.dst_type_size));
+
         /*
          * Gather the data from disk into the datatype conversion
          * buffer. Also gather data from application to background buffer
@@ -555,10 +559,6 @@ H5D__scatgath_read(const H5D_io_info_t *io_info, const H5D_dset_io_info_t *dset_
             if (n != smine_nelmts)
                 HGOTO_ERROR(H5E_IO, H5E_READERROR, FAIL, "mem gather failed");
         }
-        else
-            memset(tmp_buf, 0,
-                   smine_nelmts *
-                       MAX(dset_info->type_info.src_type_size, dset_info->type_info.dst_type_size));
 
         /*
          * Gather data
