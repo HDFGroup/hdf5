6051bc90d4c1a5a8a6845d9eb06128bff52aec72
Larry Knox <lrknox@hdfgroup.org>
2023-11-03 15:05:15 -0500

    Change libaec and zlib tar.gz filenames to those in cacheinit.cmake.

--- src/H5public.h
+++ src/H5public.h
@@ -130,11 +130,11 @@
 /**
  * For pre-releases like \c snap0. Empty string for official releases.
  */
-#define H5_VERS_SUBRELEASE "2"
+#define H5_VERS_SUBRELEASE ""
 /**
  * Full version string
  */
-#define H5_VERS_INFO "HDF5 library version: 1.12.3-2"
+#define H5_VERS_INFO "HDF5 library version: 1.12.3"
 
 #define H5check() H5check_version(H5_VERS_MAJOR, H5_VERS_MINOR, H5_VERS_RELEASE)
 
