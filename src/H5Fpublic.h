1d863d97030be2c350cfe8a2c905ec267c248365
Gerd Heber <gheber@hdfgroup.org>
2023-12-21 14:51:58 -0600

    Doc versions (#3903)
    * Added missing \since tags to H5D.
    
    * Committing clang-format changes
    
    * Fixed H5T version info.
    
    * Committing clang-format changes
    
    * Added missing version info to H5E.
    
    * Committing clang-format changes
    
    * Added version info to H5F public APIs.
    
    * Committing clang-format changes
    
    * Added missing H5Z public API version info.
    
    * Added missing version info to H5G public APIs
    
    * Added missing version info to H5I public API.
    
    * Added missing version info to H5 public APIs
    
    * Committing clang-format changes
    
    * Added missing version info to H5P public APIs
    
    * Added missing version info to H5R public APIs
    
    * Fix comment error.
    
    * Committing clang-format changes
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/H5Fpublic.h
+++ src/H5Fpublic.h
@@ -354,6 +354,9 @@ H5_DLL hid_t H5Fcreate(const char *filename, unsigned flags, hid_t fcpl_id, hid_
  * --------------------------------------------------------------------------
  * \ingroup ASYNC
  * \async_variant_of{H5Fcreate}
+ *
+ * \since 1.12.0
+ *
  */
 #ifndef H5_DOXYGEN
 H5_DLL hid_t H5Fcreate_async(const char *app_file, const char *app_func, unsigned app_line,
@@ -450,7 +453,10 @@ H5_DLL hid_t H5Fcreate_async(const char *filename, unsigned flags, hid_t fcpl_id
  *
  * \version 1.10.0 The #H5F_ACC_SWMR_WRITE and #H5F_ACC_SWMR_READ flags were added.
  *
+ * \since 1.0.0
+ *
  * \see H5Fclose()
+ *
  *
  */
 H5_DLL hid_t H5Fopen(const char *filename, unsigned flags, hid_t fapl_id);
@@ -458,6 +464,9 @@ H5_DLL hid_t H5Fopen(const char *filename, unsigned flags, hid_t fapl_id);
  * --------------------------------------------------------------------------
  * \ingroup ASYNC
  * \async_variant_of{H5Fopen}
+ *
+ * \since 1.12.0
+ *
  */
 #ifndef H5_DOXYGEN
 H5_DLL hid_t H5Fopen_async(const char *app_file, const char *app_func, unsigned app_line,
@@ -489,12 +498,17 @@ H5_DLL hid_t H5Fopen_async(const char *filename, unsigned flags, hid_t access_pl
  *       active \p file_id. E.g., one cannot close a file with H5Fclose() on
  *       \p file_id then use H5Freopen() on \p file_id to reopen it.
  *
+ * \since 1.0.0
+ *
  */
 H5_DLL hid_t H5Freopen(hid_t file_id);
 /**
  * --------------------------------------------------------------------------
  * \ingroup ASYNC
  * \async_variant_of{H5Freopen}
+ *
+ * \since 1.12.0
+ *
  */
 #ifndef H5_DOXYGEN
 H5_DLL hid_t H5Freopen_async(const char *app_file, const char *app_func, unsigned app_line, hid_t file_id,
@@ -533,12 +547,17 @@ H5_DLL hid_t H5Freopen_async(hid_t file_id, hid_t es_id);
  *            that, the OS is responsible for ensuring that the data is
  *            actually flushed to disk.
  *
+ * \since 1.0.0
+ *
  */
 H5_DLL herr_t H5Fflush(hid_t object_id, H5F_scope_t scope);
 /**
  * --------------------------------------------------------------------------
  * \ingroup ASYNC
  * \async_variant_of{H5Fflush}
+ *
+ * \since 1.12.0
+ *
  */
 #ifndef H5_DOXYGEN
 H5_DLL herr_t H5Fflush_async(const char *app_file, const char *app_func, unsigned app_line, hid_t object_id,
@@ -586,6 +605,8 @@ H5_DLL herr_t H5Fflush_async(hid_t object_id, H5F_scope_t scope, hid_t es_id);
  *       before calling H5Fclose. It is generally recommended to do so in all
  *       cases.
  *
+ * \since 1.0.0
+ *
  * \see H5Fopen()
  *
  */
@@ -594,6 +615,9 @@ H5_DLL herr_t H5Fclose(hid_t file_id);
  * --------------------------------------------------------------------------
  * \ingroup ASYNC
  * \async_variant_of{H5Fclose}
+ *
+ * \since 1.12.0
+ *
  */
 #ifndef H5_DOXYGEN
 H5_DLL herr_t H5Fclose_async(const char *app_file, const char *app_func, unsigned app_line, hid_t file_id,
@@ -656,6 +680,8 @@ H5_DLL herr_t H5Fdelete(const char *filename, hid_t fapl_id);
  *          The creation property list identifier should be released with
  *          H5Pclose().
  *
+ * \since 1.0.0
+ *
  */
 H5_DLL hid_t H5Fget_create_plist(hid_t file_id);
 /**
@@ -668,6 +694,8 @@ H5_DLL hid_t H5Fget_create_plist(hid_t file_id);
  *
  * \details H5Fget_access_plist() returns the file access property list
  *          identifier of the specified file.
+ *
+ * \since 1.0.0
  *
  */
 H5_DLL hid_t H5Fget_access_plist(hid_t file_id);
@@ -748,6 +776,8 @@ H5_DLL herr_t H5Fget_fileno(hid_t file_id, unsigned long *fileno);
  * \version 1.6.5 #H5F_OBJ_LOCAL has been added as a qualifier on the types
  *                of objects to be counted. #H5F_OBJ_LOCAL restricts the
  *                search to objects opened through current file identifier.
+ *
+ * \since 1.6.0
  *
  */
 H5_DLL ssize_t H5Fget_obj_count(hid_t file_id, unsigned types);
@@ -842,6 +872,8 @@ H5_DLL herr_t H5Fget_vfd_handle(hid_t file_id, hid_t fapl, void **file_handle);
  *       proper value to pass for \p plist is #H5P_DEFAULT, indicating the
  *       default file mount property list.
  *
+ * \since 1.0.0
+ *
  */
 H5_DLL herr_t H5Fmount(hid_t loc, const char *name, hid_t child, hid_t plist);
 /**
@@ -863,6 +895,8 @@ H5_DLL herr_t H5Fmount(hid_t loc, const char *name, hid_t child, hid_t plist);
  *          mount point was opened before the mount then it is the group in the
  *          parent; if it was opened after the mount then it is the root group
  *          of the child.
+ *
+ * \since 1.0.0
  *
  */
 H5_DLL herr_t H5Funmount(hid_t loc, const char *name);
@@ -1067,6 +1101,8 @@ H5_DLL herr_t H5Fset_mdc_config(hid_t file_id, const H5AC_cache_config_t *config
  *          See the overview of the metadata cache in the special topics section of the user manual for
  *          details on the metadata cache and its adaptive resize algorithms.
  *
+ * \since 1.8.0
+ *
  */
 H5_DLL herr_t H5Fget_mdc_hit_rate(hid_t file_id, double *hit_rate_ptr);
 /**
@@ -1098,6 +1134,8 @@ H5_DLL herr_t H5Fget_mdc_hit_rate(hid_t file_id, double *hit_rate_ptr);
  *          Current size can exceed maximum size under certain conditions. See the overview of the
  *          metadata cache in the special topics section of the user manual for a discussion of this.
  *
+ * \since 1.8.0
+ *
  */
 H5_DLL herr_t H5Fget_mdc_size(hid_t file_id, size_t *max_size_ptr, size_t *min_clean_size_ptr,
                               size_t *cur_size_ptr, int *cur_num_entries_ptr);
@@ -1126,6 +1164,8 @@ H5_DLL herr_t H5Fget_mdc_size(hid_t file_id, size_t *max_size_ptr, size_t *min_c
  * documentation,
  * you should not be using this API call.
  * \endparblock
+ *
+ * \since 1.8.0
  *
  */
 H5_DLL herr_t H5Freset_mdc_hit_rate_stats(hid_t file_id);
