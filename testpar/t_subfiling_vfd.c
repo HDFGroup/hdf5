ea174858b6adca6b98100fce06218b97d7008cc0
Glenn Song <gsong@hdfgroup.org>
2024-03-15 15:34:08 -0500

    Add test case for t_vfd

--- testpar/t_subfiling_vfd.c
+++ testpar/t_subfiling_vfd.c
@@ -3237,6 +3237,7 @@ main(int argc, char **argv)
         SKIPPED();
 #endif
 
+#if H5_HAVE_SUBFILING_VFD
     if (MAINPROCESS)
         printf("\nRe-running tests with environment variables set to the empty string\n");
 
@@ -3268,11 +3269,27 @@ main(int argc, char **argv)
     HDunsetenv("H5FD_SUBFILING_IOC_PER_NODE");
     HDunsetenv("H5FD_SUBFILING_STRIPE_SIZE");
     HDunsetenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX");
-    HDsetenv("H5FD_SUBFILING_SUBFILE_PREFIX", subfiling_subfile_prefix_saved, 1);
-    HDsetenv("H5FD_SUBFILING_IOC_SELECTION_CRITERIA", subfiling_ioc_selection_criteria_saved, 1);
-    HDsetenv("H5FD_SUBFILING_IOC_PER_NODE", subfiling_ioc_per_node_saved, 1);
-    HDsetenv("H5FD_SUBFILING_STRIPE_SIZE", subfiling_stripe_size_saved, 1);
-    HDsetenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX", subfiling_config_file_prefix_saved, 1);
+
+    if (subfiling_subfile_prefix_saved) {
+        HDsetenv("H5FD_SUBFILING_SUBFILE_PREFIX", subfiling_subfile_prefix_saved, 1);
+    }
+    if (subfiling_ioc_selection_criteria_saved) {
+        HDsetenv("H5FD_SUBFILING_IOC_SELECTION_CRITERIA", subfiling_ioc_selection_criteria_saved, 1);
+    }
+    if (subfiling_ioc_per_node_saved) {
+        HDsetenv("H5FD_SUBFILING_IOC_PER_NODE", subfiling_ioc_per_node_saved, 1);
+    }
+    if (subfiling_stripe_size_saved) {
+        HDsetenv("H5FD_SUBFILING_STRIPE_SIZE", subfiling_stripe_size_saved, 1);
+    }
+    if(subfiling_config_file_prefix_saved) {
+        HDsetenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX", subfiling_config_file_prefix_saved, 1);
+    }
+
+#else
+    if (MAINPROCESS)
+        SKIPPED();
+#endif
 
     if (nerrors)
         goto exit;
--- testpar/t_vfd.c
+++ testpar/t_vfd.c
@@ -6187,11 +6187,16 @@ main(int argc, char **argv)
 #ifdef H5_HAVE_SUBFILING_VFD
     int required = MPI_THREAD_MULTIPLE;
     int provided = 0;
+    char *subfiling_subfile_prefix_saved         = getenv("H5FD_SUBFILING_SUBFILE_PREFIX");
+    char *subfiling_ioc_selection_criteria_saved = getenv("H5FD_SUBFILING_IOC_SELECTION_CRITERIA");
+    char *subfiling_ioc_per_node_saved           = getenv("H5FD_SUBFILING_IOC_PER_NODE");
+    char *subfiling_stripe_size_saved            = getenv("H5FD_SUBFILING_STRIPE_SIZE");
+    char *subfiling_config_file_prefix_saved     = getenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX");
 #endif
     int mpi_size;
     int mpi_rank = 0;
     int ret;
-
+    
 #ifdef H5_HAVE_SUBFILING_VFD
     if (MPI_SUCCESS != MPI_Init_thread(&argc, &argv, required, &provided)) {
         printf("    MPI doesn't support MPI_Init_thread with MPI_THREAD_MULTIPLE. Exiting\n");
@@ -6249,6 +6254,58 @@ main(int argc, char **argv)
     }
 
     test_vector_io(mpi_rank, mpi_size);
+
+    if (mpi_rank == 0)
+        printf("\n --- TESTING SUBFILING VFD: environment variables set to empty --- \n");
+    
+    HDsetenv("H5FD_SUBFILING_SUBFILE_PREFIX", "", 1);
+    HDsetenv("H5FD_SUBFILING_IOC_SELECTION_CRITERIA", "", 1);
+    HDsetenv("H5FD_SUBFILING_IOC_PER_NODE", "", 1);
+    HDsetenv("H5FD_SUBFILING_STRIPE_SIZE", "", 1);
+    HDsetenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX", "", 1);
+
+    MPI_Barrier(comm);
+
+    if (mpi_rank == 0)
+        printf("\n --- TESTING MPIO VFD: selection I/O --- \n");
+
+    test_selection_io(mpi_rank, mpi_size);
+
+    if (mpi_rank == 0)
+        printf("\n --- TESTING MPIO VFD: vector I/O --- \n");
+
+    if (mpi_size < 2) {
+        if (mpi_rank == 0) {
+            printf("     Need at least 2 processes to run tests for vector I/O.");
+            SKIPPED();
+        }
+        printf("\n");
+        goto finish;
+    }
+
+    test_vector_io(mpi_rank, mpi_size);
+
+    HDunsetenv("H5FD_SUBFILING_SUBFILE_PREFIX");
+    HDunsetenv("H5FD_SUBFILING_IOC_SELECTION_CRITERIA");
+    HDunsetenv("H5FD_SUBFILING_IOC_PER_NODE");
+    HDunsetenv("H5FD_SUBFILING_STRIPE_SIZE");
+    HDunsetenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX");
+
+    if (subfiling_subfile_prefix_saved) {
+        HDsetenv("H5FD_SUBFILING_SUBFILE_PREFIX", subfiling_subfile_prefix_saved, 1);
+    }
+    if (subfiling_ioc_selection_criteria_saved) {
+        HDsetenv("H5FD_SUBFILING_IOC_SELECTION_CRITERIA", subfiling_ioc_selection_criteria_saved, 1);
+    }
+    if (subfiling_ioc_per_node_saved) {
+        HDsetenv("H5FD_SUBFILING_IOC_PER_NODE", subfiling_ioc_per_node_saved, 1);
+    }
+    if (subfiling_stripe_size_saved) {
+        HDsetenv("H5FD_SUBFILING_STRIPE_SIZE", subfiling_stripe_size_saved, 1);
+    }
+    if(subfiling_config_file_prefix_saved) {
+        HDsetenv("H5FD_SUBFILING_CONFIG_FILE_PREFIX", subfiling_config_file_prefix_saved, 1);
+    }
 
 finish:
     /* make sure all processes are finished before final report, cleanup
