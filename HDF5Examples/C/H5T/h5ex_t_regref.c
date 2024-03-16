3bd0dc46bc2f1177377779a216a82ccd05dd1d90
Jordan Henderson <jhenderson@hdfgroup.org>
2024-03-16 13:55:01 -0500

    Fix some minor warnings

--- HDF5Examples/C/H5T/h5ex_t_regref.c
+++ HDF5Examples/C/H5T/h5ex_t_regref.c
@@ -168,7 +168,7 @@ main(void)
      * Output the data to the screen.
      */
     for (i = 0; i < dims[0]; i++) {
-        printf("%s[%llu]:\n  ->", DATASET, i);
+        printf("%s[%" PRIuHSIZE "]:\n  ->", DATASET, i);
 
         /*
          * Open the referenced object, retrieve its region as a
