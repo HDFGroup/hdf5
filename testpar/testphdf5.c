685f8177b833483c285a064dca7d09b457cff4d7
Dana Robinson <derobins@hdfgroup.org>
2024-03-20 22:12:04 -0700

    Fix if block screwup

--- testpar/testphdf5.c
+++ testpar/testphdf5.c
@@ -73,17 +73,18 @@ pause_proc(void)
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
     MPI_Get_processor_name(mpi_name, &mpi_namelen);
 
-    if (MAINPROCESS)
+    if (MAINPROCESS) {
         memset(&statbuf, 0, sizeof(h5_stat_t));
-    while ((HDstat(greenlight, &statbuf) == -1) && loops < maxloop) {
-        if (!loops++) {
-            printf("Proc %d (%*s, %d): to debug, attach %d\n", mpi_rank, mpi_namelen, mpi_name, pid, pid);
-        }
-        printf("waiting(%ds) for file %s ...\n", time_int, greenlight);
-        fflush(stdout);
-        HDsleep(time_int);
+        while ((HDstat(greenlight, &statbuf) == -1) && loops < maxloop) {
+            if (!loops++) {
+                printf("Proc %d (%*s, %d): to debug, attach %d\n", mpi_rank, mpi_namelen, mpi_name, pid, pid);
+            }
+            printf("waiting(%ds) for file %s ...\n", time_int, greenlight);
+            fflush(stdout);
+            HDsleep(time_int);
 
-        memset(&statbuf, 0, sizeof(h5_stat_t));
+            memset(&statbuf, 0, sizeof(h5_stat_t));
+        }
     }
     MPI_Barrier(MPI_COMM_WORLD);
 }
