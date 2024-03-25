a043b33878f6c59892c0d8738394659d8f1eccba
Larry Knox <lrknox@hdfgroup.org>
2024-03-25 14:45:17 -0500

    Update links in documentation from develop to v1_14.

--- src/H5Fmodule.h
+++ src/H5Fmodule.h
@@ -235,10 +235,10 @@
  * Note that the root group, indicated above by /, was automatically created when the file was created.
  *
  * h5dump is described on the
- * <a href="https://docs.hdfgroup.org/hdf5/develop/_view_tools_view.html#subsecViewToolsViewContent_h5dump">
+ * <a href="https://docs.hdfgroup.org/hdf5/v1_14/_view_tools_view.html#subsecViewToolsViewContent_h5dump">
  * Tools</a>
  * page under
- * <a href="https://docs.hdfgroup.org/hdf5/develop/_view_tools_command.html">Command-line Tools</a>.
+ * <a href="https://docs.hdfgroup.org/hdf5/v1_14/_view_tools_command.html">Command-line Tools</a>.
  * The HDF5 DDL grammar is described in the document \ref DDLBNF114.
  *
  * \subsection subsec_file_summary File Function Summaries
