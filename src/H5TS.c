11b57ac66382b5e743a68889632540720a479a2c
Dana Robinson <43805+derobins@users.noreply.github.com>
2024-04-08 07:57:54 -0700

    Add NAMECHECK_ONLY to macro list in .clang-format (#4343)
    This caused H5TS.c functions to be mis-formatted

--- src/H5TS.c
+++ src/H5TS.c
@@ -414,10 +414,12 @@ H5TS__mutex_acquire(H5TS_mutex_t *mutex, unsigned int lock_count, bool *acquired
  *--------------------------------------------------------------------------
  */
 herr_t
-H5TSmutex_acquire(unsigned int lock_count, bool *acquired){
+H5TSmutex_acquire(unsigned int lock_count, bool *acquired)
+{
     FUNC_ENTER_API_NAMECHECK_ONLY
 
-        FUNC_LEAVE_API_NAMECHECK_ONLY(H5TS__mutex_acquire(&H5_g.init_lock, lock_count, acquired))}
+    FUNC_LEAVE_API_NAMECHECK_ONLY(H5TS__mutex_acquire(&H5_g.init_lock, lock_count, acquired))
+}
 /* end H5TSmutex_acquire() */
 
 /*--------------------------------------------------------------------------
@@ -440,7 +442,8 @@ H5TSmutex_acquire(unsigned int lock_count, bool *acquired){
  *
  *--------------------------------------------------------------------------
  */
-herr_t H5TS_mutex_lock(H5TS_mutex_t *mutex)
+herr_t
+H5TS_mutex_lock(H5TS_mutex_t *mutex)
 {
     herr_t ret_value = SUCCEED;
 
