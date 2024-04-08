2f4372104a312cc3030e4bb00bfbad100b76249f
Dana Robinson <43805+derobins@users.noreply.github.com>
2024-04-08 07:57:42 -0700

    Remove `#undef ERR` from H5private.h (#4346)
    This is an old work-around that is no longer necessary.
    
    The only place where we defined ERR is in h5import, and that code
    has been updated (in this PR) to rename ERR to INVALID_TOKEN.

--- src/H5private.h
+++ src/H5private.h
@@ -288,14 +288,6 @@
 #define SUCCEED 0
 #define FAIL    (-1)
 
-/* The HDF5 library uses the symbol `ERR` frequently.  So do
- * header files for libraries such as curses(3), terminfo(3), etc.
- * Remove its definition here to avoid clashes with HDF5.
- */
-#ifdef ERR
-#undef ERR
-#endif
-
 /* number of members in an array */
 #ifndef NELMTS
 #define NELMTS(X) (sizeof(X) / sizeof(X[0]))
