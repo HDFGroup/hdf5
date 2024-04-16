91151432da9b1735605b6295a72a6e18f06184c0
Quincey Koziol <quincey@koziol.cc>
2024-04-15 21:01:11 -0500

    Debug CI failure on Github
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- test/ttsafe.c
+++ test/ttsafe.c
@@ -103,39 +103,39 @@ main(int argc, char *argv[])
     TestInit(argv[0], NULL, NULL);
 
     /* Tests are generally arranged from least to most complexity... */
-    AddTest("is_threadsafe", tts_is_threadsafe, NULL, "library threadsafe status", NULL);
+//    AddTest("is_threadsafe", tts_is_threadsafe, NULL, "library threadsafe status", NULL);
 #ifdef H5_HAVE_THREADS
-    AddTest("thread_pool", tts_thread_pool, NULL, "thread pools", NULL);
+//    AddTest("thread_pool", tts_thread_pool, NULL, "thread pools", NULL);
 #ifndef H5_HAVE_STDATOMIC_H
     /* C11 atomics only tested when emulated */
-    AddTest("atomics", tts_atomics, NULL, "emulation of C11 atomics", NULL);
+//    AddTest("atomics", tts_atomics, NULL, "emulation of C11 atomics", NULL);
 #endif /* H5_HAVE_STDATOMIC_H */
 #ifndef H5_HAVE_WIN_THREADS
     /* Recursive R/W locks */
-    AddTest("rec_rwlock_1", tts_rec_rw_lock_smoke_check_1, NULL, "recursive R/W lock smoke check 1 -- basic",
-            NULL);
-    AddTest("rec_rwlock_2", tts_rec_rw_lock_smoke_check_2, NULL,
-            "recursive R/W lock smoke check 2 -- mob of readers", NULL);
-    AddTest("rec_rwlock_3", tts_rec_rw_lock_smoke_check_3, NULL,
-            "recursive R/W lock smoke check 3 -- mob of writers", NULL);
-    AddTest("rec_rwlock_4", tts_rec_rw_lock_smoke_check_4, NULL,
-            "recursive R/W lock smoke check 4 -- mixed mob", NULL);
+//    AddTest("rec_rwlock_1", tts_rec_rw_lock_smoke_check_1, NULL, "recursive R/W lock smoke check 1 -- basic",
+//            NULL);
+//    AddTest("rec_rwlock_2", tts_rec_rw_lock_smoke_check_2, NULL,
+//            "recursive R/W lock smoke check 2 -- mob of readers", NULL);
+//    AddTest("rec_rwlock_3", tts_rec_rw_lock_smoke_check_3, NULL,
+//            "recursive R/W lock smoke check 3 -- mob of writers", NULL);
+//    AddTest("rec_rwlock_4", tts_rec_rw_lock_smoke_check_4, NULL,
+//            "recursive R/W lock smoke check 4 -- mixed mob", NULL);
 #endif /* !H5_HAVE_WIN_THREADS */
 
 #ifdef H5_HAVE_THREADSAFE
-    AddTest("thread_id", tts_thread_id, NULL, "thread IDs", NULL);
+//    AddTest("thread_id", tts_thread_id, NULL, "thread IDs", NULL);
 
-    AddTest("dcreate", tts_dcreate, cleanup_dcreate, "multi-dataset creation", NULL);
-    AddTest("error", tts_error, cleanup_error, "per-thread error stacks", NULL);
+//    AddTest("dcreate", tts_dcreate, cleanup_dcreate, "multi-dataset creation", NULL);
+//    AddTest("error", tts_error, cleanup_error, "per-thread error stacks", NULL);
 #ifdef H5_HAVE_PTHREAD_H
     /* Thread cancellability only supported with pthreads ... */
     AddTest("cancel", tts_cancel, cleanup_cancel, "thread cancellation safety test", NULL);
 #endif /* H5_HAVE_PTHREAD_H */
-    AddTest("acreate", tts_acreate, cleanup_acreate, "multi-attribute creation", NULL);
-    AddTest("attr_vlen", tts_attr_vlen, cleanup_attr_vlen, "multi-file-attribute-vlen read", NULL);
+//    AddTest("acreate", tts_acreate, cleanup_acreate, "multi-attribute creation", NULL);
+//    AddTest("attr_vlen", tts_attr_vlen, cleanup_attr_vlen, "multi-file-attribute-vlen read", NULL);
 
     /* Developer API routine tests */
-    AddTest("developer", tts_develop_api, NULL, "developer API routines", NULL);
+//    AddTest("developer", tts_develop_api, NULL, "developer API routines", NULL);
 
 #else /* H5_HAVE_THREADSAFE */
 
