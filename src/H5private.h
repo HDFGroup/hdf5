b04f39e7f762f10858ad84482697b96afc6b621c
Quincey Koziol <quincey@koziol.cc>
2024-04-25 13:33:11 -0500

    Start refactoring H5E code to avoid using IDs internally (#4427)

--- src/H5private.h
+++ src/H5private.h
@@ -1563,7 +1563,7 @@ H5_DLL herr_t H5CX_pop(bool update_dxpl_props);
     }                                                                                                        \
     H5_POP_FUNC                                                                                              \
     if (err_occurred)                                                                                        \
-        (void)H5E_dump_api_stack(true);                                                                      \
+        (void)H5E_dump_api_stack();                                                                          \
     FUNC_LEAVE_API_THREADSAFE                                                                                \
     return (ret_value);                                                                                      \
     }                                                                                                        \
@@ -1575,7 +1575,7 @@ H5_DLL herr_t H5CX_pop(bool update_dxpl_props);
     } /*end scope from end of FUNC_ENTER*/                                                                   \
     H5_POP_FUNC                                                                                              \
     if (err_occurred)                                                                                        \
-        (void)H5E_dump_api_stack(true);                                                                      \
+        (void)H5E_dump_api_stack();                                                                          \
     FUNC_LEAVE_API_THREADSAFE                                                                                \
     return (ret_value);                                                                                      \
     }                                                                                                        \
@@ -1598,7 +1598,7 @@ H5_DLL herr_t H5CX_pop(bool update_dxpl_props);
     ;                                                                                                        \
     } /*end scope from end of FUNC_ENTER*/                                                                   \
     if (err_occurred)                                                                                        \
-        (void)H5E_dump_api_stack(true);                                                                      \
+        (void)H5E_dump_api_stack();                                                                          \
     FUNC_LEAVE_API_THREADSAFE                                                                                \
     return (ret_value);                                                                                      \
     }                                                                                                        \
