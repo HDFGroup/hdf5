95019547b9958bd4796823eeb5c133239b87dec7
Quincey Koziol <quincey@koziol.cc>
2024-04-25 16:33:47 -0500

    Add support for builtin_expect compiler hint (#4425)
    * Add support for __builtin_expect extension
    
    And H5_LIKELY / H5_UNLIKELY macros to wrap it
    
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>
    
    * Committing clang-format changes
    
    ---------
    
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- config/cmake/HDFTests.c
+++ config/cmake/HDFTests.c
@@ -15,6 +15,21 @@
 #define SIMPLE_TEST(x) int main(void){ x; return 0; }
 
 
+#ifdef HAVE_BUILTIN_EXPECT
+
+int
+main ()
+{
+    void *ptr = (void*) 0;
+
+    if (__builtin_expect (ptr != (void*) 0, 1))
+        return 0;
+
+    return 0;
+}
+
+#endif /* HAVE_BUILTIN_EXPECT */
+
 #ifdef HAVE_ATTRIBUTE
 
 int
