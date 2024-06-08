dca63706917a5412580032841771ed610eb045d9
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2024-06-08 06:46:09 -0500

    Move C++ and Fortran and examples to HDF5Examples folder (#4552)

---
+++ HDF5Examples/CXX/H5D/C_sourcefiles.cmake
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Define Sources, one file per application
+#-----------------------------------------------------------------------------
+set (examples
+    create
+    readdata
+    writedata
+    compound
+    extend_ds
+    chunks
+    h5group
+)
