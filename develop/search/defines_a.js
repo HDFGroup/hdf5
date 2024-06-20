487116f1ff291e0c555a5c77db020cd842a43a31
lrknox <lrknox@users.noreply.github.com>
2024-06-20 00:54:34 +0000

    deploy: 9fe7132fe3c7f5fb85ed01c8ab16dc12f755450a

--- develop/search/defines_a.js
+++ develop/search/defines_a.js
@@ -1,12 +1,8 @@
 var searchData=
 [
   ['s3comms_5fformat_5fcredential_0',['S3COMMS_FORMAT_CREDENTIAL',['../_h5_f_ds3comms_8h.html#a24add4687ae8b28b3c6e8ac8b202f0f7',1,'H5FDs3comms.h']]],
-  ['s3comms_5fhrb_5fmagic_1',['S3COMMS_HRB_MAGIC',['../_h5_f_ds3comms_8h.html#afffc42e5df289eebd183b2291f4333b3',1,'H5FDs3comms.h']]],
-  ['s3comms_5fhrb_5fnode_5fmagic_2',['S3COMMS_HRB_NODE_MAGIC',['../_h5_f_ds3comms_8h.html#a05eb3fc091e5a18803c6187b59f79b75',1,'H5FDs3comms.h']]],
-  ['s3comms_5fmax_5fcredential_5fsize_3',['S3COMMS_MAX_CREDENTIAL_SIZE',['../_h5_f_ds3comms_8h.html#a5bcfb420a43fd8756a09368612dd1647',1,'H5FDs3comms.h']]],
-  ['s3comms_5fparsed_5furl_5fmagic_4',['S3COMMS_PARSED_URL_MAGIC',['../_h5_f_ds3comms_8h.html#a731bdc70ce1bbdf8b3c9716283ec2025',1,'H5FDs3comms.h']]],
-  ['s3comms_5fs3r_5fmagic_5',['S3COMMS_S3R_MAGIC',['../_h5_f_ds3comms_8h.html#a2ae5c070188a73edf17ef86922de650b',1,'H5FDs3comms.h']]],
-  ['skip_5fno_5fcb_6',['SKIP_NO_CB',['../_h5_f_dprivate_8h.html#aae6815375b2e13c715a0c124f8b95c4f',1,'H5FDprivate.h']]],
-  ['skip_5fselection_5fcb_7',['SKIP_SELECTION_CB',['../_h5_f_dprivate_8h.html#a8f298fd60fd4b92778cf1cb26dfe6a73',1,'H5FDprivate.h']]],
-  ['skip_5fvector_5fcb_8',['SKIP_VECTOR_CB',['../_h5_f_dprivate_8h.html#a399947bdf27f1e72963feb12da85040f',1,'H5FDprivate.h']]]
+  ['s3comms_5fmax_5fcredential_5fsize_1',['S3COMMS_MAX_CREDENTIAL_SIZE',['../_h5_f_ds3comms_8h.html#a5bcfb420a43fd8756a09368612dd1647',1,'H5FDs3comms.h']]],
+  ['skip_5fno_5fcb_2',['SKIP_NO_CB',['../_h5_f_dprivate_8h.html#aae6815375b2e13c715a0c124f8b95c4f',1,'H5FDprivate.h']]],
+  ['skip_5fselection_5fcb_3',['SKIP_SELECTION_CB',['../_h5_f_dprivate_8h.html#a8f298fd60fd4b92778cf1cb26dfe6a73',1,'H5FDprivate.h']]],
+  ['skip_5fvector_5fcb_4',['SKIP_VECTOR_CB',['../_h5_f_dprivate_8h.html#a399947bdf27f1e72963feb12da85040f',1,'H5FDprivate.h']]]
 ];
