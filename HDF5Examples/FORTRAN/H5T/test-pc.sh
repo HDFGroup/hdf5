692379df68d6719c2d311769a8d5d84029f03005
Larry Knox <lrknox@hdfgroup.org>
2024-07-29 11:27:25 -0500

    Remove outdated line from Copyright header in new files. Fix typo in testframe.c.

--- HDF5Examples/FORTRAN/H5T/test-pc.sh
+++ HDF5Examples/FORTRAN/H5T/test-pc.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 #
 # Copyright by The HDF Group.
-# Copyright by the Board of Trustees of the University of Illinois.
 # All rights reserved.
 #
 # This file is part of HDF5.  The full HDF5 copyright notice, including
