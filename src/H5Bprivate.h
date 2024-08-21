3cf744ac81c5d75a07ad16c825dcb11bb4995888
Quincey Koziol <quincey@koziol.cc>
2024-08-21 09:28:59 -0500

    Eliminate another use of H5E_clear_stack() within the library (#4726)
    * Remove call to H5E_clear_stack()
    
    Also clean up a bunch of error macros and the return value from H5B_valid()

--- src/H5Bprivate.h
+++ src/H5Bprivate.h
@@ -142,5 +142,5 @@ H5_DLL H5B_shared_t *H5B_shared_new(const H5F_t *f, const H5B_class_t *type, siz
 H5_DLL herr_t        H5B_shared_free(void *_shared);
 H5_DLL herr_t H5B_debug(H5F_t *f, haddr_t addr, FILE *stream, int indent, int fwidth, const H5B_class_t *type,
                         void *udata);
-H5_DLL htri_t H5B_valid(H5F_t *f, const H5B_class_t *type, haddr_t addr);
+H5_DLL herr_t H5B_valid(H5F_t *f, const H5B_class_t *type, haddr_t addr);
 #endif /* H5Bprivate_H */
