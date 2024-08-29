d3110c8d48c7fc03d38eb36ceeb547d1f2f554e5
lrknox <lrknox@users.noreply.github.com>
2024-08-29 00:58:45 +0000

    deploy: d2be61826d7685ccd0f562ed716269544cca27ac

--- develop/search/files_2.js
+++ develop/search/files_2.js
@@ -3,7 +3,8 @@ var searchData=
   ['cmake_2dvols_2dfetchcontent_2emd_0',['cmake-vols-fetchcontent.md',['../cmake-vols-fetchcontent_8md.html',1,'']]],
   ['code_2dconventions_2emd_1',['code-conventions.md',['../code-conventions_8md.html',1,'']]],
   ['code_5fof_5fconduct_2emd_2',['CODE_OF_CONDUCT.md',['../_c_o_d_e___o_f___c_o_n_d_u_c_t_8md.html',1,'']]],
-  ['contributing_2emd_3',['CONTRIBUTING.md',['../_c_o_n_t_r_i_b_u_t_i_n_g_8md.html',1,'']]],
-  ['cookbook_2edox_4',['Cookbook.dox',['../_cookbook_8dox.html',1,'']]],
-  ['core_5fmenu_2emd_5',['core_menu.md',['../core__menu_8md.html',1,'']]]
+  ['collectivecalls_2edox_3',['CollectiveCalls.dox',['../_collective_calls_8dox.html',1,'']]],
+  ['contributing_2emd_4',['CONTRIBUTING.md',['../_c_o_n_t_r_i_b_u_t_i_n_g_8md.html',1,'']]],
+  ['cookbook_2edox_5',['Cookbook.dox',['../_cookbook_8dox.html',1,'']]],
+  ['core_5fmenu_2emd_6',['core_menu.md',['../core__menu_8md.html',1,'']]]
 ];
