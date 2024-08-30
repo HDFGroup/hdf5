7fac32e19e39464ca7ade9ff322474abf9c4de3b
H. Joe Lee <hyoklee@hdfgroup.org>
2024-08-30 11:06:52 -0500

    Fix grammar and simplify comment in H5Fint.c (#4790)

--- src/H5Fint.c
+++ src/H5Fint.c
@@ -1985,7 +1985,7 @@ H5F_open(bool try, H5F_t **_file, const char *name, unsigned flags, hid_t fcpl_i
     if ((ci_load || ci_write) && (flags & (H5F_ACC_SWMR_READ | H5F_ACC_SWMR_WRITE)))
         HGOTO_ERROR(H5E_FILE, H5E_UNSUPPORTED, FAIL, "can't have both SWMR and cache image");
 
-    /* Retain the name the file was opened with */
+    /* Retain the original filename. */
     file->open_name = H5MM_xstrdup(name);
 
     /* Short cuts */
