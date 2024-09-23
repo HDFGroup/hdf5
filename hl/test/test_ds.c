0afab4aa44eb17dd4a37ddc1030913abc50bb39e
Binh-Minh <bmribler@hdfgroup.org>
2024-09-23 14:07:10 -0400

    Fix an incorrect returned value by H5LTfind_dataset()
    H5LTfind_dataset() returns true for non-existing datasets because it only compares up to the length of the searched string, such as "Day" vs "DayNight" (issue GH-4780).
    
    This PR applied the user's patch and added tests.

--- hl/test/test_ds.c
+++ hl/test/test_ds.c
@@ -1341,10 +1341,11 @@ out:
 static int
 test_char_attachscales(const char *fileext)
 {
-    hid_t fid = -1;
-    hid_t did = -1;
-    char  dsname[32];
-    char  scalename[32];
+    hid_t  fid = -1;
+    hid_t  did = -1;
+    char   dsname[32];
+    char   scalename[32];
+    herr_t ds_existed = 0;
 
     snprintf(dsname, sizeof(dsname), "%s%s", DATASET_NAME, "ac");
 
@@ -1356,6 +1357,14 @@ test_char_attachscales(const char *fileext)
     /* make a dataset */
     if (create_char_dataset(fid, "ac", 0) < 0)
         goto out;
+
+    /* test finding dataset dsname */
+    if ((ds_existed = H5LTfind_dataset(fid, dsname)) < 0)
+        goto out;
+    if (ds_existed == 0) {
+        printf("Unexpected result: Dataset \"%s\" does exist\n", dsname);
+        goto out;
+    }
 
     if ((did = H5Dopen2(fid, dsname, H5P_DEFAULT)) >= 0) {
         snprintf(scalename, sizeof(scalename), "%s%s", DS_1_NAME, "ac");
