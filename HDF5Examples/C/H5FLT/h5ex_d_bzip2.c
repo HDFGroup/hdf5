c00a7a5f01ccabf131f5e10cda688743cb453007
Dana Robinson <derobins@hdfgroup.org>
2024-10-18 12:01:14 -0700

    Renamed COPYING to LICENSE
    This is where most people will expect to find license information. The
    COPYING_LBNL_HDF5 file has also been renamed to LICENSE_LBNL_HDF5.
    The licenses are unchanged.

--- HDF5Examples/C/H5FLT/h5ex_d_bzip2.c
+++ HDF5Examples/C/H5FLT/h5ex_d_bzip2.c
@@ -5,7 +5,7 @@
  * This file is part of the HDF5 BZIP2 filter plugin source.  The full       *
  * copyright notice, including terms governing use, modification, and        *
  * terms governing use, modification, and redistribution, is contained in    *
- * the file COPYING, which can be found at the root of the BZIP2 source code *
+ * the file LICENSE, which can be found at the root of the BZIP2 source code *
  * distribution tree.  If you do not have access to this file, you may       *
  * request a copy from help@hdfgroup.org.                                    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
