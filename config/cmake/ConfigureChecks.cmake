8e6bc7247ec3abb1d871a7f13f735d9d2cf260b4
Dana Robinson <derobins@hdfgroup.org>
2024-10-23 20:09:02 -0700

    Don't check for alarm(2) on MinGW in CMake
    alarm(2) is spotty on MinGW and often picked up as present by
    configure checks, even when it's non-functional.

--- config/cmake/ConfigureChecks.cmake
+++ config/cmake/ConfigureChecks.cmake
@@ -399,7 +399,12 @@ endif ()
 #-----------------------------------------------------------------------------
 # Check for some functions that are used
 #
-CHECK_FUNCTION_EXISTS (alarm             ${HDF_PREFIX}_HAVE_ALARM)
+if (NOT MINGW)
+  # alarm(2) support is spotty in MinGW, so assume it doesn't exist
+  #
+  # https://lists.gnu.org/archive/html/bug-gnulib/2013-03/msg00040.html
+  CHECK_FUNCTION_EXISTS (alarm             ${HDF_PREFIX}_HAVE_ALARM)
+endif ()
 CHECK_FUNCTION_EXISTS (fcntl             ${HDF_PREFIX}_HAVE_FCNTL)
 CHECK_FUNCTION_EXISTS (flock             ${HDF_PREFIX}_HAVE_FLOCK)
 CHECK_FUNCTION_EXISTS (fork              ${HDF_PREFIX}_HAVE_FORK)
