963c6c3af2ea849316372c2d9ed6dbf0e462d043
Quincey Koziol <quincey@koziol.cc>
2024-11-22 13:26:04 -0500

    Fix MSVC pragma
    Signed-off-by: Quincey Koziol <quincey@koziol.cc>

--- src/H5FL.c
+++ src/H5FL.c
@@ -273,27 +273,27 @@ done:
 #endif /* H5_HAVE_CONCURRENCY */
 
 #ifdef H5_HAVE_THREADS
-#pragma NOTE(warning : "H5_HAVE_THREADS")
+#pragma message("H5_HAVE_THREADS")
 #else /* H5_HAVE_THREADS */
-#pragma NOTE(warning : "NOT H5_HAVE_THREADS")
+#pragma message("NOT H5_HAVE_THREADS")
 #endif /* H5_HAVE_THREADS */
 
 #ifdef H5_HAVE_THREADSAFE_API
-#pragma NOTE(warning : "H5_HAVE_THREADSAFE_API")
+#pragma message("H5_HAVE_THREADSAFE_API")
 #else /* H5_HAVE_THREADSAFE_API */
-#pragma NOTE(warning : "NOT H5_HAVE_THREADSAFE_API")
+#pragma message("NOT H5_HAVE_THREADSAFE_API")
 #endif /* H5_HAVE_THREADSAFE_API */
 
 #ifdef H5_HAVE_CONCURRENCY
-#pragma NOTE(warning : "H5_HAVE_CONCURRENCY")
+#pragma message("H5_HAVE_CONCURRENCY")
 #else /* H5_HAVE_CONCURRENCY */
-#pragma NOTE(warning : "NOT H5_HAVE_CONCURRENCY")
+#pragma message("NOT H5_HAVE_CONCURRENCY")
 #endif /* H5_HAVE_CONCURRENCY */
 
 #ifdef H5_HAVE_STDATOMIC_H
-#pragma NOTE(warning : "H5_HAVE_STDATOMIC_H")
+#pragma message("H5_HAVE_STDATOMIC_H")
 #else /* H5_HAVE_STDATOMIC_H */
-#pragma NOTE(warning : "NOT H5_HAVE_STDATOMIC_H")
+#pragma message("NOT H5_HAVE_STDATOMIC_H")
 #endif /* H5_HAVE_STDATOMIC_H */
 
 /*--------------------------------------------------------------------------
