08fe651c41f97ba13caa11da44c3bc2c3f653e64
Allen Byrne <byrn@hdfgroup.org>
2025-01-03 10:12:21 -0600

    Remove non-existent link

--- java/src/hdf/hdf5lib/callbacks/H5Callbacks.java
+++ java/src/hdf/hdf5lib/callbacks/H5Callbacks.java
@@ -17,7 +17,7 @@ package hdf.hdf5lib.callbacks;
  * All callback definitions must derive from this interface.  Any
  * derived interfaces must define a single public method  named "callback".
  * You are responsible for deregistering your callback (if necessary)
- * in its {@link Object#finalize} method.  If native code attempts to call
+ * in its Object finalize method.  If native code attempts to call
  * a callback which has been GC'd, you will likely crash the VM.  If
  * there is no method to deregister the callback (e.g. <code>atexit</code>
  * in the C library), you must ensure that you always keep a live reference
