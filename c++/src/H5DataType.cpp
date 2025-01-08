6981bc02f28f5289cfb97d672e8781acf3aa451d
Matthew Larson <matthewjlar@gmail.com>
2025-01-08 08:55:06 -0600

    Use H5Tdecode2 in CPP wrapper

--- c++/src/H5DataType.cpp
+++ c++/src/H5DataType.cpp
@@ -250,7 +250,7 @@ DataType::p_decode() const
     }
 
     // Call C function to decode the binary object description
-    hid_t encoded_dtype_id = H5Tdecode1(encoded_buf);
+    hid_t encoded_dtype_id = H5Tdecode2(encoded_buf, buf_size);
 
     // If H5Tdecode fails, raise exception
     if (encoded_dtype_id < 0) {
