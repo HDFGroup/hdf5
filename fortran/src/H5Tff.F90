f1a3c477bcf96bd5949f3a66ab83473c2b6dd9c1
Matthew Larson <matthewjlar@gmail.com>
2025-01-15 08:17:30 -0600

    Use overloading in Fortran wrapper

--- fortran/src/H5Tff.F90
+++ fortran/src/H5Tff.F90
@@ -58,6 +58,11 @@ MODULE H5T
 
 #endif
 
+INTERFACE h5tdecode_f
+  MODULE PROCEDURE h5tdecode_with_size_f
+  MODULE PROCEDURE h5tdecode_auto_size_f
+END INTERFACE h5tdecode_f
+
 CONTAINS
 
 !>
@@ -1862,9 +1867,9 @@ CONTAINS
 !! \param obj_id   Object ID.
 !! \param hdferr   \fortran_error
 !!
-!! See C API: @ref H5Tdecode()
+!! See C API: @ref H5Tdecode2()
 !!
-  SUBROUTINE h5tdecode_f(buf, buf_size, obj_id, hdferr)
+SUBROUTINE h5tdecode_with_size_f(buf, buf_size, obj_id, hdferr)
     IMPLICIT NONE
     CHARACTER(LEN=*), INTENT(IN) :: buf
     INTEGER(SIZE_T), INTENT(IN) :: buf_size
@@ -1882,8 +1887,39 @@ CONTAINS
     END INTERFACE
 
     hdferr = h5tdecode_c(buf, buf_size, obj_id)
+END SUBROUTINE h5tdecode_with_size_f
 
-  END SUBROUTINE h5tdecode_f
+!>
+!! \ingroup FH5T
+!!
+!! \brief Decode A binary object description of data type and return a new object handle.
+!!
+!! \param buf      Buffer for the data space object to be decoded.
+!! \param obj_id   Object ID.
+!! \param hdferr   \fortran_error
+!!
+!! See C API: @ref H5Tdecode2()
+!!
+SUBROUTINE h5tdecode_auto_size_f(buf, obj_id, hdferr)
+    IMPLICIT NONE
+    CHARACTER(LEN=*), INTENT(IN) :: buf
+    INTEGER(HID_T), INTENT(OUT) :: obj_id
+    INTEGER, INTENT(OUT) :: hdferr
+    INTEGER(SIZE_T) :: buf_size
+    INTERFACE
+       INTEGER FUNCTION h5tdecode_c(buf, buf_size, obj_id) BIND(C,NAME='h5tdecode_c')
+         IMPORT :: C_CHAR
+         IMPORT :: HID_T, SIZE_T
+         IMPLICIT NONE
+         CHARACTER(KIND=C_CHAR), DIMENSION(*), INTENT(IN) :: buf
+         INTEGER(SIZE_T), INTENT(IN) :: buf_size
+         INTEGER(HID_T), INTENT(OUT) :: obj_id
+       END FUNCTION h5tdecode_c
+    END INTERFACE
+
+    buf_size = LEN(buf)
+    hdferr = h5tdecode_c(buf, buf_size, obj_id)
+END SUBROUTINE h5tdecode_auto_size_f
 
 !>
 !! \ingroup FH5T
