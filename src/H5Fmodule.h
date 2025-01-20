0e240e6f05d1eafc6a0689d8a8fa0b1abd003025
Allen Byrne <byrn@hdfgroup.org>
2025-01-20 10:47:50 -0600

    Fix obvious mistakes

--- src/H5Fmodule.h
+++ src/H5Fmodule.h
@@ -238,7 +238,7 @@
  * \ref subsecViewToolsViewContent_h5dump
  * page under
  * \ref ViewToolsCommand.
- * The HDF5 DDL grammar is described in the document \ref DDLBNF200.
+ * The HDF5 DDL grammar is described in the document \ref DDLBNF114.
  *
  * \subsection subsec_file_summary File Function Summaries
  * General library (\ref H5 functions and macros), (\ref H5F functions), file related
