80d149ef1f9d96236a5695550bd7bd030cc12993
Matthew Larson <matthewjlar@gmail.com>
2025-02-17 10:29:23 -0600

    Fix bad objcopy test cleanup on failure

--- test/objcopy.c
+++ test/objcopy.c
@@ -17092,6 +17092,8 @@ error:
     {
         H5Dclose(did);
         H5Dclose(did2);
+        H5Dclose(did3);
+        H5Dclose(did4);
         H5Sclose(sid);
         H5Gclose(gid);
         H5Gclose(gid2);
