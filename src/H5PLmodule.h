b0ddf88e3391d30d5dca1b67f8ebd0758f3eff3c
Aleksandar Jelenak <ajelenak@users.noreply.github.com>
2025-02-19 16:42:42 -0500

    Another try to please clang-format

--- src/H5PLmodule.h
+++ src/H5PLmodule.h
@@ -43,10 +43,9 @@
  * available on the system in a default location. The HDF5 filter plugin is discussed in detail in the
  * \ref subsec_filter_plugins_prog section.
  *
- // clang-format off
- * \subsubsection subsubsec_filter_plugins_model_apply Applying a Third-party Filter When Creating and Writing
- a Dataset
- // clang-format on
+ /* clang-format off */
+ * \subsubsection subsubsec_filter_plugins_model_apply Applying a Third-party Filter When Creating and Writing a Dataset
+ /* clang-format on */
  * A third-party filter can be added to the HDF5 filter pipeline by using the H5Pset_filter
  * function, as a user would do in the past. The identification number and the filter parameters should be
  * available to the application. For example, if the application intends to apply the HDF5 bzip2 compression
