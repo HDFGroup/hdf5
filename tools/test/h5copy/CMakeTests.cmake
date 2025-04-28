2ca79003dcb9c6d32fe3d5ed6e312e9896dba53a
Matthew Larson <matthewjlar@gmail.com>
2025-04-28 10:21:33 -0500

    Fix incorrect plugin path

--- tools/test/h5copy/CMakeTests.cmake
+++ tools/test/h5copy/CMakeTests.cmake
@@ -568,7 +568,7 @@
                 -D "TEST_MASK_ERROR=true"
                 -D "TEST_APPEND=EXIT CODE:"
                 -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}:${vol_plugin_path}"
+                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins:${vol_plugin_path}"
                 -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
                 -P "${HDF_RESOURCES_DIR}/runTest.cmake"
         )
