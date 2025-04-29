2812e587bd90073b2e8a248113f81bcacce5994a
Allen Byrne <byrn@hdfgroup.org>
2025-04-29 08:00:22 -0500

    Correct all refs to HDF5_USE_GNU_DIRS

--- HDF5Examples/config/cmake/HDFMacros.cmake
+++ HDF5Examples/config/cmake/HDFMacros.cmake
@@ -90,7 +90,7 @@ macro (HDFTEST_COPY_FILE src dest target)
 endmacro ()
 
 macro (HDF_DIR_PATHS package_prefix)
-  option (H5EX_USE_GNU_DIRS "ON to use GNU Coding Standard install directory variables, OFF to use historical settings" OFF)
+  option (H5EX_USE_GNU_DIRS "ON to use GNU Coding Standard install directory variables, OFF to use historical settings" ON)
   if (H5EX_USE_GNU_DIRS)
     include(GNUInstallDirs)
     if (NOT ${package_prefix}_INSTALL_BIN_DIR)
