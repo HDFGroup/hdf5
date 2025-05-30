849e31a3d7328b85eb50fccab41da6dc17beae79
Matthew Larson <matthewjlar@gmail.com>
2025-05-30 11:10:36 -0500

    Add h5ls test files to gentest

--- tools/test/h5gentest.c
+++ tools/test/h5gentest.c
@@ -22,6 +22,7 @@
 #include "h5repackgentest.h"
 #include "h5statgentest.h"
 #include "h5repartgentest.h"
+#include "h5lsgentest.h"
 
 static int
 gen_h5copy_files(void)
@@ -379,7 +380,55 @@ gen_h5repart_files(void)
     return EXIT_SUCCESS;
 }
 
-/*-------------------------------------------------------------------------
+static int
+gen_h5ls_files(void)
+{
+    int nerrors = 0;
+
+    gent_udfilter(H5LS_UDFILTER_FILE);
+
+    gent_all();
+    gent_group();
+    gent_dataset();
+    gent_softlink();
+    gent_softlink2();
+    gent_str();
+
+    gent_vldatatypes();
+    gent_compound_dt();
+    gent_datareg();
+    gent_empty();
+    gent_hardlink();
+    gent_loop();
+    gent_nestcomp();
+
+    gent_group_comments();
+    gent_array1();
+    gent_attr_all();
+    gent_attrreg();
+
+    gent_extlink();
+    gent_extlinks();
+    gent_null_space_group();
+
+    gent_udlink();
+
+#ifdef H5_HAVE__FLOAT_16
+    gent_float16();
+    gent_float16_be();
+#endif
+#ifdef H5_HAVE_COMPLEX_NUMBERS
+    gent_complex();
+    gent_complex_be();
+#endif
+
+    nerrors += (gent_tdset() < 0 ? 1 : 0);
+    gent_dataset_idx();
+
+    return nerrors;
+}
+
+ /*-------------------------------------------------------------------------
  * Function: usage
  *
  * Purpose: Prints a usage message on stdout stream and then returns.
@@ -404,6 +453,7 @@ usage(void)
     printf("  --h5repack      Generate h5repack test files\n");
     printf("  --h5stat        Generate h5stat test files\n");
     printf("  --h5repart      Generate h5repart test files\n");
+    printf("  --h5ls          Generate h5ls test files\n");
     return;
 }
 
@@ -414,7 +464,7 @@ int
 main(int argc, char *argv[])
 {
     /* command-line options: short and long-named parameters */
-    static const char            *s_opts   = "hacdufjrsp";
+    static const char            *s_opts   = "hacdufjrspl";
     static struct h5_long_options l_opts[] = {{"help", no_arg, 'h'},
                                               {"all", no_arg, 'a'},
                                               {"h5copy", no_arg, 'c'},
@@ -425,6 +475,7 @@ main(int argc, char *argv[])
                                               {"h5repack", no_arg, 'r'},
                                               {"h5stat", no_arg, 's'},
                                               {"h5repart", no_arg, 'p'},
+                                              {"h5ls", no_arg, 'l'},
                                               {NULL, 0, 0}};
     int                           i;
     int                           opt;
@@ -437,6 +488,7 @@ main(int argc, char *argv[])
     bool                          run_h5repack = false;
     bool                          run_h5stat   = false;
     bool                          run_h5repart = false;
+    bool                          run_h5ls     = false;
 
     /* Check for no command line parameters */
     if (argc == 1) {
@@ -476,6 +528,8 @@ main(int argc, char *argv[])
                 case 'p':
                     run_h5repart = true;
                     break;
+                case 'l':
+                    run_h5ls = true;
                 default:
                     continue;
             }
@@ -483,7 +537,7 @@ main(int argc, char *argv[])
     }
 
     if (!run_all && !run_h5copy && !run_h5diff && !run_h5dump && !run_h5fc && !run_h5jam && !run_h5repack &&
-        !run_h5stat && !run_h5repart) {
+        !run_h5stat && !run_h5repart && !run_h5ls) {
         usage();
         return EXIT_FAILURE;
     }
@@ -498,6 +552,7 @@ main(int argc, char *argv[])
         gen_h5repack_files();
         gen_h5stat_files();
         gen_h5repart_files();
+        gen_h5ls_files();
     }
     else {
         if (run_h5copy) {
@@ -523,6 +578,9 @@ main(int argc, char *argv[])
         }
         if (run_h5repart) {
             gen_h5repart_files();
+        }
+        if (run_h5ls) {
+            gen_h5ls_files();
         }
     }
 
---
+++ tools/test/h5ls/h5lsgentest.h
@@ -0,0 +1,22 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the LICENSE file, which can be found at the root of the source code       *
+ * distribution tree, or in https://www.hdfgroup.org/licenses.               *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef H5LS_GENTEST_H
+#define H5LS_GENTEST_H
+
+#include "hdf5.h"
+
+#define H5LS_UDFILTER_FILE "tudfilter.h5"
+
+herr_t gent_tdset(void);
+
+#endif /* H5LS_GENTEST_H */
