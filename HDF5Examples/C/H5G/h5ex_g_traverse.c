d92d8b1e0130ba8b3a6df1a3d265a451a641bad1
Allen Byrne <byrn@hdfgroup.org>
2025-06-09 14:37:28 -0500

    Correct declaration

--- HDF5Examples/C/H5G/h5ex_g_traverse.c
+++ HDF5Examples/C/H5G/h5ex_g_traverse.c
@@ -44,7 +44,11 @@ herr_t op_func(hid_t loc_id, const char *name, const H5L_info_t *info, void *ope
 /*
  * Function to check for duplicate groups in a path.
  */
+#if H5_VERSION_GE(1, 12, 0) && !defined(H5_USE_110_API) && !defined(H5_USE_18_API) && !defined(H5_USE_16_API)
+int group_check(hid_t loc_id, struct opdata *od, haddr_t target_addr);
+#else
 int group_check(struct opdata *od, haddr_t target_addr);
+#endif
 
 int
 main(void)
