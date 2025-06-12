72f4b400c346023b782d95909a1248138b2f32ce
Matthew Larson <matthewjlar@gmail.com>
2025-06-12 10:06:17 -0500

    Move h5import test files to shared folder

--- tools/test/h5import/CMakeTests.cmake
+++ tools/test/h5import/CMakeTests.cmake
@@ -88,7 +88,7 @@
   endforeach ()
 
   foreach (h5_file ${HDF5_REFERENCE_TEST_FILES})
-    HDFTEST_COPY_FILE("${PROJECT_SOURCE_DIR}/testfiles/${h5_file}" "${PROJECT_BINARY_DIR}/testfiles/${h5_file}" "h5import_files")
+    HDFTEST_COPY_FILE("${HDF5_TOOL_INPUT_PATH}/testfiles/${h5_file}" "${PROJECT_BINARY_DIR}/testfiles/${h5_file}" "h5import_files")
   endforeach ()
 
   foreach (h5_file ${HDF5_TOOLS_TEST_FILES})
