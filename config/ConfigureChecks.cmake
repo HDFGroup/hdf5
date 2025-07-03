f365da60af9ba2dfc200becb062e6f3e98d902a3
Allen Byrne <byrn@hdfgroup.org>
2025-07-03 10:52:47 -0500

    Set the default option state to a hard value

--- config/ConfigureChecks.cmake
+++ config/ConfigureChecks.cmake
@@ -981,7 +981,7 @@ H5ConversionTests (${HDF_PREFIX}_DISABLE_SOME_LDOUBLE_CONV FALSE "Checking IF th
 # be enabled or disabled with their respective options below
 option (HDF5_ENABLE_NONSTANDARD_FEATURES "Enable support for non-standard programming language features" ON)
 # Options for enabling or disabling individual features
-cmake_dependent_option (HDF5_ENABLE_NONSTANDARD_FEATURE_FLOAT16 "Enable support for _Float16 C datatype" ${HDF5_ENABLE_NONSTANDARD_FEATURES} HDF5_ENABLE_NONSTANDARD_FEATURES OFF)
+cmake_dependent_option (HDF5_ENABLE_NONSTANDARD_FEATURE_FLOAT16 "Enable support for _Float16 C datatype" ON HDF5_ENABLE_NONSTANDARD_FEATURES OFF)
 
 #-----------------------------------------------------------------------------
 # Check if _Float16 type is available
