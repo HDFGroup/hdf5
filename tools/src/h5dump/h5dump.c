48d5ceff8493af67486938dbd714bf9d934a899d
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2025-07-23 10:35:12 -0400

    Fix clang-format errors again

--- tools/src/h5dump/h5dump.c
+++ tools/src/h5dump/h5dump.c
@@ -1483,9 +1483,9 @@ main(int argc, char *argv[])
                 goto done;
             }
 #else
-                error_msg("ROS3 VFD is not available unless enabled when HDF5 is configured and built.\n");
-                h5tools_setstatus(EXIT_FAILURE);
-                goto done;
+            error_msg("ROS3 VFD is not available unless enabled when HDF5 is configured and built.\n");
+            h5tools_setstatus(EXIT_FAILURE);
+            goto done;
 #endif
         }
 
