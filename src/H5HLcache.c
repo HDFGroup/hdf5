3dbd95b60809e96f505cdc6bacf3423f31f7428f
Glenn Song <gsong@hdfgroup.org>
2025-09-11 18:47:22 -0500

    Add better check for overflow

--- src/H5HLcache.c
+++ src/H5HLcache.c
@@ -232,7 +232,12 @@ H5HL__fl_deserialize(H5HL_t *heap)
         const uint8_t *image; /* Pointer into image buffer */
 
         /* Sanity check */
-        if ((free_block > heap->dblk_size) || ((free_block + (2 * heap->sizeof_size)) > heap->dblk_size))
+        HDcompile_assert(sizeof(hsize_t) == sizeof(uint64_t));
+
+        if (free_block > UINT64_MAX - (2 * heap->sizeof_size))
+            HGOTO_ERROR(H5E_HEAP, H5E_BADRANGE, FAIL, "decoded heap block address overflow");
+
+        if ((free_block + (2 * heap->sizeof_size)) > heap->dblk_size)
             HGOTO_ERROR(H5E_HEAP, H5E_BADRANGE, FAIL, "bad heap free list");
 
         /* Allocate & initialize free list node */
