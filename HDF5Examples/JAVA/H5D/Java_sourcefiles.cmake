10af429c38c86c9dd796abec950641362e3812d7
Allen Byrne <byrn@hdfgroup.org>
2025-10-03 16:28:33 -0500

    Skip Unlimited examples when using FFM

--- HDF5Examples/JAVA/H5D/Java_sourcefiles.cmake
+++ HDF5Examples/JAVA/H5D/Java_sourcefiles.cmake
@@ -10,19 +10,31 @@ set (HDF_JAVA_EXAMPLES
     H5Ex_D_FillValue.java
     H5Ex_D_Hyperslab.java
     H5Ex_D_ReadWrite.java
-    H5Ex_D_UnlimitedAdd.java
-    H5Ex_D_UnlimitedMod.java
     H5Ex_D_Nbit.java
     H5Ex_D_Transform.java
     H5Ex_D_Sofloat.java
     H5Ex_D_Soint.java
 )
 
+# Unlimited examples have known FFM memory issues - only include with JNI
+if (HDF5_ENABLE_JNI)
+    set (HDF_JAVA_EXAMPLES ${HDF_JAVA_EXAMPLES}
+        H5Ex_D_UnlimitedAdd.java
+        H5Ex_D_UnlimitedMod.java
+    )
+endif ()
+
 set (HDF_JAVA_ZLIB_EXAMPLES
     H5Ex_D_Gzip.java
     H5Ex_D_Shuffle.java
-    H5Ex_D_UnlimitedGzip.java
 )
+
+# UnlimitedGzip has known FFM memory issues - only include with JNI
+if (HDF5_ENABLE_JNI)
+    set (HDF_JAVA_ZLIB_EXAMPLES ${HDF_JAVA_ZLIB_EXAMPLES}
+        H5Ex_D_UnlimitedGzip.java
+    )
+endif ()
 
 set (HDF_JAVA_SZIP_EXAMPLES
     H5Ex_D_Szip.java
