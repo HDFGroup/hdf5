1e8b5868c0c9b577c9bda5c3636d97d472435f32
Christian Wojek <christian.wojek@web.de>
2025-10-31 10:06:00 +0100

    Fix CVE-2025-44904

--- src/H5Dchunk.c
+++ src/H5Dchunk.c
@@ -4527,6 +4527,11 @@ H5D__chunk_lock(const H5D_io_info_t H5_ATTR_NDEBUG_UNUSED *io_info, const H5D_ds
 
             /* Check if the chunk exists on disk */
             if (H5_addr_defined(chunk_addr)) {
+
+                /* Unfiltered data should not be smaller on disk as in memory */
+                if (!(udata->new_unfilt_chunk ? old_pline : pline)->nused && chunk_size > chunk_alloc)
+                    HGOTO_ERROR(H5E_IO, H5E_READERROR, NULL, "unable to read raw data chunk");
+
                 size_t my_chunk_alloc = chunk_alloc; /* Allocated buffer size */
                 size_t buf_alloc      = chunk_alloc; /* [Re-]allocated buffer size */
 
