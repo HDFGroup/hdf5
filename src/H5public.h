9d9e3c8635ee11b80401c8aa809a31e956fc6570
Larry Knox <lrknox@hdfgroup.org>
2026-07-23 09:25:42 -0500

    Update so numbers for HDF5 2.2.0 and set version to 2.2.0-rc1. (#6552)
    * Turn clang-format off for too-long function protoype in H5FDros3_s3comms.c to avoid a line split that results in bin/trace parse failure.

--- src/H5public.h
+++ src/H5public.h
@@ -81,7 +81,7 @@
  *   Invalid: #define H5_VERS_SUBRELEASE -snap0
  *
  */
-#define H5_VERS_SUBRELEASE ""
+#define H5_VERS_SUBRELEASE "-rc1"
 
 /* Derived version strings - automatically generated from the above */
 /**
